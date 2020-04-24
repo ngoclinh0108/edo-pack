8ad222d2522d0d08adb7759a755c93b67c31bbaf
Luc Ta <luc.ta@cptech.vn>
2020-04-25 05:15:51 +0700

    update

--- script/c410000008.lua
+++ script/c410000008.lua
@@ -1,6 +1,39 @@
 --Divine Flames Of Sun Phoenix
 local root,id=GetID()
 
-function root.initial_effect(c)
+root.listed_names={10000010,10000080,10000090}
 
+function root.initial_effect(c)
+	--search
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,1))
+	e2:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_HAND)
+	e2:SetCost(root.e2cost)
+	e2:SetTarget(root.e2tg)
+	e2:SetOperation(root.e2op)
+	c:RegisterEffect(e2)
+end
+
+function root.e2filter(c)
+	return c:IsCode(10000010,10000080,10000090) and c:IsAbleToHand()
+end
+
+function root.e2cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return e:GetHandler():IsDiscardable() end
+	Duel.SendtoGrave(e:GetHandler(),REASON_COST+REASON_DISCARD)
+end
+
+function root.e2tg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
+	if chk==0 then return Duel.IsExistingMatchingCard(root.e2filter,tp,LOCATION_DECK,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK)
+end
+
+function root.e2op(e,tp,eg,ep,ev,re,r,rp,chk)
+	local g=Duel.SelectMatchingCard(tp,root.thfilter,tp,LOCATION_DECK,0,1,1,nil)
+	if #g>0 then
+		Duel.SendtoHand(g,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,g)
+	end
 end
