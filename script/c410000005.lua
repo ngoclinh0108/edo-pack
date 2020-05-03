15582e5342640cefd660e8d54a5d92c47b00c826
Luc Ta <luc.ta@cptech.vn>
2020-05-03 21:41:45 +0700

    update

--- script/c410000005.lua
+++ script/c410000005.lua
@@ -7,6 +7,7 @@ function root.initial_effect(c)
 	--activate
 	local act=Effect.CreateEffect(c)
 	act:SetType(EFFECT_TYPE_ACTIVATE)
+	act:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
 	act:SetCode(EVENT_FREE_CHAIN)
 	act:SetTarget(root.acttg)
 	act:SetOperation(root.actop)
