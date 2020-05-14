92d1e4515263a364e206e0b4aaa430b56f5f3aee
Luc Ta <luc.ta@cptech.vn>
2020-05-15 01:36:15 +0700

    update

--- script/c410000003.lua
+++ script/c410000003.lua
@@ -94,10 +94,10 @@ function root.initial_effect(c)
 		local owner=false
 		local effs={c:GetCardEffect()}
 		for _,eff in ipairs(effs) do
-			owner=(eff:GetOwner()~=c and not eff:GetOwner():IsCode(0,10000080,10000090)
+			owner=(eff:GetOwner()~=c and not eff:GetOwner():IsCode(0,10000080,10000090,410000010)
 				and not eff:IsHasProperty(EFFECT_FLAG_IGNORE_IMMUNE)
 				and (eff:GetTarget()==aux.PersistentTargetFilter or not eff:IsHasType(EFFECT_TYPE_GRANT+EFFECT_TYPE_FIELD)))
-				and (eff:GetOwner()~=c and not eff:GetOwner():IsCode(0,10000080,10000090)
+				and (eff:GetOwner()~=c and not eff:GetOwner():IsCode(0,10000080,10000090,410000010)
 				and not eff:IsHasProperty(EFFECT_FLAG_IGNORE_IMMUNE)
 				and (eff:GetTarget()==aux.PersistentTargetFilter or not eff:IsHasType(EFFECT_TYPE_GRANT+EFFECT_TYPE_FIELD)))
 				or owner
@@ -184,7 +184,8 @@ function root.initial_effect(c)
 	local e6=Effect.CreateEffect(c)
 	e6:SetDescription(aux.Stringid(id,1))
 	e6:SetCategory(CATEGORY_ATKCHANGE+CATEGORY_DEFCHANGE)
-	e6:SetType(EFFECT_TYPE_IGNITION)
+	e6:SetType(EFFECT_TYPE_QUICK_O)
+	e6:SetCode(EVENT_FREE_CHAIN)
 	e6:SetRange(LOCATION_MZONE)
 	e6:SetCountLimit(1)
 	e6:SetCondition(function(e,tp,eg,ep,ev,re,r,rp) return e:GetHandler():GetFlagEffect(id)>0 end)
--- script/c410000010.lua
+++ script/c410000010.lua
@@ -29,10 +29,10 @@ function root.initial_effect(c)
 	e2:SetOperation(root.e2op)
 	c:RegisterEffect(e2)
 
-	--monster reborn
+	--gain LP
 	local e3=Effect.CreateEffect(c)
 	e3:SetDescription(aux.Stringid(id,2))
-	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e3:SetCategory(CATEGORY_RECOVER)
 	e3:SetType(EFFECT_TYPE_ACTIVATE)
 	e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
 	e3:SetCode(EVENT_FREE_CHAIN)
@@ -41,6 +41,18 @@ function root.initial_effect(c)
 	e3:SetTarget(root.e3tg)
 	e3:SetOperation(root.e3op)
 	c:RegisterEffect(e3)
+
+	--monster reborn
+	local e4=Effect.CreateEffect(c)
+	e4:SetDescription(aux.Stringid(id,3))
+	e4:SetCategory(CATEGORY_TOHAND+CATEGORY_SEARCH)
+	e4:SetType(EFFECT_TYPE_ACTIVATE)
+	e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE+EFFECT_FLAG_CANNOT_NEGATE)
+	e4:SetCode(EVENT_FREE_CHAIN)
+	e4:SetCountLimit(1,id)
+	e4:SetTarget(root.e4tg)
+	e4:SetOperation(root.e4op)
+	c:RegisterEffect(e4)
 end
 
 function root.e1filter(c)
@@ -61,9 +73,9 @@ function root.e1op(e,tp,eg,ep,ev,re,r,rp,chk)
 end
 
 function root.e2filter1(c,ft,tp)
-	local code1,code3=c:GetOriginalCodeRule()
+	local code1,code2=c:GetOriginalCodeRule()
 	return (ft>0 or (c:IsControler(tp) and c:GetSequence()<5)) and (c:IsControler(tp) or c:IsFaceup())
-		and (code1==10000080 or code3==10000080)
+		and (code1==10000080 or code2==10000080)
 end
 
 function root.e2filter2(c,e,tp)
@@ -105,29 +117,55 @@ function root.e2op(e,tp,eg,ep,ev,re,r,rp)
 	Duel.SpecialSummonComplete()
 end
 
-function root.e3filter1(c)
-	return c:IsRace(RACE_DIVINE) and c:IsDiscardable()
-end
-
-function root.e3filter2(c)
-	return c:IsCode(83764718) and c:IsAbleToHand()
+function root.e3filter(c)
+	local code1,code2=c:GetOriginalCodeRule()
+	return (code1==10000010 or code2==10000010)
+		and c:IsFaceup() and c:GetAttack()>0
 end
 
 function root.e3cost(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(root.e3filter1,tp,LOCATION_HAND,0,1,e:GetHandler()) end
-	Duel.DiscardHand(tp,root.e3filter1,1,1,REASON_COST+REASON_DISCARD)
+	if chk==0 then return Duel.CheckReleaseGroupCost(tp,root.e3filter,1,false,nil,nil) end
+	local tc=Duel.SelectReleaseGroupCost(tp,root.e3filter,1,1,false,nil,nil):GetFirst()
+	e:SetLabel(tc:GetAttack())
+	Duel.Release(tc,REASON_COST)
 end
 
 function root.e3tg(e,tp,eg,ep,ev,re,r,rp,chk)
-	if chk==0 then return Duel.IsExistingMatchingCard(root.e3filter2,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
-	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+	if chk==0 then return true end
+	local rec=e:GetLabel()
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(rec)
+	Duel.SetOperationInfo(0,CATEGORY_RECOVER,nil,0,tp,rec)
 end
 
 function root.e3op(e,tp,eg,ep,ev,re,r,rp)
+	local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+	Duel.Recover(p,d,REASON_EFFECT)
+end
+
+function root.e4filter1(c)
+	return c:IsRace(RACE_DIVINE) and c:IsDiscardable()
+end
+
+function root.e4filter2(c)
+	return c:IsCode(83764718) and c:IsAbleToHand()
+end
+
+function root.e4cost(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(root.e4filter1,tp,LOCATION_HAND,0,1,e:GetHandler()) end
+	Duel.DiscardHand(tp,root.e4filter1,1,1,REASON_COST+REASON_DISCARD)
+end
+
+function root.e4tg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then return Duel.IsExistingMatchingCard(root.e4filter2,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,nil) end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_DECK+LOCATION_GRAVE)
+end
+
+function root.e4op(e,tp,eg,ep,ev,re,r,rp)
 	local c=e:GetHandler()
 
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
-	local g=Duel.SelectMatchingCard(tp,root.e3filter2,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
+	local g=Duel.SelectMatchingCard(tp,root.e4filter2,tp,LOCATION_DECK+LOCATION_GRAVE,0,1,1,nil)
 	if #g>0 then
 		Duel.SendtoHand(g,nil,REASON_EFFECT)
 		Duel.ConfirmCards(1-tp,g)
@@ -146,7 +184,7 @@ function root.e3op(e,tp,eg,ep,ev,re,r,rp)
 	local ec2=Effect.CreateEffect(c)
 	ec2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	ec2:SetCode(EVENT_SPSUMMON_SUCCESS)
-	ec2:SetOperation(root.e3regop)
+	ec2:SetOperation(root.e4regop)
 	ec2:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(ec2,tp)
 
@@ -155,27 +193,27 @@ function root.e3op(e,tp,eg,ep,ev,re,r,rp)
 	ec3:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
 	ec3:SetCode(EVENT_PHASE+PHASE_END)
 	ec3:SetCountLimit(1)
-	ec3:SetOperation(root.e3gyop)
+	ec3:SetOperation(root.e4gyop)
 	ec3:SetReset(RESET_PHASE+PHASE_END)
 	Duel.RegisterEffect(ec3,tp)
 end
 
-function root.e3regop(e,tp,eg,ep,ev,re,r,rp)
+function root.e4regop(e,tp,eg,ep,ev,re,r,rp)
 	local g=eg:Filter(function(c,tp,re)
-		local code1,code3=c:GetOriginalCodeRule()
-		return c:IsFaceup() and (code1==CARD_RA or code3==CARD_RA) and c:IsControler(tp) and re and re:GetHandler():IsCode(83764718)
+		local code1,code2=c:GetOriginalCodeRule()
+		return c:IsFaceup() and (code1==CARD_RA or code2==CARD_RA) and c:IsControler(tp) and re and re:GetHandler():IsCode(83764718)
 	end,nil,tp,re)
 	if #g==0 then return end
 
 	for tc in aux.Next(g) do
-		tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(id,3))
+		tc:RegisterFlagEffect(id,RESET_EVENT+RESETS_STANDARD+RESET_PHASE+PHASE_END,EFFECT_FLAG_CLIENT_HINT,1,0,aux.Stringid(id,4))
 	end
 end
 
-function root.e3gyop(e,tp,eg,ep,ev,re,r,rp)
+function root.e4gyop(e,tp,eg,ep,ev,re,r,rp)
 	local g=Duel.GetMatchingGroup(function(c)
-		local code1,code3=c:GetOriginalCodeRule()
-		return (code1==CARD_RA or code3==CARD_RA) and c:GetFlagEffect(id)~=0
+		local code1,code2=c:GetOriginalCodeRule()
+		return (code1==CARD_RA or code2==CARD_RA) and c:GetFlagEffect(id)~=0
 	end,tp,LOCATION_MZONE,0,nil)
 
 	if #g>0 then
