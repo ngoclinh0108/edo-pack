27db180b08eaf8724654c106024cd90ef73f6d57
Luc Ta <luc.ta@cptech.vn>
2020-06-27 01:40:02 +0700

    update

--- build/modules/set-paths.lua
+++
@@ -1,4 +0,0 @@
-local version = _VERSION:match("%d+%.%d+")
-package.path = ("./?.lua;./?/init.lua;modules/share/lua/%s/?.lua;modules/share/lua/%s/?/init.lua;%s")
-  :format(version, version, package.path)
-package.cpath = ("modules/lib/lua/%s/?.dll;%s"):format(version, package.cpath)
--- build/res/composer/fonts/README.md
+++
@@ -1,5 +0,0 @@
-### Note
-
-This folder is supposed to keep font files that are used by the Composer module, that
-generates card pics from your extension pack data. However, the official fonts may not be
-free, and thus cannot be published in this repository.
