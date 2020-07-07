736f0a0346cd5fb16a5aa00a818b7190208a3ada
Luc Ta <luc.ta@cptech.vn>
2020-07-07 23:40:52 +0700

    update

--- fabrica/ygopic.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-set ygofab_home="C:\Users\tripl\AppData\Local\YGOFabrica"
-set back="%cd%"
-cd "%ygofab_home%"
-luajit -l modules.set-paths scripts/ygopic.lua %back% %*
-cd %back%
-@echo on
