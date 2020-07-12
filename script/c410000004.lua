1dca3a906c6c177ac8c163db0348d389c48da4af
Luc Ta <luc.ta@cptech.vn>
2020-07-13 01:49:17 +0700

    update

--- script/c410000004.lua
+++ script/c410000004.lua
@@ -11,75 +11,75 @@ function s.initial_effect(c)
 
     -- startup
     Dimension.RegisterEffect(c, function(e, tp)
-        local e1 = Effect.CreateEffect(c)
-        e1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
-        e1:SetCode(EVENT_SUMMON_SUCCESS)
-        e1:SetCondition(s.e1con)
-        e1:SetOperation(s.e1op)
-        Duel.RegisterEffect(e1, tp)
+        local dms = Effect.CreateEffect(c)
+        dms:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
+        dms:SetCode(EVENT_SUMMON_SUCCESS)
+        dms:SetCondition(s.dmscon)
+        dms:SetOperation(s.dmsop)
+        Duel.RegisterEffect(dms, tp)
     end)
 
     -- race
-    local e2 = Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_SINGLE)
-    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e2:SetCode(EFFECT_ADD_RACE)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetValue(RACE_PYRO + RACE_WINGEDBEAST)
-    c:RegisterEffect(e2)
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetCode(EFFECT_ADD_RACE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetValue(RACE_PYRO + RACE_WINGEDBEAST)
+    c:RegisterEffect(e1)
 
     -- attack limit
-    local e3 = Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_SINGLE)
-    e3:SetCode(EFFECT_CANNOT_ATTACK)
-    c:RegisterEffect(e3)
+    local e2 = Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_CANNOT_ATTACK)
+    c:RegisterEffect(e2)
 
     -- cannot be targeted
+    local e3 = Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e3:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetValue(1)
+    c:RegisterEffect(e3)
+
+    -- battle indes & damage avoid
     local e4 = Effect.CreateEffect(c)
     e4:SetType(EFFECT_TYPE_SINGLE)
     e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e4:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+    e4:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
     e4:SetRange(LOCATION_MZONE)
-    e4:SetValue(1)
+    e4:SetValue(function(e) return e:GetHandler():IsDefensePos() end)
     c:RegisterEffect(e4)
-
-    -- battle indes & damage avoid
-    local e5 = Effect.CreateEffect(c)
-    e5:SetType(EFFECT_TYPE_SINGLE)
-    e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e5:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-    e5:SetRange(LOCATION_MZONE)
-    e5:SetValue(function(e) return e:GetHandler():IsDefensePos() end)
-    c:RegisterEffect(e5)
-    local e5b = e5:Clone()
-    e5b:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
-    c:RegisterEffect(e5b)
+    local e4b = e4:Clone()
+    e4b:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+    c:RegisterEffect(e4b)
 
     -- standard form
-    local e6 = Effect.CreateEffect(c)
-    e6:SetDescription(aux.Stringid(id, 0))
-    e6:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e6:SetType(EFFECT_TYPE_IGNITION)
-    e6:SetProperty(EFFECT_FLAG_BOTH_SIDE)
-    e6:SetRange(LOCATION_MZONE)
-    e6:SetCountLimit(1)
-    e6:SetCost(s.e6cost)
-    e6:SetTarget(s.e6tg)
-    e6:SetOperation(s.e6op)
-    c:RegisterEffect(e6)
+    local e5 = Effect.CreateEffect(c)
+    e5:SetDescription(aux.Stringid(id, 0))
+    e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e5:SetType(EFFECT_TYPE_IGNITION)
+    e5:SetProperty(EFFECT_FLAG_BOTH_SIDE)
+    e5:SetRange(LOCATION_MZONE)
+    e5:SetCountLimit(1)
+    e5:SetCost(s.e5cost)
+    e5:SetTarget(s.e5tg)
+    e5:SetOperation(s.e5op)
+    c:RegisterEffect(e5)
 end
 
-function s.e1filter(c, dc)
+function s.dmsfilter(c, dc)
     return c:GetOwner() == dc:GetOwner() and c:IsCode(CARD_RA)
 end
 
-function s.e1con(e, tp, eg, ep, ev, re, r, rp)
-    return eg:IsExists(s.e1filter, 1, nil, e:GetHandler())
+function s.dmscon(e, tp, eg, ep, ev, re, r, rp)
+    return eg:IsExists(s.dmsfilter, 1, nil, e:GetHandler())
 end
 
-function s.e1op(e, tp, eg, ep, ev, re, r, rp)
+function s.dmsop(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
-    local mc = eg:Filter(s.e1filter, nil, c):GetFirst()
+    local mc = eg:Filter(s.dmsfilter, nil, c):GetFirst()
     if not mc then return end
     Duel.BreakEffect()
 
@@ -87,7 +87,7 @@ function s.e1op(e, tp, eg, ep, ev, re, r, rp)
                      mc:GetPosition())
 end
 
-function s.e6cost(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e5cost(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
     local mc = c:GetMaterial():GetFirst()
 
@@ -100,7 +100,7 @@ function s.e6cost(e, tp, eg, ep, ev, re, r, rp, chk)
     Dimension.SendToDimension(c, REASON_COST)
 end
 
-function s.e6tg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e5tg(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
     local mc = c:GetMaterial():GetFirst()
 
@@ -109,7 +109,7 @@ function s.e6tg(e, tp, eg, ep, ev, re, r, rp, chk)
     Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, mc, 1, 0, 0)
 end
 
-function s.e6op(e, tp, eg, ep, ev, re, r, rp)
+function s.e5op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     local tc = c:GetMaterial():GetFirst()
     if not tc then return end
