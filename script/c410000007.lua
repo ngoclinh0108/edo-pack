c53ad6315a74b295cbbfb3a220b6f48f87d76f13
Luc Ta <luc.ta@cptech.vn>
2020-07-13 01:45:16 +0700

    update

--- script/c410000007.lua
+++ script/c410000007.lua
@@ -54,11 +54,11 @@ end
 
 function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then return true end
-    local g = Duel.GetMatchingGroup(aux.TRUE, tp, 0, LOCATION_MZONE, nil)
+    local g = Duel.GetMatchingGroup(aux.TRUE, tp, 0, LOCATION_ONFIELD, nil)
     Duel.SetOperationInfo(0, CATEGORY_TOGRAVE, g, #g, 0, 0)
 end
 
 function s.e3op(e, tp, eg, ep, ev, re, r, rp)
-    local g = Duel.GetMatchingGroup(aux.TRUE, tp, 0, LOCATION_MZONE, nil)
+    local g = Duel.GetMatchingGroup(aux.TRUE, tp, 0, LOCATION_ONFIELD, nil)
     Duel.SendtoGrave(g, REASON_EFFECT)
 end
