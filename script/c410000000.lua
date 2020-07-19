21370089e71877107424935b12a66529bf567a5e
Luc Ta <luc.ta@cptech.vn>
2020-07-20 06:24:59 +0700

    update

--- script/c410000000.lua
+++ script/c410000000.lua
@@ -3,48 +3,74 @@ local s, id = GetID()
 
 function s.initial_effect(c)
     -- activate
-    local act = Effect.CreateEffect(c)
-    act:SetType(EFFECT_TYPE_ACTIVATE)
-    act:SetCode(EVENT_FREE_CHAIN)
-    act:SetTarget(function(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
-        if chk == 0 then return true end
-        Duel.SetChainLimit(function(e, ep, tp) return tp == ep end)
-    end)
-    c:RegisterEffect(act)
-
-    -- immune
     local e1 = Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e1:SetCode(EFFECT_IMMUNE_EFFECT)
-    e1:SetRange(LOCATION_FZONE)
-    e1:SetValue(function(e, te)
-        return te:GetOwnerPlayer() ~= e:GetOwnerPlayer()
-    end)
+    e1:SetCategory(CATEGORY_TOGRAVE)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetTarget(s.e1tg)
+    e1:SetOperation(s.e1op)
     c:RegisterEffect(e1)
 
-    -- protect grave
+    -- immune
     local e2 = Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetCode(EFFECT_CANNOT_REMOVE)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e2:SetCode(EFFECT_IMMUNE_EFFECT)
     e2:SetRange(LOCATION_FZONE)
-    e2:SetTargetRange(LOCATION_GRAVE, 0)
-    e2:SetTarget(function(e, c)
-        return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_DIVINE)
+    e2:SetValue(function(e, te)
+        return te:GetOwnerPlayer() ~= e:GetOwnerPlayer()
     end)
     c:RegisterEffect(e2)
-    local e2b = e2:Clone()
-    e2b:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-    e2b:SetValue(aux.tgoval)
-    c:RegisterEffect(e2b)
+
+    -- protect grave
+    local e3 = Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_FIELD)
+    e3:SetCode(EFFECT_CANNOT_REMOVE)
+    e3:SetRange(LOCATION_FZONE)
+    e3:SetTargetRange(LOCATION_GRAVE, 0)
+    e3:SetTarget(function(e, c)
+        return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_DIVINE)
+    end)
+    c:RegisterEffect(e3)
+    local e3b = e3:Clone()
+    e3b:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+    e3b:SetValue(aux.tgoval)
+    c:RegisterEffect(e3b)
 
     -- extra summon
-    local e3 = Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id, 0))
-    e3:SetType(EFFECT_TYPE_FIELD)
-    e3:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
-    e3:SetRange(LOCATION_FZONE)
-    e3:SetTargetRange(LOCATION_HAND + LOCATION_MZONE, 0)
-    e3:SetTarget(aux.TargetBoolFunction(Card.IsAttribute, ATTRIBUTE_DIVINE))
-    c:RegisterEffect(e3)
+    local e4 = Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id, 0))
+    e4:SetType(EFFECT_TYPE_FIELD)
+    e4:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+    e4:SetRange(LOCATION_FZONE)
+    e4:SetTargetRange(LOCATION_HAND + LOCATION_MZONE, 0)
+    e4:SetTarget(aux.TargetBoolFunction(Card.IsAttribute, ATTRIBUTE_DIVINE))
+    c:RegisterEffect(e4)
+end
+
+function s.e1filter(c)
+    return c:IsAttribute(ATTRIBUTE_DIVINE) and c:IsAbleToGrave()
+end
+
+function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    if chk == 0 then
+        return Duel.IsExistingMatchingCard(s.e1filter, tp,
+                                           LOCATION_HAND + LOCATION_DECK, 0, 1,
+                                           nil)
+    end
+
+    Duel.SetOperationInfo(0, CATEGORY_TOGRAVE, nil, 1, tp,
+                          LOCATION_HAND + LOCATION_DECK)
+    Duel.SetChainLimit(function(e, ep, tp) return tp == ep end)
+end
+
+function s.e1op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    if not c:IsRelateToEffect(e) then return end
+
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_TOGRAVE)
+    local g = Duel.SelectMatchingCard(tp, s.e1filter, tp,
+                                      LOCATION_HAND + LOCATION_DECK, 0, 1, 1,
+                                      nil)
+    if #g > 0 then Duel.SendtoGrave(g, REASON_EFFECT) end
 end
