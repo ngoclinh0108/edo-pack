e0b8329c0d46aa7fd5cc637f4ff9e8b713f89177
Luc Ta <luc.ta@cptech.vn>
2020-07-25 00:29:44 +0700

    update

--- script/c410000006.lua
+++ script/c410000006.lua
@@ -2,5 +2,13 @@
 local s, id = GetID()
 
 function s.initial_effect(c)
-    c:SetUniqueOnField(1,0,id)
+    c:SetUniqueOnField(1, 0, id)
+
+    -- cannot be tributed
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_UNRELEASABLE_SUM)
+    e1:SetValue(aux.TargetBoolFunction(aux.NOT(Card.IsAttribute),
+                                       ATTRIBUTE_DIVINE))
+    c:RegisterEffect(e1)
 end
--- script/c410000008.lua
+++ script/c410000008.lua
@@ -4,37 +4,56 @@ local s, id = GetID()
 function s.initial_effect(c)
     c:SetUniqueOnField(1, 0, id)
 
-    -- token
+    -- cannot be tributed
     local e1 = Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id, 0))
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON + CATEGORY_TOKEN)
-    e1:SetType(EFFECT_TYPE_IGNITION)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetCountLimit(1, id)
-    e1:SetCost(s.e1cost)
-    e1:SetTarget(s.e1tg)
-    e1:SetOperation(s.e1op)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_UNRELEASABLE_SUM)
+    e1:SetValue(aux.TargetBoolFunction(aux.NOT(Card.IsAttribute),
+                                       ATTRIBUTE_DIVINE))
     c:RegisterEffect(e1)
+
+    -- token
+    local e2 = Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id, 0))
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON + CATEGORY_TOKEN)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1, id)
+    e2:SetCost(s.e2cost)
+    e2:SetTarget(s.e2tg)
+    e2:SetOperation(s.e2op)
+    c:RegisterEffect(e2)
+
+    -- gain lp
+    local e3 = Effect.CreateEffect(c)
+    e3:SetCategory(CATEGORY_RECOVER)
+    e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_F)
+    e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_DELAY)
+    e3:SetCode(EVENT_RELEASE)
+    e3:SetCondition(s.e3con)
+    e3:SetTarget(s.e3tg)
+    e3:SetOperation(s.e3op)
+    c:RegisterEffect(e3)
 end
 
-function s.e1filter(c)
+function s.e2filter(c)
     return c:IsAttribute(ATTRIBUTE_DIVINE) and not c:IsPublic()
 end
 
-function s.e1cost(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e2cost(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then
-        return Duel.IsExistingMatchingCard(s.e1filter, tp, LOCATION_HAND, 0, 1,
+        return Duel.IsExistingMatchingCard(s.e2filter, tp, LOCATION_HAND, 0, 1,
                                            nil)
     end
 
     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_CONFIRM)
-    local g = Duel.SelectMatchingCard(tp, s.e1filter, tp, LOCATION_HAND, 0, 1,
+    local g = Duel.SelectMatchingCard(tp, s.e2filter, tp, LOCATION_HAND, 0, 1,
                                       1, nil)
     Duel.ConfirmCards(1 - tp, g)
     Duel.ShuffleHand(tp)
 end
 
-function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then
         return not Duel.IsPlayerAffectedByEffect(tp, CARD_BLUEEYES_SPIRIT) and
                    Duel.GetLocationCount(tp, LOCATION_MZONE) >= 2 and
@@ -48,7 +67,7 @@ function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
     Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 2, tp, 0)
 end
 
-function s.e1op(e, tp, eg, ep, ev, re, r, rp)
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
     if Duel.IsPlayerAffectedByEffect(tp, CARD_BLUEEYES_SPIRIT) or
         Duel.GetLocationCount(tp, LOCATION_MZONE) < 2 or
         not Duel.IsPlayerCanSpecialSummonMonster(tp, 410000000, 0x54b,
@@ -62,4 +81,17 @@ function s.e1op(e, tp, eg, ep, ev, re, r, rp)
                                POS_FACEUP_DEFENSE)
     end
     Duel.SpecialSummonComplete()
+end
+
+function s.e3con(e, tp, eg, ep, ev, re, r, rp)
+    return e:GetHandler():IsReason(REASON_SUMMON)
+end
+
+function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return true end
+    Duel.SetOperationInfo(0, CATEGORY_RECOVER, nil, 0, tp, 3000)
+end
+
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+    Duel.Recover(tp, 3000, REASON_EFFECT)
 end
