09f387f517d9c6600971d68502de0cab12707c89
Luc Ta <luc.ta@cptech.vn>
2020-08-15 15:56:58 +0700

    update

--- script/c410000009.lua
+++ script/c410000009.lua
@@ -2,4 +2,114 @@
 local s, id = GetID()
 
 function s.initial_effect(c)
+    -- special summon self
+    local e1 = Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCondition(s.e1con)
+    e1:SetTarget(s.e1tg)
+    e1:SetOperation(s.e1op)
+    c:RegisterEffect(e1)
+
+    -- untargetable
+    local e2 = Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetCode(EFFECT_CANNOT_SELECT_BATTLE_TARGET)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetTargetRange(0, LOCATION_MZONE)
+    e2:SetValue(s.e2val)
+    c:RegisterEffect(e2)
+    local e2b = Effect.CreateEffect(c)
+    e2b:SetType(EFFECT_TYPE_FIELD)
+    e2b:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+    e2b:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+    e2b:SetRange(LOCATION_MZONE)
+    e2b:SetTargetRange(LOCATION_MZONE, LOCATION_MZONE)
+    e2b:SetTarget(s.e2tg)
+    e2b:SetValue(aux.tgoval)
+    c:RegisterEffect(e2b)
+
+    -- indes
+    local e3 = Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e3:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCountLimit(1)
+    e3:SetValue(s.e3val)
+    c:RegisterEffect(e3)
+
+    -- special summon
+    local e4 = Effect.CreateEffect(c)
+    e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e4:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e4:SetProperty(EFFECT_FLAG_DELAY)
+    e4:SetCode(EVENT_DESTROYED)
+    e4:SetCondition(s.e4con)
+    e4:SetTarget(s.e4tg)
+    e4:SetOperation(s.e4op)
+    c:RegisterEffect(e4)
+end
+
+function s.e1con(e, tp, eg, ep, ev, re, r, rp)
+    return Duel.GetAttacker():GetControler() ~= tp
+end
+
+function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    local c = e:GetHandler()
+    if chk == 0 then
+        return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
+                   c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
+    end
+
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, c, 1, 0, 0)
+end
+
+function s.e1op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    if not c:IsRelateToEffect(e) then return end
+
+    Duel.SpecialSummon(c, 0, tp, tp, false, false, POS_FACEUP)
+end
+
+function s.e2tg(e, c) return c:IsSetCard(0x13a) and c ~= e:GetHandler() end
+
+function s.e2val(e, c)
+    return c:IsFaceup() and c:IsSetCard(0x13a) and c ~= e:GetHandler()
+end
+
+function s.e3val(e, re, r, rp) return (r & REASON_BATTLE + REASON_EFFECT) ~= 0 end
+
+function s.e4filter(c, e, tp)
+    return c:IsSetCard(0x13a) and not c:IsCode(id) and
+               c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
+end
+
+function s.e4con(e, tp, eg, ep, ev, re, r, rp)
+    return (r & REASON_EFFECT + REASON_BATTLE) ~= 0
+end
+
+function s.e4tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then
+        return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
+                   Duel.IsExistingMatchingCard(s.e4filter, tp, LOCATION_HAND +
+                                                   LOCATION_DECK +
+                                                   LOCATION_GRAVE, 0, 1, nil, e,
+                                               tp)
+    end
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp,
+                          LOCATION_HAND + LOCATION_DECK + LOCATION_GRAVE)
+end
+
+function s.e4op(e, tp, eg, ep, ev, re, r, rp)
+    if Duel.GetLocationCount(tp, LOCATION_MZONE) <= 0 then return end
+
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
+    local g = Duel.SelectMatchingCard(tp, aux.NecroValleyFilter(s.e4filter), tp,
+                                      LOCATION_HAND + LOCATION_DECK +
+                                          LOCATION_GRAVE, 0, 1, 1, nil, e, tp)
+
+    if #g > 0 then Duel.SpecialSummon(g, 0, tp, tp, false, false, POS_FACEUP) end
 end
--- script/c410000014.lua
+++ script/c410000014.lua
@@ -48,46 +48,33 @@ function s.initial_effect(c)
     local e3b = e3:Clone()
     e3b:SetCode(EFFECT_CANNOT_DISEFFECT)
     c:RegisterEffect(e3b)
-
-    -- grave protect
-    local e4 = Effect.CreateEffect(c)
-    e4:SetType(EFFECT_TYPE_FIELD)
-    e4:SetCode(EFFECT_CANNOT_REMOVE)
-    e4:SetRange(LOCATION_SZONE)
-    e4:SetTargetRange(LOCATION_GRAVE, 0)
-    e4:SetTarget(aux.TargetBoolFunction(Card.IsSetCard, 0x13a))
-    c:RegisterEffect(e4)
-    local e4b = e4:Clone()
-    e4b:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-    e4b:SetValue(aux.tgoval)
-    c:RegisterEffect(e4b)
-
+   
     -- search card
+    local e4 = Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id, 0))
+    e4:SetCategory(CATEGORY_TOHAND + CATEGORY_SEARCH)
+    e4:SetType(EFFECT_TYPE_QUICK_O)
+    e4:SetCode(EVENT_FREE_CHAIN)
+    e4:SetRange(LOCATION_SZONE)
+    e4:SetHintTiming(0, TIMING_END_PHASE)
+    e4:SetCountLimit(1)
+    e4:SetCost(s.e4cost)
+    e4:SetTarget(s.e4tg)
+    e4:SetOperation(s.e4op)
+    c:RegisterEffect(e4)
+
+    -- summon holactie
     local e5 = Effect.CreateEffect(c)
-    e5:SetDescription(aux.Stringid(id, 0))
-    e5:SetCategory(CATEGORY_TOHAND + CATEGORY_SEARCH)
+    e5:SetDescription(aux.Stringid(id, 3))
+    e5:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e5:SetType(EFFECT_TYPE_QUICK_O)
     e5:SetCode(EVENT_FREE_CHAIN)
     e5:SetRange(LOCATION_SZONE)
-    e5:SetHintTiming(0, TIMING_END_PHASE)
     e5:SetCountLimit(1)
     e5:SetCost(s.e5cost)
     e5:SetTarget(s.e5tg)
     e5:SetOperation(s.e5op)
     c:RegisterEffect(e5)
-
-    -- summon holactie
-    local e6 = Effect.CreateEffect(c)
-    e6:SetDescription(aux.Stringid(id, 3))
-    e6:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e6:SetType(EFFECT_TYPE_QUICK_O)
-    e6:SetCode(EVENT_FREE_CHAIN)
-    e6:SetRange(LOCATION_SZONE)
-    e6:SetCountLimit(1)
-    e6:SetCost(s.e6cost)
-    e6:SetTarget(s.e6tg)
-    e6:SetOperation(s.e6op)
-    c:RegisterEffect(e6)
 end
 
 function s.e1con(e, tp, eg, ep, ev, re, r, rp)
@@ -124,31 +111,31 @@ function s.e3val(e, ct)
     return p == tp and te:GetHandler():IsSetCard(0x13a)
 end
 
-function s.e5filter1(c)
+function s.e4filter1(c)
     return c:IsAttribute(ATTRIBUTE_DIVINE) and not c:IsPublic()
 end
 
-function s.e5filter2(c)
+function s.e4filter2(c)
     return c:IsSetCard(0x13a) and not c:IsCode(id) and
                c:IsLocation(LOCATION_DECK + LOCATION_GRAVE) and c:IsAbleToHand()
 end
 
-function s.e5cost(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e4cost(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then
-        return Duel.IsExistingMatchingCard(s.e5filter1, tp, LOCATION_HAND, 0, 1,
+        return Duel.IsExistingMatchingCard(s.e4filter1, tp, LOCATION_HAND, 0, 1,
                                            nil)
     end
 
     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_CONFIRM)
-    local g = Duel.SelectMatchingCard(tp, s.e5filter1, tp, LOCATION_HAND, 0, 1,
+    local g = Duel.SelectMatchingCard(tp, s.e4filter1, tp, LOCATION_HAND, 0, 1,
                                       1, nil)
     Duel.ConfirmCards(1 - tp, g)
     Duel.ShuffleHand(tp)
 end
 
-function s.e5tg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e4tg(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then
-        return Duel.IsExistingMatchingCard(s.e5filter2, tp,
+        return Duel.IsExistingMatchingCard(s.e4filter2, tp,
                                            LOCATION_DECK + LOCATION_GRAVE, 0, 1,
                                            nil)
     end
@@ -157,9 +144,9 @@ function s.e5tg(e, tp, eg, ep, ev, re, r, rp, chk)
     Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, 0, 0)
 end
 
-function s.e5op(e, tp, eg, ep, ev, re, r, rp)
+function s.e4op(e, tp, eg, ep, ev, re, r, rp)
     Duel.Hint(HINT_SELECTMSG, tp, aux.Stringid(id, 0))
-    local tc = Duel.SelectMatchingCard(tp, s.e5filter2, tp,
+    local tc = Duel.SelectMatchingCard(tp, s.e4filter2, tp,
                                        LOCATION_DECK + LOCATION_GRAVE, 0, 1, 1,
                                        nil):GetFirst()
     if not tc then return end
@@ -168,29 +155,29 @@ function s.e5op(e, tp, eg, ep, ev, re, r, rp)
     Duel.ConfirmCards(1 - tp, tc)
 end
 
-function s.e6filter1(c, code)
+function s.e5filter1(c, code)
     local code2, code3 = c:GetOriginalCodeRule()
     return code2 == code or code3 == code
 end
 
-function s.e6filter2(c, e, tp)
+function s.e5filter2(c, e, tp)
     return c:IsCode(10000040) and
                c:IsCanBeSpecialSummoned(e, 0, tp, true, false, POS_FACEUP_ATTACK)
 end
 
-function s.e6rescon(sg, e, tp, mg)
+function s.e5rescon(sg, e, tp, mg)
     return aux.ChkfMMZ(1)(sg, e, tp, mg) and
-               sg:IsExists(s.e6check, 1, nil, sg, Group.CreateGroup(), 10000000,
+               sg:IsExists(s.e5check, 1, nil, sg, Group.CreateGroup(), 10000000,
                            10000020, CARD_RA)
 end
 
-function s.e6check(c, sg, g, code, ...)
+function s.e5check(c, sg, g, code, ...)
     local code2, code3 = c:GetOriginalCodeRule()
     if code ~= code2 and code ~= code3 then return false end
     local res
     if ... then
         g:AddCard(c)
-        res = sg:IsExists(s.e6check, 1, g, sg, g, ...)
+        res = sg:IsExists(s.e5check, 1, g, sg, g, ...)
         g:RemoveCard(c)
     else
         res = true
@@ -198,32 +185,32 @@ function s.e6check(c, sg, g, code, ...)
     return res
 end
 
-function s.e6cost(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e5cost(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
     local rg = Duel.GetReleaseGroup(tp)
-    local g1 = rg:Filter(s.e6filter1, nil, 10000000)
-    local g2 = rg:Filter(s.e6filter1, nil, 10000020)
-    local g3 = rg:Filter(s.e6filter1, nil, CARD_RA)
+    local g1 = rg:Filter(s.e5filter1, nil, 10000000)
+    local g2 = rg:Filter(s.e5filter1, nil, 10000020)
+    local g3 = rg:Filter(s.e5filter1, nil, CARD_RA)
     local g = g1:Clone()
     g:Merge(g2)
     g:Merge(g3)
 
     if chk == 0 then
         return c:IsAbleToGraveAsCost() and #g1 > 0 and #g2 > 0 and #g3 > 0 and
-                   aux.SelectUnselectGroup(g, e, tp, 3, 3, s.e6rescon, 0)
+                   aux.SelectUnselectGroup(g, e, tp, 3, 3, s.e5rescon, 0)
     end
 
     Duel.SendtoGrave(c, REASON_COST)
 
-    local sg = aux.SelectUnselectGroup(g, e, tp, 3, 3, s.e6rescon, 1, tp,
-                                       HINTMSG_RELEASE, s.e6rescon, nil, true)
+    local sg = aux.SelectUnselectGroup(g, e, tp, 3, 3, s.e5rescon, 1, tp,
+                                       HINTMSG_RELEASE, s.e5rescon, nil, true)
     Duel.Release(sg, REASON_COST)
 end
 
-function s.e6tg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e5tg(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then
         return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
-                   Duel.IsExistingMatchingCard(s.e6filter2, tp, LOCATION_HAND +
+                   Duel.IsExistingMatchingCard(s.e5filter2, tp, LOCATION_HAND +
                                                    LOCATION_DECK +
                                                    LOCATION_GRAVE, 0, 1, nil, e,
                                                tp)
@@ -233,9 +220,9 @@ function s.e6tg(e, tp, eg, ep, ev, re, r, rp, chk)
     Duel.SetChainLimit(aux.FALSE)
 end
 
-function s.e6op(e, tp, eg, ep, ev, re, r, rp)
+function s.e5op(e, tp, eg, ep, ev, re, r, rp)
     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
-    local tc = Duel.SelectMatchingCard(tp, s.e6filter2, tp, LOCATION_HAND +
+    local tc = Duel.SelectMatchingCard(tp, s.e5filter2, tp, LOCATION_HAND +
                                            LOCATION_DECK + LOCATION_GRAVE, 0, 1,
                                        1, nil, e, tp):GetFirst()
     if not tc then return end
