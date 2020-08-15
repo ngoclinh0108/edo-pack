d718e59f7a432ff1dbf8778bc22e46f44b21d654
Luc Ta <luc.ta@cptech.vn>
2020-08-16 02:58:34 +0700

    update

--- script/c410000006.lua
+++ script/c410000006.lua
@@ -2,46 +2,91 @@
 local s, id = GetID()
 
 function s.initial_effect(c)
-    -- 3 tribute
+    -- disable
     local e1 = Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetCode(EFFECT_TRIPLE_TRIBUTE)
-    e1:SetValue(aux.TargetBoolFunction(Card.IsAttribute, ATTRIBUTE_DIVINE))
+    e1:SetCategory(CATEGORY_DISABLE)
+    e1:SetType(EFFECT_TYPE_QUICK_O)
+    e1:SetCode(EVENT_CHAINING)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCountLimit(1, id)
+    e1:SetCondition(s.e1con)
+    e1:SetCost(s.e1cost)
+    e1:SetTarget(s.e1tg)
+    e1:SetOperation(s.e1op)
     c:RegisterEffect(e1)
 
-    -- summon token
+    -- 3 tribute
     local e2 = Effect.CreateEffect(c)
-    e2:SetCategory(CATEGORY_SPECIAL_SUMMON + CATEGORY_TOKEN)
-    e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
-    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_DELAY)
-    e2:SetCode(EVENT_RELEASE)
-    e2:SetCondition(s.e2con)
-    e2:SetTarget(s.e2tg)
-    e2:SetOperation(s.e2op)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_TRIPLE_TRIBUTE)
+    e2:SetValue(aux.TargetBoolFunction(Card.IsAttribute, ATTRIBUTE_DIVINE))
     c:RegisterEffect(e2)
 
-    -- extra material
+    -- summon token
     local e3 = Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_SINGLE)
-    e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
-    e3:SetCode(EFFECT_ADD_EXTRA_TRIBUTE)
-    e3:SetTargetRange(0, LOCATION_MZONE)
-    e3:SetValue(POS_FACEUP)
-    local e3reg = Effect.CreateEffect(c)
-    e3reg:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_GRANT)
-    e3reg:SetRange(LOCATION_GRAVE)
-    e3reg:SetTargetRange(LOCATION_HAND, 0)
-    e3reg:SetTarget(aux.TargetBoolFunction(Card.IsAttribute, ATTRIBUTE_DIVINE))
-    e3reg:SetLabelObject(e3)
-    c:RegisterEffect(e3reg)
+    e3:SetCategory(CATEGORY_SPECIAL_SUMMON + CATEGORY_TOKEN)
+    e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_DELAY)
+    e3:SetCode(EVENT_RELEASE)
+    e3:SetCondition(s.e3con)
+    e3:SetTarget(s.e3tg)
+    e3:SetOperation(s.e3op)
+    c:RegisterEffect(e3)
+
+    -- extra material
+    local e4 = Effect.CreateEffect(c)
+    e4:SetType(EFFECT_TYPE_SINGLE)
+    e4:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+    e4:SetCode(EFFECT_ADD_EXTRA_TRIBUTE)
+    e4:SetTargetRange(0, LOCATION_MZONE)
+    e4:SetValue(POS_FACEUP)
+    local e4reg = Effect.CreateEffect(c)
+    e4reg:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_GRANT)
+    e4reg:SetRange(LOCATION_GRAVE)
+    e4reg:SetTargetRange(LOCATION_HAND, 0)
+    e4reg:SetTarget(aux.TargetBoolFunction(Card.IsAttribute, ATTRIBUTE_DIVINE))
+    e4reg:SetLabelObject(e4)
+    c:RegisterEffect(e4reg)
 end
 
-function s.e2con(e, tp, eg, ep, ev, re, r, rp)
+function s.e1filter(c)
+    return c:IsAttribute(ATTRIBUTE_DIVINE) and c:IsType(TYPE_MONSTER) and
+               not c:IsPublic()
+end
+
+function s.e1con(e, tp, eg, ep, ev, re, r, rp)
+    return rp ~= tp and not re:GetHandler():IsDisabled() and
+               Duel.IsChainDisablable(ev)
+end
+
+function s.e1cost(e, tp, eg, ep, ev, re, r, rp, chk)
+    local c = e:GetHandler()
+    if chk == 0 then
+        return Duel.IsExistingMatchingCard(s.e1filter, tp, LOCATION_HAND, 0, 1,
+                                           nil) and c:IsDiscardable()
+    end
+
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_CONFIRM)
+    local g = Duel.SelectMatchingCard(tp, s.e1filter, tp, LOCATION_HAND, 0, 1,
+                                      1, nil)
+    Duel.ConfirmCards(1 - tp, g)
+    Duel.ShuffleHand(tp)
+    Duel.SendtoGrave(c, REASON_COST + REASON_DISCARD)
+end
+
+function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return not re:GetHandler():IsStatus(STATUS_DISABLED) end
+    Duel.SetOperationInfo(0, CATEGORY_DISABLE, eg, 1, 0, 0)
+end
+
+function s.e1op(e, tp, eg, ep, ev, re, r, rp) Duel.NegateEffect(ev) end
+
+function s.e3con(e, tp, eg, ep, ev, re, r, rp)
     return e:GetHandler():IsReason(REASON_SUMMON) and
                re:GetHandler():IsAttribute(ATTRIBUTE_DIVINE)
 end
 
-function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then
         return not Duel.IsPlayerAffectedByEffect(tp, CARD_BLUEEYES_SPIRIT) and
                    Duel.GetLocationCount(tp, LOCATION_MZONE) >= 2 and
@@ -55,7 +100,7 @@ function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
     Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 2, tp, 0)
 end
 
-function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
     if Duel.IsPlayerAffectedByEffect(tp, CARD_BLUEEYES_SPIRIT) or
         Duel.GetLocationCount(tp, LOCATION_MZONE) < 2 or
         not Duel.IsPlayerCanSpecialSummonMonster(tp, 410000000, 0x13a,
