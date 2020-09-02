5209c65fc66bb799151d61abe8258a7838b6180d
Luc Ta <luc.ta@cptech.vn>
2020-09-02 19:43:39 +0700

    fix

--- script/c410000019.lua
+++ script/c410000019.lua
@@ -29,10 +29,12 @@ function s.initial_effect(c)
 
     -- cannot be target
     local e2 = Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetType(EFFECT_TYPE_FIELD)
     e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
     e2:SetRange(LOCATION_MZONE)
+    e2:SetTargetRange(LOCATION_MZONE, 0)
+    e2:SetTarget(function(e, c) return c:IsSetCard(0x13a) end)
     e2:SetValue(aux.tgoval)
     c:RegisterEffect(e2)
 
