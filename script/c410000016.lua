5c1b097e3c647104d5753ad0b606002450e9cec3
Luc Ta <luc.ta@cptech.vn>
2020-09-02 15:28:01 +0700

    fix

--- script/c410000016.lua
+++ script/c410000016.lua
@@ -2,24 +2,31 @@
 local s, id = GetID()
 
 function s.initial_effect(c)
-    -- 3 tribute
+    -- special summon when attack
     local e1 = Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetCode(EFFECT_TRIPLE_TRIBUTE)
-    e1:SetValue(aux.TargetBoolFunction(Card.IsAttribute, ATTRIBUTE_DIVINE))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
+    e1:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e1:SetRange(LOCATION_HAND + LOCATION_GRAVE)
+    e1:SetCountLimit(1, id)
+    e1:SetCondition(s.e1con)
+    e1:SetTarget(s.e1tg)
+    e1:SetOperation(s.e1op)
     c:RegisterEffect(e1)
 
-    -- special summon when attack
+    -- no damage
     local e2 = Effect.CreateEffect(c)
-    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e2:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
-    e2:SetCode(EVENT_ATTACK_ANNOUNCE)
-    e2:SetRange(LOCATION_HAND + LOCATION_GRAVE)
-    e2:SetCountLimit(1, id)
-    e2:SetCondition(s.e2con)
-    e2:SetTarget(s.e2tg)
+    e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_F)
+    e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP + EFFECT_FLAG_DELAY)
+    e2:SetCode(EVENT_SUMMON_SUCCESS)
     e2:SetOperation(s.e2op)
     c:RegisterEffect(e2)
+    local e2b = e3:Clone()
+    e2b:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+    c:RegisterEffect(e2b)
+    local e2c = e2:Clone()
+    e2c:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e2c)
 
     -- special summon when destroyed
     local e3 = Effect.CreateEffect(c)
@@ -33,12 +40,12 @@ function s.initial_effect(c)
     c:RegisterEffect(e3)
 end
 
-function s.e2con(e, tp, eg, ep, ev, re, r, rp)
+function s.e1con(e, tp, eg, ep, ev, re, r, rp)
     return Duel.GetAttackTarget() == nil and
                Duel.GetAttacker():IsControler(1 - tp)
 end
 
-function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
     if chk == 0 then
         return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
@@ -48,23 +55,25 @@ function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
     Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, c, 1, 0, 0)
 end
 
-function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+function s.e1op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     if not c:IsRelateToEffect(e) then return end
+    Duel.SpecialSummon(c, 0, tp, tp, false, false, POS_FACEUP)
+end
 
-    if Duel.SpecialSummon(c, 0, tp, tp, false, false, POS_FACEUP) ~= 0 then
-        local ec1 = Effect.CreateEffect(c)
-        ec1:SetType(EFFECT_TYPE_FIELD)
-        ec1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-        ec1:SetCode(EFFECT_CHANGE_DAMAGE)
-        ec1:SetTargetRange(1, 0)
-        ec1:SetValue(0)
-        ec1:SetReset(RESET_PHASE + PHASE_END)
-        Duel.RegisterEffect(ec1, tp)
-        local ec2 = ec1:Clone()
-        ec2:SetCode(EFFECT_NO_EFFECT_DAMAGE)
-        Duel.RegisterEffect(ec2, tp)
-    end
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    local ec1 = Effect.CreateEffect(c)
+    ec1:SetType(EFFECT_TYPE_FIELD)
+    ec1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    ec1:SetCode(EFFECT_CHANGE_DAMAGE)
+    ec1:SetTargetRange(1, 0)
+    ec1:SetValue(0)
+    ec1:SetReset(RESET_PHASE + PHASE_END)
+    Duel.RegisterEffect(ec1, tp)
+    local ec2 = ec1:Clone()
+    ec2:SetCode(EFFECT_NO_EFFECT_DAMAGE)
+    Duel.RegisterEffect(ec2, tp)
 end
 
 function s.e3filter(c, e, tp)
