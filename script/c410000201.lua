a4f474889efc0e13f8237fb6970062a64928ecb7
Luc Ta <luc.ta@cptech.vn>
2020-09-27 03:59:41 +0700

    fix

--- script/c410000201.lua
+++ script/c410000201.lua
@@ -2,21 +2,70 @@
 local s, id = GetID()
 
 function s.initial_effect(c)
-    -- destroy monster
+    -- special summon
     local e1 = Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id, 0))
-    e1:SetCategory(CATEGORY_DESTROY)
-    e1:SetType(EFFECT_TYPE_IGNITION)
-    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetCountLimit(1)
-    e1:SetCost(s.e1cost)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCondition(s.e1con)
     e1:SetTarget(s.e1tg)
     e1:SetOperation(s.e1op)
     c:RegisterEffect(e1)
+
+    -- destroy
+    local e2 = Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id, 0))
+    e2:SetCategory(CATEGORY_DESTROY)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1)
+    e2:SetCost(s.e2cost)
+    e2:SetTarget(s.e2tg)
+    e2:SetOperation(s.e2op)
+    c:RegisterEffect(e2)
 end
 
-function s.e1cost(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e1filter(c)
+    return c:IsReleasableByEffect() and c:IsLevel(1) and
+               c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsType(TYPE_TUNER)
+end
+
+function s.e1con(e, c)
+    if c == nil then return true end
+    local tp = c:GetControler()
+    local g = Duel.GetMatchingGroup(s.e1filter, tp,
+                                    LOCATION_HAND + LOCATION_ONFIELD, 0, nil)
+
+    return aux.SelectUnselectGroup(g, e, tp, 1, 1, aux.ChkfMMZ(1), 0)
+end
+
+function s.e1tg(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+
+    local g = Duel.GetMatchingGroup(s.e1filter, tp,
+                                    LOCATION_HAND + LOCATION_ONFIELD, 0, c)
+    local rg = aux.SelectUnselectGroup(g, e, tp, 1, 1, aux.ChkfMMZ(1), 1, tp,
+                                       HINTMSG_RELEASE)
+
+    if #rg > 0 then
+        rg:KeepAlive()
+        e:SetLabelObject(rg)
+        return true
+    end
+    return false
+end
+
+function s.e1op(e, tp, eg, ep, ev, re, r, rp, c)
+    local g = e:GetLabelObject()
+    if not g then return end
+
+    Duel.Release(g, REASON_COST)
+    g:DeleteGroup()
+end
+
+function s.e2cost(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
     if chk == 0 then return c:GetAttackAnnouncedCount() == 0 end
 
@@ -28,7 +77,7 @@ function s.e1cost(e, tp, eg, ep, ev, re, r, rp, chk)
     c:RegisterEffect(ec1)
 end
 
-function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     if chk == 0 then
         return Duel.IsExistingTarget(aux.TRUE, tp, 0, LOCATION_MZONE, 1, nil)
     end
@@ -39,7 +88,7 @@ function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     Duel.SetOperationInfo(0, CATEGORY_DESTROY, g, 1, 0, 0)
 end
 
-function s.e1op(e, tp, eg, ep, ev, re, r, rp)
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
     local tc = Duel.GetFirstTarget()
     if not tc or not tc:IsRelateToEffect(e) then return end
 
