e401630dad87513cf0909d317557f2c38ff21459
Luc Ta <luc.ta@cptech.vn>
2020-09-26 20:37:23 +0700

    fix

--- script/c410000008.lua
+++ script/c410000008.lua
@@ -13,13 +13,102 @@ function s.initial_effect(c)
     c:RegisterEffect(code)
 
     -- cannot be target
-    local e1 = Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_FIELD)
-    e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-    e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetTargetRange(LOCATION_MZONE, 0)
-    e1:SetTarget(function(e, c) return c:IsSetCard(0x13a) end)
-    e1:SetValue(aux.tgoval)
-    c:RegisterEffect(e1)
+    local e2 = Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+    e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetTargetRange(LOCATION_MZONE, 0)
+    e2:SetTarget(function(e, c) return c:IsSetCard(0x13a) end)
+    e2:SetValue(aux.tgoval)
+    c:RegisterEffect(e2)
+
+    -- banish
+    local e3 = Effect.CreateEffect(c)
+    e3:SetDescription(1107)
+    e3:SetCategory(CATEGORY_REMOVE)
+    e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_F)
+    e3:SetCode(EVENT_BATTLED)
+    e3:SetCondition(s.e3con)
+    e3:SetTarget(s.e3tg)
+    e3:SetOperation(s.e3op)
+    c:RegisterEffect(e3)
+
+    -- search spell/trap
+    local e4 = Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id, 0))
+    e4:SetCategory(CATEGORY_TOHAND)
+    e4:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
+    e4:SetCode(EVENT_PHASE + PHASE_END)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCountLimit(1, id)
+    e4:SetCost(s.e4cost)
+    e4:SetTarget(s.e4tg)
+    e4:SetOperation(s.e4op)
+    c:RegisterEffect(e4)
+    aux.GlobalCheck(s, function()
+        local e4reg = Effect.CreateEffect(c)
+        e4reg:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
+        e4reg:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+        e4reg:SetCode(EVENT_SUMMON_SUCCESS)
+        e4reg:SetLabel(id)
+        e4reg:SetOperation(aux.sumreg)
+        Duel.RegisterEffect(e4reg, 0)
+        local e4regb = e4reg:Clone()
+        e4regb:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
+        Duel.RegisterEffect(e4regb, 0)
+        local e4regc = e4reg:Clone()
+        e4regc:SetCode(EVENT_SPSUMMON_SUCCESS)
+        Duel.RegisterEffect(e4regc, 0)
+    end)
+end
+
+function s.e3con(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    local bc = c:GetBattleTarget()
+    e:SetLabelObject(bc)
+
+    return bc and bc:IsStatus(STATUS_BATTLE_DESTROYED) and
+               c:IsStatus(STATUS_OPPO_BATTLE)
+end
+
+function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return true end
+    Duel.SetOperationInfo(0, CATEGORY_REMOVE, e:GetLabelObject(), 1, 0, 0)
+end
+
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+    local bc = e:GetLabelObject()
+    if not bc:IsRelateToBattle() or not bc:IsAbleToRemove() then return end
+
+    Duel.Remove(bc, POS_FACEUP, REASON_EFFECT)
+end
+
+function s.e4filter(c)
+    return c:IsType(TYPE_SPELL + TYPE_TRAP) and c:IsAbleToHand()
+end
+
+function s.e4cost(e, tp, eg, ep, ev, re, r, rp, chk)
+    local c = e:GetHandler()
+    if chk == 0 then return c:GetFlagEffect(id) ~= 0 end
+    c:ResetFlagEffect(id)
+end
+
+function s.e4tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    if chk == 0 then
+        return Duel.IsExistingTarget(s.e4filter, tp,
+                                     LOCATION_DECK + LOCATION_GRAVE, 0, 1, nil)
+    end
+    Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, 0, 0)
+end
+
+function s.e4op(e, tp, eg, ep, ev, re, r, rp)
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_ATOHAND)
+    local g = Duel.SelectMatchingCard(tp, s.e4filter, tp,
+                                      LOCATION_DECK + LOCATION_GRAVE, 0, 1, 1,
+                                      nil)
+    if #g > 0 then
+        Duel.SendtoHand(g, nil, REASON_EFFECT)
+        Duel.ConfirmCards(1 - tp, g)
+    end
 end
