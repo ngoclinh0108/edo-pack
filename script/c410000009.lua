f34b83f62997ae7170c0f98916dcafcb0f9d8985
Luc Ta <luc.ta@cptech.vn>
2020-09-26 21:05:35 +0700

    fix

--- script/c410000009.lua
+++ script/c410000009.lua
@@ -2,41 +2,41 @@
 local s, id = GetID()
 
 function s.initial_effect(c)
-    -- special summon when attack
+    -- synchro level
     local e1 = Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
-    e1:SetCode(EVENT_ATTACK_ANNOUNCE)
-    e1:SetRange(LOCATION_HAND + LOCATION_GRAVE)
-    e1:SetCountLimit(1, id)
-    e1:SetCondition(s.e1con)
-    e1:SetTarget(s.e1tg)
-    e1:SetOperation(s.e1op)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_SYNCHRO_LEVEL)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetValue(s.e1val)
     c:RegisterEffect(e1)
-
-    -- no damage
+    
+    -- special summon when attack
     local e2 = Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_F)
-    e2:SetCode(EVENT_LEAVE_FIELD)
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_ATTACK_ANNOUNCE)
+    e2:SetRange(LOCATION_HAND + LOCATION_GRAVE)
+    e2:SetCountLimit(1, id)
     e2:SetCondition(s.e2con)
+    e2:SetTarget(s.e2tg)
     e2:SetOperation(s.e2op)
     c:RegisterEffect(e2)
 
-    -- synchro level
+    -- no damage
     local e3 = Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_SINGLE)
-    e3:SetCode(EFFECT_SYNCHRO_LEVEL)
-    e3:SetRange(LOCATION_MZONE)
-    e3:SetValue(s.e3val)
+    e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_F)
+    e3:SetCode(EVENT_LEAVE_FIELD)
+    e3:SetCondition(s.e3con)
+    e3:SetOperation(s.e3op)
     c:RegisterEffect(e3)
 end
 
-function s.e1con(e, tp, eg, ep, ev, re, r, rp)
+function s.e2con(e, tp, eg, ep, ev, re, r, rp)
     return Duel.GetAttackTarget() == nil and
                Duel.GetAttacker():IsControler(1 - tp)
 end
 
-function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
     if chk == 0 then
         return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
@@ -46,17 +46,17 @@ function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
     Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, c, 1, 0, 0)
 end
 
-function s.e1op(e, tp, eg, ep, ev, re, r, rp)
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     if not c:IsRelateToEffect(e) then return end
     Duel.SpecialSummon(c, 0, tp, tp, false, false, POS_FACEUP)
 end
 
-function s.e2con(e, tp, eg, ep, ev, re, r, rp)
+function s.e3con(e, tp, eg, ep, ev, re, r, rp)
     return e:GetHandler():IsReason(REASON_DESTROY)
 end
 
-function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     local ec1 = Effect.CreateEffect(c)
     ec1:SetType(EFFECT_TYPE_FIELD)
@@ -71,4 +71,4 @@ function s.e2op(e, tp, eg, ep, ev, re, r, rp)
     Duel.RegisterEffect(ec2, tp)
 end
 
-function s.e3val(e, c) return 2 * 65536 + e:GetHandler():GetLevel() end
+function s.e1val(e, c) return 2 * 65536 + e:GetHandler():GetLevel() end
