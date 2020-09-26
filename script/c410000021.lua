9e812874b341990b996888c1788a8f35f13c3d27
Luc Ta <luc.ta@cptech.vn>
2020-09-27 02:42:19 +0700

    fix

--- script/c410000021.lua
+++ script/c410000021.lua
@@ -27,14 +27,30 @@ function s.initial_effect(c)
     e2:SetValue(s.e2val)
     c:RegisterEffect(e2)
 
-    -- destroy replace
+    -- damage reduce
     local e3 = Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_CONTINUOUS)
-    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e3:SetCode(EFFECT_DESTROY_REPLACE)
+    e3:SetType(EFFECT_TYPE_FIELD)
+    e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e3:SetCode(EFFECT_CHANGE_DAMAGE)
     e3:SetRange(LOCATION_MZONE)
-    e3:SetTarget(s.e3tg)
+    e3:SetTargetRange(1, 0)
+    e3:SetValue(function(e, re, val, r, rp, rc)
+        if r & REASON_EFFECT ~= 0 then return 0 end
+        return val
+    end)
     c:RegisterEffect(e3)
+    local e3b = e3:Clone()
+    e3b:SetCode(EFFECT_NO_EFFECT_DAMAGE)
+    c:RegisterEffect(e3b)
+
+    -- destroy replace
+    local e4 = Effect.CreateEffect(c)
+    e4:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_CONTINUOUS)
+    e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e4:SetCode(EFFECT_DESTROY_REPLACE)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetTarget(s.e4tg)
+    c:RegisterEffect(e4)
 end
 
 function s.xyzcheck(g, tp, sc) return g:GetClassCount(Card.GetAttribute) == #g end
@@ -49,7 +65,7 @@ end
 
 function s.e1op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
-    
+
     local ct = Duel.GetLocationCount(1 - tp, LOCATION_MZONE)
     if ct == 0 then return end
     if ct > 3 then ct = 3 end
@@ -74,7 +90,7 @@ end
 
 function s.e2val(e, te) return te:GetOwner() ~= e:GetOwner() end
 
-function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e4tg(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
     if chk == 0 then
         return not c:IsReason(REASON_REPLACE) and
