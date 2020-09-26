6455e14fd3488d5f000d3c98cb095e55ac957fc7
Luc Ta <luc.ta@cptech.vn>
2020-09-27 04:31:06 +0700

    fix

--- script/c410000201.lua
+++ script/c410000201.lua
@@ -2,6 +2,18 @@
 local s, id = GetID()
 
 function s.initial_effect(c)
+    --Normal monster
+	local normal=Effect.CreateEffect(c)
+	normal:SetType(EFFECT_TYPE_SINGLE)
+	normal:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+	normal:SetCode(EFFECT_ADD_TYPE)
+	normal:SetValue(TYPE_NORMAL)
+	c:RegisterEffect(normal)
+	local normalb=normal:Clone()
+	normalb:SetCode(EFFECT_REMOVE_TYPE)
+	normalb:SetValue(TYPE_EFFECT)
+    c:RegisterEffect(normalb)
+    
     -- special summon
     local e1 = Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_FIELD)
