72489c4ce61fe3900c4e5b8b2d72ba2a0cf51f5d
Luc Ta <luc.ta@cptech.vn>
2020-09-27 00:22:33 +0700

    fix

--- script/c410000021.lua
+++ script/c410000021.lua
@@ -2,5 +2,13 @@
 local s, id = GetID()
 
 function s.initial_effect(c)
-    -- effects
+    c:EnableReviveLimit()
+
+    -- xyz summon
+    Xyz.AddProcedure(c, s.xyzfilter, 7, 3)
+end
+
+function s.xyzfilter(c, sc, SUMMON_TYPE_XYZ, tp)
+    return Duel.GetFlagEffect(c:GetControler(), id) == 0 and
+               c:IsSetCard(0x13a, sc, SUMMON_TYPE_XYZ, tp)
 end
