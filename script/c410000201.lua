dd5a55fef2618de8eb2a0eb84d23dba554f90f70
Luc Ta <luc.ta@cptech.vn>
2020-09-27 03:46:37 +0700

    fix

---
+++ script/c410000201.lua
@@ -0,0 +1,5 @@
+-- Blue-Eyes Sacred White Dragon
+local s, id = GetID()
+function s.initial_effect(c)
+  -- effects
+end
---
+++ script/c410000301.lua
@@ -0,0 +1,5 @@
+-- Red-Eyes Sacred Black Dragon
+local s, id = GetID()
+function s.initial_effect(c)
+  -- effects
+end
