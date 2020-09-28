c86370148b380bdaad0bd11bcbd0a4c602e7d9fa
Luc Ta <luc.ta@cptech.vn>
2020-09-28 20:27:04 +0700

    fix

--- script/utility.lua
+++ script/utility.lua
@@ -1,15 +1,17 @@
 -- init
-if not aux.Utility then aux.Utility = {} end
-if not Utility then Utility = aux.Utility end
+if not aux.UtilityProcedure then aux.UtilityProcedure = {} end
+if not Utility then Utility = aux.UtilityProcedure end
 
 -- function
-function Utility.IsSetCardListed(c,...)
-	if not c.listed_series then return false end
-	local codes={...}
-	for _,code in ipairs(codes) do
-		for _,ccode in ipairs(c.listed_series) do
-			if code==ccode then return true end
-		end
-	end
-	return false
+function Utility.IsSetCardListed(c, ...)
+    if not c.listed_series then return false end
+
+    local codes = {...}
+    for _, code in ipairs(codes) do
+        for _, ccode in ipairs(c.listed_series) do
+            if code == ccode then return true end
+        end
+    end
+
+    return false
 end
