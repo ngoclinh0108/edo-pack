b545fca2fae770402c9c80065ca94b5d8daef459
Luc Ta <luc.ta@cptech.vn>
2020-10-11 23:36:56 +0700

    fix

--- script/c410000035.lua
+++ script/c410000035.lua
@@ -22,4 +22,14 @@ function s.initial_effect(c)
     splimit:SetRange(LOCATION_EXTRA)
     splimit:SetValue(aux.synlimit)
     c:RegisterEffect(splimit)
+
+    -- indes
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetTargetRange(LOCATION_MZONE, 0)
+    e1:SetTarget(function(e, c) return c:IsRace(RACE_SPELLCASTER) end)
+    e1:SetValue(1)
+    c:RegisterEffect(e1)
 end
