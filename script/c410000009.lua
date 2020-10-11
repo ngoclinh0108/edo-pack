7aa992ff87d2c9c4d04c7756fabb4bb51c84f88d
Luc Ta <luc.ta@cptech.vn>
2020-10-11 18:17:49 +0700

    fix

--- script/c410000009.lua
+++ script/c410000009.lua
@@ -22,12 +22,10 @@ function s.initial_effect(c)
     -- special summon
     local e1 = Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_FIELD)
-    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
     e1:SetCode(EFFECT_SPSUMMON_PROC)
     e1:SetRange(LOCATION_HAND)
     e1:SetCondition(s.e1con)
-    e1:SetTarget(s.e1tg)
-    e1:SetOperation(s.e1op)
     c:RegisterEffect(e1)
 
     -- indes
@@ -54,34 +52,18 @@ function s.initial_effect(c)
     c:RegisterEffect(e3)
 end
 
+function s.e1filter(c)
+    return c:IsFaceup() and c:IsLevelAbove(7) and c:IsSetCard(0x13a) and
+               c:IsRace(RACE_SPELLCASTER)
+end
+
 function s.e1con(e, c)
     if c == nil then return true end
     local tp = c:GetControler()
 
-    local rg = Duel.GetMatchingGroup(Card.IsDiscardable, tp, LOCATION_HAND, 0,
-                                     e:GetHandler())
-    return aux.SelectUnselectGroup(rg, e, tp, 1, 1, aux.ChkfMMZ(1), 0, c)
-end
-
-function s.e1tg(e, tp, eg, ep, ev, re, r, rp)
-    local rg = Duel.GetMatchingGroup(Card.IsDiscardable, tp, LOCATION_HAND, 0,
-                                     e:GetHandler())
-    local g = aux.SelectUnselectGroup(rg, e, tp, 1, 1, aux.ChkfMMZ(1), 1, tp,
-                                      HINTMSG_DISCARD, nil, nil, true)
-
-    if #g > 0 then
-        g:KeepAlive()
-        e:SetLabelObject(g)
-        return true
-    end
-    return false
-end
-
-function s.e1op(e, tp, eg, ep, ev, re, r, rp, c)
-    local g = e:GetLabelObject()
-    if not g then return end
-    Duel.SendtoGrave(g, REASON_DISCARD + REASON_COST)
-    g:DeleteGroup()
+    return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
+               Duel.IsExistingMatchingCard(s.e1filter, tp, LOCATION_MZONE, 0, 1,
+                                           c)
 end
 
 function s.e3con(e, tp, eg, ep, ev, re, r, rp)
--- script/c410000012.lua
+++ script/c410000012.lua
@@ -18,6 +18,8 @@ function s.initial_effect(c)
     e1:SetCode(EFFECT_SPSUMMON_PROC)
     e1:SetRange(LOCATION_HAND)
     e1:SetCondition(s.e1con)
+    e1:SetTarget(s.e1tg)
+    e1:SetOperation(s.e1op)
     c:RegisterEffect(e1)
 
     -- reset a monster ATK
@@ -50,10 +52,31 @@ end
 function s.e1con(e, c)
     if c == nil then return true end
     local tp = c:GetControler()
-    return Duel.GetFieldGroupCount(tp, LOCATION_MZONE, 0) == 0 or
-               Duel.IsExistingMatchingCard(
-                   aux.FilterFaceupFunction(Card.IsAttribute, ATTRIBUTE_DARK),
-                   tp, 0, LOCATION_MZONE, 1, nil)
+
+    local rg = Duel.GetMatchingGroup(Card.IsDiscardable, tp, LOCATION_HAND, 0,
+                                     e:GetHandler())
+    return aux.SelectUnselectGroup(rg, e, tp, 1, 1, aux.ChkfMMZ(1), 0, c)
+end
+
+function s.e1tg(e, tp, eg, ep, ev, re, r, rp)
+    local rg = Duel.GetMatchingGroup(Card.IsDiscardable, tp, LOCATION_HAND, 0,
+                                     e:GetHandler())
+    local g = aux.SelectUnselectGroup(rg, e, tp, 1, 1, aux.ChkfMMZ(1), 1, tp,
+                                      HINTMSG_DISCARD, nil, nil, true)
+
+    if #g > 0 then
+        g:KeepAlive()
+        e:SetLabelObject(g)
+        return true
+    end
+    return false
+end
+
+function s.e1op(e, tp, eg, ep, ev, re, r, rp, c)
+    local g = e:GetLabelObject()
+    if not g then return end
+    Duel.SendtoGrave(g, REASON_DISCARD + REASON_COST)
+    g:DeleteGroup()
 end
 
 function s.e2filter(c) return c:IsFaceup() and c:GetAttack() ~=
