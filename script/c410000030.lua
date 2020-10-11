914878de976a8ae2084a09498056c00b0ac89548
Luc Ta <luc.ta@cptech.vn>
2020-10-11 23:31:28 +0700

    fix

--- script/c410000030.lua
+++ script/c410000030.lua
@@ -9,6 +9,7 @@ function s.initial_effect(c)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
     e1:SetCost(s.effcost)
+    e1:SetTarget(s.efftg)
     e1:SetOperation(s.e1op)
     c:RegisterEffect(e1)
 
@@ -18,6 +19,7 @@ function s.initial_effect(c)
     e2:SetType(EFFECT_TYPE_ACTIVATE)
     e2:SetCode(EVENT_FREE_CHAIN)
     e2:SetCost(s.effcost)
+    e2:SetTarget(s.efftg)
     e2:SetOperation(s.e2op)
     c:RegisterEffect(e2)
 
@@ -27,6 +29,7 @@ function s.initial_effect(c)
     e3:SetType(EFFECT_TYPE_ACTIVATE)
     e3:SetCode(EVENT_FREE_CHAIN)
     e3:SetCost(s.effcost)
+    e3:SetTarget(s.efftg)
     e3:SetOperation(s.e3op)
     c:RegisterEffect(e3)
 end
@@ -41,7 +44,13 @@ function s.effcost(e, tp, eg, ep, ev, re, r, rp, chk)
     local rg = Duel.SelectReleaseGroupCost(tp, s.efffilter, 1, 1, false, nil,
                                            nil)
     Duel.Release(rg, REASON_COST)
+end
+
+function s.efftg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return true end
+
     Duel.Hint(HINT_OPSELECTED, 1 - tp, e:GetDescription())
+    Duel.SetChainLimit(aux.FALSE)
 end
 
 function s.e1op(e, tp, eg, ep, ev, re, r, rp)
