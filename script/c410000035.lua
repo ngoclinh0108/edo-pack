527de41141f918b1396fd53212cfc5fb31b5b50e
Luc Ta <luc.ta@cptech.vn>
2020-10-12 00:14:53 +0700

    fix

--- script/c410000035.lua
+++ script/c410000035.lua
@@ -23,15 +23,45 @@ function s.initial_effect(c)
     splimit:SetValue(aux.synlimit)
     c:RegisterEffect(splimit)
 
-    -- indes
+    -- no effect damage
     local e1 = Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_FIELD)
-    e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e1:SetCode(EFFECT_CHANGE_DAMAGE)
     e1:SetRange(LOCATION_MZONE)
-    e1:SetTargetRange(LOCATION_MZONE, 0)
-    e1:SetTarget(function(e, c) return c:IsRace(RACE_SPELLCASTER) end)
-    e1:SetValue(1)
+    e1:SetTargetRange(1, 0)
+    e1:SetValue(function(e, re, val, r, rp, rc)
+        if (r & REASON_EFFECT) ~= 0 then return 0 end
+        return val
+    end)
     c:RegisterEffect(e1)
+    local e1b = e1:Clone()
+    e1b:SetCode(EFFECT_NO_EFFECT_DAMAGE)
+    c:RegisterEffect(e1b)
+
+    -- indes
+    local e2 = Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetTargetRange(LOCATION_MZONE, 0)
+    e2:SetTarget(function(e, c) return c:IsRace(RACE_SPELLCASTER) end)
+    e2:SetValue(1)
+    c:RegisterEffect(e2)
+
+    -- negate
+    local e3 = Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id, 0))
+    e3:SetCategory(CATEGORY_DISABLE)
+    e3:SetType(EFFECT_TYPE_QUICK_O)
+    e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e3:SetCode(EVENT_FREE_CHAIN)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetHintTiming(0, TIMINGS_CHECK_MONSTER)
+    e3:SetCountLimit(1, id)
+    e3:SetTarget(s.e3tg)
+    e3:SetOperation(s.e3op)
+    c:RegisterEffect(e3)
 
     -- special summon
     local e4 = Effect.CreateEffect(c)
@@ -43,6 +73,54 @@ function s.initial_effect(c)
     e4:SetTarget(s.e4tg)
     e4:SetOperation(s.e4op)
     c:RegisterEffect(e4)
+end
+
+function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    if chk == 0 then
+        return Duel.IsExistingTarget(aux.disfilter1, tp, 0, LOCATION_ONFIELD, 1,
+                                     nil)
+    end
+
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_NEGATE)
+    local g = Duel.SelectTarget(tp, aux.disfilter1, tp, 0, LOCATION_ONFIELD, 1,
+                                1, nil)
+
+    Duel.SetOperationInfo(0, CATEGORY_DISABLE, g, 1, 0, 0)
+end
+
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    local tc = Duel.GetFirstTarget()
+    if not tc or not tc:IsRelateToEffect(e) then return end
+    if (tc:IsFacedown() or tc:IsDisabled()) and not tc:IsType(TYPE_TRAPMONSTER) then
+        return
+    end
+
+    Duel.NegateRelatedChain(tc, RESET_TURN_SET)
+
+    local ec1 = Effect.CreateEffect(c)
+    ec1:SetType(EFFECT_TYPE_SINGLE)
+    ec1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    ec1:SetCode(EFFECT_DISABLE)
+    ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
+    tc:RegisterEffect(ec1)
+
+    local ec2 = Effect.CreateEffect(c)
+    ec2:SetType(EFFECT_TYPE_SINGLE)
+    ec2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    ec2:SetCode(EFFECT_DISABLE_EFFECT)
+    ec2:SetValue(RESET_TURN_SET)
+    ec2:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
+    tc:RegisterEffect(ec2)
+
+    if tc:IsType(TYPE_TRAPMONSTER) then
+        local ec3 = Effect.CreateEffect(c)
+        ec3:SetType(EFFECT_TYPE_SINGLE)
+        ec3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+        ec3:SetCode(EFFECT_DISABLE_TRAPMONSTER)
+        ec3:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
+        tc:RegisterEffect(ec3)
+    end
 end
 
 function s.e4filter(c, e, tp)
