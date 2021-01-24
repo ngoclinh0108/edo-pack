08a5faea63d56cf230c969c92a195892e239364e
Luc Ta <luc.ta@cptech.vn>
2021-01-24 15:30:36 +0700

    fix

--- script/c410000000.lua
+++ script/c410000000.lua
@@ -70,15 +70,28 @@ function s.initial_effect(c)
     e4:SetTarget(aux.TargetBoolFunction(Card.IsAttribute, ATTRIBUTE_DIVINE))
     c:RegisterEffect(e4)
 
-    -- shuffle deck
-    local e5 = Effect.CreateEffect(c)
+    -- draw
+    local e5=Effect.CreateEffect(c)
     e5:SetDescription(aux.Stringid(id, 2))
+	e5:SetCategory(CATEGORY_DRAW)
     e5:SetType(EFFECT_TYPE_IGNITION)
+    e5:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e5:SetCode(EVENT_FREE_CHAIN)
     e5:SetRange(LOCATION_FZONE)
-    e5:SetCountLimit(1)
-    e5:SetTarget(s.e5tg)
-    e5:SetOperation(s.e5op)
-    c:RegisterEffect(e5)
+    e5:SetCost(s.e5cost)
+	e5:SetTarget(s.e5tg)
+	e5:SetOperation(s.e5op)
+	c:RegisterEffect(e5)
+
+    -- shuffle deck
+    local e6 = Effect.CreateEffect(c)
+    e6:SetDescription(aux.Stringid(id, 3))
+    e6:SetType(EFFECT_TYPE_IGNITION)
+    e6:SetRange(LOCATION_FZONE)
+    e6:SetCountLimit(1)
+    e6:SetTarget(s.e6tg)
+    e6:SetOperation(s.e6op)
+    c:RegisterEffect(e6)
 end
 
 function s.actcost(e, tp, eg, ep, ev, re, r, rp, chk)
@@ -155,13 +168,40 @@ function s.e3op(e, tp, eg, ep, ev, re, r, rp)
     end
 end
 
-function s.e5tg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e5filter(c)
+	return c:IsAttribute(ATTRIBUTE_DIVINE) and c:IsType(TYPE_MONSTER) and not c:IsPublic()
+end
+
+function s.e5cost(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsExistingMatchingCard(s.e5filter,tp,LOCATION_HAND,0,1,nil) end
+    
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_CONFIRM)
+	local g=Duel.SelectMatchingCard(tp,s.e5filter,tp,LOCATION_HAND,0,1,1,nil)
+	Duel.ConfirmCards(1-tp,g)
+	Duel.ShuffleHand(tp)
+end
+
+function s.e5tg(e,tp,eg,ep,ev,re,r,rp,chk)
+    if chk==0 then return Duel.IsPlayerCanDraw(tp,2) end
+    
+	Duel.SetTargetPlayer(tp)
+	Duel.SetTargetParam(2)
+	Duel.SetOperationInfo(0,CATEGORY_DRAW,nil,0,tp,2)
+end
+
+function s.e5op(e,tp,eg,ep,ev,re,r,rp)
+    local p,d=Duel.GetChainInfo(0,CHAININFO_TARGET_PLAYER,CHAININFO_TARGET_PARAM)
+    
+	Duel.Draw(p,d,REASON_EFFECT)
+end
+
+function s.e6tg(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then return Duel.GetFieldGroupCount(tp, LOCATION_DECK, 0) > 0 end
 
     Duel.Hint(HINT_OPSELECTED, 1 - tp, e:GetDescription())
 end
 
-function s.e5op(e, tp, eg, ep, ev, re, r, rp)
+function s.e6op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     if not c:IsRelateToEffect(e) then return end
 
