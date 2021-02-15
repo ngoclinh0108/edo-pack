1c8c0436a7c6d5c888ea95359f6b3259613e4947
Luc Ta <luc.ta@cptech.vn>
2021-02-16 05:44:07 +0700

    fix

--- script/c410000026.lua
+++ script/c410000026.lua
@@ -2,6 +2,8 @@
 Duel.LoadScript("util.lua")
 local s, id = GetID()
 
+s.listed_names = {410000000}
+
 function s.initial_effect(c)
     -- activate
     local e1 = Effect.CreateEffect(c)
@@ -13,26 +15,33 @@ function s.initial_effect(c)
     e1:SetOperation(s.e1op)
     c:RegisterEffect(e1)
 
-    -- add to hand
+    -- act in hand
     local e2 = Effect.CreateEffect(c)
-    e2:SetCategory(CATEGORY_TOHAND)
-    e2:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
-    e2:SetCode(EVENT_PHASE + PHASE_DRAW)
-    e2:SetRange(LOCATION_DECK)
-    e2:SetCost(s.e2cost)
-    e2:SetTarget(s.e2tg)
-    e2:SetOperation(s.e2op)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_QP_ACT_IN_NTPHAND)
+    e2:SetCondition(function(e, tp) return Duel.IsEnvironment(410000000, tp) end)
     c:RegisterEffect(e2)
 
-    -- salvage
+    -- add to hand
     local e3 = Effect.CreateEffect(c)
     e3:SetCategory(CATEGORY_TOHAND)
-    e3:SetType(EFFECT_TYPE_IGNITION)
-    e3:SetRange(LOCATION_GRAVE)
-    e3:SetCondition(aux.exccon)
+    e3:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
+    e3:SetCode(EVENT_PHASE + PHASE_DRAW)
+    e3:SetRange(LOCATION_DECK)
+    e3:SetCost(s.e3cost)
     e3:SetTarget(s.e3tg)
     e3:SetOperation(s.e3op)
     c:RegisterEffect(e3)
+
+    -- salvage
+    local e4 = Effect.CreateEffect(c)
+    e4:SetCategory(CATEGORY_TOHAND)
+    e4:SetType(EFFECT_TYPE_IGNITION)
+    e4:SetRange(LOCATION_GRAVE)
+    e4:SetCondition(aux.exccon)
+    e4:SetTarget(s.e4tg)
+    e4:SetOperation(s.e4op)
+    c:RegisterEffect(e4)
 end
 
 function s.e1filter(c)
@@ -79,9 +88,9 @@ end
 
 function s.e1discon(e, tp, eg, ep, ev, re, r, rp)
     local code = e:GetLabel()
-    local code1, code3 = re:GetHandler():GetOriginalCodeRule()
+    local code1, code4 = re:GetHandler():GetOriginalCodeRule()
     return rp ~= tp and re:IsActiveType(TYPE_MONSTER) and
-               (code1 == code or code3 == code)
+               (code1 == code or code4 == code)
 end
 
 function s.e1disop(e, tp, eg, ep, ev, re, r, rp)
@@ -91,11 +100,11 @@ end
 
 function s.e1distg(e, c)
     local code = e:GetLabel()
-    local code1, code3 = c:GetOriginalCodeRule()
-    return code1 == code or code3 == code
+    local code1, code4 = c:GetOriginalCodeRule()
+    return code1 == code or code4 == code
 end
 
-function s.e2cost(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e3cost(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
     if chk == 0 then return Duel.GetActivityCount(tp, ACTIVITY_ATTACK) == 0 end
 
@@ -108,13 +117,13 @@ function s.e2cost(e, tp, eg, ep, ev, re, r, rp, chk)
     Duel.RegisterEffect(ec1, tp)
 end
 
-function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
     if chk == 0 then return c:IsAbleToHand() end
     Duel.SetOperationInfo(0, CATEGORY_TOHAND, c, 1, 0, 0)
 end
 
-function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     if not c:IsRelateToEffect(e) then return end
 
@@ -122,22 +131,22 @@ function s.e2op(e, tp, eg, ep, ev, re, r, rp)
     Duel.ConfirmCards(1 - tp, c)
 end
 
-function s.e3filter(c) return c:IsFaceup() and c:IsAbleToHand() end
+function s.e4filter(c) return c:IsFaceup() and c:IsAbleToHand() end
 
-function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+function s.e4tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     local c = e:GetHandler()
     if chk == 0 then
         return c:IsAbleToDeck() and
-                   Duel.IsExistingMatchingCard(s.e3filter, tp, LOCATION_REMOVED,
+                   Duel.IsExistingMatchingCard(s.e4filter, tp, LOCATION_REMOVED,
                                                0, 1, nil)
     end
 
     Duel.SetOperationInfo(0, CATEGORY_TODECK, nil, 1, tp, LOCATION_MZONE)
 end
 
-function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+function s.e4op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
-    local g = Duel.GetMatchingGroup(s.e3filter, tp, LOCATION_REMOVED, 0, nil)
+    local g = Duel.GetMatchingGroup(s.e4filter, tp, LOCATION_REMOVED, 0, nil)
 
     if #g > 0 and Duel.SendtoDeck(c, nil, 2, REASON_EFFECT) > 0 then
         Duel.BreakEffect()
