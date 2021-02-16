ae63c1a3e26d368ca0c9cd34c2d30c529ec0035d
Luc Ta <luc.ta@cptech.vn>
2021-02-17 01:05:20 +0700

    fix

--- script/c410000203.lua
+++ script/c410000203.lua
@@ -38,12 +38,16 @@ function s.initial_effect(c)
     e2:SetOperation(s.e2op)
     c:RegisterEffect(e2)
 
-    -- destroy
+    -- summon & destroy
     local e3 = Effect.CreateEffect(c)
     e3:SetDescription(aux.Stringid(id, 1))
-    e3:SetCategory(CATEGORY_DESTROY)
-    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetCategory(CATEGORY_SPECIAL_SUMMON + CATEGORY_DESTROY)
+    e3:SetType(EFFECT_TYPE_QUICK_O)
+    e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
+    e3:SetCode(EVENT_FREE_CHAIN)
     e3:SetRange(LOCATION_MZONE)
+    e3:SetHintTiming(TIMING_SPSUMMON, TIMING_BATTLE_START)
+    e3:SetCondition(s.e3con)
     e3:SetCost(s.e3cost)
     e3:SetTarget(s.e3tg)
     e3:SetOperation(s.e3op)
@@ -80,6 +84,15 @@ function s.e3filter(c, e, tp)
                c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
 end
 
+function s.e3con(e, tp, eg, ep, ev, re, r, rp)
+    local ph = Duel.GetCurrentPhase()
+    if Duel.GetTurnPlayer() == tp then
+        return ph == PHASE_MAIN1 or ph == PHASE_MAIN2
+    else
+        return (ph >= PHASE_BATTLE_START and ph <= PHASE_BATTLE)
+    end
+end
+
 function s.e3cost(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then return e:GetHandler():IsReleasable() end
     Duel.Release(e:GetHandler(), REASON_COST)
@@ -87,24 +100,24 @@ end
 
 function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then
-        return Duel.IsExistingMatchingCard(aux.TRUE, tp, 0, LOCATION_MZONE, 1,
-                                           nil)
+        return Duel.IsExistingMatchingCard(s.e3filter, tp, LOCATION_HAND +
+                                               LOCATION_DECK + LOCATION_GRAVE,
+                                           0, 1, nil, e, tp)
     end
 
-    local g = Duel.GetMatchingGroup(aux.TRUE, tp, 0, LOCATION_MZONE, nil)
-    Duel.SetOperationInfo(0, CATEGORY_DESTROY, g, #g, 0, 0)
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp,
+                          LOCATION_HAND + LOCATION_DECK + LOCATION_GRAVE)
+    local dg = Duel.GetMatchingGroup(aux.TRUE, tp, 0, LOCATION_MZONE, nil)
+    if #dg > 0 then Duel.SetOperationInfo(0, CATEGORY_DESTROY, dg, #dg, 0, 0) end
 end
 
 function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+    local tg = Duel.SelectMatchingCard(tp, s.e3filter, tp, LOCATION_HAND +
+                                           LOCATION_DECK + LOCATION_GRAVE, 0, 1,
+                                       1, nil, e, tp)
+    if #tg == 0 then return end
+    Duel.SpecialSummon(tg, 0, tp, tp, false, false, POS_FACEUP)
+
     local dg = Duel.GetMatchingGroup(aux.TRUE, tp, 0, LOCATION_MZONE, nil)
-    if Duel.Destroy(dg, REASON_EFFECT) > 0 then
-        local g = Duel.GetMatchingGroup(s.e3filter, tp, LOCATION_HAND +
-                                            LOCATION_DECK + LOCATION_GRAVE, 0,
-                                        nil, e, tp)
-        if #g > 0 and Duel.SelectYesNo(tp, aux.Stringid(id, 2)) then
-            Duel.BreakEffect()
-            local sg = g:Select(tp, 1, 1, nil)
-            Duel.SpecialSummon(sg, 0, tp, tp, false, false, POS_FACEUP)
-        end
-    end
+    Duel.Destroy(dg, REASON_EFFECT)
 end
