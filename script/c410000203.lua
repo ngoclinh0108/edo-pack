37f1023981db7a226ba037a583c1010c761a1d77
Luc Ta <luc.ta@cptech.vn>
2021-02-16 20:12:37 +0700

    fix

--- script/c410000203.lua
+++ script/c410000203.lua
@@ -10,6 +10,20 @@ function s.initial_effect(c)
     -- synchro summon
     Synchro.AddProcedure(c, s.synfilter, 1, 1,
                          aux.FilterBoolFunction(Card.IsSetCard, 0xdd), 1, 1)
+
+    -- atk up
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetValue(function(e, c)
+        local ct = Duel.GetMatchingGroupCount(Card.IsRace, c:GetControler(),
+                                              LOCATION_GRAVE, 0, nil,
+                                              RACE_DRAGON)
+        return ct * 500
+    end)
+    c:RegisterEffect(e1)
 end
 
 function s.synfilter(c) return c:IsLevel(1) and c:IsAttribute(ATTRIBUTE_LIGHT) end
