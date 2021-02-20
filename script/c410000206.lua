4247454f785bf8f8ec9feb2f3212d4ddc5e4f1af
Luc Ta <luc.ta@cptech.vn>
2021-02-21 02:52:39 +0700

    fix

--- script/c410000206.lua
+++ script/c410000206.lua
@@ -2,6 +2,7 @@
 Duel.LoadScript("util.lua")
 local s, id = GetID()
 
+s.listed_names = {CARD_BLUEEYES_W_DRAGON}
 s.listed_series = {0xdd}
 
 function s.initial_effect(c)
@@ -24,68 +25,111 @@ function s.initial_effect(c)
     end)
     c:RegisterEffect(e1)
 
-    -- search
+    -- special summon
     local e2 = Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id, 1))
-    e2:SetCategory(CATEGORY_TOHAND + CATEGORY_SEARCH)
-    e2:SetType(EFFECT_TYPE_IGNITION)
-    e2:SetRange(LOCATION_FZONE)
-    e2:SetCountLimit(1, id)
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP + EFFECT_FLAG_DELAY)
+    e2:SetCode(EVENT_DESTROYED)
+    e2:SetCondition(s.e2con)
     e2:SetTarget(s.e2tg)
     e2:SetOperation(s.e2op)
     c:RegisterEffect(e2)
 
-    -- atk up
+    -- search
     local e3 = Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id, 2))
-    e3:SetCategory(CATEGORY_ATKCHANGE + CATEGORY_DEFCHANGE)
+    e3:SetDescription(aux.Stringid(id, 1))
+    e3:SetCategory(CATEGORY_TOHAND + CATEGORY_SEARCH)
     e3:SetType(EFFECT_TYPE_IGNITION)
-    e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
     e3:SetRange(LOCATION_FZONE)
     e3:SetCountLimit(1, id)
     e3:SetTarget(s.e3tg)
     e3:SetOperation(s.e3op)
     c:RegisterEffect(e3)
+
+    -- atk up
+    local e4 = Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id, 2))
+    e4:SetCategory(CATEGORY_ATKCHANGE + CATEGORY_DEFCHANGE)
+    e4:SetType(EFFECT_TYPE_IGNITION)
+    e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e4:SetRange(LOCATION_FZONE)
+    e4:SetCountLimit(1, id)
+    e4:SetTarget(s.e4tg)
+    e4:SetOperation(s.e4op)
+    c:RegisterEffect(e4)
 end
 
-function s.e2filter(c)
-    if not c:IsAbleToHand() then return false end
+function s.e2filter(c, e, tp)
+    return c:IsCode(CARD_BLUEEYES_W_DRAGON) and
+               c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
+end
+
+function s.e2con(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    return rp == 1 - tp and c:IsReason(REASON_EFFECT) and
+               c:IsPreviousLocation(LOCATION_FZONE) and
+               c:IsPreviousControler(tp)
+end
+
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then
+        return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
+                   Duel.IsExistingTarget(s.e2filter, tp, LOCATION_HAND +
+                                             LOCATION_DECK + LOCATION_GRAVE, 0,
+                                         1, nil, e, tp)
+    end
+
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, 0,
+                          LOCATION_HAND + LOCATION_DECK + LOCATION_GRAVE)
+end
+
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
+    local g = Duel.SelectMatchingCard(tp, s.e2filter, tp, LOCATION_HAND +
+                                          LOCATION_DECK + LOCATION_GRAVE, 0, 1,
+                                      1, nil, e, tp)
+    if #g > 0 then Duel.SpecialSummon(g, 0, tp, tp, false, false, POS_FACEUP) end
+end
+
+function s.e3filter(c)
+    if not c:IsAbleToHand() or c:IsCode(id) then return false end
     return ((c:IsSetCard(0xdd) and c:IsType(TYPE_MONSTER))) or
                ((aux.IsCodeListed(c, CARD_BLUEEYES_W_DRAGON) or
                    aux.IsCodeListed(c, 23995346)) and
                    c:IsType(TYPE_SPELL + TYPE_TRAP))
 end
 
-function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then
-        return Duel.IsExistingMatchingCard(s.e2filter, tp, LOCATION_DECK, 0, 1,
+        return Duel.IsExistingMatchingCard(s.e3filter, tp, LOCATION_DECK, 0, 1,
                                            nil)
     end
     Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, 0, LOCATION_DECK)
 end
 
-function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     if not c:IsRelateToEffect(e) then return end
 
     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_ATOHAND)
-    local g = Duel.SelectMatchingCard(tp, s.e2filter, tp, LOCATION_DECK,
-                                      0, 1, 1, nil)
+    local g = Duel.SelectMatchingCard(tp, s.e3filter, tp, LOCATION_DECK, 0, 1,
+                                      1, nil)
     if #g == 0 then return end
 
     Duel.SendtoHand(g, nil, REASON_EFFECT)
     Duel.ConfirmCards(1 - tp, g)
 end
 
-function s.e3filter(c)
+function s.e4filter(c)
     return c:IsSetCard(0xdd) and c:IsType(TYPE_MONSTER) and c:IsAbleToGrave()
 end
 
-function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+function s.e4tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     if chk == 0 then
         return Duel.IsExistingTarget(Card.IsFaceup, tp, LOCATION_MZONE, 0, 1,
                                      nil) and
-                   Duel.IsExistingMatchingCard(s.e3filter, tp,
+                   Duel.IsExistingMatchingCard(s.e4filter, tp,
                                                LOCATION_HAND + LOCATION_DECK, 0,
                                                1, nil)
     end
@@ -94,18 +138,18 @@ function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     Duel.SelectTarget(tp, Card.IsFaceup, tp, LOCATION_MZONE, 0, 1, 1, nil)
 end
 
-function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+function s.e4op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     if not c:IsRelateToEffect(e) then return end
 
     local tc = Duel.GetFirstTarget()
     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_TOGRAVE)
-    local sc = Duel.SelectMatchingCard(tp, s.e3filter, tp,
+    local sc = Duel.SelectMatchingCard(tp, s.e4filter, tp,
                                        LOCATION_HAND + LOCATION_DECK, 0, 1, 1,
                                        nil):GetFirst()
     if not sc then return end
 
-    if Duel.SendtoGrave(gc, REASON_EFFECT) ~= 0 and
+    if Duel.SendtoGrave(sc, REASON_EFFECT) ~= 0 and
         sc:IsLocation(LOCATION_GRAVE) and tc:IsRelateToEffect(e) and
         tc:IsFaceup() then
         local ec1 = Effect.CreateEffect(c)
