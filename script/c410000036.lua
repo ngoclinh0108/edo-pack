fb2ff354df27c96bc35db603d417a99d113fd3f4
Luc Ta <luc.ta@cptech.vn>
2021-02-28 11:31:28 +0700

    fix

--- script/c410000036.lua
+++ script/c410000036.lua
@@ -8,22 +8,6 @@ function s.initial_effect(c)
     -- xyz summon
     Xyz.AddProcedure(c, s.xyzfilter, 8, 2, nil, nil, 2, nil)
 
-    -- attribute
-    local attribute = Effect.CreateEffect(c)
-    attribute:SetType(EFFECT_TYPE_SINGLE)
-    attribute:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
-    attribute:SetCode(EFFECT_ADD_ATTRIBUTE)
-    attribute:SetValue(ATTRIBUTE_DARK)
-    c:RegisterEffect(attribute)
-
-    -- race
-    local race = Effect.CreateEffect(c)
-    race:SetType(EFFECT_TYPE_SINGLE)
-    race:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
-    race:SetCode(EFFECT_ADD_RACE)
-    race:SetValue(RACE_DRAGON)
-    c:RegisterEffect(race)
-
     -- activation and effects cannot be negated
     local inact = Effect.CreateEffect(c)
     inact:SetType(EFFECT_TYPE_FIELD)
