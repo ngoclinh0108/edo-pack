82dfa6ee3a03a645960b1aafea0fa6d30d564609
Luc Ta <luc.ta@cptech.vn>
2021-03-01 21:38:07 +0700

    fix

---
+++ script/c410000500.lua
@@ -0,0 +1,11 @@
+-- Mausoleum of the Signer Dragons
+Duel.LoadScript("util.lua")
+local s, id = GetID()
+
+function s.initial_effect(c)
+    -- activate
+    local act = Effect.CreateEffect(c)
+    act:SetType(EFFECT_TYPE_ACTIVATE)
+    act:SetCode(EVENT_FREE_CHAIN)
+    c:RegisterEffect(act)
+end
