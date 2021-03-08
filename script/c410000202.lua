9a9c12551a8d10a17290652c7d0a73e8a516a504
Luc Ta <luc.ta@cptech.vn>
2021-03-08 22:26:47 +0700

    fix

--- script/c410000202.lua
+++ script/c410000202.lua
@@ -25,19 +25,24 @@ function s.initial_effect(c)
     e1b:SetValue(function(e, re, tp) return tp ~= e:GetHandlerPlayer() end)
     c:RegisterEffect(e1b)
 
-    -- act limit
+    -- multi-attack & act limit
     local e2 = Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-    e2:SetCode(EFFECT_CANNOT_ACTIVATE)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetTargetRange(0, 1)
-    e2:SetValue(1)
-    e2:SetCondition(function(e)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_EXTRA_ATTACK)
+    e2:SetValue(2)
+    c:RegisterEffect(e2)
+    local e2b = Effect.CreateEffect(c)
+    e2b:SetType(EFFECT_TYPE_FIELD)
+    e2b:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e2b:SetCode(EFFECT_CANNOT_ACTIVATE)
+    e2b:SetRange(LOCATION_MZONE)
+    e2b:SetTargetRange(0, 1)
+    e2b:SetValue(1)
+    e2b:SetCondition(function(e)
         local c = e:GetHandler()
         return Duel.GetAttacker() == c or Duel.GetAttackTarget() == c
     end)
-    c:RegisterEffect(e2)
+    c:RegisterEffect(e2b)
 
     -- destroy
     local e3 = Effect.CreateEffect(c)
@@ -46,7 +51,7 @@ function s.initial_effect(c)
     e3:SetType(EFFECT_TYPE_IGNITION)
     e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
     e3:SetRange(LOCATION_MZONE)
-    e3:SetCountLimit(1, 0, EFFECT_COUNT_CODE_SINGLE)
+    e3:SetCountLimit(1)
     e3:SetCost(s.e3cost)
     e3:SetTarget(s.e3tg)
     e3:SetOperation(s.e3op)
@@ -57,18 +62,6 @@ function s.initial_effect(c)
     e3b:SetValue(s.e3matcheck)
     e3b:SetLabelObject(e3)
     c:RegisterEffect(e3b)
-
-    -- multi-attack
-    local e4 = Effect.CreateEffect(c)
-    e4:SetDescription(aux.Stringid(id, 1))
-    e4:SetType(EFFECT_TYPE_IGNITION)
-    e4:SetRange(LOCATION_MZONE)
-    e4:SetCountLimit(1, 0, EFFECT_COUNT_CODE_SINGLE)
-    e4:SetCondition(s.e4con)
-    e4:SetCost(s.e4cost)
-    e4:SetTarget(s.e4tg)
-    e4:SetOperation(s.e4op)
-    c:RegisterEffect(e4)
 end
 
 function s.e3cost(e, tp, eg, ep, ev, re, r, rp, chk)
@@ -109,36 +102,3 @@ function s.e3matcheck(e, c)
     local ct = mg:FilterCount(Card.IsCode, nil, CARD_BLUEEYES_W_DRAGON)
     e:GetLabelObject():SetLabel(ct)
 end
-
-function s.e4con(e, tp, eg, ep, ev, re, r, rp) return Duel.IsAbleToEnterBP() end
-
-function s.e4cost(e, tp, eg, ep, ev, re, r, rp, chk)
-    local c = e:GetHandler()
-    if chk == 0 then
-        return Duel.CheckReleaseGroupCost(tp, nil, 2, false, nil, c)
-    end
-
-    local g = Duel.SelectReleaseGroupCost(tp, nil, 2, 2, false, nil, c)
-    Duel.Release(g, REASON_COST)
-end
-
-function s.e4tg(e, tp, eg, ep, ev, re, r, rp, chk)
-    local c = e:GetHandler()
-    if chk == 0 then return not c:IsHasEffect(EFFECT_EXTRA_ATTACK) end
-end
-
-function s.e4op(e, tp, eg, ep, ev, re, r, rp)
-    local c = e:GetHandler()
-    if not c:IsRelateToEffect(e) then return end
-
-    local ec1 = Effect.CreateEffect(c)
-    ec1:SetDescription(aux.Stringid(id, 1))
-    ec1:SetType(EFFECT_TYPE_SINGLE)
-    ec1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_CLIENT_HINT)
-    ec1:SetCode(EFFECT_EXTRA_ATTACK)
-    ec1:SetValue(2)
-    ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
-    c:RegisterEffect(ec1)
-end
-
-function s.e4actcon(e) return Duel.GetAttacker() == e:GetLabelObject() end
--- script/c410000302.lua
+++ script/c410000302.lua
@@ -25,55 +25,41 @@ function s.initial_effect(c)
     e1b:SetValue(function(e, re, tp) return tp ~= e:GetHandlerPlayer() end)
     c:RegisterEffect(e1b)
 
-    -- act limit
-    local e2 = Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-    e2:SetCode(EFFECT_CANNOT_ACTIVATE)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetTargetRange(0, 1)
-    e2:SetValue(1)
-    e2:SetCondition(function(e)
-        local c = e:GetHandler()
-        return Duel.GetAttacker() == c or Duel.GetAttackTarget() == c
-    end)
-    c:RegisterEffect(e2)
-
     -- down atk/def
+    local e2 = Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id, 0))
+    e2:SetCategory(CATEGORY_ATKCHANGE + CATEGORY_DEFCHANGE)
+    e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetCode(EVENT_FREE_CHAIN)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1)
+    e2:SetTarget(s.e2tg)
+    e2:SetOperation(s.e2op)
+    c:RegisterEffect(e2)
+    local e2b = Effect.CreateEffect(c)
+    e2b:SetType(EFFECT_TYPE_SINGLE)
+    e2b:SetCode(EFFECT_MATERIAL_CHECK)
+    e2b:SetValue(s.e2matcheck)
+    e2b:SetLabelObject(e2)
+    c:RegisterEffect(e2b)
+
+    -- damage
     local e3 = Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id, 0))
-    e3:SetCategory(CATEGORY_ATKCHANGE + CATEGORY_DEFCHANGE)
-    e3:SetType(EFFECT_TYPE_QUICK_O)
+    e3:SetDescription(aux.Stringid(id, 1))
+    e3:SetCategory(CATEGORY_DAMAGE + CATEGORY_TOHAND + CATEGORY_SPECIAL_SUMMON)
+    e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
     e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e3:SetCode(EVENT_FREE_CHAIN)
+    e3:SetCode(EVENT_BATTLE_DAMAGE)
     e3:SetRange(LOCATION_MZONE)
-    e3:SetCountLimit(1)
+    e3:SetCountLimit(1, id)
+    e3:SetCondition(s.e3con)
     e3:SetTarget(s.e3tg)
     e3:SetOperation(s.e3op)
     c:RegisterEffect(e3)
-    local e3b = Effect.CreateEffect(c)
-    e3b:SetType(EFFECT_TYPE_SINGLE)
-    e3b:SetCode(EFFECT_MATERIAL_CHECK)
-    e3b:SetValue(s.e3matcheck)
-    e3b:SetLabelObject(e3)
-    c:RegisterEffect(e3b)
-
-    -- damage
-    local e4 = Effect.CreateEffect(c)
-    e4:SetDescription(aux.Stringid(id, 1))
-    e4:SetCategory(CATEGORY_DAMAGE + CATEGORY_TOHAND + CATEGORY_SPECIAL_SUMMON)
-    e4:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
-    e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e4:SetCode(EVENT_BATTLE_DAMAGE)
-    e4:SetRange(LOCATION_MZONE)
-    e4:SetCountLimit(1, id)
-    e4:SetCondition(s.e4con)
-    e4:SetTarget(s.e4tg)
-    e4:SetOperation(s.e4op)
-    c:RegisterEffect(e4)
 end
 
-function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     local ct = e:GetLabel()
     if chk == 0 then
         return Duel.IsExistingTarget(Card.IsFaceup, tp, 0, LOCATION_MZONE, 1,
@@ -85,7 +71,7 @@ function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
                                 nil)
 end
 
-function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     local g = Duel.GetTargetCards(e)
     if #g == 0 then return end
@@ -106,47 +92,47 @@ function s.e3op(e, tp, eg, ep, ev, re, r, rp)
     end
 end
 
-function s.e3matcheck(e, c)
+function s.e2matcheck(e, c)
     local mg = c:GetMaterial()
     local ct = mg:FilterCount(Card.IsCode, nil, CARD_REDEYES_B_DRAGON)
     e:GetLabelObject():SetLabel(ct)
 end
 
-function s.e4filter(c, e, tp)
+function s.e3filter(c, e, tp)
     return (c:IsSetCard(0x3b) or c:IsRace(RACE_DRAGON)) and
                c:IsType(TYPE_MONSTER) and
-               (s.e4check1(c) or s.e4check2(c, e, tp))
+               (s.e3check1(c) or s.e3check2(c, e, tp))
 end
 
-function s.e4check1(c) return c:IsAbleToHand() end
+function s.e3check1(c) return c:IsAbleToHand() end
 
-function s.e4check2(c, e, tp)
+function s.e3check2(c, e, tp)
     return c:IsCanBeSpecialSummoned(e, 0, tp, false, false) and
                Duel.GetLocationCount(tp, LOCATION_MZONE) > 0
 end
 
-function s.e4con(e, tp, eg, ep, ev, re, r, rp) return ep ~= tp end
+function s.e3con(e, tp, eg, ep, ev, re, r, rp) return ep ~= tp end
 
-function s.e4tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     if chk == 0 then
-        return Duel.IsExistingTarget(s.e4filter, tp, LOCATION_GRAVE, 0, 1, nil,
+        return Duel.IsExistingTarget(s.e3filter, tp, LOCATION_GRAVE, 0, 1, nil,
                                      e, tp)
     end
 
     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_TARGET)
-    local tc = Duel.SelectTarget(tp, s.e4filter, tp, LOCATION_GRAVE, 0, 1, 1,
+    local tc = Duel.SelectTarget(tp, s.e3filter, tp, LOCATION_GRAVE, 0, 1, 1,
                                  nil, e, tp):GetFirst()
 
     Duel.SetOperationInfo(0, CATEGORY_DAMAGE, nil, 0, 1 - tp, tc:GetBaseAttack())
 end
 
-function s.e4op(e, tp, eg, ep, ev, re, r, rp)
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
     local tc = Duel.GetFirstTarget()
     if not tc or not tc:IsRelateToEffect(e) then return end
 
     if Duel.Damage(1 - tp, tc:GetBaseAttack(), REASON_EFFECT) > 0 then
-        local b1 = s.e4check1(tc)
-        local b2 = s.e4check2(tc, e, tp)
+        local b1 = s.e3check1(tc)
+        local b2 = s.e3check2(tc, e, tp)
         local op = 0
         if b1 and b2 then
             op = Duel.SelectOption(tp, 573, 2)
