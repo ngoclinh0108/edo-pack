d0c6d2bc340968a6dc2a47242600a84f2010685e
Luc Ta <luc.ta@cptech.vn>
2021-03-14 23:44:37 +0700

    fix

--- script/c410000400.lua
+++ script/c410000400.lua
@@ -2,15 +2,17 @@
 local s, id = GetID()
 Duel.LoadScript("util.lua")
 
+s.listed_names = {CARD_NEOS}
+
 function s.global_effect(c, tp)
-  -- Elemental HERO Neos
-  local eg1 = Effect.CreateEffect(c)
-  eg1:SetType(EFFECT_TYPE_SINGLE)
-  eg1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
-  eg1:SetCode(EFFECT_ADD_CODE)
-  eg1:SetValue(CARD_NEOS)
-  Utility.RegisterGlobalEffect(c, eg1, Card.IsCode, 14124483)
-  Utility.RegisterGlobalEffect(c, eg1, Card.IsCode, 64655485)
+    -- Elemental HERO Neos
+    local eg1 = Effect.CreateEffect(c)
+    eg1:SetType(EFFECT_TYPE_SINGLE)
+    eg1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+    eg1:SetCode(EFFECT_ADD_CODE)
+    eg1:SetValue(CARD_NEOS)
+    Utility.RegisterGlobalEffect(c, eg1, Card.IsCode, 14124483)
+    Utility.RegisterGlobalEffect(c, eg1, Card.IsCode, 64655485)
 end
 
 function s.initial_effect(c)
@@ -19,4 +21,12 @@ function s.initial_effect(c)
     act:SetType(EFFECT_TYPE_ACTIVATE)
     act:SetCode(EVENT_FREE_CHAIN)
     c:RegisterEffect(act)
+
+    -- may not return
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetCode(42015635)
+    e1:SetRange(LOCATION_FZONE)
+    e1:SetTargetRange(LOCATION_MZONE, LOCATION_MZONE)
+    c:RegisterEffect(e1)
 end
---
+++ script/c410000401.lua
@@ -0,0 +1,6 @@
+-- Spacian HERO Neos
+local s, id = GetID()
+Duel.LoadScript("util.lua")
+
+function s.initial_effect(c)
+end
