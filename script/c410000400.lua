1ff6f5ce205c1adab0a04543ab86cf2b28be28d4
Luc Ta <luc.ta@cptech.vn>
2021-03-15 20:31:02 +0700

    fix

--- script/c410000400.lua
+++ script/c410000400.lua
@@ -3,6 +3,7 @@ local s, id = GetID()
 Duel.LoadScript("util.lua")
 
 s.listed_names = {CARD_NEOS}
+s.listed_series = {0x8, 0x9}
 
 function s.global_effect(c, tp)
     -- Elemental HERO Neos
@@ -38,4 +39,75 @@ function s.initial_effect(c)
     e2:SetRange(LOCATION_FZONE)
     e2:SetTargetRange(LOCATION_MZONE, LOCATION_MZONE)
     c:RegisterEffect(e2)
+
+    -- extra summon
+    local e3 = Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id, 0))
+    e3:SetType(EFFECT_TYPE_FIELD)
+    e3:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+    e3:SetRange(LOCATION_FZONE)
+    e3:SetTargetRange(LOCATION_HAND, 0)
+    e3:SetValue(0x1)
+    e3:SetTarget(aux.TargetBoolFunction(Card.IsSetCard, 0x8))
+    c:RegisterEffect(e3)
+
+    -- special summon
+    local e4 = Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id, 1))
+    e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e4:SetType(EFFECT_TYPE_IGNITION)
+    e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e4:SetRange(LOCATION_FZONE)
+    e4:SetCountLimit(1)
+    e4:SetCost(s.e4cost)
+    e4:SetTarget(s.e4tg)
+    e4:SetOperation(s.e4op)
+    c:RegisterEffect(e4)
+end
+
+function s.e4confilter(c, tp)
+    return c:IsLevel(4) and c:IsRace(RACE_WARRIOR) and
+               (Duel.GetMZoneCount(tp, c, tp) > 0 or
+                   (c:IsControler(tp) and c:GetSequence() < 5)) and
+               (c:IsControler(tp) or c:IsFaceup())
+end
+
+function s.e4tgfilter(c, e, tp)
+    return c:IsCanBeSpecialSummoned(e, 0, tp, false, false, POS_FACEUP) and
+               c:IsSetCard(0x9)
+end
+
+function s.e4cost(e, tp, eg, ep, ev, re, r, rp, chk)
+    local ft = Duel.GetLocationCount(tp, LOCATION_MZONE)
+    if chk == 0 then
+        return ft > -1 and
+                   Duel.CheckReleaseGroupCost(tp, s.e4confilter, 1, false, nil,
+                                              nil, tp)
+    end
+
+    local g = Duel.SelectReleaseGroupCost(tp, s.e4confilter, 1, 1, false, nil,
+                                          nil, tp)
+    Duel.Release(g, REASON_COST)
+end
+
+function s.e4tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    if chk == 0 then
+        return Duel.IsExistingMatchingCard(s.e4tgfilter, tp,
+                                           LOCATION_DECK + LOCATION_GRAVE, 0, 1,
+                                           nil, e, tp)
+    end
+
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, 0,
+                          LOCATION_DECK + LOCATION_GRAVE)
+end
+
+function s.e4op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    if not c:IsRelateToEffect(e) then return end
+
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
+    local g = Duel.SelectTarget(tp, s.e4tgfilter, tp,
+                                LOCATION_DECK + LOCATION_GRAVE, 0, 1, 1, nil, e,
+                                tp)
+    if #g > 0 then Duel.SpecialSummon(g, 0, tp, tp, false, false, POS_FACEUP) end
 end
