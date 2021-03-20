ef41d0fb7b75e72920afdbbf95e5612f3d5f9117
Luc Ta <luc.ta@cptech.vn>
2021-03-20 17:20:33 +0700

    fix

--- script/c410000403.lua
+++ script/c410000403.lua
@@ -26,13 +26,14 @@ function s.initial_effect(c)
     end)
     c:RegisterEffect(splimit)
 
-    -- atk up
+    -- change name
     local e1 = Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e1:SetCode(EFFECT_UPDATE_ATTACK)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetValue(s.e1val)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE + EFFECT_FLAG_CANNOT_DISABLE +
+                       EFFECT_FLAG_CANNOT_NEGATE)
+    e1:SetCode(EFFECT_CHANGE_CODE)
+    e1:SetRange(LOCATION_MZONE + LOCATION_GRAVE)
+    e1:SetValue(CARD_NEOS)
     c:RegisterEffect(e1)
 
     -- make a second attack in a row
@@ -43,16 +44,18 @@ function s.initial_effect(c)
     e2:SetCondition(s.e2con)
     e2:SetOperation(s.e2op)
     c:RegisterEffect(e2)
-end
 
-function s.e1filter(c)
-    return c:IsFaceup() and c:IsType(TYPE_MONSTER) and
-               (c:IsSetCard(0x1f) or c:IsSetCard(0x8))
-end
-
-function s.e1val(e, c)
-    return Duel.GetMatchingGroupCount(s.e1filter, c:GetControler(),
-                                      LOCATION_GRAVE, 0, nil) * 100
+    -- special summon
+    local e3 = Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id, 1))
+    e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCountLimit(1, id)
+    e3:SetCost(s.e3cost)
+    e3:SetTarget(s.e3tg)
+    e3:SetOperation(s.e3op)
+    c:RegisterEffect(e3)
 end
 
 function s.e2con(e, tp, eg, ep, ev, re, r, rp)
@@ -63,3 +66,57 @@ function s.e2con(e, tp, eg, ep, ev, re, r, rp)
 end
 
 function s.e2op(e, tp, eg, ep, ev, re, r, rp) Duel.ChainAttack() end
+
+function s.e3filter(c, e, tp)
+    return
+        c:IsCanBeSpecialSummoned(e, 0, tp, false, false, POS_FACEUP_DEFENSE) and
+            c:IsSetCard(0x1f)
+end
+
+function s.e3cost(e, tp, eg, ep, ev, re, r, rp, chk)
+    local c = e:GetHandler()
+    if chk == 0 then return c:GetAttackAnnouncedCount() == 0 end
+
+    local ec1 = Effect.CreateEffect(c)
+    ec1:SetDescription(3206)
+    ec1:SetType(EFFECT_TYPE_SINGLE)
+    ec1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_OATH +
+                        EFFECT_FLAG_CLIENT_HINT)
+    ec1:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+    ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
+    c:RegisterEffect(ec1)
+end
+
+function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    if chk == 0 then
+        return Duel.IsExistingTarget(s.e3filter, tp, LOCATION_HAND +
+                                         LOCATION_DECK + LOCATION_GRAVE, 0, 1,
+                                     nil, e, tp)
+    end
+
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, 0,
+                          LOCATION_HAND + LOCATION_DECK + LOCATION_GRAVE)
+end
+
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    local g = Duel.GetMatchingGroup(s.e3filter, tp, LOCATION_HAND +
+                                        LOCATION_DECK + LOCATION_GRAVE, 0, nil,
+                                    e, tp)
+    if #g > 1 then
+        Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
+        g = g:Select(tp, 1, 1)
+    end
+    if #g == 0 then return end
+    local tc = g:GetFirst()
+
+    Duel.SpecialSummon(tc, 0, tp, tp, false, false, POS_FACEUP_DEFENSE)
+    local ec1 = Effect.CreateEffect(c)
+    ec1:SetType(EFFECT_TYPE_SINGLE)
+    ec1:SetCode(EFFECT_DISABLE)
+    ec1:SetReset(RESET_EVENT + RESETS_STANDARD)
+    tc:RegisterEffect(ec1, true)
+    local ec2 = ec1:Clone()
+    ec2:SetCode(EFFECT_DISABLE_EFFECT)
+    tc:RegisterEffect(ec2, true)
+end
