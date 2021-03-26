712237362340e896aac831ea142fe666b9afb223
Luc Ta <luc.ta@cptech.vn>
2021-03-26 20:20:48 +0700

    fix

--- script/c410000400.lua
+++ script/c410000400.lua
@@ -55,14 +55,67 @@ function s.initial_effect(c)
     e3:SetTargetRange(LOCATION_MZONE, LOCATION_MZONE)
     c:RegisterEffect(e3)
 
-    -- extra summon
+    -- summon with 1 tribute
     local e4 = Effect.CreateEffect(c)
     e4:SetDescription(aux.Stringid(id, 0))
     e4:SetType(EFFECT_TYPE_FIELD)
-    e4:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
-    e4:SetRange(LOCATION_FZONE)
+    e4:SetCode(EFFECT_SUMMON_PROC)
+    e4:SetRange(LOCATION_SZONE)
     e4:SetTargetRange(LOCATION_HAND, 0)
-    e4:SetValue(0x1)
-    e4:SetTarget(aux.TargetBoolFunction(Card.IsSetCard, 0x8))
+    e4:SetCondition(s.e4con)
+    e4:SetTarget(aux.FieldSummonProcTg(s.e4tg, s.e4tgsum))
+    e4:SetOperation(s.e4op)
+    e4:SetValue(SUMMON_TYPE_TRIBUTE)
     c:RegisterEffect(e4)
+    local e4b = e4:Clone()
+    e4b:SetCode(EFFECT_SET_PROC)
+    c:RegisterEffect(e4b)
+
+    -- extra summon
+    local e5 = Effect.CreateEffect(c)
+    e5:SetDescription(aux.Stringid(id, 1))
+    e5:SetType(EFFECT_TYPE_FIELD)
+    e5:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
+    e5:SetRange(LOCATION_FZONE)
+    e5:SetTargetRange(LOCATION_HAND, 0)
+    e5:SetValue(0x1)
+    e5:SetTarget(aux.TargetBoolFunction(Card.IsSetCard, 0x8))
+    c:RegisterEffect(e5)
+end
+
+function s.e4filter(c, tp) return c:IsControler(tp) or c:IsFaceup() end
+
+function s.e4con(e, c, minc)
+    if c == nil then return true end
+    local tp = c:GetControler()
+    local mg = Duel.GetMatchingGroup(s.e4filter, tp, LOCATION_MZONE,
+                                     LOCATION_MZONE, nil, tp)
+    return minc <= 1 and Duel.CheckTribute(c, 1, 1, mg)
+end
+
+function s.e4tg(e, c) return c:IsLevelAbove(7) end
+
+function s.e4tgsum(e, tp, eg, ep, ev, re, r, rp, chk, c)
+    local mg = Duel.GetMatchingGroup(s.e4filter, tp, LOCATION_MZONE,
+                                     LOCATION_MZONE, nil, tp)
+    if Duel.GetLocationCount(tp, LOCATION_MZONE) <= 0 then
+        mg = mg:Filter(Card.IsControler, nil, tp)
+    end
+
+    local sg = Duel.SelectTribute(tp, c, 1, 1, mg, nil, nil, true)
+    if sg then
+        sg:KeepAlive()
+        e:SetLabelObject(sg)
+        return true
+    end
+    return false
+end
+
+function s.e4op(e, tp, eg, ep, ev, re, r, rp, c)
+    local sg = e:GetLabelObject()
+    if not sg then return end
+
+    c:SetMaterial(sg)
+    Duel.Release(sg, REASON_SUMMON + REASON_MATERIAL)
+    sg:DeleteGroup()
 end
--- script/c410000401.lua
+++ script/c410000401.lua
@@ -6,47 +6,37 @@ s.listed_names = {CARD_NEOS}
 s.listed_series = {0x8}
 
 function s.initial_effect(c)
-    -- summon with 1 tribute
-    local e1 = aux.AddNormalSummonProcedure(c, true, true, 1, 1,
-                                            SUMMON_TYPE_TRIBUTE,
-                                            aux.Stringid(id, 0), s.e1filter)
-
     -- special summon
-    local e2 = Effect.CreateEffect(c)
-    e2:SetDescription(2)
-    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e2:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
-    e2:SetProperty(EFFECT_FLAG_DELAY + EFFECT_FLAG_DAMAGE_STEP)
-    e2:SetCode(EVENT_DESTROYED)
-    e2:SetRange(LOCATION_HAND + LOCATION_DECK + LOCATION_GRAVE)
-    e2:SetCountLimit(id)
-    e2:SetCondition(s.e2con)
-    e2:SetTarget(s.e2tg)
-    e2:SetOperation(s.e2op)
-    c:RegisterEffect(e2)
-    local e2b = e2:Clone()
-    e2b:SetCode(EVENT_LEAVE_FIELD)
-    c:RegisterEffect(e2b)
+    local e1 = Effect.CreateEffect(c)
+    e1:SetDescription(2)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
+    e1:SetProperty(EFFECT_FLAG_DELAY + EFFECT_FLAG_DAMAGE_STEP)
+    e1:SetCode(EVENT_DESTROYED)
+    e1:SetRange(LOCATION_HAND + LOCATION_DECK + LOCATION_GRAVE)
+    e1:SetCountLimit(id)
+    e1:SetCondition(s.e1con)
+    e1:SetTarget(s.e1tg)
+    e1:SetOperation(s.e1op)
+    c:RegisterEffect(e1)
+    local e1b = e1:Clone()
+    e1b:SetCode(EVENT_LEAVE_FIELD)
+    c:RegisterEffect(e1b)
 end
 
 function s.e1filter(c, tp)
-    return (c:IsControler(tp) or c:IsFaceup()) and
-               (c:IsSetCard(0x8) or c:IsSetCard(0x9))
-end
-
-function s.e2filter(c, tp)
     return
         c:IsPreviousLocation(LOCATION_MZONE) and c:IsPreviousControler(tp) and
             c:IsPreviousPosition(POS_FACEUP) and c:IsType(TYPE_FUSION) and
             aux.IsMaterialListCode(c, CARD_NEOS)
 end
 
-function s.e2con(e, tp, eg, ep, ev, re, r, rp)
-    return eg:IsExists(s.e2filter, 1, nil, tp) and
+function s.e1con(e, tp, eg, ep, ev, re, r, rp)
+    return eg:IsExists(s.e1filter, 1, nil, tp) and
                (r & REASON_BATTLE + REASON_EFFECT) ~= 0
 end
 
-function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
     if chk == 0 then
         return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
@@ -56,7 +46,7 @@ function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
     Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, c, 1, tp, c:GetLocation())
 end
 
-function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+function s.e1op(e, tp, eg, ep, ev, re, r, rp)
     if Duel.GetLocationCount(tp, LOCATION_MZONE) <= 0 then return end
     Duel.SpecialSummon(e:GetHandler(), 0, tp, tp, false, false, POS_FACEUP)
 end
