270e4677105ac636c4c2b3584bf9999635596524
Luc Ta <luc.ta@cptech.vn>
2021-05-08 00:05:51 +0700

    fix

--- script/c400000002.lua
+++
@@ -1,9 +0,0 @@
--- Test
-local s, id = GetID()
-function s.initial_effect(c)
-  -- activate
-  local e1 = Effect.CreateEffect(c)
-  e1:SetType(EFFECT_TYPE_ACTIVATE)
-  e1:SetCode(EVENT_FREE_CHAIN)
-  c:RegisterEffect(e1)
-end
