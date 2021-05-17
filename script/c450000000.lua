33b6549531d1acfb194f8c570886af53c0fdd4b7
Luc Ta <luc.ta@cptech.vn>
2021-05-17 23:27:47 +0700

    update

--- script/c450000000.lua
+++ script/c450000000.lua
@@ -1,6 +1,8 @@
--- Pendulum Gate
+-- Supreme Gate
 Duel.LoadScript("util.lua")
 local s, id = GetID()
+
+s.listed_names = {13331639}
 
 function s.initial_effect(c)
     -- activate
@@ -8,4 +10,72 @@ function s.initial_effect(c)
     act:SetType(EFFECT_TYPE_ACTIVATE)
     act:SetCode(EVENT_FREE_CHAIN)
     c:RegisterEffect(act)
+
+    -- cannot be target & indes
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+    e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+    e1:SetRange(LOCATION_FZONE)
+    e1:SetTargetRange(LOCATION_PZONE, 0)
+    e1:SetValue(aux.tgoval)
+    c:RegisterEffect(e1)
+    local e1b = e1:clone()
+    e1b:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+    e1b:SetValue(aux.indoval)
+    c:RegisterEffect(e1b)
+
+    -- destroy & search
+    local e2 = Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id, 0))
+    e2:SetCategory(CATEGORY_DESTROY + CATEGORY_TOHAND + CATEGORY_SEARCH)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetRange(LOCATION_FZONE)
+    e2:SetCountLimit(1, id)
+    e2:SetTarget(s.e2tg)
+    e2:SetOperation(s.e2op)
+    c:RegisterEffect(e2)
+end
+
+function s.e2filter(c)
+    return not c:IsCode(id) and c:IsAbleToHand() and
+               aux.IsCodeListed(c, CARD_RA) and c:IsType(TYPE_SPELL + TYPE_TRAP)
+end
+
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    local c = e:GetHandler()
+    if chk == 0 then
+        return Duel.IsExistingTarget(Card.IsFaceup, tp, LOCATION_ONFIELD, 0, 1,
+                                     c) and
+                   Duel.IsExistingMatchingCard(s.e2filter, tp,
+                                               LOCATION_DECK + LOCATION_GRAVE,
+                                               0, 1, nil)
+    end
+
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_DESTROY)
+    local g = Duel.SelectTarget(tp, Card.IsFaceup, tp, LOCATION_ONFIELD, 0, 1,
+                                1, c)
+
+    Duel.SetOperationInfo(0, CATEGORY_DESTROY, g, 1, 0, 0)
+    Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp,
+                          LOCATION_DECK + LOCATION_GRAVE)
+end
+
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+    if not c:IsRelateToEffect(e) then return end
+
+    local tc = Duel.GetFirstTarget()
+    if not tc:IsRelateToEffect(e) or Duel.Destroy(tc, REASON_EFFECT) == 0 then
+        return
+    end
+
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_ATOHAND)
+    local g = Duel.SelectMatchingCard(tp, s.e2filter, tp,
+                                      LOCATION_DECK + LOCATION_GRAVE, 0, 1, 1,
+                                      nil)
+    if #g > 0 then
+        Duel.SendtoHand(g, nil, REASON_EFFECT)
+        Duel.ConfirmCards(1 - tp, g)
+    end
 end
