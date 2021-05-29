e8bbf4a9ffa4a4e2add236c9858dd483c014725a
Luc Ta <luc.ta@cptech.vn>
2021-05-30 06:48:53 +0700

    update

--- script/c950000007.lua
+++ script/c950000007.lua
@@ -2,6 +2,28 @@
 Duel.LoadScript("util.lua")
 local s, id = GetID()
 
+s.listed_series = {0x10f8, 0x20f8}
+
 function s.initial_effect(c)
-  -- effects
+    -- pendulum summon
+    Pendulum.AddProcedure(c)
+
+    -- scale
+    local pe1 = Effect.CreateEffect(c)
+    pe1:SetType(EFFECT_TYPE_SINGLE)
+    pe1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    pe1:SetCode(EFFECT_CHANGE_LSCALE)
+    pe1:SetRange(LOCATION_PZONE)
+    pe1:SetCondition(s.pe1con)
+    pe1:SetValue(4)
+    c:RegisterEffect(pe1)
+    local pe1b = pe1:Clone()
+    pe1b:SetCode(EFFECT_CHANGE_RSCALE)
+    c:RegisterEffect(pe1b)
+end
+
+function s.pe1con(e)
+    return not Duel.IsExistingMatchingCard(function(c)
+        return c:IsSetCard(0x10f8) or c:IsSetCard(0x20f8)
+    end, e:GetHandlerPlayer(), LOCATION_PZONE, 0, 1, e:GetHandler())
 end
