f3084b5592d2098b7463f9010ccc4ad8b07a94d7
Luc Ta <luc.ta@cptech.vn>
2021-06-13 14:10:44 +0700

    fix

--- script/c950000015.lua
+++ script/c950000015.lua
@@ -4,4 +4,24 @@ local s, id = GetID()
 
 function s.initial_effect(c)
     c:EnableReviveLimit()
+
+    -- fusion summon
+    Fusion.AddProcMix(c, false, false, function(c, sc, sumtype, tp)
+        return c:IsSetCard(0x1050, sc, sumtype, tp) and
+                   c:IsType(TYPE_FUSION, sc, sumtype, tp)
+    end, function(c, fc, sumtype, tp)
+        return c:GetOriginalLevel() >= 7 and
+                   c:IsAttribute(ATTRIBUTE_DARK, fc, sumtype, tp)
+    end)
+
+    -- special summon limit
+    local splimit = Effect.CreateEffect(c)
+    splimit:SetType(EFFECT_TYPE_SINGLE)
+    splimit:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+    splimit:SetCode(EFFECT_SPSUMMON_CONDITION)
+    splimit:SetValue(function(e, se, sp, st)
+        return not e:GetHandler():IsLocation(LOCATION_EXTRA) or
+                   aux.fuslimit(e, se, sp, st)
+    end)
+    c:RegisterEffect(splimit)
 end
