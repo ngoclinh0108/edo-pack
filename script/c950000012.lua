9c61aafd48a68af62ac2c7efae64a69da1b101b2
Luc Ta <luc.ta@cptech.vn>
2021-06-17 00:12:30 +0700

    update

--- script/c950000012.lua
+++ script/c950000012.lua
@@ -48,4 +48,51 @@ function s.initial_effect(c)
                        LOCATION_EXTRA)
     end)
     c:RegisterEffect(me1)
+
+    -- damage
+    local me2 = Effect.CreateEffect(c)
+    me2:SetDescription(aux.Stringid(id, 0))
+    me2:SetCategory(CATEGORY_DAMAGE)
+    me2:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
+    me2:SetProperty(EFFECT_FLAG_DELAY + EFFECT_FLAG_DAMAGE_STEP +
+                        EFFECT_FLAG_PLAYER_TARGET)
+    me2:SetCode(EVENT_DESTROYED)
+    me2:SetRange(LOCATION_MZONE)
+    me2:SetCountLimit(1, id)
+    me2:SetCondition(s.me2con)
+    me2:SetTarget(s.me2tg)
+    me2:SetOperation(s.me2op)
+    c:RegisterEffect(me2)
+end
+
+function s.me2filter(c, tp)
+    return c:IsPreviousLocation(LOCATION_ONFIELD) and
+               not c:IsPreviousControler(tp) and
+               c:IsReason(REASON_BATTLE + REASON_EFFECT)
+end
+
+function s.me2con(e, tp, eg, ep, ev, re, r, rp)
+    local rc = re:GetOwner()
+    return rc:IsOriginalSetCard(0x1050) and rc:IsRace(RACE_DRAGON) and
+               eg:IsExists(s.me2filter, 1, nil, tp)
+end
+
+function s.me2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return true end
+
+    local dmg = 0
+    local g = eg:Filter(s.me2filter, nil, tp)
+    for tc in aux.Next(g) do
+        if tc:GetBaseAttack() > 0 then dmg = dmg + tc:GetBaseAttack() end
+    end
+
+    Duel.SetTargetPlayer(1 - tp)
+    Duel.SetTargetParam(dmg)
+    Duel.SetOperationInfo(0, CATEGORY_DAMAGE, nil, 0, 1 - tp, dmg)
+end
+
+function s.me2op(e, tp, eg, ep, ev, re, r, rp)
+    local p, d = Duel.GetChainInfo(0, CHAININFO_TARGET_PLAYER,
+                                   CHAININFO_TARGET_PARAM)
+    Duel.Damage(p, d, REASON_EFFECT)
 end
