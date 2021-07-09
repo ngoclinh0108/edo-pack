3e5ef452f8faec5c137f6d4a468749bab9f47a20
Luc Ta <luc.ta@cptech.vn>
2021-07-10 03:47:42 +0700

    fix

--- script/c940000000.lua
+++ script/c940000000.lua
@@ -9,3 +9,9 @@ function s.initial_effect(c)
     act:SetCode(EVENT_FREE_CHAIN)
     c:RegisterEffect(act)
 end
+
+function s.deck_edit(tp)
+    Utility.DeckEditAddCardToDeck(tp, 60992364) -- ZW - Leo Arms
+    Utility.DeckEditAddCardToDeck(tp, 2896663) -- ZW - Dragonic Halberd
+    Utility.DeckEditAddCardToDeck(tp, 31123642) -- ZS - Utopic Sage
+end
--- script/c940000001.lua
+++ script/c940000001.lua
@@ -1,9 +1,43 @@
--- Number Z100: Genesis Numeron Dragon
+-- Rank-Change-Magic Emperor Force
 Duel.LoadScript("util.lua")
 local s, id = GetID()
 
-s.xyz_number = 100
-
 function s.initial_effect(c)
-    c:EnableReviveLimit()
+end
+
+function s.deck_edit(tp)
+    Utility.DeckEditAddCardToDeck(tp, 43490025, 65305468) -- Number F0: Utopic Future Slash
+    Utility.DeckEditAddCardToDeck(tp, 26973555, 65305468) -- Number F0: Utopic Future Dragon
+    Utility.DeckEditAddCardToDeck(tp, 79747096, 15232745) -- Number C1
+    Utility.DeckEditAddCardToDeck(tp, 69757518, 90126061) -- Number C5
+    Utility.DeckEditAddCardToDeck(tp, 6387204, 9161357) -- Number C6
+    Utility.DeckEditAddCardToDeck(tp, 32559361, 1992816) -- Number C9
+    Utility.DeckEditAddCardToDeck(tp, 33776843, 88120966) -- Number C15
+    Utility.DeckEditAddCardToDeck(tp, 49221191, 65676461) -- Number C32
+    Utility.DeckEditAddCardToDeck(tp, 84124261, 84013237) -- Number 39: Utopia Roots
+    Utility.DeckEditAddCardToDeck(tp, 62517849, 84013237) -- Number 39: Utopia Double
+    Utility.DeckEditAddCardToDeck(tp, 86532744, 84013237) -- Number S39: Utopia Prime
+    Utility.DeckEditAddCardToDeck(tp, 56832966, 84013237) -- Number S39: Utopia the Lightning    
+    Utility.DeckEditAddCardToDeck(tp, 56840427, 84013237) -- Number C39: Utopia Ray
+    Utility.DeckEditAddCardToDeck(tp, 66970002, 84013237) -- Number C39: Utopia Ray V
+    Utility.DeckEditAddCardToDeck(tp, 87911394, 84013237) -- Number C39: Utopia Ray Victory
+    Utility.DeckEditAddCardToDeck(tp, 68679595, 84013237) -- Leo Utopia Ray
+    Utility.DeckEditAddCardToDeck(tp, 75402014, 84013237) -- Dragonic Utopia Ray
+    Utility.DeckEditAddCardToDeck(tp, 21521304, 84013237) -- Number 39: Utopia Beyond
+    Utility.DeckEditAddCardToDeck(tp, 69170557, 75433814) -- Number C40
+    Utility.DeckEditAddCardToDeck(tp, 32446630, 56051086) -- Number C43
+    Utility.DeckEditAddCardToDeck(tp, 96864105, 36076683) -- Number C73
+    Utility.DeckEditAddCardToDeck(tp, 20563387, 93568288) -- Number C80
+    Utility.DeckEditAddCardToDeck(tp, 6165656, 48995978) -- Number C88
+    Utility.DeckEditAddCardToDeck(tp, 47017574, 97403510) -- Number C92
+    Utility.DeckEditAddCardToDeck(tp, 77205367, 55727845) -- Number C96
+    Utility.DeckEditAddCardToDeck(tp, 940000002, 57314798) -- Number Z100
+    Utility.DeckEditAddCardToDeck(tp, 12744567, 48739166) -- Number C101
+    Utility.DeckEditAddCardToDeck(tp, 67173574, 49678559) -- Number C102
+    Utility.DeckEditAddCardToDeck(tp, 20785975, 94380860) -- Number C103
+    Utility.DeckEditAddCardToDeck(tp, 49456901, 2061963) -- Number C104
+    Utility.DeckEditAddCardToDeck(tp, 85121942, 59627393) -- Number C105
+    Utility.DeckEditAddCardToDeck(tp, 55888045, 63746411) -- Number C106
+    Utility.DeckEditAddCardToDeck(tp, 68396121, 88177324) -- Number C107
+    Utility.DeckEditAddCardToDeck(tp, 15862758, 89477759) -- Number iC1000
 end
