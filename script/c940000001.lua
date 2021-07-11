51905b4cf279de64104eb08c00a63fa63fe0810b
Luc Ta <luc.ta@cptech.vn>
2021-07-11 07:43:28 +0700

    fix

--- script/c940000001.lua
+++ script/c940000001.lua
@@ -82,8 +82,8 @@ function s.e1filter2(c, e, tp, mc)
                mc:IsCanBeXyzMaterial(c, tp) and
                Duel.GetLocationCountFromEx(tp, tp, mc, c) > 0 and
                c:IsCanBeSpecialSummoned(e, SUMMON_TYPE_XYZ, tp, false, false) and
-               c:IsSetCard(0x48) and c.xyz_number and c.xyz_number >= 1 and
-               c.xyz_number <= 99 and
+               c:IsSetCard(0x48) and c:IsRace(mc:GetRace()) and c.xyz_number and
+               c.xyz_number >= 1 and c.xyz_number <= 99 and
                (c:IsRank(rk + 1) or c:IsRank(rk + 2) or c:IsRank(rk + 3) or
                    c:IsRank(rk - 1) or c:IsRank(rk - 2) or c:IsRank(rk - 3))
 end
