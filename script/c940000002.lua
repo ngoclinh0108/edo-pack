e17638e4a569df52b39da5a314451d1e72ee3cfa
Luc Ta <luc.ta@cptech.vn>
2021-07-11 07:56:26 +0700

    fix

--- script/c940000002.lua
+++ script/c940000002.lua
@@ -100,17 +100,29 @@ function s.initial_effect(c)
     e1:SetOperation(s.e1op)
     c:RegisterEffect(e1)
 
-    -- summon
     local e3 = Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id, 2))
-    e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetDescription(aux.Stringid(id, 1))
+    e3:SetType(EFFECT_TYPE_QUICK_O)
     e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e3:SetCode(EVENT_FREE_CHAIN)
+    e3:SetHintTiming(0, TIMINGS_CHECK_MONSTER)
     e3:SetRange(LOCATION_MZONE)
     e3:SetCountLimit(1)
     e3:SetTarget(s.e3tg)
     e3:SetOperation(s.e3op)
     c:RegisterEffect(e3)
+
+    -- summon
+    local e4 = Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id, 2))
+    e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e4:SetType(EFFECT_TYPE_IGNITION)
+    e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCountLimit(1)
+    e4:SetTarget(s.e4tg)
+    e4:SetOperation(s.e4op)
+    c:RegisterEffect(e4)
 end
 
 function s.xyzfilter(c, xyz, sumtype, tp)
@@ -192,26 +204,58 @@ function s.e1op(e, tp, eg, ep, ev, re, r, rp)
     end
 end
 
-function s.e3filter(c, e, tp)
+function s.e3filter(c, tp)
+    return c:IsFaceup() and not c:IsType(TYPE_TOKEN) and
+               (c:IsControler(tp) or c:IsAbleToChangeControler())
+end
+
+function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    local c = e:GetHandler()
+    if chk == 0 then
+        return c:IsType(TYPE_XYZ) and
+                   Duel.IsExistingTarget(s.e3filter, tp,
+                                         LOCATION_ONFIELD + LOCATION_REMOVED, 0,
+                                         1, c, tp)
+    end
+
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_TARGET)
+    Duel.SelectTarget(tp, s.e3filter, tp, LOCATION_ONFIELD + LOCATION_REMOVED,
+                      0, 1, 1, c, tp)
+end
+
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    local tc = Duel.GetFirstTarget()
+    if not c:IsRelateToEffect(e) or tc:IsFacedown() or
+        not tc:IsRelateToEffect(e) or tc:IsImmuneToEffect(e) then return end
+
+    local og = tc:GetOverlayGroup()
+    if #og > 0 then Duel.Overlay(c, og) end
+    Duel.Overlay(c, Group.FromCards(tc))
+end
+
+function s.e4filter(c, e, tp)
     return c:IsSetCard(0x48) and c:IsType(TYPE_XYZ) and not c:IsCode(id) and
                c:IsCanBeSpecialSummoned(e, SUMMON_TYPE_XYZ, tp, true, true)
 end
 
-function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+function s.e4tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     if chk == 0 then
         return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
-                   Duel.IsExistingTarget(s.e3filter, tp, LOCATION_GRAVE, 0, 1,
+                   Duel.IsExistingTarget(s.e4filter, tp, LOCATION_GRAVE, 0, 1,
                                          nil, e, tp)
     end
 
     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
-    local g = Duel.SelectTarget(tp, s.e3filter, tp, LOCATION_GRAVE, 0, 1, 1,
+    local g = Duel.SelectTarget(tp, s.e4filter, tp, LOCATION_GRAVE, 0, 1, 1,
                                 nil, e, tp)
     Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, g, 1, 0, 0)
 end
 
-function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+function s.e4op(e, tp, eg, ep, ev, re, r, rp)
     local tc = Duel.GetFirstTarget()
     if not tc:IsRelateToEffect(e) then return end
+
     Duel.SpecialSummon(tc, SUMMON_TYPE_XYZ, tp, tp, true, true, POS_FACEUP)
+    tc:CompleteProcedure()
 end
