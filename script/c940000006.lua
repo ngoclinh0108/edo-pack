dfa25101e09c55d9eb5b7bc9ce902cf979c9dd4c
Luc Ta <luc.ta@cptech.vn>
2021-07-12 09:50:54 +0700

    fix

--- script/c940000006.lua
+++ script/c940000006.lua
@@ -1,9 +1,9 @@
--- Astral Onomatopoeia
+-- Utopic Astral Genesis
 Duel.LoadScript("util.lua")
 local s, id = GetID()
 
-s.listed_series={0x54,0x59,0x82,0x8f}
+s.listed_series = {0x54, 0x59, 0x82, 0x8f}
 
 function s.initial_effect(c)
-    c:AddSetcodesRule(0x54,0x59,0x82,0x8f)
+    c:AddSetcodesRule(0x54, 0x59, 0x82, 0x8f)
 end
