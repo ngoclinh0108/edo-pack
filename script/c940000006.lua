df1659464aec95418e7bf5142b96ff5dd142511d
Luc Ta <luc.ta@cptech.vn>
2021-07-12 10:12:08 +0700

    fix

--- script/c940000006.lua
+++ script/c940000006.lua
@@ -2,10 +2,20 @@
 Duel.LoadScript("util.lua")
 local s, id = GetID()
 
-s.listed_series = {0x54, 0x59, 0x82, 0x8f}
+s.listed_series = {0x54, 0x59, 0x82, 0x8f, 0x48}
 
 function s.initial_effect(c)
     c:AddSetcodesRule(0x54, 0x59, 0x82, 0x8f)
+
+    local e1 = Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id, 0))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1, id)
+    e1:SetCost(s.e1cost)
+    e1:SetOperation(s.e1op)
+    c:RegisterEffect(e1)
 
     -- effect gains
     local e2 = Effect.CreateEffect(c)
@@ -14,6 +24,64 @@ function s.initial_effect(c)
     e2:SetCondition(s.e2con)
     e2:SetOperation(s.e2op)
     c:RegisterEffect(e2)
+end
+
+function s.e1filter1(c) return c:IsSetCard(0x48) and c:IsType(TYPE_XYZ) end
+
+function s.e1filter2(c)
+    return c:IsFaceup() and c:IsType(TYPE_MONSTER) and
+               Utility.IsSetCard(c, 0x54, 0x59, 0x82, 0x8f)
+end
+
+function s.e1cost(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then
+        return Duel.IsExistingMatchingCard(s.e1filter1, tp, LOCATION_EXTRA, 0,
+                                           1, nil)
+    end
+
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_CONFIRM)
+    local tc = Duel.SelectMatchingCard(tp, s.e1filter1, tp, LOCATION_EXTRA, 0,
+                                       1, 1, nil):GetFirst()
+    Duel.ConfirmCards(1 - tp, tc)
+    e:SetLabelObject(tc)
+end
+
+function s.e1op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    local tc = e:GetLabelObject()
+
+    local rk = tc:GetRank()
+    local attr = tc:GetAttribute()
+    local g = Duel.GetMatchingGroup(s.e1filter2, tp, LOCATION_MZONE, 0, nil)
+    for tc in aux.Next(g) do
+        local ec1 = Effect.CreateEffect(c)
+        ec1:SetType(EFFECT_TYPE_SINGLE)
+        ec1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+        ec1:SetCode(EFFECT_CHANGE_LEVEL)
+        ec1:SetValue(rk)
+        ec1:SetReset(RESET_EVENT + RESETS_STANDARD)
+        tc:RegisterEffect(ec1)
+        local ec1b = ec1:Clone()
+        ec1b:SetCode(EFFECT_CHANGE_ATTRIBUTE)
+        ec1b:SetValue(attr)
+        tc:RegisterEffect(ec1b)
+    end
+
+    local ge1 = Effect.CreateEffect(c)
+    ge1:SetType(EFFECT_TYPE_FIELD)
+    ge1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    ge1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+    ge1:SetTargetRange(1, 0)
+    ge1:SetTarget(function(e, c)
+        return not c:IsType(TYPE_XYZ) and c:IsLocation(LOCATION_EXTRA)
+    end)
+    ge1:SetReset(RESET_PHASE + PHASE_END)
+    Duel.RegisterEffect(ge1, tp)
+    aux.RegisterClientHint(c, EFFECT_FLAG_OATH, tp, 1, 0, aux.Stringid(id, 1),
+                           nil)
+    aux.addTempLizardCheck(c, tp, function(e, c)
+        return not c:IsOriginalType(TYPE_XYZ)
+    end)
 end
 
 function s.e2con(e, tp, eg, ep, ev, re, r, rp) return r == REASON_XYZ end
