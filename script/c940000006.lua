c365f6c2e88c6d7fde1fef8d6ea7f204dbc3cbab
Luc Ta <luc.ta@cptech.vn>
2021-07-12 09:55:17 +0700

    update

--- script/c940000006.lua
+++ script/c940000006.lua
@@ -6,4 +6,57 @@ s.listed_series = {0x54, 0x59, 0x82, 0x8f}
 
 function s.initial_effect(c)
     c:AddSetcodesRule(0x54, 0x59, 0x82, 0x8f)
+
+    -- effect gains
+    local e2 = Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_CONTINUOUS)
+    e2:SetCode(EVENT_BE_MATERIAL)
+    e2:SetCondition(s.e2con)
+    e2:SetOperation(s.e2op)
+    c:RegisterEffect(e2)
+end
+
+function s.e2con(e, tp, eg, ep, ev, re, r, rp) return r == REASON_XYZ end
+
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    local rc = c:GetReasonCard()
+
+    local ec1 = Effect.CreateEffect(rc)
+    ec1:SetCategory(CATEGORY_DRAW)
+    ec1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_F)
+    ec1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    ec1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    ec1:SetCondition(s.e2drcon)
+    ec1:SetTarget(s.e2drtg)
+    ec1:SetOperation(s.e2drop)
+    ec1:SetReset(RESET_EVENT + RESETS_STANDARD)
+    rc:RegisterEffect(ec1, true)
+
+    if not rc:IsType(TYPE_EFFECT) then
+        local ec2 = Effect.CreateEffect(c)
+        ec2:SetType(EFFECT_TYPE_SINGLE)
+        ec2:SetCode(EFFECT_ADD_TYPE)
+        ec2:SetValue(TYPE_EFFECT)
+        ec2:SetReset(RESET_EVENT + RESETS_STANDARD)
+        rc:RegisterEffect(ec2, true)
+    end
+end
+
+function s.e2drcon(e, tp, eg, ep, ev, re, r, rp)
+    return e:GetHandler():IsSummonType(SUMMON_TYPE_XYZ)
+end
+
+function s.e2drtg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return true end
+
+    Duel.SetTargetPlayer(tp)
+    Duel.SetTargetParam(1)
+    Duel.SetOperationInfo(0, CATEGORY_DRAW, nil, 0, tp, 1)
+end
+
+function s.e2drop(e, tp, eg, ep, ev, re, r, rp)
+    local p, d = Duel.GetChainInfo(0, CHAININFO_TARGET_PLAYER,
+                                   CHAININFO_TARGET_PARAM)
+    Duel.Draw(p, d, REASON_EFFECT)
 end
