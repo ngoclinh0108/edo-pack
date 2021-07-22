64ac616f10ef8ad5a8f1005a2bbff29d04a3b469
Luc Ta <luc.ta@cptech.vn>
2021-07-23 00:16:35 +0700

    update

--- script/c930000005.lua
+++ script/c930000005.lua
@@ -7,13 +7,21 @@ s.listed_series = {0xa042}
 
 function s.initial_effect(c)
     c:EnableReviveLimit()
-    UtilNordic.NordicGodEffect(c, SUMMON_TYPE_LINK)
+    UtilNordic.NordicGodEffect(c, SUMMON_TYPE_LINK, true)
 
     -- link summon
-    Link.AddProcedure(c, nil, 3, 3, function(g, lc, sumtype, tp)
-        return g:IsExists(function(c, lc, sumtype, tp)
-            return c:IsSetCard(0xa042, lc, sumtype, tp) and
-                       c:IsType(TYPE_TUNER, lc, sumtype, tp)
-        end, 1, nil, lc, sumtype, tp)
-    end)
+    Link.AddProcedure(c, s.lnkfilter1, 3, 3, s.lnkcheck)
+end
+
+function s.lnkfilter1(c) return c:HasLevel() end
+
+function s.lnkfilter2(c, lc, sumtype, tp)
+    return c:IsSetCard(0xa042, lc, sumtype, tp) and
+               c:IsType(TYPE_TUNER, lc, sumtype, tp)
+end
+
+function s.lnkcheck(g, lc, sumtype, tp)
+    local mg = g:Filter(s.lnkfilter1, nil, lc, sumtype, tp)
+    return mg:CheckWithSumEqual(Card.GetLevel, 10, 3, 3) and
+               mg:IsExists(s.lnkfilter2, 1, nil, lc, sumtype, tp)
 end
