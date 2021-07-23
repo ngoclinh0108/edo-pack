12aa267491268e489309ef460e07bffcbd50ada6
Luc Ta <luc.ta@cptech.vn>
2021-07-23 22:21:22 +0700

    update

--- script/c930000024.lua
+++ script/c930000024.lua
@@ -4,5 +4,14 @@ Duel.LoadScript("util.lua")
 Duel.LoadScript("util_nordic.lua")
 
 function s.initial_effect(c)
-    -- effects
+    -- spirit return
+    aux.EnableSpiritReturn(c, EVENT_SUMMON_SUCCESS, EVENT_FLIP)
+
+    -- special summon limit
+    local splimit = Effect.CreateEffect(c)
+    splimit:SetType(EFFECT_TYPE_SINGLE)
+    splimit:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+    splimit:SetCode(EFFECT_SPSUMMON_CONDITION)
+    splimit:SetValue(aux.FALSE)
+    c:RegisterEffect(splimit)
 end
