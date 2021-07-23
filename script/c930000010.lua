8a3be759d421e1003faa649789dc2e04b90f4c63
Luc Ta <luc.ta@cptech.vn>
2021-07-23 13:58:02 +0700

    update

---
+++ script/c930000010.lua
@@ -0,0 +1,8 @@
+-- Hildr, Valkyria of the Nordic Ascendant
+local s, id = GetID()
+Duel.LoadScript("util.lua")
+Duel.LoadScript("util_nordic.lua")
+
+function s.initial_effect(c)
+    -- effects
+end
