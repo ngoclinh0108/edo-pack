52487ef41f7d91d5aafe735b69dc4dfe130562c2
Luc Ta <luc.ta@cptech.vn>
2021-07-28 18:29:11 +0700

    fix

--- script/c930000002.lua
+++ script/c930000002.lua
@@ -27,7 +27,7 @@ function s.initial_effect(c)
     e1:SetTarget(s.e1tg)
     e1:SetOperation(s.e1op)
     c:RegisterEffect(e1)
-    
+
     -- check card
     local e2 = Effect.CreateEffect(c)
     e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
@@ -39,24 +39,26 @@ function s.initial_effect(c)
     c:RegisterEffect(e2)
 end
 
-
-function s.e1check(c, e, tp, ev)
-    local loc = Duel.GetChainInfo(ev, CHAININFO_TRIGGERING_LOCATION)
+function s.e1check(c, e, tp, loc)
     return loc == LOCATION_MZONE and c:IsLocation(LOCATION_GRAVE) and
                c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
 end
 
 function s.e1con(e, tp, eg, ep, ev, re, r, rp)
-    local de, dp = Duel.GetChainInfo(ev, CHAININFO_DISABLE_REASON,
-                                     CHAININFO_DISABLE_PLAYER)
-    return de and dp ~= tp and rp == tp and re:IsActiveType(TYPE_MONSTER)
+    local de, dp, loc = Duel.GetChainInfo(ev, CHAININFO_DISABLE_REASON,
+                                          CHAININFO_DISABLE_PLAYER,
+                                          CHAININFO_TRIGGERING_LOCATION)
+    if de and dp ~= tp and rp == tp and re:IsActiveType(TYPE_MONSTER) then
+        e:SetLabel(loc)
+        return true
+    end
+    return false
 end
 
 function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
     local rc = re:GetHandler()
     if chk == 0 then return rc and Utility.CheckEffectCanApply(re, e, tp) end
-
-    if s.e1check(rc, e, tp, ev) then
+    if s.e1check(rc, e, tp, e:GetLabel()) then
         Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, rc, 1, 0, 0)
     end
 end
@@ -66,8 +68,9 @@ function s.e1op(e, tp, eg, ep, ev, re, r, rp)
     local rc = re:GetHandler()
     if c:IsFacedown() or not c:IsRelateToEffect(e) or not rc then return end
 
+    Utility.HintCard(rc)
     Utility.ApplyEffect(re, e, tp)
-    if s.e1check(rc, e, tp, ev) then
+    if s.e1check(rc, e, tp, e:GetLabel()) then
         Duel.SpecialSummon(rc, 0, tp, tp, false, false, rc:GetPreviousPosition())
     end
 end
