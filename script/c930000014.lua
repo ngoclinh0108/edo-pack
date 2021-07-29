535d581cd46290897c8555ac3fc013aa83423ba1
Luc Ta <luc.ta@cptech.vn>
2021-07-30 02:40:15 +0700

    update

--- script/c930000014.lua
+++ script/c930000014.lua
@@ -1,95 +1,99 @@
--- Beyla of the Nordic Champions
+-- Hervor of the Nordic Champions
 local s, id = GetID()
 Duel.LoadScript("util.lua")
 Duel.LoadScript("util_nordic.lua")
 
-s.listed_series = {0x4b, 0x42}
+s.listed_names = {UtilNordic.EINHERJAR_TOKEN}
+s.listed_series = {0x42}
 
 function s.initial_effect(c)
-    -- extra summon
+    -- special summon token
     local e1 = Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_IGNITION)
-    e1:SetRange(LOCATION_HAND)
-    e1:SetCountLimit(1, id + 1000000, EFFECT_COUNT_CODE_OATH)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON + CATEGORY_TOKEN)
+    e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCode(EVENT_TO_GRAVE)
+    e1:SetCountLimit(1, id + 1000000)
     e1:SetCondition(s.e1con)
+    e1:SetTarget(s.e1tg)
     e1:SetOperation(s.e1op)
     c:RegisterEffect(e1)
 
-    -- summon
+    -- special summon (self)
     local e2 = Effect.CreateEffect(c)
-    e2:SetCategory(CATEGORY_SUMMON)
-    e2:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
-    e2:SetCode(EVENT_SUMMON_SUCCESS)
-    e2:SetRange(LOCATION_HAND)
-    e2:SetCountLimit(1, id + 2000000, EFFECT_COUNT_CODE_OATH)
-    e2:SetCondition(s.e2con)
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetRange(LOCATION_GRAVE)
+    e2:SetCountLimit(1, id + 2000000)
     e2:SetTarget(s.e2tg)
     e2:SetOperation(s.e2op)
     c:RegisterEffect(e2)
-    local e2b = e2:Clone()
-    e2b:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
-    c:RegisterEffect(e2b)
-    local e3b = e2:Clone()
-    e3b:SetCode(EVENT_SPSUMMON_SUCCESS)
-    c:RegisterEffect(e3b)
 end
 
 function s.e1con(e, tp, eg, ep, ev, re, r, rp)
-    return Duel.GetTurnPlayer() == tp and not e:GetHandler():IsPublic()
+    local rc = re:GetHandler()
+    return e:GetHandler():IsReason(REASON_COST) and rc:IsSetCard(0x42) and
+               rc:IsType(TYPE_MONSTER)
+end
+
+function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then
+        return Duel.GetLocationCount(tp, LOCATION_MZONE) >= 1 and
+                   Duel.IsPlayerCanSpecialSummonMonster(tp,
+                                                        UtilNordic.EINHERJAR_TOKEN,
+                                                        0, TYPES_TOKEN, 1000,
+                                                        1000, 4, RACE_WARRIOR,
+                                                        ATTRIBUTE_EARTH)
+    end
+
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp, 0)
+    Duel.SetOperationInfo(0, CATEGORY_TOKEN, nil, 1, 0, 0)
 end
 
 function s.e1op(e, tp, eg, ep, ev, re, r, rp)
-    local c = e:GetHandler()
-    c:RegisterFlagEffect(id, RESET_EVENT + RESETS_STANDARD + RESET_PHASE +
-                             PHASE_END, 0, 1)
+    if Duel.GetLocationCount(tp, LOCATION_MZONE) < 1 or
+        not Duel.IsPlayerCanSpecialSummonMonster(tp, UtilNordic.EINHERJAR_TOKEN,
+                                                 0, TYPES_TOKEN, 1000, 1000, 4,
+                                                 RACE_WARRIOR, ATTRIBUTE_EARTH) then
+        return
+    end
 
-    local ec1 = Effect.CreateEffect(c)
-    ec1:SetType(EFFECT_TYPE_SINGLE)
-    ec1:SetCode(EFFECT_PUBLIC)
-    ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
-    c:RegisterEffect(ec1)
-
-    local ec2 = Effect.CreateEffect(c)
-    ec2:SetDescription(aux.Stringid(id, 0))
-    ec2:SetType(EFFECT_TYPE_FIELD)
-    ec2:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
-    ec2:SetRange(LOCATION_HAND)
-    ec2:SetTargetRange(LOCATION_HAND + LOCATION_MZONE, 0)
-    ec2:SetCondition(function(e)
-        return e:GetHandler():GetFlagEffect(id) ~= 0 and
-                   e:GetHandler():IsPublic()
-    end)
-    ec2:SetTarget(function(e, c)
-        return c:IsSetCard(0x42) and c ~= e:GetHandler()
-    end)
-    ec2:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
-    c:RegisterEffect(ec2)
+    local token = Duel.CreateToken(tp, UtilNordic.EINHERJAR_TOKEN)
+    Duel.SpecialSummon(token, 0, tp, tp, false, false, POS_FACEUP)
 end
 
-function s.e2con(e, tp, eg, ep, ev, re, r, rp)
-    local ec = eg:GetFirst()
-    return ep == tp and ec:IsSetCard(0x42)
+function s.e2filter(c)
+    return c:IsFaceup() and c:IsSetCard(0x42) and c:IsLevelAbove(3)
 end
 
-function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     local c = e:GetHandler()
-    if chk == 0 then return c:IsSummonable(true, nil) end
-    Duel.SetOperationInfo(0, CATEGORY_SUMMON, c, 1, 0, 0)
+    if chk == 0 then
+        return
+            Duel.IsExistingTarget(s.e2filter, tp, LOCATION_MZONE, 0, 1, nil) and
+                Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
+                c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
+    end
+
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_FACEUP)
+    Duel.SelectTarget(tp, s.e2filter, tp, LOCATION_MZONE, 0, 1, 1, nil)
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, c, 1, 0, 0)
 end
 
 function s.e2op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
-    if c:IsRelateToEffect(e) then Duel.Summon(tp, c, true, nil) end
+    local tc = Duel.GetFirstTarget()
+    if tc:IsFacedown() or not tc:IsRelateToEffect(e) or tc:IsImmuneToEffect(e) or
+        tc:GetLevel() < 2 then return end
 
     local ec1 = Effect.CreateEffect(c)
-    ec1:SetDescription(aux.Stringid(id, 1))
-    ec1:SetType(EFFECT_TYPE_FIELD)
-    ec1:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_CLIENT_HINT)
-    ec1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-    ec1:SetTargetRange(1, 0)
-    ec1:SetTarget(function(e, c)
-        return c:IsLocation(LOCATION_EXTRA) and not c:IsSetCard(0x4b)
-    end)
-    ec1:SetReset(RESET_PHASE + PHASE_END)
-    Duel.RegisterEffect(ec1, tp)
+    ec1:SetType(EFFECT_TYPE_SINGLE)
+    ec1:SetCode(EFFECT_UPDATE_LEVEL)
+    ec1:SetValue(-2)
+    ec1:SetReset(RESET_EVENT + RESETS_STANDARD)
+    tc:RegisterEffect(ec1)
+    if c:IsRelateToEffect(e) then
+        Duel.SpecialSummon(c, 0, tp, tp, false, false, POS_FACEUP_DEFENSE)
+    end
 end
