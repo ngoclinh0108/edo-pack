d72687b3dd319c1e7bea1522c12dc62cc80ea5d9
Luc Ta <luc.ta@cptech.vn>
2021-07-29 07:48:51 +0700

    update

--- script/c930000019.lua
+++ script/c930000019.lua
@@ -6,21 +6,87 @@ Duel.LoadScript("util_nordic.lua")
 s.listed_series = {0x4b, 0x42}
 
 function s.initial_effect(c)
-    -- synchro substitute
+    -- special summon
     local e1 = Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
-    e1:SetCode(61777313)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCountLimit(1, id, EFFECT_COUNT_CODE_OATH)
+    e1:SetCondition(s.e1con)
+    e1:SetOperation(s.e1op)
     c:RegisterEffect(e1)
 
-    -- synchro limit
+    -- synchro substitute
     local e2 = Effect.CreateEffect(c)
     e2:SetType(EFFECT_TYPE_SINGLE)
     e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
-    e2:SetCode(EFFECT_CANNOT_BE_SYNCHRO_MATERIAL)
-    e2:SetValue(function(e, c)
+    e2:SetCode(61777313)
+    c:RegisterEffect(e2)
+
+    -- synchro limit
+    local e3 = Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+    e3:SetCode(EFFECT_CANNOT_BE_SYNCHRO_MATERIAL)
+    e3:SetValue(function(e, c)
         if not c then return false end
         return not c:IsSetCard(0x4b)
     end)
-    c:RegisterEffect(e2)
+    c:RegisterEffect(e3)
+
+    -- lv change
+    local e4 = Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id, 0))
+    e4:SetType(EFFECT_TYPE_IGNITION)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCountLimit(1)
+    e4:SetTarget(s.e4tg)
+    e4:SetOperation(s.e4op)
+    c:RegisterEffect(e4)
+end
+
+function s.e1filter(c)
+    return c:IsSetCard(0x42) and c:IsType(TYPE_MONSTER) and
+               c:IsAbleToGraveAsCost()
+end
+
+function s.e1con(e, c)
+    if c == nil then return true end
+    local tp = c:GetControler()
+    return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
+               Duel.IsExistingMatchingCard(s.e1filter, tp,
+                                           LOCATION_HAND + LOCATION_DECK, 0, 1,
+                                           c)
+end
+
+function s.e1op(e, tp, eg, ep, ev, re, r, rp, c)
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_TOGRAVE)
+    local g = Duel.SelectMatchingCard(tp, s.e1filter, tp,
+                                      LOCATION_HAND + LOCATION_DECK, 0, 1, 1, c)
+    Duel.SendtoGrave(g, REASON_COST)
+end
+
+function s.e4tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return true end
+
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_EFFECT)
+    local op = Duel.SelectOption(tp, aux.Stringid(id, 1), aux.Stringid(id, 2))
+    e:SetLabel(op)
+end
+
+function s.e4op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    if c:IsFacedown() or not c:IsRelateToEffect(e) then return end
+
+    local ec1 = Effect.CreateEffect(c)
+    ec1:SetType(EFFECT_TYPE_SINGLE)
+    ec1:SetCode(EFFECT_UPDATE_LEVEL)
+    if e:GetLabel() == 0 then
+        ec1:SetValue(1)
+    else
+        ec1:SetValue(-1)
+    end
+    ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
+    c:RegisterEffect(ec1)
 end
