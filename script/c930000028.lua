6388f8c12806ea215d8982b57a359fb5b4b4f05a
Luc Ta <luc.ta@cptech.vn>
2021-08-02 02:05:01 +0700

    update

--- script/c930000028.lua
+++ script/c930000028.lua
@@ -10,13 +10,24 @@ function s.initial_effect(c)
     -- activate
     local e1 = Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
-    e1:SetProperty(EFFECT_FLAG_CARD_TARGET + EFFECT_FLAG_CANNOT_DISABLE +
-                       EFFECT_FLAG_CANNOT_NEGATE + EFFECT_FLAG_CANNOT_INACTIVATE)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
     e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetCountLimit(1, id + 1000000)
     e1:SetCondition(s.e1con)
     e1:SetTarget(s.e1tg)
     e1:SetOperation(s.e1op)
     c:RegisterEffect(e1)
+
+    -- to hand
+    local e2 = Effect.CreateEffect(c)
+    e2:SetCategory(CATEGORY_TOHAND)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_GRAVE)
+    e1:SetCountLimit(1, id + 2000000)
+    e1:SetCondition(s.e2con)
+    e2:SetTarget(s.e2tg)
+    e2:SetOperation(s.e2op)
+    c:RegisterEffect(e2)
 end
 
 function s.e1filter(c)
@@ -57,9 +68,7 @@ function s.e1op(e, tp, eg, ep, ev, re, r, rp)
     local ec2 = Effect.CreateEffect(c)
     ec2:SetCategory(CATEGORY_DAMAGE)
     ec2:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_F)
-    ec2:SetProperty(EFFECT_FLAG_DELAY + EFFECT_FLAG_PLAYER_TARGET +
-                        EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_CANNOT_NEGATE +
-                        EFFECT_FLAG_CANNOT_INACTIVATE)
+    ec2:SetProperty(EFFECT_FLAG_DELAY + EFFECT_FLAG_PLAYER_TARGET)
     ec2:SetCode(EVENT_BATTLE_DESTROYING)
     ec2:SetLabelObject(tc)
     ec2:SetCondition(s.e1dmgcon)
@@ -86,4 +95,25 @@ function s.e1dmgop(e, tp, eg, ep, ev, re, r, rp)
     local p, d = Duel.GetChainInfo(0, CHAININFO_TARGET_PLAYER,
                                    CHAININFO_TARGET_PARAM)
     Duel.Damage(p, d, REASON_EFFECT)
+end
+
+function s.e2con(e, tp, eg, ep, ev, re, r, rp)
+    return aux.exccon(e, tp, eg, ep, ev, re, r, rp) and
+               Duel.IsExistingMatchingCard(
+                   aux.FilterFaceupFunction(Card.IsCode, 30604579), tp,
+                   LOCATION_MZONE, 0, 1, nil)
+end
+
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    local c = e:GetHandler()
+    if chk == 0 then return c:IsAbleToHand() end
+
+    Duel.SetOperationInfo(0, CATEGORY_TOHAND, c, 1, 0, 0)
+end
+
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    if not c:IsRelateToEffect(e) then return end
+    Duel.SendtoHand(c, nil, REASON_EFFECT)
+    Duel.ConfirmCards(1 - tp, c)
 end
