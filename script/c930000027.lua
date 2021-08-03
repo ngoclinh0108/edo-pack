d9b94ef4197d535fda120ef658bac6055dc326c7
Luc Ta <luc.ta@cptech.vn>
2021-08-04 06:49:44 +0700

    update

--- script/c930000027.lua
+++ script/c930000027.lua
@@ -3,5 +3,39 @@ local s, id = GetID()
 Duel.LoadScript("util.lua")
 Duel.LoadScript("util_nordic.lua")
 
+s.listed_names = {930000002}
+s.listed_series = {0x4b, 0x42}
+
 function s.initial_effect(c)
+    -- to hand
+    local e2 = Effect.CreateEffect(c)
+    e2:SetCategory(CATEGORY_TOHAND)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_GRAVE)
+    e1:SetCountLimit(1, id + 2000000)
+    e2:SetCondition(s.e2con)
+    e2:SetTarget(s.e2tg)
+    e2:SetOperation(s.e2op)
+    c:RegisterEffect(e2)
+end
+
+function s.e2con(e, tp, eg, ep, ev, re, r, rp)
+    return aux.exccon(e) and
+               Duel.IsExistingMatchingCard(
+                   aux.FilterFaceupFunction(Card.IsCode, 930000002), tp,
+                   LOCATION_MZONE, 0, 1, nil)
+end
+
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    local c = e:GetHandler()
+    if chk == 0 then return c:IsAbleToHand() end
+
+    Duel.SetOperationInfo(0, CATEGORY_TOHAND, c, 1, 0, 0)
+end
+
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    if not c:IsRelateToEffect(e) then return end
+    Duel.SendtoHand(c, nil, REASON_EFFECT)
+    Duel.ConfirmCards(1 - tp, c)
 end
--- script/c930000034.lua
+++ script/c930000034.lua
@@ -6,25 +6,23 @@ Duel.LoadScript("util_nordic.lua")
 s.listed_series = {0x4b, 0x42}
 
 function s.initial_effect(c)
-    -- indes
+    -- recover
     local e1 = Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_DESTROY + CATEGORY_RECOVER)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
     e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetHintTiming(0, TIMING_BATTLE_START + TIMING_END_PHASE)
     e1:SetTarget(s.e1tg)
     e1:SetOperation(s.e1op)
     c:RegisterEffect(e1)
 
     -- return to hand
     local e2 = Effect.CreateEffect(c)
-    e2:SetCategory(CATEGORY_DESTROY + CATEGORY_TOHAND)
-    e2:SetType(EFFECT_TYPE_QUICK_O)
-    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e2:SetCode(EVENT_FREE_CHAIN)
+    e2:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
+    e2:SetCode(EFFECT_DESTROY_REPLACE)
     e2:SetRange(LOCATION_GRAVE)
-    e2:SetCountLimit(1, id)
     e2:SetTarget(s.e2tg)
+    e2:SetValue(s.e2val)
     e2:SetOperation(s.e2op)
     c:RegisterEffect(e2)
 end
@@ -39,59 +37,69 @@ function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     end
 
     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_FACEUP)
-    Duel.SelectTarget(tp, s.e1filter, tp, LOCATION_MZONE, 0, 1, 1, nil)
+    local tc = Duel.SelectTarget(tp, s.e1filter, tp, LOCATION_MZONE, 0, 1, 1,
+                                 nil):GetFirst()
+
+    local rec
+    local op = Duel.SelectOption(tp, aux.Stringid(id, 0), aux.Stringid(id, 1))
+    e:SetLabel(op)
+    if op == 0 then
+        rec = tc:GetAttack()
+    else
+        rec = tc:GetDefense()
+    end
+
+    Duel.SetOperationInfo(0, CATEGORY_DESTROY, tc, 1, 0, 0)
+    Duel.SetOperationInfo(0, CATEGORY_RECOVER, nil, 0, tp, rec)
 end
 
 function s.e1op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     local tc = Duel.GetFirstTarget()
-    if tc:IsFacedown() or not tc:IsRelateToEffect(e) then return end
+    if tc:IsFacedown() or not tc:IsRelateToEffect(e) or
+        Duel.Destroy(tc, REASON_EFFECT) == 0 then return end
 
-    local ec1 = Effect.CreateEffect(c)
-    ec1:SetDescription(3066)
-    ec1:SetType(EFFECT_TYPE_SINGLE)
-    ec1:SetProperty(EFFECT_FLAG_SINGLE_RANGE + EFFECT_FLAG_CLIENT_HINT)
-    ec1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
-    ec1:SetRange(LOCATION_MZONE)
-    ec1:SetValue(1)
-    ec1:SetReset(RESET_EVENT + RESETS_STANDARD_DISABLE + RESET_PHASE + PHASE_END)
-    tc:RegisterEffect(ec1)
-    local ec1b = ec1:Clone()
-    ec1b:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
-    ec1b:SetValue(function(e, re, tp) return e:GetHandlerPlayer() == 1 - tp end)
-    tc:RegisterEffect(ec1b)
+    local rec
+    if e:GetLabel() == 0 then
+        rec = tc:GetAttack()
+    else
+        rec = tc:GetDefense()
+    end
+    Duel.Recover(tp, rec, REASON_EFFECT)
+
     if tc:IsSetCard(0x4b) then
-        local ec2 = ec1:Clone()
-        ec2:SetDescription(3210)
-        ec2:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
-        tc:RegisterEffect(ec2)
+        local ec1 = Effect.CreateEffect(c)
+        ec1:SetDescription(aux.Stringid(id, 2))
+        ec1:SetType(EFFECT_TYPE_FIELD)
+        ec1:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_CLIENT_HINT)
+        ec1:SetCode(EFFECT_CHANGE_DAMAGE)
+        ec1:SetTargetRange(1, 0)
+        ec1:SetValue(0)
+        ec1:SetReset(RESET_PHASE + PHASE_END)
+        Duel.RegisterEffect(ec1, tp)
+        local ec1b = ec1:Clone()
+        ec1b:SetCode(EFFECT_NO_EFFECT_DAMAGE)
+        Duel.RegisterEffect(ec1b, tp)
     end
 end
 
-function s.e2filter(c) return c:IsFaceup() and c:IsSetCard(0x4b) end
+function s.e2filter(c, tp)
+    return c:IsControler(tp) and not c:IsReason(REASON_REPLACE) and
+               c:IsLocation(LOCATION_MZONE) and c:IsFaceup() and
+               c:IsReason(REASON_EFFECT + REASON_BATTLE) and c:IsSetCard(0x4b)
+end
 
-function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
     if chk == 0 then
-        return Duel.IsExistingTarget(s.e2filter, tp, LOCATION_MZONE, 0, 1, nil)
+        return c:IsAbleToRemove() and eg:IsExists(s.e2filter, 1, nil, tp)
     end
 
-    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_FACEUP)
-    local g =
-        Duel.SelectTarget(tp, s.e2filter, tp, LOCATION_MZONE, 0, 1, 1, nil)
-
-    Duel.SetOperationInfo(0, CATEGORY_DESTROY, g, #g, 0, 0)
-    Duel.SetOperationInfo(0, CATEGORY_TOHAND, c, 1, 0, 0)
+    return Duel.SelectEffectYesNo(tp, c, 96)
 end
 
-function s.e2op(e, tp, eg, ep, ev, re, r, rp)
-    local c = e:GetHandler()
-    local tc = Duel.GetFirstTarget()
-    if not c:IsRelateToEffect(e) or tc:IsFacedown() or
-        not tc:IsRelateToEffect(e) or Duel.Destroy(tc, REASON_EFFECT) == 0 then
-        return
-    end
+function s.e2val(e, c) return s.e2filter(c, e:GetHandlerPlayer()) end
 
-    Duel.SendtoHand(c, nil, REASON_EFFECT)
-    Duel.ConfirmCards(1 - tp, c)
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+    Duel.Remove(e:GetHandler(), POS_FACEUP, REASON_EFFECT)
 end
