c25be75811193f59591e06dde59438b234671097
Luc Ta <luc.ta@cptech.vn>
2021-08-04 23:13:03 +0700

    update

--- script/c930000002.lua
+++ script/c930000002.lua
@@ -15,15 +15,16 @@ function s.initial_effect(c)
         return c:IsSetCard(0x4042, sc, sumtype, tp) or
                    c:IsHasEffect(EFFECT_SYNSUB_NORDIC)
     end, 1, 1, Synchro.NonTuner(nil), 2, 99)
-
-    -- apply negated effect
+    
+    -- negate
     local e1 = Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id, 0))
-    e1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
-    e1:SetProperty(EFFECT_FLAG_DELAY + EFFECT_FLAG_CANNOT_NEGATE +
-                       EFFECT_FLAG_CANNOT_INACTIVATE)
-    e1:SetCode(EVENT_CHAIN_NEGATED)
+    e1:SetCategory(CATEGORY_DISABLE)
+    e1:SetType(EFFECT_TYPE_QUICK_O)
+    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP + EFFECT_FLAG_DAMAGE_CAL)
+    e1:SetCode(EVENT_CHAINING)
     e1:SetRange(LOCATION_MZONE)
+    e1:SetCountLimit(1)
     e1:SetCondition(s.e1con)
     e1:SetTarget(s.e1tg)
     e1:SetOperation(s.e1op)
@@ -41,40 +42,23 @@ function s.initial_effect(c)
     c:RegisterEffect(e2)
 end
 
-function s.e1check(c, e, tp, loc)
-    return loc == LOCATION_MZONE and c:IsLocation(LOCATION_GRAVE) and
-               c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
-end
-
 function s.e1con(e, tp, eg, ep, ev, re, r, rp)
-    local de, dp, loc = Duel.GetChainInfo(ev, CHAININFO_DISABLE_REASON,
-                                          CHAININFO_DISABLE_PLAYER,
-                                          CHAININFO_TRIGGERING_LOCATION)
-    if de and dp ~= tp and rp == tp and re:IsActiveType(TYPE_MONSTER) then
-        e:SetLabel(loc)
-        return true
-    end
-    return false
+    if e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) then return false end
+    if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
+    local tg = Duel.GetChainInfo(ev, CHAININFO_TARGET_CARDS)
+    return tg and rp ~= tp and Duel.IsChainDisablable(ev)
 end
 
 function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
-    local rc = re:GetHandler()
-    if chk == 0 then return rc and Utility.CheckEffectCanApply(re, e, tp) end
-    if s.e1check(rc, e, tp, e:GetLabel()) then
-        Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, rc, 1, 0, 0)
-    end
+    if chk == 0 then return true end
+    Duel.SetOperationInfo(0, CATEGORY_DISABLE, eg, 1, 0, 0)
 end
 
 function s.e1op(e, tp, eg, ep, ev, re, r, rp)
-    local c = e:GetHandler()
-    local rc = re:GetHandler()
-    if c:IsFacedown() or not c:IsRelateToEffect(e) or not rc then return end
-
-    Utility.HintCard(rc)
+    if not Duel.NegateEffect(ev) then return end
+    if not Utility.CheckEffectCanApply(re, e, tp) or
+        not Duel.SelectYesNo(tp, aux.Stringid(id, 1)) then return end
     Utility.ApplyEffect(re, e, tp)
-    if s.e1check(rc, e, tp, e:GetLabel()) then
-        Duel.SpecialSummon(rc, 0, tp, tp, false, false, rc:GetPreviousPosition())
-    end
 end
 
 function s.e2filter(c)
--- script/c930000004.lua
+++ script/c930000004.lua
@@ -16,15 +16,14 @@ function s.initial_effect(c)
                    c:IsSetCard(0x42, sc, sumtype, tp)
     end, 1, 1, Synchro.NonTuner(nil), 2, 99)
 
-    -- negate
+    -- apply negated effect
     local e1 = Effect.CreateEffect(c)
     e1:SetDescription(aux.Stringid(id, 0))
-    e1:SetCategory(CATEGORY_DISABLE)
-    e1:SetType(EFFECT_TYPE_QUICK_O)
-    e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP + EFFECT_FLAG_DAMAGE_CAL)
-    e1:SetCode(EVENT_CHAINING)
+    e1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
+    e1:SetProperty(EFFECT_FLAG_DELAY + EFFECT_FLAG_CANNOT_NEGATE +
+                       EFFECT_FLAG_CANNOT_INACTIVATE)
+    e1:SetCode(EVENT_CHAIN_NEGATED)
     e1:SetRange(LOCATION_MZONE)
-    e1:SetCountLimit(1)
     e1:SetCondition(s.e1con)
     e1:SetTarget(s.e1tg)
     e1:SetOperation(s.e1op)
@@ -42,23 +41,40 @@ function s.initial_effect(c)
     c:RegisterEffect(e2)
 end
 
+function s.e1check(c, e, tp, loc)
+    return loc == LOCATION_MZONE and c:IsLocation(LOCATION_GRAVE) and
+               c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
+end
+
 function s.e1con(e, tp, eg, ep, ev, re, r, rp)
-    if e:GetHandler():IsStatus(STATUS_BATTLE_DESTROYED) then return false end
-    if not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then return false end
-    local tg = Duel.GetChainInfo(ev, CHAININFO_TARGET_CARDS)
-    return tg and rp ~= tp and Duel.IsChainDisablable(ev)
+    local de, dp, loc = Duel.GetChainInfo(ev, CHAININFO_DISABLE_REASON,
+                                          CHAININFO_DISABLE_PLAYER,
+                                          CHAININFO_TRIGGERING_LOCATION)
+    if de and dp ~= tp and rp == tp and re:IsActiveType(TYPE_MONSTER) then
+        e:SetLabel(loc)
+        return true
+    end
+    return false
 end
 
 function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
-    if chk == 0 then return true end
-    Duel.SetOperationInfo(0, CATEGORY_DISABLE, eg, 1, 0, 0)
+    local rc = re:GetHandler()
+    if chk == 0 then return rc and Utility.CheckEffectCanApply(re, e, tp) end
+    if s.e1check(rc, e, tp, e:GetLabel()) then
+        Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, rc, 1, 0, 0)
+    end
 end
 
 function s.e1op(e, tp, eg, ep, ev, re, r, rp)
-    if not Duel.NegateEffect(ev) then return end
-    if not Utility.CheckEffectCanApply(re, e, tp) or
-        not Duel.SelectYesNo(tp, aux.Stringid(id, 1)) then return end
+    local c = e:GetHandler()
+    local rc = re:GetHandler()
+    if c:IsFacedown() or not c:IsRelateToEffect(e) or not rc then return end
+
+    Utility.HintCard(rc)
     Utility.ApplyEffect(re, e, tp)
+    if s.e1check(rc, e, tp, e:GetLabel()) then
+        Duel.SpecialSummon(rc, 0, tp, tp, false, false, rc:GetPreviousPosition())
+    end
 end
 
 function s.e2filter(c) return c:IsFaceup() and c:IsSetCard(0x4b) end
