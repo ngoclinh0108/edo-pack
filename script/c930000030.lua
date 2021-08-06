d41c1723fb0496451413cdfab1415f37667ab155
Luc Ta <luc.ta@cptech.vn>
2021-08-07 05:56:28 +0700

    update

--- script/c930000030.lua
+++ script/c930000030.lua
@@ -4,7 +4,7 @@ Duel.LoadScript("util.lua")
 Duel.LoadScript("util_nordic.lua")
 
 s.listed_names = {67098114}
-s.listed_series = {0x4b}
+s.listed_series = {0x4b, 0x42}
 
 function s.initial_effect(c)
     -- destroy
@@ -25,45 +25,57 @@ function s.initial_effect(c)
         ge1:SetCode(EVENT_BATTLE_DESTROYING)
         ge1:SetOperation(s.e1regop1)
         Duel.RegisterEffect(ge1, 0)
-        local ge2 = ge1:Clone()
-        ge2:SetCode(EVENT_CHAINING)
-        ge2:SetOperation(s.e1regop2)
-        Duel.RegisterEffect(ge2, 0)
+        local ge3 = ge1:Clone()
+        ge3:SetCode(EVENT_CHAINING)
+        ge3:SetOperation(s.e1regop2)
+        Duel.RegisterEffect(ge3, 0)
         local ge3 = ge1:Clone()
         ge3:SetCode(EVENT_CHAIN_NEGATED)
         ge3:SetOperation(s.e1regop3)
         Duel.RegisterEffect(ge3, 0)
     end)
 
-    -- to hand
+    -- act in hand
     local e2 = Effect.CreateEffect(c)
-    e2:SetCategory(CATEGORY_TOHAND)
-    e2:SetType(EFFECT_TYPE_IGNITION)
-    e2:SetRange(LOCATION_GRAVE)
-    e1:SetCountLimit(1, id + 2000000)
-    e2:SetCondition(s.e2con)
-    e2:SetTarget(s.e2tg)
-    e2:SetOperation(s.e2op)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_TRAP_ACT_IN_HAND)
+    e2:SetCondition(function(e)
+        return Duel.IsExistingMatchingCard(
+                   aux.FilterFaceupFunction(Card.IsSetCard, 0x4b),
+                   e:GetHandlerPlayer(), LOCATION_MZONE, 0, 1, nil)
+    end)
     c:RegisterEffect(e2)
+
+    -- to hand
+    local e3 = Effect.CreateEffect(c)
+    e3:SetCategory(CATEGORY_TOHAND)
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetRange(LOCATION_GRAVE)
+    e1:SetCountLimit(1, id + 2000000)
+    e3:SetCondition(s.e3con)
+    e3:SetTarget(s.e3tg)
+    e3:SetOperation(s.e3op)
+    c:RegisterEffect(e3)
 end
 
-function s.e1filter(c) return c:IsFaceup() and c:GetFlagEffect(id) ~= 0 end
+function s.e1filter1(c) return c:IsFaceup() and Utility.IsSetCard(c, 0x4b, 0x42) end
+
+function s.e1filter2(c) return c:IsFaceup() and c:GetFlagEffect(id) ~= 0 end
 
 function s.e1con(e, tp, eg, ep, ev, re, r, rp)
     return Duel.GetTurnPlayer() == tp or
-               Duel.IsExistingMatchingCard(
-                   aux.FilterFaceupFunction(Card.IsSetCard, 0x4b), tp,
-                   LOCATION_MZONE, 0, 1, nil)
+               Duel.IsExistingMatchingCard(s.e1filter1, tp, LOCATION_MZONE, 0,
+                                           1, nil)
 end
 
 function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     if chk == 0 then
-        return Duel.IsExistingTarget(s.e1filter, tp, LOCATION_MZONE,
+        return Duel.IsExistingTarget(s.e1filter2, tp, LOCATION_MZONE,
                                      LOCATION_MZONE, 1, nil)
     end
 
     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_DESTROY)
-    local g = Duel.SelectTarget(tp, s.e1filter, tp, LOCATION_MZONE,
+    local g = Duel.SelectTarget(tp, s.e1filter2, tp, LOCATION_MZONE,
                                 LOCATION_MZONE, 1, 1, nil)
     Duel.SetOperationInfo(0, CATEGORY_DESTROY, g, 1, 0, 0)
     if e:IsHasType(EFFECT_TYPE_ACTIVATE) then Duel.SetChainLimit(aux.FALSE) end
@@ -99,21 +111,21 @@ function s.e1regop3(e, tp, eg, ep, ev, re, r, rp)
     end
 end
 
-function s.e2con(e, tp, eg, ep, ev, re, r, rp)
+function s.e3con(e, tp, eg, ep, ev, re, r, rp)
     return aux.exccon(e) and
                Duel.IsExistingMatchingCard(
                    aux.FilterFaceupFunction(Card.IsCode, 67098114), tp,
                    LOCATION_MZONE, 0, 1, nil)
 end
 
-function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
     if chk == 0 then return c:IsAbleToHand() end
 
     Duel.SetOperationInfo(0, CATEGORY_TOHAND, c, 1, 0, 0)
 end
 
-function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     if not c:IsRelateToEffect(e) then return end
     Duel.SendtoHand(c, nil, REASON_EFFECT)
