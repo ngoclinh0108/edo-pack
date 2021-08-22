791644bdcd8eed92e984ad7cb6cbccec0de985d2
Luc Ta <luc.ta@cptech.vn>
2021-08-22 17:25:41 +0700

    fix

--- script/c910000009.lua
+++ script/c910000009.lua
@@ -2,7 +2,7 @@
 Duel.LoadScript("util.lua")
 local s, id = GetID()
 
-s.listed_names = {59514116}
+s.listed_names = {59514116, CARD_DARK_MAGICIAN, CARD_DARK_MAGICIAN_GIRL}
 
 function s.initial_effect(c)
     c:EnableReviveLimit()
@@ -18,6 +18,17 @@ function s.initial_effect(c)
     attribute:SetCode(EFFECT_ADD_ATTRIBUTE)
     attribute:SetValue(ATTRIBUTE_DARK)
     c:RegisterEffect(attribute)
+
+    -- search
+    local e1 = Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_TOHAND + CATEGORY_SEARCH + CATEGORY_TODECK)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCountLimit(1, id + 1000000)
+    e1:SetCost(s.e1cost)
+    e1:SetTarget(s.e1tg)
+    e1:SetOperation(s.e1op)
+    c:RegisterEffect(e1)
 
     -- no activate
     local e2 = Effect.CreateEffect(c)
@@ -42,6 +53,46 @@ function s.initial_effect(c)
     e3:SetTarget(s.e3tg)
     e3:SetOperation(s.e3op)
     c:RegisterEffect(e3)
+end
+
+function s.e1filter(c)
+    if not c:IsMonster() or not c:IsAbleToHand() then return false end
+    return c:IsCode(CARD_DARK_MAGICIAN, CARD_DARK_MAGICIAN_GIRL) or
+               (aux.IsCodeListed(c, CARD_DARK_MAGICIAN, CARD_DARK_MAGICIAN_GIRL) and
+                   not c:IsType(TYPE_RITUAL))
+end
+
+function s.e1cost(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return not e:GetHandler():IsPublic() end
+    Duel.ConfirmCards(1 - tp, e:GetHandler())
+end
+
+function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then
+        return Duel.IsExistingMatchingCard(s.e1filter, tp, LOCATION_DECK, 0, 1,
+                                           nil)
+    end
+
+    Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp, LOCATION_DECK)
+    Duel.SetOperationInfo(0, CATEGORY_TODECK, nil, 1, tp, LOCATION_HAND)
+end
+
+function s.e1op(e, tp, eg, ep, ev, re, r, rp)
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_ATOHAND)
+    local tc = Duel.SelectMatchingCard(tp, s.e1filter, tp, LOCATION_DECK, 0, 1,
+                                       1, nil):GetFirst()
+    if tc and Duel.SendtoHand(tc, nil, REASON_EFFECT) > 0 and
+        tc:IsLocation(LOCATION_HAND) then
+        Duel.ConfirmCards(1 - tp, tc)
+        Duel.ShuffleHand(tp)
+        Duel.ShuffleDeck(tp)
+        Duel.BreakEffect()
+
+        Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_TODECK)
+        local g = Duel.SelectMatchingCard(tp, Card.IsAbleToDeck, tp,
+                                          LOCATION_HAND, 0, 1, 1, nil)
+        Duel.SendtoDeck(g, nil, SEQ_DECKTOP, REASON_EFFECT)
+    end
 end
 
 function s.e2con(e, tp, eg, ep, ev, re, r, rp)
