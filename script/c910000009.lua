f9c555ac6a5932bc49b8f5fb715c7f984cff849b
Luc Ta <luc.ta@cptech.vn>
2021-08-31 18:48:26 +0700

    update

--- script/c910000009.lua
+++ script/c910000009.lua
@@ -5,9 +5,9 @@ local s, id = GetID()
 s.listed_series = {0x13a}
 
 function s.initial_effect(c)
-    -- special summon & atk up
+    -- special summon & draw
     local e1 = Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_SPECIAL_SUMMON + CATEGORY_ATKCHANGE)
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON + CATEGORY_DRAW)
     e1:SetType(EFFECT_TYPE_QUICK_O)
     e1:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
     e1:SetCode(EVENT_FREE_CHAIN)
@@ -29,12 +29,12 @@ function s.initial_effect(c)
     e2:SetValue(function(e, re, r, rp) return (r & REASON_BATTLE) ~= 0 end)
     c:RegisterEffect(e2)
 
-    -- draw
+    -- destroy
     local e3 = Effect.CreateEffect(c)
-    e3:SetCategory(CATEGORY_DRAW)
-    e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_F)
-    e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-    e3:SetCode(EVENT_RELEASE)
+    e3:SetDescription(aux.Stringid(id, 0))
+    e3:SetCategory(CATEGORY_DESTROY)
+    e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e3:SetCode(EVENT_BATTLE_START)
     e3:SetTarget(s.e3tg)
     e3:SetOperation(s.e3op)
     c:RegisterEffect(e3)
@@ -46,7 +46,6 @@ function s.e1con(e, tp, eg, ep, ev, re, r, rp)
 
     local tc = Duel.GetAttacker()
     if tc:IsControler(1 - tp) then tc = Duel.GetAttackTarget() end
-    e:SetLabelObject(tc)
     return tc and tc:IsSetCard(0x13a) and tc:IsRelateToBattle() and
                Duel.GetAttackTarget() ~= nil
 end
@@ -59,37 +58,36 @@ function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
     end
 
     Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, c, 1, 0, 0)
+    Duel.SetOperationInfo(0, CATEGORY_DRAW, nil, 0, tp, 1)
 end
 
 function s.e1op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
-    local tc = e:GetLabelObject()
     if not c:IsRelateToEffect(e) or Duel.GetLocationCount(tp, LOCATION_MZONE) ==
-        0 or Duel.SpecialSummon(c, 0, tp, tp, false, false, POS_FACEUP) == 0 then
-        return
-    end
+        0 then return end
 
-    if not tc:IsRelateToBattle() or tc:IsFacedown() or not tc:IsControler(tp) then
-        return
+    if Duel.SpecialSummon(c, 0, tp, tp, false, false, POS_FACEUP) > 0 then
+        Duel.BreakEffect()
+        Duel.Draw(tp, 1, REASON_EFFECT)
     end
-
-    local ec1 = Effect.CreateEffect(c)
-    ec1:SetType(EFFECT_TYPE_SINGLE)
-    ec1:SetCode(EFFECT_UPDATE_ATTACK)
-    ec1:SetValue(c:GetAttack())
-    ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_DAMAGE_CAL)
-    tc:RegisterEffect(ec1)
 end
 
 function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
-    if chk == 0 then return true end
-    Duel.SetTargetPlayer(tp)
-    Duel.SetTargetParam(1)
-    Duel.SetOperationInfo(0, CATEGORY_DRAW, nil, 0, tp, 1)
+    local c = e:GetHandler()
+    local bc = Duel.GetAttackTarget()
+    if chk == 0 then
+        return Duel.GetAttacker() == c and bc and
+                   bc:IsPosition(POS_FACEDOWN_DEFENSE)
+    end
+
+    Duel.SetOperationInfo(0, CATEGORY_DESTROY, bc, 1, 0, 0)
 end
 
 function s.e3op(e, tp, eg, ep, ev, re, r, rp)
-    local p, d = Duel.GetChainInfo(0, CHAININFO_TARGET_PLAYER,
-                                   CHAININFO_TARGET_PARAM)
-    Duel.Draw(p, d, REASON_EFFECT)
+    local bc = Duel.GetAttackTarget()
+    if not bc:IsRelateToBattle() or not bc:IsPosition(POS_FACEDOWN_DEFENSE) then
+        return
+    end
+
+    Duel.Destroy(bc, REASON_EFFECT)
 end
