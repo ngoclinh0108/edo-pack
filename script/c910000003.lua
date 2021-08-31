7e5b4a35d19c132101c5455315d7c8e75cdcbf12
Luc Ta <luc.ta@cptech.vn>
2021-09-01 03:18:59 +0700

    update

--- script/c910000003.lua
+++ script/c910000003.lua
@@ -8,6 +8,7 @@ s.listed_names = {71703785, 42006475}
 
 function s.initial_effect(c)
     c:EnableReviveLimit()
+    c:SetSPSummonOnce(id)
 
     -- fusion material
     Fusion.AddProcMix(c, true, true, {71703785, 42006475},
@@ -25,18 +26,83 @@ function s.initial_effect(c)
     e1:SetValue(1)
     c:RegisterEffect(e1)
 
+    -- draw
+    local e2reg = Effect.CreateEffect(c)
+    e2reg:SetType(EFFECT_TYPE_CONTINUOUS + EFFECT_TYPE_FIELD)
+    e2reg:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    e2reg:SetCode(EVENT_CHAINING)
+    e2reg:SetRange(LOCATION_MZONE)
+    e2reg:SetOperation(aux.chainreg)
+    c:RegisterEffect(e2reg)
+    local e2 = Effect.CreateEffect(c)
+    e2:SetDescription(1108)
+    e2:SetCategory(CATEGORY_DRAW)
+    e2:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
+    e2:SetCode(EVENT_CHAIN_SOLVING)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1)
+    e2:SetCondition(s.e2con)
+    e2:SetTarget(s.e2tg)
+    e2:SetOperation(s.e2op)
+    c:RegisterEffect(e2)
+
+    -- special summon
     local e3 = Effect.CreateEffect(c)
     e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
     e3:SetProperty(EFFECT_FLAG_DELAY)
     e3:SetCode(EVENT_DESTROYED)
+    e3:SetCost(s.e3cost)
     e3:SetTarget(s.e3tg)
     e3:SetOperation(s.e3op)
     c:RegisterEffect(e3)
 end
 
+function s.e2con(e, tp, eg, ep, ev, re, r, rp)
+    return e:GetHandler():GetFlagEffect(1) > 0
+end
+
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return Duel.IsPlayerCanDraw(tp, 1) end
+
+    Duel.SetTargetPlayer(tp)
+    Duel.SetTargetParam(1)
+    Duel.SetOperationInfo(0, CATEGORY_DRAW, nil, 0, tp, 1)
+end
+
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    local p, d = Duel.GetChainInfo(0, CHAININFO_TARGET_PLAYER,
+                                   CHAININFO_TARGET_PARAM)
+    if Duel.Draw(p, d, REASON_EFFECT) == 0 then return end
+
+    local tc = Duel.GetOperatedGroup():GetFirst()
+    if tc:IsType(TYPE_SPELL + TYPE_TRAP) and tc:IsSSetable() and
+        Duel.GetLocationCount(tp, LOCATION_SZONE) > 0 and
+        Duel.SelectYesNo(tp, 1153) then
+        Duel.SSet(tp, tc, tp, false)
+
+        local ec1 = Effect.CreateEffect(c)
+        ec1:SetType(EFFECT_TYPE_SINGLE)
+        ec1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+        if tc:IsType(TYPE_QUICKPLAY) then
+            ec1:SetCode(EFFECT_QP_ACT_IN_SET_TURN)
+        elseif tc:IsType(TYPE_TRAP) then
+            ec1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
+        end
+        ec1:SetReset(RESET_EVENT + RESETS_STANDARD)
+        tc:RegisterEffect(ec1)
+    end
+end
+
 function s.e3filter(c, e, tp, code)
     return c:IsCode(code) and c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
+end
+
+function s.e3cost(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return e:GetHandler():IsAbleToExtraAsCost() end
+    Duel.SendtoDeck(e:GetHandler(), nil, 2, REASON_COST)
 end
 
 function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
