d6ffe772864c59bc0d2e0361dcc44964c199f7b6
Luc Ta <luc.ta@cptech.vn>
2021-08-31 20:55:42 +0700

    update

--- script/c910000011.lua
+++ script/c910000011.lua
@@ -11,24 +11,25 @@ function s.initial_effect(c)
     e1:SetValue(25652259)
     c:RegisterEffect(e1)
 
-    -- special summon (self)
+    -- special summon
     local e2 = Effect.CreateEffect(c)
     e2:SetType(EFFECT_TYPE_FIELD)
     e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
     e2:SetCode(EFFECT_SPSUMMON_PROC)
     e2:SetRange(LOCATION_HAND)
+    e2:SetCountLimit(1, id + 100000, EFFECT_COUNT_CODE_OATH)
     e2:SetCondition(s.e2con)
     e2:SetTarget(s.e2tg)
     e2:SetOperation(s.e2op)
     c:RegisterEffect(e2)
 
-    -- special summon (other)
+    -- to hand
     local e3 = Effect.CreateEffect(c)
-    e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e3:SetCategory(CATEGORY_TOHAND + CATEGORY_SEARCH)
     e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
     e3:SetProperty(EFFECT_FLAG_DELAY)
     e3:SetCode(EVENT_SUMMON_SUCCESS)
-    e3:SetCountLimit(1, id)
+    e3:SetCountLimit(1, id + 200000)
     e3:SetTarget(s.e3tg)
     e3:SetOperation(s.e3op)
     c:RegisterEffect(e3)
@@ -37,7 +38,10 @@ function s.initial_effect(c)
     c:RegisterEffect(e3b)
 end
 
-function s.e2filter(c) return c:IsRace(RACE_WARRIOR) and not c:IsPublic() end
+function s.e2filter(c)
+    return c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_WARRIOR) and
+               not c:IsPublic()
+end
 
 function s.e2con(e, c)
     if c == nil then return true end
@@ -70,45 +74,31 @@ function s.e2op(e, tp, eg, ep, ev, re, r, rp, c)
     g:DeleteGroup()
 end
 
-function s.e3filter(c, e, tp)
+function s.e3filter(c)
     return c:IsLevel(4) and c:IsAttribute(ATTRIBUTE_LIGHT) and
-               c:IsRace(RACE_WARRIOR) and not c:IsCode(id) and
-               c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
+               c:IsRace(RACE_WARRIOR) and not c:IsCode(25652259) and
+               c:IsAbleToHand()
 end
 
 function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then
-        return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
-                   Duel.IsExistingMatchingCard(s.e3filter, tp, LOCATION_HAND +
-                                                   LOCATION_DECK +
-                                                   LOCATION_GRAVE, 0, 1, nil, e,
-                                               tp)
+        return Duel.IsExistingMatchingCard(s.e3filter, tp, LOCATION_HAND +
+                                               LOCATION_DECK + LOCATION_GRAVE,
+                                           0, 1, nil)
     end
 
-    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp,
+    Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp,
                           LOCATION_HAND + LOCATION_DECK + LOCATION_GRAVE)
 end
 
 function s.e3op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
-    if Duel.GetLocationCount(tp, LOCATION_MZONE) <= 0 then return end
-
-    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
-    local tc = Duel.SelectMatchingCard(tp, s.e3filter, tp, LOCATION_HAND +
-                                           LOCATION_DECK + LOCATION_GRAVE, 0, 1,
-                                       1, nil, e, tp):GetFirst()
-    if not tc then return end
-
-    Duel.SpecialSummon(tc, 0, tp, tp, false, false, POS_FACEUP)
-    local ec1 = Effect.CreateEffect(c)
-    ec1:SetDescription(aux.Stringid(id, 0))
-    ec1:SetType(EFFECT_TYPE_SINGLE)
-    ec1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_CLIENT_HINT)
-    ec1:SetCode(EFFECT_CANNOT_BE_MATERIAL)
-    ec1:SetValue(function(e, tc)
-        if not tc then return false end
-        return not (tc:IsAttribute(ATTRIBUTE_LIGHT) and tc:IsRace(RACE_WARRIOR))
-    end)
-    ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
-    tc:RegisterEffect(ec1)
+    local g = Utility.SelectMatchingCard(tp, aux.NecroValleyFilter(s.e3filter),
+                                         tp, LOCATION_HAND + LOCATION_DECK +
+                                             LOCATION_GRAVE, 0, 1, 1, nil,
+                                         HINTMSG_ATOHAND)
+    if #g > 0 then
+        Duel.SendtoHand(g, nil, REASON_EFFECT)
+        Duel.ConfirmCards(1 - tp, g)
+    end
 end
--- script/c910000012.lua
+++ script/c910000012.lua
@@ -13,11 +13,15 @@ function s.initial_effect(c)
 
     -- special summon (self)
     local e2 = Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-    e2:SetCode(EFFECT_SPSUMMON_PROC)
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetCode(EVENT_FREE_CHAIN)
     e2:SetRange(LOCATION_HAND)
-    e2:SetCondition(s.e2con)
+    e2:SetHintTiming(0, TIMING_MAIN_END)
+    e2:SetCountLimit(1, id + 100000)
+    e2:SetCondition(s.e1con)
+    e2:SetTarget(s.e1tg)
+    e2:SetOperation(s.e1op)
     c:RegisterEffect(e2)
 
     -- special summon (other)
@@ -26,7 +30,7 @@ function s.initial_effect(c)
     e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
     e3:SetProperty(EFFECT_FLAG_DELAY)
     e3:SetCode(EVENT_SUMMON_SUCCESS)
-    e3:SetCountLimit(1, id)
+    e3:SetCountLimit(1, id + 200000)
     e3:SetCondition(s.e3con)
     e3:SetTarget(s.e3tg)
     e3:SetOperation(s.e3op)
@@ -37,16 +41,29 @@ function s.initial_effect(c)
 end
 
 function s.e2filter(c)
-    return c:IsFaceup() and c:IsRace(RACE_WARRIOR) and not c:IsCode(id)
+    return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_LIGHT) and
+               c:IsRace(RACE_WARRIOR) and not c:IsCode(64788463)
 end
 
-function s.e2con(e, c)
-    if c == nil then return true end
-    local tp = c:GetControler()
+function s.e2con(e, tp, eg, ep, ev, re, r, rp)
+    return
+        Duel.IsMainPhase() and Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
+            Duel.IsExistingMatchingCard(s.e2filter, tp, LOCATION_MZONE, 0, 1,
+                                        nil)
+end
 
-    return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
-               Duel.IsExistingMatchingCard(s.e2filter, tp, LOCATION_MZONE, 0, 1,
-                                           nil)
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    local c = e:GetHandler()
+    if chk == 0 then return c:IsCanBeSpecialSummoned(e, 0, tp, false, false) end
+
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, c, 1, 0, 0)
+end
+
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    if not c:IsRelateToEffect(e) or Duel.GetLocationCount(tp, LOCATION_MZONE) ==
+        0 then return end
+    Duel.SpecialSummon(c, 0, tp, tp, false, false, POS_FACEUP)
 end
 
 function s.e3filter1(c)
@@ -82,10 +99,11 @@ function s.e3op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     if Duel.GetLocationCount(tp, LOCATION_MZONE) <= 0 then return end
 
-    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
-    local tc = Duel.SelectMatchingCard(tp, s.e3filter2, tp, LOCATION_HAND +
-                                           LOCATION_DECK + LOCATION_GRAVE, 0, 1,
-                                       1, nil, e, tp):GetFirst()
+    local tc = Utility.SelectMatchingCard(tp,
+                                          aux.NecroValleyFilter(s.e3filter2),
+                                          tp, LOCATION_HAND + LOCATION_DECK +
+                                              LOCATION_GRAVE, 0, 1, 1, nil,
+                                          HINTMSG_SPSUMMON, e, tp):GetFirst()
     if not tc then return end
 
     Duel.SpecialSummon(tc, 0, tp, tp, false, false, POS_FACEUP)
