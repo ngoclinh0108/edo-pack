61ace077ba07a59bd87e378bbbfd673bc91c834a
Luc Ta <luc.ta@cptech.vn>
2021-09-11 11:48:41 +0700

    update

--- script/c910000022.lua
+++ script/c910000022.lua
@@ -78,20 +78,17 @@ end
 
 function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     if chk == 0 then
-        return Duel.IsExistingTarget(Card.IsAbleToHand, tp,
-                                     LOCATION_GRAVE + LOCATION_REMOVED, 0, 1,
-                                     nil)
+        return Duel.IsExistingTarget(Card.IsAbleToHand, tp, LOCATION_GRAVE, 0,
+                                     1, nil)
     end
 
-    Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp,
-                          LOCATION_GRAVE + LOCATION_REMOVED)
+    Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp, LOCATION_GRAVE)
 end
 
 function s.e1op(e, tp, eg, ep, ev, re, r, rp)
     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_RTOHAND)
     local g = Duel.SelectTarget(tp, aux.NecroValleyFilter(Card.IsAbleToHand),
-                                tp, LOCATION_GRAVE + LOCATION_REMOVED, 0, 1, 1,
-                                nil)
+                                tp, LOCATION_GRAVE, 0, 1, 1, nil)
 
     if #g > 0 then
         Duel.SendtoHand(g, nil, REASON_EFFECT)
