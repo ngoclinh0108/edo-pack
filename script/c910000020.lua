a27af34df42ed844027c86b48cf3ffd3eae22908
Luc Ta <luc.ta@cptech.vn>
2021-09-11 11:29:13 +0700

    update

--- script/c910000020.lua
+++ script/c910000020.lua
@@ -15,6 +15,14 @@ function s.initial_effect(c)
                    c:IsType(TYPE_NORMAL, sc, sumtype, tp)
     end)
 
+    -- attribute
+    local attribute = Effect.CreateEffect(c)
+    attribute:SetType(EFFECT_TYPE_SINGLE)
+    attribute:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+    attribute:SetCode(EFFECT_ADD_ATTRIBUTE)
+    attribute:SetValue(ATTRIBUTE_DARK)
+    c:RegisterEffect(attribute)
+
     -- special summon limit
     local splimit = Effect.CreateEffect(c)
     splimit:SetType(EFFECT_TYPE_SINGLE)
