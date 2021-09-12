f65cf9051d98bf5f666cbe2d4d758c3f97973347
Luc Ta <luc.ta@cptech.vn>
2021-09-13 00:48:33 +0700

    update

---
+++ script/c911000000.lua
@@ -0,0 +1,94 @@
+-- Blue-Eyes Light Dragon
+Duel.LoadScript("util.lua")
+local s, id = GetID()
+
+function s.initial_effect(c)
+    -- special summon
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    e1:SetCode(EFFECT_SPSUMMON_PROC)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCountLimit(1, id, EFFECT_COUNT_CODE_OATH)
+    e1:SetCondition(s.e1con)
+    e1:SetTarget(s.e1tg)
+    e1:SetOperation(s.e1op)
+    c:RegisterEffect(e1)
+
+    -- destroy
+    local e2 = Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id, 0))
+    e2:SetCategory(CATEGORY_DESTROY)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1)
+    e2:SetCost(s.e2cost)
+    e2:SetTarget(s.e2tg)
+    e2:SetOperation(s.e2op)
+    c:RegisterEffect(e2)
+end
+
+function s.e1filter(c)
+    return c:IsLevel(1) and c:IsAttribute(ATTRIBUTE_LIGHT) and
+               c:IsType(TYPE_TUNER) and not c:IsPublic()
+end
+
+function s.e1con(e, c)
+    if c == nil then return true end
+    local tp = c:GetControler()
+    local g = Duel.GetMatchingGroup(s.e1filter, tp, LOCATION_HAND, 0, nil)
+    return #g > 0 and aux.SelectUnselectGroup(g, e, tp, 1, 1, aux.ChkfMMZ(1), 0)
+end
+
+function s.e1tg(e, tp, eg, ep, ev, re, r, rp, c)
+    local g = Duel.GetMatchingGroup(s.e1filter, tp, LOCATION_HAND, 0, nil)
+    local sg = aux.SelectUnselectGroup(g, e, tp, 1, 1, aux.ChkfMMZ(1), 1, tp,
+                                       HINTMSG_CONFIRM, nil, nil, true)
+    if #sg > 0 then
+        sg:KeepAlive()
+        e:SetLabelObject(sg)
+        return true
+    end
+    return false
+end
+
+function s.e1op(e, tp, eg, ep, ev, re, r, rp, c)
+    local g = e:GetLabelObject()
+    if not g then return end
+
+    Duel.ConfirmCards(1 - tp, g)
+    Duel.ShuffleHand(tp)
+    g:DeleteGroup()
+end
+
+function s.e2cost(e, tp, eg, ep, ev, re, r, rp, chk)
+    local c = e:GetHandler()
+    if chk == 0 then return c:GetAttackAnnouncedCount() == 0 end
+
+    local ec1 = Effect.CreateEffect(c)
+    ec1:SetDescription(3206)
+    ec1:SetType(EFFECT_TYPE_SINGLE)
+    ec1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_OATH +
+                        EFFECT_FLAG_CLIENT_HINT)
+    ec1:SetCode(EFFECT_CANNOT_ATTACK)
+    ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
+    e:GetHandler():RegisterEffect(ec1)
+end
+
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    if chk == 0 then
+        return Duel.IsExistingTarget(aux.TRUE, tp, 0, LOCATION_MZONE, 1, nil)
+    end
+
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_DESTROY)
+    local g = Duel.SelectTarget(tp, aux.TRUE, tp, 0, LOCATION_MZONE, 1, 1, nil)
+
+    Duel.SetOperationInfo(0, CATEGORY_DESTROY, g, 1, 0, 0)
+end
+
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+    local tc = Duel.GetFirstTarget()
+    if not tc or not tc:IsRelateToEffect(e) then return end
+    Duel.Destroy(tc, REASON_EFFECT)
+end
--- script/c911000001.lua
+++ script/c911000001.lua
@@ -1,94 +1,71 @@
--- Blue-Eyes Light Dragon
-Duel.LoadScript("util.lua")
+-- Blue-Eyes Divine Dragon
 local s, id = GetID()
 
+s.material = {CARD_BLUEEYES_W_DRAGON}
+s.material_setcode = {0xdd}
+s.listed_names = {CARD_BLUEEYES_W_DRAGON}
+
 function s.initial_effect(c)
-    -- special summon
+    c:EnableReviveLimit()
+
+    -- fusion summon
+    Fusion.AddProcMixN(c, false, false, CARD_BLUEEYES_W_DRAGON, 3)
+
+    -- special summon limit
+    local splimit = Effect.CreateEffect(c)
+    splimit:SetType(EFFECT_TYPE_SINGLE)
+    splimit:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+    splimit:SetCode(EFFECT_SPSUMMON_CONDITION)
+    splimit:SetValue(function(e, se, sp, st)
+        return not e:GetHandler():IsLocation(LOCATION_EXTRA) or
+                   aux.fuslimit(e, se, sp, st)
+    end)
+    c:RegisterEffect(splimit)
+
+    -- activation and effects cannot be negated
     local e1 = Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_FIELD)
-    e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
-    e1:SetCode(EFFECT_SPSUMMON_PROC)
-    e1:SetRange(LOCATION_HAND)
-    e1:SetCountLimit(1, id, EFFECT_COUNT_CODE_OATH)
-    e1:SetCondition(s.e1con)
-    e1:SetTarget(s.e1tg)
-    e1:SetOperation(s.e1op)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetCode(EFFECT_CANNOT_DISABLE)
     c:RegisterEffect(e1)
+    local e1b = Effect.CreateEffect(c)
+    e1b:SetType(EFFECT_TYPE_FIELD)
+    e1b:SetCode(EFFECT_CANNOT_DISEFFECT)
+    e1b:SetRange(LOCATION_MZONE)
+    e1b:SetValue(function(e, ct)
+        local te = Duel.GetChainInfo(ct, CHAININFO_TRIGGERING_EFFECT)
+        return te:GetHandler() == e:GetHandler()
+    end)
+    c:RegisterEffect(e1b)
+    local e1c = e1b:Clone()
+    e1c:SetCode(EFFECT_CANNOT_INACTIVATE)
+    c:RegisterEffect(e1c)
 
-    -- destroy
+    -- immune
     local e2 = Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id, 0))
-    e2:SetCategory(CATEGORY_DESTROY)
-    e2:SetType(EFFECT_TYPE_IGNITION)
-    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e2:SetCode(EFFECT_CANNOT_RELEASE)
     e2:SetRange(LOCATION_MZONE)
-    e2:SetCountLimit(1)
-    e2:SetCost(s.e2cost)
-    e2:SetTarget(s.e2tg)
-    e2:SetOperation(s.e2op)
+    e2:SetTargetRange(0, 1)
+    e2:SetTarget(function(e, tc) return tc == e:GetHandler() end)
     c:RegisterEffect(e2)
-end
-
-function s.e1filter(c)
-    return c:IsLevel(1) and c:IsAttribute(ATTRIBUTE_LIGHT) and
-               c:IsType(TYPE_TUNER) and not c:IsPublic()
-end
-
-function s.e1con(e, c)
-    if c == nil then return true end
-    local tp = c:GetControler()
-    local g = Duel.GetMatchingGroup(s.e1filter, tp, LOCATION_HAND, 0, nil)
-    return #g > 0 and aux.SelectUnselectGroup(g, e, tp, 1, 1, aux.ChkfMMZ(1), 0)
-end
-
-function s.e1tg(e, tp, eg, ep, ev, re, r, rp, c)
-    local g = Duel.GetMatchingGroup(s.e1filter, tp, LOCATION_HAND, 0, nil)
-    local sg = aux.SelectUnselectGroup(g, e, tp, 1, 1, aux.ChkfMMZ(1), 1, tp,
-                                       HINTMSG_CONFIRM, nil, nil, true)
-    if #sg > 0 then
-        sg:KeepAlive()
-        e:SetLabelObject(sg)
-        return true
-    end
-    return false
-end
-
-function s.e1op(e, tp, eg, ep, ev, re, r, rp, c)
-    local g = e:GetLabelObject()
-    if not g then return end
-
-    Duel.ConfirmCards(1 - tp, g)
-    Duel.ShuffleHand(tp)
-    g:DeleteGroup()
-end
-
-function s.e2cost(e, tp, eg, ep, ev, re, r, rp, chk)
-    local c = e:GetHandler()
-    if chk == 0 then return c:GetAttackAnnouncedCount() == 0 end
-
-    local ec1 = Effect.CreateEffect(c)
-    ec1:SetDescription(3206)
-    ec1:SetType(EFFECT_TYPE_SINGLE)
-    ec1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_OATH +
-                        EFFECT_FLAG_CLIENT_HINT)
-    ec1:SetCode(EFFECT_CANNOT_ATTACK)
-    ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
-    e:GetHandler():RegisterEffect(ec1)
-end
-
-function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
-    if chk == 0 then
-        return Duel.IsExistingTarget(aux.TRUE, tp, 0, LOCATION_MZONE, 1, nil)
-    end
-
-    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_DESTROY)
-    local g = Duel.SelectTarget(tp, aux.TRUE, tp, 0, LOCATION_MZONE, 1, 1, nil)
-
-    Duel.SetOperationInfo(0, CATEGORY_DESTROY, g, 1, 0, 0)
-end
-
-function s.e2op(e, tp, eg, ep, ev, re, r, rp)
-    local tc = Duel.GetFirstTarget()
-    if not tc or not tc:IsRelateToEffect(e) then return end
-    Duel.Destroy(tc, REASON_EFFECT)
+    local e2b = Effect.CreateEffect(c)
+    e2b:SetType(EFFECT_TYPE_SINGLE)
+    e2b:SetCode(EFFECT_CANNOT_BE_MATERIAL)
+    e2b:SetValue(function(e, tc)
+        if not tc then return false end
+        return tc:GetControler() ~= e:GetHandlerPlayer()
+    end)
+    c:RegisterEffect(e2b)
+    local e2c = Effect.CreateEffect(c)
+    e2c:SetType(EFFECT_TYPE_SINGLE)
+    e2c:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e2c:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+    e2c:SetRange(LOCATION_MZONE)
+    e2c:SetValue(aux.tgoval)
+    c:RegisterEffect(e2c)
+    local e2d = e2c:Clone()
+    e2d:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+    e2d:SetValue(function(e, re, tp) return tp ~= e:GetHandlerPlayer() end)
+    c:RegisterEffect(e2d)
 end
