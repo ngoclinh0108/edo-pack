17485849678af774b68e7a9afa56dc10e9a0e1fa
Luc Ta <luc.ta@cptech.vn>
2021-09-13 02:07:36 +0700

    update

--- script/c911000001.lua
+++ script/c911000001.lua
@@ -50,4 +50,124 @@ function s.initial_effect(c)
     e1d:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
     e1d:SetValue(function(e, re, tp) return tp ~= e:GetHandlerPlayer() end)
     c:RegisterEffect(e1d)
+
+    -- act limit
+    local e2 = Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e2:SetCode(EFFECT_CANNOT_ACTIVATE)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetTargetRange(0, 1)
+    e2:SetCondition(function(e) return Duel.GetAttacker() == e:GetHandler() end)
+    e2:SetValue(1)
+    c:RegisterEffect(e2)
+
+    -- multi attack
+    local e3 = Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id, 1))
+    e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e3:SetCode(EVENT_DAMAGE_STEP_END)
+    e3:SetCountLimit(1)
+    e3:SetCondition(s.e3con)
+    e3:SetCost(s.e3cost)
+    e3:SetOperation(s.e3op)
+    c:RegisterEffect(e3)
+    aux.GlobalCheck(s, function()
+        local e3check = Effect.CreateEffect(c)
+        e3check:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
+        e3check:SetCode(EVENT_ATTACK_ANNOUNCE)
+        e3check:SetOperation(s.e3checkop)
+        Duel.RegisterEffect(e3check, 0)
+
+        s[0] = 0
+        s[1] = 0
+        aux.AddValuesReset(function()
+            s[0] = 0
+            s[1] = 0
+        end)
+    end)
+
+    -- destroy
+    local e4 = Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id, 2))
+    e4:SetCategory(CATEGORY_DESTROY)
+    e4:SetType(EFFECT_TYPE_IGNITION)
+    e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCountLimit(1)
+    e4:SetCost(s.e4cost)
+    e4:SetTarget(s.e4tg)
+    e4:SetOperation(s.e4op)
+    c:RegisterEffect(e4)
+end
+
+function s.e3checkop(e, tp, eg, ep, ev, re, r, rp)
+    local tc = eg:GetFirst()
+    if tc:GetFlagEffect(id) > 0 then return end
+    s[ep] = s[ep] + 1
+    tc:RegisterFlagEffect(id, RESET_EVENT + RESETS_STANDARD + RESET_PHASE +
+                              PHASE_END, 0, 1)
+end
+
+function s.e3con(e, tp, eg, ep, ev, re, r, rp)
+    return Duel.GetAttacker() == e:GetHandler()
+end
+
+function s.e3cost(e, tp, eg, ep, ev, re, r, rp, chk)
+    local c = e:GetHandler()
+    if chk == 0 then return s[tp] < 2 end
+
+    local ec1 = Effect.CreateEffect(c)
+    ec1:SetType(EFFECT_TYPE_FIELD)
+    ec1:SetProperty(EFFECT_FLAG_OATH)
+    ec1:SetCode(EFFECT_CANNOT_ATTACK)
+    ec1:SetTargetRange(LOCATION_MZONE, 0)
+    ec1:SetTarget(function(e, c) return e:GetLabel() ~= c:GetFieldID() end)
+    ec1:SetLabel(c:GetFieldID())
+    ec1:SetReset(RESET_PHASE + PHASE_END)
+    Duel.RegisterEffect(ec1, tp)
+end
+
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    if not c:IsRelateToEffect(e) then return end
+
+    local ec1 = Effect.CreateEffect(c)
+    ec1:SetDescription(aux.Stringid(id, 0))
+    ec1:SetType(EFFECT_TYPE_SINGLE)
+    ec1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_CLIENT_HINT)
+    ec1:SetCode(EFFECT_EXTRA_ATTACK)
+    ec1:SetValue(2)
+    ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
+    c:RegisterEffect(ec1)
+end
+
+function s.e4cost(e, tp, eg, ep, ev, re, r, rp, chk)
+    local c = e:GetHandler()
+    if chk == 0 then return c:GetAttackAnnouncedCount() == 0 end
+
+    local ec1 = Effect.CreateEffect(c)
+    ec1:SetDescription(3206)
+    ec1:SetType(EFFECT_TYPE_SINGLE)
+    ec1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_OATH +
+                        EFFECT_FLAG_CLIENT_HINT)
+    ec1:SetCode(EFFECT_CANNOT_ATTACK)
+    ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
+    c:RegisterEffect(ec1)
+end
+
+function s.e4tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    if chk == 0 then
+        return Duel.IsExistingTarget(aux.TRUE, tp, 0, LOCATION_ONFIELD, 1, nil)
+    end
+
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_DESTROY)
+    local g =
+        Duel.SelectTarget(tp, aux.TRUE, tp, 0, LOCATION_ONFIELD, 1, 3, nil)
+    Duel.SetOperationInfo(0, CATEGORY_DESTROY, g, #g, 0, 0)
+end
+
+function s.e4op(e, tp, eg, ep, ev, re, r, rp)
+    local g = Duel.GetTargetCards(e):Filter(Card.IsRelateToEffect, nil, e)
+    if #g > 0 then Duel.Destroy(g, REASON_EFFECT) end
 end
