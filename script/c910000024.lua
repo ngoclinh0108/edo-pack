86a0c05879fd3d40f0b48f10e23a36a4bbb7a0ef
Luc Ta <luc.ta@cptech.vn>
2021-09-12 23:45:56 +0700

    update

--- script/c910000024.lua
+++ script/c910000024.lua
@@ -2,6 +2,8 @@
 Duel.LoadScript("util.lua")
 local s, id = GetID()
 
+s.listed_series = {0x13a}
+
 function s.initial_effect(c)
     c:EnableReviveLimit()
 
@@ -41,6 +43,16 @@ function s.initial_effect(c)
         return c:IsAttackBelow(e:GetHandler():GetAttack())
     end)
     c:RegisterEffect(e2)
+
+    -- special summon
+    local e3 = Effect.CreateEffect(c)
+    e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e3:SetProperty(EFFECT_FLAG_DELAY)
+    e3:SetCode(EVENT_DESTROYED)
+    e3:SetTarget(s.e3tg)
+    e3:SetOperation(s.e3op)
+    c:RegisterEffect(e3)
 end
 
 function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
@@ -61,4 +73,29 @@ function s.e1op(e, tp, eg, ep, ev, re, r, rp)
     if not tc or not tc:IsRelateToEffect(e) then return end
 
     Duel.Destroy(tc, REASON_EFFECT)
+end
+
+function s.e3filter(c, e, tp)
+    return c:IsMonster() and c:IsSetCard(0x13a) and
+               c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
+end
+
+function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    local loc = LOCATION_HAND + LOCATION_DECK + LOCATION_GRAVE
+    if chk == 0 then
+        return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
+                   Duel.IsExistingMatchingCard(s.e3filter, tp, loc, 0, 1, nil,
+                                               e, tp)
+    end
+
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp, loc)
+end
+
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+    if Duel.GetLocationCount(tp, LOCATION_MZONE) == 0 then return end
+
+    local g = Utility.SelectMatchingCard(tp, aux.NecroValleyFilter(s.e3filter),
+                                         tp, LOCATION_HAND + LOCATION_DECK +
+                                             LOCATION_GRAVE, 0, 1, 1, nil, e, tp)
+    if #g > 0 then Duel.SpecialSummon(g, 0, tp, tp, true, false, POS_FACEUP) end
 end
