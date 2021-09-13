be91177d2ffd6050561dd4aae31fb53c61024399
Luc Ta <luc.ta@cptech.vn>
2021-09-13 13:46:00 +0700

    fix

--- script/util_divine.lua
+++ script/util_divine.lua
@@ -111,7 +111,6 @@ function Divine.DivineHierarchy(s, c, divine_hierarchy,
     noleave:SetTarget(function(e, tp, eg, ep, ev, re, r, rp, chk)
         local c = e:GetHandler()
         if chk == 0 then
-            Debug.Message(c:IsReason(REASON_EFFECT))
             return
                 c:IsReason(REASON_EFFECT) and r & REASON_EFFECT ~= 0 and re and
                     re:IsActiveType(TYPE_SPELL + TYPE_TRAP) and
