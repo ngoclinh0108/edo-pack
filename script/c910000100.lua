fc5b473b473fb261ccda433c489047232c641f9b
Luc Ta <luc.ta@cptech.vn>
2021-09-13 11:43:48 +0700

    update

--- script/c910000100.lua
+++ script/c910000100.lua
@@ -9,6 +9,8 @@ function s.initial_effect(c)
     local e1 = Effect.CreateEffect(c)
     e1:SetCategory(CATEGORY_TOHAND + CATEGORY_SEARCH + CATEGORY_DECKDES)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_CANNOT_INACTIVATE +
+                       EFFECT_FLAG_CANNOT_NEGATE)
     e1:SetCode(EVENT_FREE_CHAIN)
     e1:SetCountLimit(1, id, EFFECT_COUNT_CODE_OATH)
     e1:SetTarget(s.e1tg)
