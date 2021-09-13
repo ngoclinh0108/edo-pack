149ef16ec4677a8710ee29fb19c9aaffe5c0579a
Luc Ta <luc.ta@cptech.vn>
2021-09-13 07:56:07 +0700

    update

--- script/c910000102.lua
+++ script/c910000102.lua
@@ -2,8 +2,6 @@
 Duel.LoadScript("util.lua")
 local s, id = GetID()
 
-s.listed_names = {410000000}
-
 function s.initial_effect(c)
     -- activate
     local e1 = Effect.CreateEffect(c)
@@ -20,42 +18,20 @@ function s.initial_effect(c)
     e2:SetType(EFFECT_TYPE_SINGLE)
     e2:SetCode(EFFECT_QP_ACT_IN_NTPHAND)
     e2:SetCondition(function(e)
-        return
-            Duel.GetFieldGroupCount(e:GetHandlerPlayer(), LOCATION_MZONE, 0) ==
-                0
+        local tp = e:GetHandlerPlayer()
+        return Duel.GetFieldGroupCount(tp, LOCATION_MZONE, 0) == 0
     end)
     c:RegisterEffect(e2)
 
-    -- add to hand
-    local e3 = Effect.CreateEffect(c)
-    e3:SetDescription(573)
-    e3:SetCategory(CATEGORY_TOHAND)
-    e3:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
-    e3:SetCode(EVENT_PHASE + PHASE_STANDBY)
-    e3:SetRange(LOCATION_DECK)
-    e3:SetCountLimit(1, id)
-    e3:SetCondition(function(e, tp) return Duel.IsEnvironment(410000000, tp) end)
-    e3:SetTarget(function(e, tp, eg, ep, ev, re, r, rp, chk)
-        if chk == 0 then return e:GetHandler():IsAbleToHand() end
-        Duel.SetOperationInfo(0, CATEGORY_TOHAND, e:GetHandler(), 1, 0, 0)
-    end)
-    e3:SetOperation(function(e, tp, eg, ep, ev, re, r, rp)
-        local c = e:GetHandler()
-        if not c:IsRelateToEffect(e) then return end
-        Duel.SendtoHand(c, nil, REASON_EFFECT)
-        Duel.ConfirmCards(1 - tp, c)
-    end)
-    c:RegisterEffect(e3)
-
     -- salvage
-    local e4 = Effect.CreateEffect(c)
-    e4:SetCategory(CATEGORY_TOHAND)
-    e4:SetType(EFFECT_TYPE_IGNITION)
-    e4:SetRange(LOCATION_GRAVE)
-    e4:SetCondition(aux.exccon)
-    e4:SetTarget(s.e4tg)
-    e4:SetOperation(s.e4op)
-    c:RegisterEffect(e4)
+    local e3 = Effect.CreateEffect(c)
+    e3:SetCategory(CATEGORY_TOHAND)
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetRange(LOCATION_GRAVE)
+    e3:SetCondition(aux.exccon)
+    e3:SetTarget(s.e3tg)
+    e3:SetOperation(s.e3op)
+    c:RegisterEffect(e3)
 end
 
 function s.e1filter(c)
@@ -102,9 +78,9 @@ end
 
 function s.e1discon(e, tp, eg, ep, ev, re, r, rp)
     local code = e:GetLabel()
-    local code1, code4 = re:GetHandler():GetOriginalCodeRule()
+    local code1, code3 = re:GetHandler():GetOriginalCodeRule()
     return rp ~= tp and re:IsActiveType(TYPE_MONSTER) and
-               (code1 == code or code4 == code)
+               (code1 == code or code3 == code)
 end
 
 function s.e1disop(e, tp, eg, ep, ev, re, r, rp)
@@ -114,35 +90,32 @@ end
 
 function s.e1distg(e, c)
     local code = e:GetLabel()
-    local code1, code4 = c:GetOriginalCodeRule()
-    return code1 == code or code4 == code
+    local code1, code3 = c:GetOriginalCodeRule()
+    return code1 == code or code3 == code
 end
 
-function s.e4filter(c) return c:IsFaceup() and c:IsAbleToHand() end
+function s.e3filter(c) return c:IsFaceup() and c:IsAbleToHand() end
 
-function s.e4tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     local c = e:GetHandler()
     if chk == 0 then
         return c:IsAbleToDeck() and
-                   Duel.IsExistingMatchingCard(s.e4filter, tp, LOCATION_REMOVED,
+                   Duel.IsExistingMatchingCard(s.e3filter, tp, LOCATION_REMOVED,
                                                0, 1, nil)
     end
 
     Duel.SetOperationInfo(0, CATEGORY_TODECK, nil, 1, tp, LOCATION_MZONE)
+    Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp, LOCATION_REMOVED)
 end
 
-function s.e4op(e, tp, eg, ep, ev, re, r, rp)
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
-    local g = Duel.GetMatchingGroup(s.e4filter, tp, LOCATION_REMOVED, 0, nil)
+    local g = Duel.GetMatchingGroup(s.e3filter, tp, LOCATION_REMOVED, 0, nil)
 
     if #g > 0 and Duel.SendtoDeck(c, nil, SEQ_DECKSHUFFLE, REASON_EFFECT) > 0 then
         Duel.BreakEffect()
 
-        if #g > 1 then
-            Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_ATOHAND)
-            g = g:Select(tp, 1, 1, nil)
-        end
-
+        g = Utility.GroupSelect(g, tp, 1, 1, nil)
         Duel.SendtoHand(g, nil, REASON_EFFECT)
         Duel.ConfirmCards(1 - tp, g)
     end
