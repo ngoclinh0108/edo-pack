244569193a13b66ad53a110242cdcb19204833e9
Luc Ta <luc.ta@cptech.vn>
2021-09-14 09:21:37 +0700

    update

--- script/c910000105.lua
+++ script/c910000105.lua
@@ -3,71 +3,76 @@ Duel.LoadScript("util.lua")
 local s, id = GetID()
 
 function s.initial_effect(c)
-    -- activate
+    c:SetUniqueOnField(1, 0, id)
+    c:EnableCounterPermit(COUNTER_SPELL)
+    c:SetCounterLimit(COUNTER_SPELL, 3)
+
+    -- activate & remain field
     local e1 = Effect.CreateEffect(c)
     e1:SetCategory(CATEGORY_POSITION)
     e1:SetType(EFFECT_TYPE_ACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetHintTiming(TIMINGS_CHECK_MONSTER + TIMING_MAIN_END)
+    e1:SetCondition(s.e1con)
     e1:SetTarget(s.e1tg)
     e1:SetOperation(s.e1op)
     c:RegisterEffect(e1)
+    local e1b = Effect.CreateEffect(c)
+    e1b:SetType(EFFECT_TYPE_SINGLE)
+    e1b:SetCode(EFFECT_REMAIN_FIELD)
+    c:RegisterEffect(e1b)
 
-    -- cannot attack
+    -- add counter
     local e2 = Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+    e2:SetCategory(CATEGORY_COUNTER)
+    e2:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
     e2:SetRange(LOCATION_SZONE)
-    e2:SetTargetRange(0, LOCATION_MZONE)
+    e2:SetCountLimit(1)
+    e2:SetCode(EVENT_PHASE + PHASE_END)
+    e2:SetCondition(function(e, tp) return Duel.GetTurnPlayer() == 1 - tp end)
+    e2:SetOperation(function(e) e:GetHandler():AddCounter(COUNTER_SPELL, 1) end)
     c:RegisterEffect(e2)
 
-    -- remain field
+    -- cannot attack
     local e3 = Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_SINGLE)
-    e3:SetCode(EFFECT_REMAIN_FIELD)
+    e3:SetType(EFFECT_TYPE_FIELD)
+    e3:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
+    e3:SetRange(LOCATION_SZONE)
+    e3:SetTargetRange(0, LOCATION_MZONE)
+    e3:SetCondition(function(e)
+        return e:GetHandler():GetCounter(COUNTER_SPELL) <= 2
+    end)
     c:RegisterEffect(e3)
+
+    -- draw
+    local e4 = Effect.CreateEffect(c)
+    e4:SetDescription(1108)
+    e4:SetCategory(CATEGORY_DRAW)
+    e4:SetType(EFFECT_TYPE_IGNITION)
+    e4:SetRange(LOCATION_SZONE)
+    e4:SetCondition(s.e4con)
+    e4:SetCost(s.e4cost)
+    e4:SetTarget(s.e4tg)
+    e4:SetOperation(s.e4op)
+    c:RegisterEffect(e4)
+end
+
+function s.e1con(e, tp, eg, ep, ev, re, r, rp)
+    return Duel.GetCurrentPhase() == PHASE_MAIN1
 end
 
 function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
-    local c = e:GetHandler()
-    if chk == 0 then return true end
-
-    c:SetTurnCounter(0)
-    local e1 = Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
-    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
-    e1:SetCode(EVENT_PHASE + PHASE_END)
-    e1:SetRange(LOCATION_SZONE)
-    e1:SetCountLimit(1)
-    e1:SetCondition(function(e, tp) return tp ~= Duel.GetTurnPlayer() end)
-    e1:SetOperation(s.e1desop)
-    e1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END +
-                    RESET_OPPO_TURN, 3)
-    c:RegisterEffect(e1)
-
-    local e2 = Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_SINGLE)
-    e2:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE +
-                       EFFECT_FLAG_IGNORE_IMMUNE + EFFECT_FLAG_SET_AVAILABLE)
-    e2:SetCode(1082946)
-    e2:SetLabelObject(e1)
-    e2:SetOwnerPlayer(tp)
-    e2:SetOperation(function(e, tp, eg, ep, ev, re, r, rp)
-        s.e1desop(e:GetLabelObject(), tp, eg, ep, ev, e, r, rp)
-    end)
-    e2:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END +
-                    RESET_OPPO_TURN, 3)
-    c:RegisterEffect(e2)
-
+    if chk == 0 then return e:IsHasType(EFFECT_TYPE_ACTIVATE) end
     local g = Duel.GetMatchingGroup(Card.IsFacedown, tp, 0, LOCATION_MZONE, nil)
     Duel.SetOperationInfo(0, CATEGORY_POSITION, g, #g, 0, 0)
 end
 
 function s.e1op(e, tp, eg, ep, ev, re, r, rp)
     local g = Duel.GetMatchingGroup(Card.IsFacedown, tp, 0, LOCATION_MZONE, nil)
-    if #g > 0 then
-        Duel.ChangePosition(sg, POS_FACEUP_ATTACK, POS_FACEUP_ATTACK,
-                            POS_FACEUP_DEFENSE, POS_FACEUP_DEFENSE)
-    end
+    if #g == 0 then return end
+
+    Duel.ChangePosition(g, POS_FACEUP_ATTACK, POS_FACEUP_ATTACK,
+                        POS_FACEUP_DEFENSE, POS_FACEUP_DEFENSE, true)
 end
 
 function s.e1desop(e, tp, eg, ep, ev, re, r, rp)
@@ -80,4 +85,26 @@ function s.e1desop(e, tp, eg, ep, ev, re, r, rp)
         Duel.Destroy(c, REASON_RULE)
         if re then re:Reset() end
     end
+end
+
+function s.e4con(e, tp, eg, ep, ev, re, r, rp)
+    return e:GetHandler():GetCounter(COUNTER_SPELL) >= 3
+end
+
+function s.e4cost(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return e:GetHandler():IsAbleToGraveAsCost() end
+    Duel.SendtoGrave(e:GetHandler(), REASON_COST)
+end
+
+function s.d4tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return Duel.IsPlayerCanDraw(tp, 3) end
+    Duel.SetTargetPlayer(tp)
+    Duel.SetTargetParam(3)
+    Duel.SetOperationInfo(0, CATEGORY_DRAW, nil, 0, tp, 3)
+end
+
+function s.e4op(e, tp, eg, ep, ev, re, r, rp)
+    local p, d = Duel.GetChainInfo(0, CHAININFO_TARGET_PLAYER,
+                                   CHAININFO_TARGET_PARAM)
+    Duel.Draw(p, d, REASON_EFFECT)
 end
