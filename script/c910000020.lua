4f4509fd07fdc89a3b653214696672401512d800
Luc Ta <luc.ta@cptech.vn>
2021-09-15 23:02:42 +0700

    update

--- script/c910000020.lua
+++ script/c910000020.lua
@@ -43,7 +43,7 @@ function s.initial_effect(c)
     e2:SetCode(EFFECT_QP_ACT_IN_NTPHAND)
     e2:SetRange(LOCATION_MZONE)
     e2:SetTargetRange(LOCATION_HAND, 0)
-    e2:SetCountLimit(1, id)
+    e2:SetCountLimit(1)
     e2:SetCondition(function(e)
         return Duel.GetTurnPlayer() ~= e:GetHandlerPlayer()
     end)
@@ -51,6 +51,29 @@ function s.initial_effect(c)
     local e2b = e2:Clone()
     e2b:SetCode(EFFECT_TRAP_ACT_IN_HAND)
     c:RegisterEffect(e2b)
+
+    -- atk
+    local e3 = Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id, 0))
+    e3:SetCategory(CATEGORY_ATKCHANGE)
+    e3:SetType(EFFECT_TYPE_QUICK_O)
+    e3:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCondition(s.e3con)
+    e3:SetCost(s.e3cost)
+    e3:SetOperation(s.e3op)
+    c:RegisterEffect(e3, false, REGISTER_FLAG_DETACH_XMAT)
+
+    -- special summon (destroyed)
+    local e4 = Effect.CreateEffect(c)
+    e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e4:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e4:SetProperty(EFFECT_FLAG_DELAY)
+    e4:SetCode(EVENT_DESTROYED)
+    e4:SetCondition(s.e4con)
+    e4:SetTarget(s.e4tg)
+    e4:SetOperation(s.e4op)
+    c:RegisterEffect(e4)
 end
 
 function s.spfilter(c)
@@ -93,4 +116,65 @@ function s.spop(e, tp, eg, ep, ev, re, r, rp, c)
 
     Duel.Overlay(c, mc)
     mg:DeleteGroup()
+end
+
+function s.e3con(e, tp, eg, ep, ev, re, r, rp)
+    return e:GetHandler():GetBattleTarget() ~= nil
+end
+
+function s.e3cost(e, tp, eg, ep, ev, re, r, rp, chk)
+    local c = e:GetHandler()
+    if chk == 0 then
+        return c:CheckRemoveOverlayCard(tp, 1, REASON_COST) and
+                   c:GetFlagEffect(id) == 0
+    end
+
+    c:RemoveOverlayCard(tp, 1, 1, REASON_COST)
+    c:RegisterFlagEffect(id, RESET_EVENT + RESETS_STANDARD + RESET_PHASE +
+                             PHASE_DAMAGE_CAL, 0, 1)
+end
+
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
+
+    local ec1 = Effect.CreateEffect(c)
+    ec1:SetType(EFFECT_TYPE_SINGLE)
+    ec1:SetCode(EFFECT_SET_ATTACK_FINAL)
+    ec1:SetReset(RESET_PHASE + PHASE_DAMAGE_CAL)
+    ec1:SetValue(function(e, c) return e:GetHandler():GetAttack() * 2 end)
+    c:RegisterEffect(ec1)
+end
+
+function s.e4filter(c, e, tp)
+    return c:IsCode(71703785) and
+               c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
+end
+
+function s.e4con(e, tp, eg, ep, ev, re, r, rp)
+    return (r & REASON_EFFECT + REASON_BATTLE) ~= 0
+end
+
+function s.e4tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then
+        return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
+                   Duel.IsExistingMatchingCard(s.e4filter, tp, LOCATION_HAND +
+                                                   LOCATION_DECK +
+                                                   LOCATION_GRAVE, 0, 1, nil, e,
+                                               tp)
+    end
+
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp,
+                          LOCATION_HAND + LOCATION_DECK + LOCATION_GRAVE)
+end
+
+function s.e4op(e, tp, eg, ep, ev, re, r, rp)
+    if Duel.GetLocationCount(tp, LOCATION_MZONE) <= 0 then return end
+
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
+    local g = Duel.SelectMatchingCard(tp, aux.NecroValleyFilter(s.e4filter), tp,
+                                      LOCATION_HAND + LOCATION_DECK +
+                                          LOCATION_GRAVE, 0, 1, 1, nil, e, tp)
+
+    if #g > 0 then Duel.SpecialSummon(g, 0, tp, tp, false, false, POS_FACEUP) end
 end
