a5ae8239377b0add44d0e1ae1cad392fdbb38015
Luc Ta <luc.ta@cptech.vn>
2021-09-17 04:21:01 +0700

    update

--- script/c910000014.lua
+++ script/c910000014.lua
@@ -11,9 +11,20 @@ function s.initial_effect(c)
     e1:SetProperty(EFFECT_FLAG_UNCOPYABLE)
     e1:SetCode(EFFECT_SPSUMMON_PROC)
     e1:SetRange(LOCATION_HAND)
-    e1:SetCountLimit(1, id, EFFECT_COUNT_CODE_OATH)
+    e1:SetCountLimit(1, {id, 1}, EFFECT_COUNT_CODE_OATH)
     e1:SetCondition(s.e1con)
     c:RegisterEffect(e1)
+
+    -- to hand
+    local e2 = Effect.CreateEffect(c)
+    e2:SetCategory(CATEGORY_TOHAND)
+    e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET + EFFECT_FLAG_DELAY)
+    e2:SetCode(EVENT_TO_GRAVE)
+    e2:SetCountLimit(1, {id, 2})
+    e2:SetTarget(s.e2tg)
+    e2:SetOperation(s.e2op)
+    c:RegisterEffect(e2)
 
     -- extra material
     local e3 = Effect.CreateEffect(c)
@@ -40,4 +51,28 @@ function s.e1con(e, c)
                Duel.IsExistingMatchingCard(
                    aux.FilterFaceupFunction(Card.IsAttackAbove, 2000), tp, 0,
                    LOCATION_MZONE, 1, nil)
+end
+
+function s.e2filter(c)
+    return c:IsSetCard(0x13a) and c:IsAbleToHand() and not c:IsCode(id)
+end
+
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    if chk == 0 then
+        return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
+                   Duel.IsExistingTarget(s.e1filter, tp, LOCATION_GRAVE, 0, 1,
+                                         nil)
+    end
+
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_RTOHAND)
+    local g =
+        Duel.SelectTarget(tp, s.e1filter, tp, LOCATION_GRAVE, 0, 1, 1, nil)
+
+    Duel.SetOperationInfo(0, CATEGORY_TOHAND, g, #g, 0, 0)
+end
+
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+    local tc = Duel.GetFirstTarget()
+    if not tc or not tc:IsRelateToEffect(e) then return end
+    Duel.SendtoHand(tc, nil, REASON_EFFECT)
 end
