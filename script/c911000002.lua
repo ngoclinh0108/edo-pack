201f3a0d36fc62eaff3d6762930679eb6b988f86
Luc Ta <luc.ta@cptech.vn>
2021-09-16 16:25:16 +0700

    update

--- script/c911000002.lua
+++ script/c911000002.lua
@@ -9,8 +9,28 @@ function s.initial_effect(c)
     c:EnableReviveLimit()
 
     -- synchro summon
-    Synchro.AddProcedure(c, nil, 1, 1, Synchro.NonTunerEx(Card.IsSetCard, 0xdd),
+    Synchro.AddProcedure(c, nil, 1, 1,
+                         aux.NOT(Synchro.NonTunerEx(Card.IsType, TYPE_EFFECT)),
                          1, 1)
+
+    -- special summon
+    local sp = Effect.CreateEffect(c)
+    sp:SetType(EFFECT_TYPE_FIELD)
+    sp:SetProperty(EFFECT_FLAG_UNCOPYABLE)
+    sp:SetCode(EFFECT_SPSUMMON_PROC)
+    sp:SetRange(LOCATION_GRAVE)
+    sp:SetCountLimit(1, id, EFFECT_COUNT_CODE_OATH)
+    sp:SetCondition(s.spcon)
+    sp:SetTarget(s.sptg)
+    sp:SetOperation(s.spop)
+    c:RegisterEffect(sp)
+
+    -- indes
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_CONTINUOUS)
+    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetOperation(s.e1op)
+    c:RegisterEffect(e1)
 
     -- atk up
     local e2 = Effect.CreateEffect(c)
@@ -52,25 +72,67 @@ function s.initial_effect(c)
     e4:SetType(EFFECT_TYPE_IGNITION)
     e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
     e4:SetRange(LOCATION_MZONE)
-    e4:SetCondition(function(e, tp, eg, ep, ev, re, r, rp)
-        return not e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO) or
-                   not e:GetHandler():GetMaterial()
-                       :IsExists(Card.IsCode, 1, nil, CARD_BLUEEYES_W_DRAGON)
-    end)
     e4:SetCost(s.e4cost)
     e4:SetTarget(s.e4tg)
     e4:SetOperation(s.e4op)
     c:RegisterEffect(e4)
-    local e4b = e4:Clone()
-    e4b:SetType(EFFECT_TYPE_QUICK_O)
-    e4b:SetProperty(EFFECT_FLAG_CARD_TARGET + EFFECT_FLAG_DAMAGE_STEP)
-    e4b:SetCode(EVENT_FREE_CHAIN)
-    e4b:SetCondition(function(e, tp, eg, ep, ev, re, r, rp)
-        return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO) and
-                   e:GetHandler():GetMaterial():IsExists(Card.IsCode, 1, nil,
-                                                         CARD_BLUEEYES_W_DRAGON)
-    end)
-    c:RegisterEffect(e4b)
+end
+
+function s.spfilter(c)
+    return c:IsFaceup() and c:IsCode(CARD_BLUEEYES_W_DRAGON) and
+               c:IsReleasable()
+end
+
+function s.spcon(e, c)
+    if not aux.exccon(e) then return false end
+    if c == nil then return true end
+    local tp = c:GetControler()
+
+    local eff = {c:GetCardEffect(EFFECT_NECRO_VALLEY)}
+    for _, te in ipairs(eff) do
+        local op = te:GetOperation()
+        if not op or op(e, c) then return false end
+    end
+
+    local g = Duel.GetMatchingGroup(s.spfilter, tp, LOCATION_MZONE, 0, nil)
+    return aux.SelectUnselectGroup(g, e, tp, 1, 1, aux.ChkfMMZ(1), 0)
+end
+
+function s.sptg(e, tp, eg, ep, ev, re, r, rp, c)
+    local g = Duel.GetMatchingGroup(s.spfilter, tp, LOCATION_MZONE, 0, nil)
+    g = aux.SelectUnselectGroup(g, e, tp, 1, 1, aux.ChkfMMZ(1), 1, tp,
+                                HINTMSG_RELEASE, nil, nil, true)
+    if #g > 0 then
+        g:KeepAlive()
+        e:SetLabelObject(g)
+        return true
+    end
+    return false
+end
+
+function s.spop(e, tp, eg, ep, ev, re, r, rp, c)
+    local g = e:GetLabelObject()
+    if not g then return end
+
+    Duel.Release(g, REASON_COST)
+    g:DeleteGroup()
+end
+
+function s.e1op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    local g = Duel.GetMatchingGroup(aux.FilterFaceupFunction(Card.IsRace,
+                                                             RACE_DRAGON), tp,
+                                    LOCATION_MZONE, 0, nil)
+    for tc in aux.Next(g) do
+        local ec1 = Effect.CreateEffect(c)
+        ec1:SetDescription(3009)
+        ec1:SetType(EFFECT_TYPE_SINGLE)
+        ec1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_CLIENT_HINT)
+        ec1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+        ec1:SetValue(1)
+        ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END, 2)
+        tc:RegisterEffect(ec1)
+    end
 end
 
 function s.e3filter(c, tp)
