2910687dbf7a8e115331d5888ef99072be1176ab
Luc Ta <luc.ta@cptech.vn>
2021-09-17 04:04:44 +0700

    update

--- script/c910000010.lua
+++ script/c910000010.lua
@@ -5,47 +5,43 @@ local s, id = GetID()
 s.listed_series = {0x13a}
 
 function s.initial_effect(c)
-    -- disable
+    -- double tribute
     local e1 = Effect.CreateEffect(c)
-    e1:SetDescription(1117)
-    e1:SetCategory(CATEGORY_DISABLE)
-    e1:SetType(EFFECT_TYPE_QUICK_O)
-    e1:SetCode(EVENT_CHAINING)
-    e1:SetRange(LOCATION_HAND + LOCATION_MZONE)
-    e1:SetCountLimit(1, {id, 1})
-    e1:SetCondition(s.e1con)
-    e1:SetCost(s.e1cost)
-    e1:SetTarget(s.e1tg)
-    e1:SetOperation(s.e1op)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_CANNOT_NEGATE)
+    e1:SetCode(EFFECT_DOUBLE_TRIBUTE)
+    e1:SetValue(function(e, c) return c:IsSetCard(0x13a) end)
     c:RegisterEffect(e1)
 
-    -- summon
+    -- disable
     local e2 = Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id, 0))
-    e2:SetCategory(CATEGORY_SUMMON)
-    e2:SetType(EFFECT_TYPE_IGNITION)
-    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetCountLimit(1, {id, 2})
+    e2:SetDescription(1117)
+    e2:SetCategory(CATEGORY_DISABLE)
+    e2:SetType(EFFECT_TYPE_QUICK_O)
+    e2:SetCode(EVENT_CHAINING)
+    e2:SetRange(LOCATION_HAND + LOCATION_MZONE)
+    e2:SetCountLimit(1, {id, 1})
+    e2:SetCondition(s.e2con)
     e2:SetCost(s.e2cost)
     e2:SetTarget(s.e2tg)
     e2:SetOperation(s.e2op)
     c:RegisterEffect(e2)
 
-    -- to hand
+    -- summon
     local e3 = Effect.CreateEffect(c)
-    e3:SetCategory(CATEGORY_TOHAND)
-    e3:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
-    e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP + EFFECT_FLAG_DELAY)
-    e3:SetCode(EVENT_CHAIN_NEGATED)
-    e3:SetRange(LOCATION_GRAVE)
-    e3:SetCondition(s.e3con)
+    e3:SetDescription(aux.Stringid(id, 0))
+    e3:SetCategory(CATEGORY_SUMMON)
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCountLimit(1, {id, 2})
+    e3:SetCost(s.e3cost)
     e3:SetTarget(s.e3tg)
     e3:SetOperation(s.e3op)
     c:RegisterEffect(e3)
 end
 
-function s.e1con(e, tp, eg, ep, ev, re, r, rp)
+function s.e2con(e, tp, eg, ep, ev, re, r, rp)
     local ch = Duel.GetCurrentChain(true) - 1
     if ch <= 0 then return false end
     local cp = Duel.GetChainInfo(ch, CHAININFO_TRIGGERING_CONTROLER)
@@ -59,21 +55,21 @@ function s.e1con(e, tp, eg, ep, ev, re, r, rp)
                cec:IsType(TYPE_MONSTER)
 end
 
-function s.e1cost(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e2cost(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then return e:GetHandler():IsReleasable() end
     Duel.Release(e:GetHandler(), REASON_COST)
 end
 
-function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then return not re:GetHandler():IsStatus(STATUS_DISABLED) end
     Duel.SetOperationInfo(0, CATEGORY_DISABLE, eg, 1, 0, 0)
 end
 
-function s.e1op(e, tp, eg, ep, ev, re, r, rp) Duel.NegateEffect(ev) end
+function s.e2op(e, tp, eg, ep, ev, re, r, rp) Duel.NegateEffect(ev) end
 
-function s.e2filter(c) return c:IsSetCard(0x13a) and c:IsSummonable(true, nil) end
+function s.e3filter(c) return c:IsSetCard(0x13a) and c:IsSummonable(true, nil) end
 
-function s.e2cost(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e3cost(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
     if chk == 0 then
         return c:IsPosition(POS_FACEUP_ATTACK) and c:IsCanChangePosition()
@@ -82,71 +78,32 @@ function s.e2cost(e, tp, eg, ep, ev, re, r, rp, chk)
     Duel.ChangePosition(c, POS_FACEUP_DEFENSE)
 end
 
-function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
-    local c = e:GetHandler()
+function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then
-        local ec1 = Effect.CreateEffect(c)
-        ec1:SetType(EFFECT_TYPE_SINGLE)
-        ec1:SetProperty(EFFECT_CANNOT_DISABLE)
-        ec1:SetCode(EFFECT_DOUBLE_TRIBUTE)
-        ec1:SetValue(function(e, c) return c:IsSetCard(0x13a) end)
-        c:RegisterEffect(ec1)
-
-        local res = Duel.IsExistingMatchingCard(s.e2filter, tp,
-                                                LOCATION_HAND + LOCATION_MZONE,
-                                                0, 1, nil)
-        ec1:Reset()
-        return res
+        return Duel.IsExistingMatchingCard(s.e3filter, tp,
+                                           LOCATION_HAND + LOCATION_MZONE, 0, 1,
+                                           nil)
     end
 
     Duel.SetOperationInfo(0, CATEGORY_SUMMON, nil, 1, 0, 0)
 end
 
-function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
 
-    local ec1 = Effect.CreateEffect(c)
-    ec1:SetDescription(aux.Stringid(id, 1))
-    ec1:SetType(EFFECT_TYPE_SINGLE)
-    ec1:SetProperty(EFFECT_CANNOT_DISABLE + EFFECT_FLAG_CLIENT_HINT)
-    ec1:SetCode(EFFECT_DOUBLE_TRIBUTE)
-    ec1:SetValue(function(e, c) return c:IsSetCard(0x13a) end)
-    c:RegisterEffect(ec1)
-
-    local ec2 = Effect.CreateEffect(c)
-    ec2:SetDescription(aux.Stringid(id, 2))
-    ec2:SetType(EFFECT_TYPE_FIELD)
-    ec2:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_CLIENT_HINT)
-    ec2:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
-    ec2:SetTargetRange(1, 0)
-    ec2:SetTarget(function(e, c) return not c:IsSetCard(0x13a) end)
-    ec2:SetReset(RESET_PHASE + PHASE_END)
-    Duel.RegisterEffect(ec2, tp)
-
     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SUMMON)
-    local tc = Utility.SelectMatchingCard(HINTMSG_SUMMON, tp, s.e2filter, tp,
+    local tc = Utility.SelectMatchingCard(HINTMSG_SUMMON, tp, s.e3filter, tp,
                                           LOCATION_HAND + LOCATION_MZONE, 0, 1,
                                           1, nil):GetFirst()
     if tc then Duel.Summon(tp, tc, true, nil) end
-    ec1:Reset()
-end
 
-function s.e3con(e, tp, eg, ep, ev, re, r, rp)
-    local loc = Duel.GetChainInfo(ev, CHAININFO_TRIGGERING_LOCATION)
-    return
-        rp == tp and loc == LOCATION_MZONE and re:IsActiveType(TYPE_MONSTER) and
-            re:GetHandler():IsSetCard(0x13a)
-end
-
-function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
-    local c = e:GetHandler()
-    if chk == 0 then return c:IsAbleToHand() end
-
-    Duel.SetOperationInfo(0, CATEGORY_TOHAND, c, 1, 0, 0)
-end
-
-function s.e3op(e, tp, eg, ep, ev, re, r, rp)
-    local c = e:GetHandler()
-    if not c:IsRelateToEffect(e) then return end
-    Duel.SendtoHand(c, nil, REASON_EFFECT)
+    local ec1 = Effect.CreateEffect(c)
+    ec1:SetDescription(aux.Stringid(id, 1))
+    ec1:SetType(EFFECT_TYPE_FIELD)
+    ec1:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_CLIENT_HINT)
+    ec1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+    ec1:SetTargetRange(1, 0)
+    ec1:SetTarget(function(e, c) return not c:IsSetCard(0x13a) end)
+    ec1:SetReset(RESET_PHASE + PHASE_END)
+    Duel.RegisterEffect(ec1, tp)
 end
