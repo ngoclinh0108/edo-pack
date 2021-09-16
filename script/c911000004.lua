91a339a2a9ec8798ace2265bd1743916448179f6
Luc Ta <luc.ta@cptech.vn>
2021-09-16 12:23:26 +0700

    fix

--- script/c911000004.lua
+++ script/c911000004.lua
@@ -4,4 +4,9 @@ local s, id = GetID()
 
 function s.initial_effect(c)
     c:EnableReviveLimit()
+
+    -- link summon
+    Link.AddProcedure(c, aux.NOT(
+                          aux.FilterBoolFunctionEx(Card.IsType, TYPE_TOKEN)), 3,
+                      3)
 end
