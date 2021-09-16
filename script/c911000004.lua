03c8e46bc323da0fd127e0e09637f36666297a34
Luc Ta <luc.ta@cptech.vn>
2021-09-17 02:18:49 +0700

    update

--- script/c911000004.lua
+++ script/c911000004.lua
@@ -9,7 +9,9 @@ function s.initial_effect(c)
     c:EnableReviveLimit()
 
     -- link summon
-    Link.AddProcedure(c, aux.FilterBoolFunctionEx(Card.IsSetCard, 0xdd), 3, 3)
+    Link.AddProcedure(c, nil, 3, 4, function(g, lc, sumtype, tp)
+        return g:IsExists(Card.IsSetCard, 1, nil, 0xdd, lc, sumtype, tp)
+    end)
 
     -- special summon limit
     local splimit = Effect.CreateEffect(c)
@@ -74,9 +76,9 @@ function s.initial_effect(c)
     e4:SetDescription(aux.Stringid(id, 2))
     e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e4:SetType(EFFECT_TYPE_IGNITION)
+    e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
     e4:SetRange(LOCATION_MZONE)
     e4:SetCountLimit(1, id)
-    e4:SetCost(s.e4cost)
     e4:SetTarget(s.e4tg)
     e4:SetOperation(s.e4op)
     c:RegisterEffect(e4)
@@ -186,43 +188,47 @@ function s.e2val(e, c)
     end
 end
 
-function s.e4filter(c, e, tp)
-    return c:IsCanBeSpecialSummoned(e, 0, tp, false, false) and
-               c:IsType(TYPE_NORMAL)
-end
-
-function s.e4cost(e, tp, eg, ep, ev, re, r, rp, chk)
-    local c = e:GetHandler()
-    if chk == 0 then return c:GetAttackAnnouncedCount() == 0 end
-
-    local ec1 = Effect.CreateEffect(c)
-    ec1:SetDescription(3206)
-    ec1:SetType(EFFECT_TYPE_SINGLE)
-    ec1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_OATH +
-                        EFFECT_FLAG_CLIENT_HINT)
-    ec1:SetCode(EFFECT_CANNOT_ATTACK)
-    ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
-    c:RegisterEffect(ec1)
+function s.e4filter(c, e, tp, zone)
+    return (c:IsFaceup() or not c:IsLocation(LOCATION_REMOVED)) and
+               c:IsCanBeSpecialSummoned(e, 0, tp, false, false,
+                                        POS_FACEUP_DEFENSE, tp, zone) and
+               c:IsSetCard(0xdd)
 end
 
 function s.e4tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    local zone = e:GetHandler():GetLinkedZone(tp) & 0x1f
     if chk == 0 then
         return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
-                   Duel.IsExistingMatchingCard(s.e4filter, tp,
-                                               LOCATION_HAND + LOCATION_GRAVE,
-                                               0, 1, nil, e, tp)
+                   Duel.IsExistingTarget(s.e4filter, tp,
+                                         LOCATION_GRAVE + LOCATION_REMOVED, 0,
+                                         1, nil, e, tp, zone)
     end
 
-    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp,
-                          LOCATION_HAND + LOCATION_GRAVE)
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
+    local g = Duel.SelectTarget(tp, s.e4filter, tp,
+                                LOCATION_GRAVE + LOCATION_REMOVED, 0, 1, 1, nil,
+                                e, tp, zone)
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, g, #g, 0, 0)
 end
 
 function s.e4op(e, tp, eg, ep, ev, re, r, rp)
-    if Duel.GetLocationCount(tp, LOCATION_MZONE) == 0 then return end
+    local c = e:GetHandler()
+    local tc = Duel.GetFirstTarget()
+    local zone = c:GetLinkedZone(tp) & 0x1f
 
-    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
-    local g = Duel.SelectMatchingCard(tp, aux.NecroValleyFilter(s.e4filter), tp,
-                                      LOCATION_HAND + LOCATION_GRAVE, 0, 1, 1,
-                                      nil, e, tp)
-    if #g > 0 then Duel.SpecialSummon(g, 0, tp, tp, false, false, POS_FACEUP) end
+    if tc and tc:IsRelateToEffect(e) and zone ~= 0 then
+        Duel.SpecialSummon(tc, 0, tp, tp, false, false, POS_FACEUP_DEFENSE, zone)
+    end
+
+    local ec1 = Effect.CreateEffect(c)
+    ec1:SetDescription(aux.Stringid(id, 3))
+    ec1:SetType(EFFECT_TYPE_FIELD)
+    ec1:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_OATH)
+    ec1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+    ec1:SetTargetRange(1, 0)
+    ec1:SetTarget(function(e, c)
+        return c:IsLocation(LOCATION_EXTRA) and c:IsSetCard(0xdd)
+    end)
+    ec1:SetReset(RESET_PHASE + PHASE_END)
+    Duel.RegisterEffect(ec1, tp)
 end
