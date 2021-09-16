7e39b3873ef0475b8d9ca8db6a073cf125999ba3
Luc Ta <luc.ta@cptech.vn>
2021-09-16 11:52:10 +0700

    update

--- script/c911000002.lua
+++ script/c911000002.lua
@@ -2,6 +2,7 @@
 Duel.LoadScript("util.lua")
 local s, id = GetID()
 
+s.listed_names = {CARD_BLUEEYES_W_DRAGON}
 s.listed_series = {0xdd}
 
 function s.initial_effect(c)
@@ -11,91 +12,117 @@ function s.initial_effect(c)
     Synchro.AddProcedure(c, nil, 1, 1, Synchro.NonTunerEx(Card.IsSetCard, 0xdd),
                          1, 1)
 
-    -- special summon limit
-    local splimit = Effect.CreateEffect(c)
-    splimit:SetType(EFFECT_TYPE_SINGLE)
-    splimit:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE +
-                            EFFECT_FLAG_SINGLE_RANGE)
-    splimit:SetCode(EFFECT_SPSUMMON_CONDITION)
-    splimit:SetRange(LOCATION_EXTRA)
-    splimit:SetValue(aux.synlimit)
-    c:RegisterEffect(splimit)
-
-    -- atk up
+    -- indes
     local e1 = Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e1:SetCode(EFFECT_UPDATE_ATTACK)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetValue(function(e, c)
-        local tp = c:GetControler()
-        return Duel.GetMatchingGroupCount(Card.IsRace, tp, LOCATION_GRAVE, 0,
-                                          nil, RACE_DRAGON) * 500
-    end)
+    e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_CONTINUOUS)
+    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetOperation(s.e1op)
     c:RegisterEffect(e1)
 
-    -- disable
+    -- atk up
     local e2 = Effect.CreateEffect(c)
-    e2:SetDescription(1117)
-    e2:SetCategory(CATEGORY_DISABLE)
-    e2:SetType(EFFECT_TYPE_QUICK_O)
-    e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP + EFFECT_FLAG_DAMAGE_CAL)
-    e2:SetCode(EVENT_CHAINING)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e2:SetCode(EFFECT_UPDATE_ATTACK)
     e2:SetRange(LOCATION_MZONE)
-    e2:SetCondition(s.e2con)
-    e2:SetTarget(s.e2tg)
-    e2:SetOperation(s.e2op)
+    e2:SetValue(function(e, c)
+        local tp = c:GetControler()
+        return Duel.GetMatchingGroupCount(Card.IsRace, tp, LOCATION_GRAVE, 0,
+                                          nil, RACE_DRAGON) * 300
+    end)
     c:RegisterEffect(e2)
-    local e2b = Effect.CreateEffect(c)
-    e2b:SetType(EFFECT_TYPE_SINGLE)
-    e2b:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE +
-                        EFFECT_FLAG_SINGLE_RANGE)
-    e2b:SetRange(LOCATION_MZONE)
-    e2b:SetCode(3682106)
-    c:RegisterEffect(e2b)
 
-    -- destroy
+    -- disable
     local e3 = Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id, 0))
-    e3:SetCategory(CATEGORY_DESTROY)
-    e3:SetType(EFFECT_TYPE_IGNITION)
-    e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e3:SetDescription(1117)
+    e3:SetCategory(CATEGORY_DISABLE)
+    e3:SetType(EFFECT_TYPE_QUICK_O)
+    e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP + EFFECT_FLAG_DAMAGE_CAL)
+    e3:SetCode(EVENT_CHAINING)
     e3:SetRange(LOCATION_MZONE)
-    e3:SetCost(s.e3cost)
+    e3:SetCondition(s.e3con)
     e3:SetTarget(s.e3tg)
     e3:SetOperation(s.e3op)
     c:RegisterEffect(e3)
+    local e3b = Effect.CreateEffect(c)
+    e3b:SetType(EFFECT_TYPE_SINGLE)
+    e3b:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE +
+                        EFFECT_FLAG_SINGLE_RANGE)
+    e3b:SetRange(LOCATION_MZONE)
+    e3b:SetCode(3682106)
+    c:RegisterEffect(e3b)
+
+    -- destroy
+    local e4 = Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id, 0))
+    e4:SetCategory(CATEGORY_DESTROY)
+    e4:SetType(EFFECT_TYPE_IGNITION)
+    e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCondition(function(e, tp, eg, ep, ev, re, r, rp)
+        return not e:GetHandler():GetMaterial():IsExists(Card.IsCode, 1, nil,
+                                                         CARD_BLUEEYES_W_DRAGON)
+    end)
+    e4:SetCost(s.e4cost)
+    e4:SetTarget(s.e4tg)
+    e4:SetOperation(s.e4op)
+    c:RegisterEffect(e4)
+    local e4b = e4:Clone()
+    e4b:SetType(EFFECT_TYPE_QUICK_O)
+    e4b:SetCode(EVENT_FREE_CHAIN)
+    e4b:SetCondition(function(e, tp, eg, ep, ev, re, r, rp)
+        return e:GetHandler():GetMaterial():IsExists(Card.IsCode, 1, nil,
+                                                     CARD_BLUEEYES_W_DRAGON)
+    end)
+    c:RegisterEffect(e4b)
 end
 
-function s.e2filter(c, tp)
+function s.e1op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    local g = Duel.GetMatchingGroup(aux.FilterFaceupFunction(Card.IsRace,
+                                                             RACE_DRAGON), tp,
+                                    LOCATION_MZONE, 0, nil)
+    for tc in aux.Next(g) do
+        local ec1 = Effect.CreateEffect(c)
+        ec1:SetDescription(3001)
+        ec1:SetType(EFFECT_TYPE_SINGLE)
+        ec1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_CLIENT_HINT)
+        ec1:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+        ec1:SetValue(1)
+        ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END, 2)
+        tc:RegisterEffect(ec1)
+    end
+end
+
+function s.e3filter(c, tp)
     return c:IsControler(tp) and c:IsLocation(LOCATION_MZONE) and
                c:IsSetCard(0xdd)
 end
 
-function s.e2con(e, tp, eg, ep, ev, re, r, rp)
+function s.e3con(e, tp, eg, ep, ev, re, r, rp)
     if rp == tp or not re:IsHasProperty(EFFECT_FLAG_CARD_TARGET) then
         return false
     end
 
     local tg = Duel.GetChainInfo(ev, CHAININFO_TARGET_CARDS)
-    return tg and tg:IsExists(s.e2filter, 1, nil, tp) and
+    return tg and tg:IsExists(s.e3filter, 1, nil, tp) and
                Duel.IsChainDisablable(ev)
 end
 
-function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then return true end
 
     Duel.SetOperationInfo(0, CATEGORY_DISABLE, eg, 1, 0, 0)
 end
 
-function s.e2op(e, tp, eg, ep, ev, re, r, rp, chk) Duel.NegateEffect(ev) end
+function s.e3op(e, tp, eg, ep, ev, re, r, rp, chk) Duel.NegateEffect(ev) end
 
-function s.e3cost(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e4cost(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then return e:GetHandler():IsReleasable() end
     Duel.Release(e:GetHandler(), REASON_COST)
 end
 
-function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+function s.e4tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     local c = e:GetHandler()
     if chk == 0 then
         return Duel.IsExistingTarget(aux.TRUE, tp, LOCATION_ONFIELD,
@@ -108,7 +135,7 @@ function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     Duel.SetOperationInfo(0, CATEGORY_DESTROY, g, #g, 0, 0)
 end
 
-function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+function s.e4op(e, tp, eg, ep, ev, re, r, rp)
     local g = Duel.GetTargetCards(e)
     if #g == 0 then return end
 
