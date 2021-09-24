5092cb8dd16cb7f8c1d476b9a3560867e2657961
Luc Ta <luc.ta@cptech.vn>
2021-09-24 11:44:36 +0700

    update

--- script/c910000019.lua
+++ script/c910000019.lua
@@ -11,20 +11,6 @@ function s.initial_effect(c)
     -- xyz summon
     Xyz.AddProcedure(c, aux.FilterBoolFunctionEx(Card.IsSetCard, 0xcf), 8, 2)
 
-    -- attribute
-    local attribute = Effect.CreateEffect(c)
-    attribute:SetType(EFFECT_TYPE_SINGLE)
-    attribute:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
-    attribute:SetCode(EFFECT_ADD_ATTRIBUTE)
-    attribute:SetValue(ATTRIBUTE_DARK)
-    c:RegisterEffect(attribute)
-    local race = Effect.CreateEffect(c)
-    race:SetType(EFFECT_TYPE_SINGLE)
-    race:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
-    race:SetCode(EFFECT_ADD_RACE)
-    race:SetValue(RACE_DRAGON)
-    c:RegisterEffect(race)
-
     -- special summon limit
     local splimit = Effect.CreateEffect(c)
     splimit:SetType(EFFECT_TYPE_SINGLE)
