385706529276551c32d815a466218d852bdbe560
Luc Ta <luc.ta@cptech.vn>
2021-10-29 23:05:40 +0700

    update

--- script/c910000109.lua
+++ script/c910000109.lua
@@ -2,10 +2,33 @@
 Duel.LoadScript("util.lua")
 local s, id = GetID()
 
+s.listed_series = {0x13a}
+
 function s.initial_effect(c)
-    -- activate
-    local act = Effect.CreateEffect(c)
-    act:SetType(EFFECT_TYPE_ACTIVATE)
-    act:SetCode(EVENT_FREE_CHAIN)
-    c:RegisterEffect(act)
+    c:SetUniqueOnField(1, 0, id)
+
+    -- search
+    local e1 = Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_TOHAND + CATEGORY_SEARCH)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetCountLimit(1, id, EFFECT_COUNT_CODE_OATH)
+    e1:SetOperation(s.e1op)
+    c:RegisterEffect(e1)
+end
+
+function s.e1filter(c)
+    return (c:IsAttribute(ATTRIBUTE_DIVINE) or c:IsSetCard(0x13a)) and
+               c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+
+function s.e1op(e, tp, eg, ep, ev, re, r, rp)
+    if not e:GetHandler():IsRelateToEffect(e) then return end
+
+    local g = Duel.GetMatchingGroup(s.e1filter, tp, LOCATION_DECK, 0, nil)
+    if #g > 0 and Duel.SelectYesNo(tp, aux.Stringid(id, 0)) then
+        local sg = Utility.GroupSelect(HINT_SELECTMSG, g, tp, 1, 1)
+        Duel.SendtoHand(sg, nil, REASON_EFFECT)
+        Duel.ConfirmCards(1 - tp, sg)
+    end
 end
