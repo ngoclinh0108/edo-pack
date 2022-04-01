2ccfd9f3530e0dd66cc38bd1f27a09190fd9e54f
Luc THQ <luc.ta@cptech.vn>
2022-04-02 02:25:11 +0700

    init

--- ygofab.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-setlocal
-set "YGOFAB_ROOT=D:/Games/EdoPro/repositories/triplesix/_fabrica"
-set "LUA_PATH=%YGOFAB_ROOT%/?.lua;%YGOFAB_ROOT%/?/init.lua;%YGOFAB_ROOT%/modules/share/lua/5.1/?.lua;%YGOFAB_ROOT%/modules/share/lua/5.1/?/init.lua"
-set "LUA_CPATH=%YGOFAB_ROOT%/modules/lib/lua/5.1/?.dll"
-set "PATH=%YGOFAB_ROOT%/luajit;%YGOFAB_ROOT%/vips/bin;%PATH%"
-luajit "%YGOFAB_ROOT%/scripts/ygofab.lua" %*
-endlocal
-@echo on
