5130e07764b04c8c1ba9cca3864cf7b4a1727a16
Luc THQ <luc.ta@cptech.vn>
2022-04-03 04:33:26 +0700

    up

--- pack/dracodeity/script/c990000000.lua
+++ pack/dracodeity/script/c990000000.lua
@@ -10,36 +10,26 @@ function s.initial_effect(c)
     -- link summon
     Link.AddProcedure(c, nil, 3, 3)
 
-    -- attribute
-    local e1 = Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE + EFFECT_FLAG_CANNOT_DISABLE +
-                       EFFECT_FLAG_CANNOT_NEGATE)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetCode(EFFECT_REMOVE_ATTRIBUTE)
-    e1:SetValue(ATTRIBUTE_LIGHT)
-    c:RegisterEffect(e1)
-
     -- extra material
-    local e2 = Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_SET_AVAILABLE)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetCode(EFFECT_EXTRA_MATERIAL)
-    e2:SetTargetRange(1, 0)
-    e2:SetValue(s.e2val)
-    c:RegisterEffect(e2)
-    local e2b = Effect.CreateEffect(c)
-    e2b:SetType(EFFECT_TYPE_FIELD)
-    e2b:SetRange(LOCATION_MZONE)
-    e2b:SetCode(EFFECT_ADD_RACE)
-    e2b:SetTargetRange(0, LOCATION_MZONE)
-    e2b:SetCondition(function(e)
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_SET_AVAILABLE)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCode(EFFECT_EXTRA_MATERIAL)
+    e1:SetTargetRange(1, 0)
+    e1:SetValue(s.e1val)
+    c:RegisterEffect(e1)
+    local e1b = Effect.CreateEffect(c)
+    e1b:SetType(EFFECT_TYPE_FIELD)
+    e1b:SetRange(LOCATION_MZONE)
+    e1b:SetCode(EFFECT_ADD_RACE)
+    e1b:SetTargetRange(0, LOCATION_MZONE)
+    e1b:SetCondition(function(e)
         return Duel.GetFlagEffect(e:GetHandlerPlayer(), id) > 0
     end)
-    e2b:SetTarget(aux.TargetBoolFunction(Card.IsFaceup))
-    e2b:SetValue(RACE_DRAGON)
-    c:RegisterEffect(e2b)
+    e1b:SetTarget(aux.TargetBoolFunction(Card.IsFaceup))
+    e1b:SetValue(RACE_DRAGON)
+    c:RegisterEffect(e1b)
 end
 
 function s.deck_edit(tp)
@@ -52,7 +42,7 @@ function s.deck_edit(tp)
     Utility.DeckEditAddCardToDeck(tp, 990000007)
 end
 
-function s.e2val(chk, summon_type, e, ...)
+function s.e1val(chk, summon_type, e, ...)
     if chk == 0 then
         local tp, sc = ...
         if summon_type ~= SUMMON_TYPE_LINK or
