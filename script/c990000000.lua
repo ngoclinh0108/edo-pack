2d9ac33e9abd41b7c4476d8fcf487f0b9ed1f43c
Luc THQ <luc.ta@cptech.vn>
2022-04-03 22:23:59 +0700

    up

--- script/c990000000.lua
+++ script/c990000000.lua
@@ -13,47 +13,58 @@ function s.initial_effect(c)
     -- summon cannot be negated
     local e1 = Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetCode(EFFECT_CANNOT_DISABLE_SPSUMMON)
     e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+    e1:SetCode(EFFECT_CANNOT_DISABLE_SPSUMMON)
     e1:SetCondition(function(e)
         return e:GetHandler():GetSummonType() == SUMMON_TYPE_LINK
     end)
     c:RegisterEffect(e1)
 
-    -- extra material
+    -- link limit
     local e2 = Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_SET_AVAILABLE)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetCode(EFFECT_EXTRA_MATERIAL)
-    e2:SetTargetRange(1, 0)
-    e2:SetValue(s.e2val)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetProperty(EFFECT_FLAG_UNCOPYABLE + EFFECT_FLAG_CANNOT_DISABLE)
+    e2:SetCode(EFFECT_CANNOT_BE_LINK_MATERIAL)
+    e2:SetValue(function(e, c)
+        if not c then return false end
+        return not c:IsRace(RACE_HIGHDRAGON)
+    end)
     c:RegisterEffect(e2)
-    local e2b = Effect.CreateEffect(c)
-    e2b:SetType(EFFECT_TYPE_FIELD)
-    e2b:SetRange(LOCATION_MZONE)
-    e2b:SetCode(EFFECT_ADD_RACE)
-    e2b:SetTargetRange(0, LOCATION_MZONE)
-    e2b:SetCondition(function(e)
+
+    -- extra material
+    local e3 = Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_FIELD)
+    e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_SET_AVAILABLE)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCode(EFFECT_EXTRA_MATERIAL)
+    e3:SetTargetRange(1, 0)
+    e3:SetValue(s.e3val)
+    c:RegisterEffect(e3)
+    local e3b = Effect.CreateEffect(c)
+    e3b:SetType(EFFECT_TYPE_FIELD)
+    e3b:SetRange(LOCATION_MZONE)
+    e3b:SetCode(EFFECT_ADD_RACE)
+    e3b:SetTargetRange(0, LOCATION_MZONE)
+    e3b:SetCondition(function(e)
         return Duel.GetFlagEffect(e:GetHandlerPlayer(), id) > 0
     end)
-    e2b:SetTarget(aux.TargetBoolFunction(Card.IsFaceup))
-    e2b:SetValue(RACE_DRAGON)
-    c:RegisterEffect(e2b)
+    e3b:SetTarget(aux.TargetBoolFunction(Card.IsFaceup))
+    e3b:SetValue(RACE_DRAGON)
+    c:RegisterEffect(e3b)
 
     -- special summon
-    local e3 = Effect.CreateEffect(c)
-    e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
-    e3:SetType(EFFECT_TYPE_QUICK_O)
-    e3:SetRange(LOCATION_GRAVE)
-    e3:SetCode(EVENT_FREE_CHAIN)
-    e3:SetCountLimit(1, id)
-    e3:SetHintTiming(0, TIMINGS_CHECK_MONSTER + TIMING_MAIN_END)
-    e3:SetCondition(s.e3con)
-    e3:SetCost(s.e3cost)
-    e3:SetTarget(s.e3tg)
-    e3:SetOperation(s.e3op)
-    c:RegisterEffect(e3)
+    local e4 = Effect.CreateEffect(c)
+    e4:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e4:SetType(EFFECT_TYPE_QUICK_O)
+    e4:SetRange(LOCATION_GRAVE)
+    e4:SetCode(EVENT_FREE_CHAIN)
+    e4:SetCountLimit(1, id)
+    e4:SetHintTiming(0, TIMINGS_CHECK_MONSTER + TIMING_MAIN_END)
+    e4:SetCondition(s.e4con)
+    e4:SetCost(s.e4cost)
+    e4:SetTarget(s.e4tg)
+    e4:SetOperation(s.e4op)
+    c:RegisterEffect(e4)
 end
 
 function s.deck_edit(tp)
@@ -66,7 +77,7 @@ function s.deck_edit(tp)
     Utility.DeckEditAddCardToDeck(tp, 990000007)
 end
 
-function s.e2val(chk, summon_type, e, ...)
+function s.e3val(chk, summon_type, e, ...)
     if chk == 0 then
         local tp, sc = ...
         if summon_type ~= SUMMON_TYPE_LINK or
@@ -82,26 +93,27 @@ function s.e2val(chk, summon_type, e, ...)
     end
 end
 
-function s.e3filter(c)
+function s.e4filter(c)
     return c:IsFaceup() and c:IsLinkMonster() and c:IsAbleToRemoveAsCost()
 end
 
-function s.e3con(e, tp, eg, ep, ev, re, r, rp)
-    return Duel.GetCurrentPhase() ~= PHASE_END
+function s.e4con(e, tp, eg, ep, ev, re, r, rp)
+    return Duel.GetCurrentPhase() ~= PHASE_END and
+               aux.exccon(e, tp, eg, ep, ev, re, r, rp)
 end
 
-function s.e3cost(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e4cost(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
     if chk == 0 then
-        return Duel.IsExistingMatchingCard(s.e3filter, tp, LOCATION_MZONE, 0, 1,
+        return Duel.IsExistingMatchingCard(s.e4filter, tp, LOCATION_MZONE, 0, 1,
                                            nil)
     end
 
-    local tc = Utility.SelectMatchingCard(HINTMSG_REMOVE, tp, s.e3filter, tp,
+    local tc = Utility.SelectMatchingCard(HINTMSG_REMOVE, tp, s.e4filter, tp,
                                           LOCATION_MZONE, 0, 1, 1, nil):GetFirst()
     if Duel.Remove(tc, POS_FACEUP, REASON_COST + REASON_TEMPORARY) ~= 0 then
         local ec1 = Effect.CreateEffect(c)
-        ec1:SetDescription(aux.Stringid(id, 0))
+        ec1:SetDescription(aux.Stringid(id, 1))
         ec1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
         ec1:SetCode(EVENT_PHASE + PHASE_END)
         ec1:SetLabelObject(tc)
@@ -114,7 +126,7 @@ function s.e3cost(e, tp, eg, ep, ev, re, r, rp, chk)
     end
 end
 
-function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e4tg(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
     if chk == 0 then
         return Duel.GetLocationCount(tp, LOCATION_MZONE) > 0 and
@@ -124,7 +136,7 @@ function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
     Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, c, 1, 0, 0)
 end
 
-function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+function s.e4op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     if c:IsRelateToEffect(e) and
         Duel.SpecialSummon(c, 0, tp, tp, true, false, POS_FACEUP) > 0 then
@@ -132,7 +144,7 @@ function s.e3op(e, tp, eg, ep, ev, re, r, rp)
                                  PHASE_END, 0, 1)
 
         local ec1 = Effect.CreateEffect(c)
-        ec1:SetDescription(aux.Stringid(id, 1))
+        ec1:SetDescription(aux.Stringid(id, 2))
         ec1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
         ec1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
         ec1:SetCode(EVENT_PHASE + PHASE_END)
@@ -148,6 +160,8 @@ function s.e3op(e, tp, eg, ep, ev, re, r, rp)
         Duel.RegisterEffect(ec1, tp)
     end
 
+    aux.RegisterClientHint(c, EFFECT_FLAG_OATH, 1 - tp, 1, 0,
+                           aux.Stringid(id, 0), nil)
     local ec2 = Effect.CreateEffect(c)
     ec2:SetType(EFFECT_TYPE_FIELD)
     ec2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
