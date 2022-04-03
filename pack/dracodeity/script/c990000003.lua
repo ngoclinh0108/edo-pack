1e61cca3e58bbc5ce7cc29bff7af2e7cf4938db8
Luc THQ <luc.ta@cptech.vn>
2022-04-04 01:40:35 +0700

    up

--- pack/dracodeity/script/c990000003.lua
+++ pack/dracodeity/script/c990000003.lua
@@ -6,4 +6,84 @@ local s, id = GetID()
 function s.initial_effect(c)
     UtilityDracodeity.RegisterSummon(c, ATTRIBUTE_FIRE)
     UtilityDracodeity.RegisterEffect(c, id)
+
+    -- battle indes
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+    e1:SetTargetRange(LOCATION_MZONE, 0)
+    e1:SetTarget(function(e, c) return c:GetMutualLinkedGroupCount() > 0 end)
+    e1:SetValue(1)
+    c:RegisterEffect(e1)
+
+    -- destroy
+    local e2 = Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id, 0))
+    e2:SetCategory(CATEGORY_DESTROY)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1, id)
+    e2:SetTarget(s.e2tg)
+    e2:SetOperation(s.e2op)
+    c:RegisterEffect(e2)
+
+    -- inflict damage
+    local e3 = Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id, 1))
+    e3:SetCategory(CATEGORY_DAMAGE)
+    e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e3:SetCode(EVENT_BATTLE_DESTROYING)
+    e3:SetCondition(aux.bdcon)
+    e3:SetTarget(s.e3tg)
+    e3:SetOperation(s.e3op)
+    c:RegisterEffect(e3)
+end
+
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    local c = e:GetHandler()
+    local ct = c:GetLinkedGroupCount()
+
+    if chk == 0 then
+        return
+            Duel.IsExistingTarget(aux.TRUE, tp, 0, LOCATION_ONFIELD, 1, nil) and
+                ct > 0
+    end
+
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_DESTROY)
+    local g = Duel.SelectTarget(tp, aux.TRUE, tp, 0, LOCATION_ONFIELD, 1, ct,
+                                nil)
+
+    for tc in aux.Next(g) do Duel.SetChainLimit(s.e2limit(tc)) end
+    Duel.SetOperationInfo(0, CATEGORY_DESTROY, g, #g, 0, 0)
+end
+
+function s.e2limit(tc) return
+    function(e, lp, tp) return e:GetHandler() ~= tc end end
+
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+    local g = Duel.GetTargetCards(e):Filter(Card.IsRelateToEffect, nil, e)
+    Duel.Destroy(g, REASON_EFFECT)
+end
+
+function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return true end
+
+    local c = e:GetHandler()
+    local bc = c:GetBattleTarget()
+    local dmg = bc:GetAttack()
+    if bc:GetAttack() < bc:GetDefense() then dmg = bc:GetDefense() end
+    if dmg < 0 then dmg = 0 end
+
+    Duel.SetTargetPlayer(1 - tp)
+    Duel.SetTargetParam(dmg)
+    Duel.SetOperationInfo(0, CATEGORY_DAMAGE, nil, 0, 1 - tp, dmg)
+end
+
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+    local p, d = Duel.GetChainInfo(0, CHAININFO_TARGET_PLAYER,
+                                   CHAININFO_TARGET_PARAM)
+    Duel.Damage(p, d, REASON_EFFECT)
 end
--- script/util_dracodeity.lua
+++ script/util_dracodeity.lua
@@ -19,10 +19,35 @@ function UtilityDracodeity.RegisterEffect(c, id)
     -- summon cannot be negated
     local summon_safe = Effect.CreateEffect(c)
     summon_safe:SetType(EFFECT_TYPE_SINGLE)
-    summon_safe:SetCode(EFFECT_CANNOT_DISABLE_SPSUMMON)
     summon_safe:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+    summon_safe:SetCode(EFFECT_CANNOT_DISABLE_SPSUMMON)
     summon_safe:SetCondition(function(e)
         return e:GetHandler():GetSummonType() == SUMMON_TYPE_LINK
     end)
     c:RegisterEffect(summon_safe)
+
+    -- cannot switch control
+    local noswitch = Effect.CreateEffect(c)
+    noswitch:SetType(EFFECT_TYPE_SINGLE)
+    noswitch:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    noswitch:SetRange(LOCATION_MZONE)
+    noswitch:SetCode(EFFECT_CANNOT_CHANGE_CONTROL)
+    c:RegisterEffect(noswitch)
+
+    -- cannot be tributed, or be used as a material
+    local norelease = Effect.CreateEffect(c)
+    norelease:SetType(EFFECT_TYPE_FIELD)
+    norelease:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    norelease:SetRange(LOCATION_MZONE)
+    norelease:SetCode(EFFECT_CANNOT_RELEASE)
+    norelease:SetTargetRange(0, 1)
+    norelease:SetTarget(function(e, tc) return tc == e:GetHandler() end)
+    c:RegisterEffect(norelease)
+    local nomaterial = Effect.CreateEffect(c)
+    nomaterial:SetType(EFFECT_TYPE_SINGLE)
+    nomaterial:SetCode(EFFECT_CANNOT_BE_MATERIAL)
+    nomaterial:SetValue(function(e, tc)
+        return tc and tc:GetControler() ~= e:GetHandlerPlayer()
+    end)
+    c:RegisterEffect(nomaterial)
 end
