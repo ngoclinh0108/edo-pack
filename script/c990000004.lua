d6af6e71312edf40d00408992032e62d6da38a46
Luc THQ <luc.ta@cptech.vn>
2022-04-05 03:43:57 +0700

    up

--- script/c990000004.lua
+++ script/c990000004.lua
@@ -7,14 +7,34 @@ function s.initial_effect(c)
     UtilityDracodeity.RegisterSummon(c, ATTRIBUTE_WATER)
     UtilityDracodeity.RegisterEffect(c, id)
 
-    -- effect untargetable
+    -- cannot be returned
     local e1 = Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_FIELD)
-    e1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+    e1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
     e1:SetRange(LOCATION_MZONE)
-    e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-    e1:SetTargetRange(LOCATION_MZONE, 0)
-    e1:SetTarget(function(e, c) return c:GetMutualLinkedGroupCount() > 0 end)
-    e1:SetValue(aux.tgoval)
+    e1:SetCode(EVENT_CHAIN_SOLVING)
+    e1:SetOperation(s.e1op)
     c:RegisterEffect(e1)
+end
+
+function s.e1op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    if rp == tp then return end
+    local g = Duel.GetMatchingGroup(function(tc)
+        return tc:GetMutualLinkedGroupCount() > 0
+    end, tp, LOCATION_MZONE, 0, nil)
+    if #g == 0 then return end
+
+    for tc in aux.Next(g) do
+        local ec1 = Effect.CreateEffect(c)
+        ec1:SetType(EFFECT_TYPE_FIELD)
+        ec1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+        ec1:SetCode(EFFECT_CANNOT_TO_HAND)
+        ec1:SetRange(LOCATION_MZONE)
+        ec1:SetTargetRange(0, 1)
+        ec1:SetReset(RESET_CHAIN)
+        tc:RegisterEffect(ec1)
+        local ec2 = ec1:Clone()
+        ec2:SetCode(EFFECT_CANNOT_TO_DECK)
+        tc:RegisterEffect(ec2)
+    end
 end
