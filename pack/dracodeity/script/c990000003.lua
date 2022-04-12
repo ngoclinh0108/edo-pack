5917ab59ac6a643cff9824ce93d0f5dedc87a18b
Luc THQ <luc.ta@cptech.vn>
2022-04-13 02:23:06 +0700

    up

--- pack/dracodeity/script/c990000003.lua
+++ pack/dracodeity/script/c990000003.lua
@@ -17,47 +17,67 @@ function s.initial_effect(c)
     e1:SetValue(1)
     c:RegisterEffect(e1)
 
-    -- inflict damage
+    -- destroy
     local e2 = Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id, 0))
-    e2:SetCategory(CATEGORY_DAMAGE)
-    e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
-    e2:SetProperty(EFFECT_FLAG_DELAY + EFFECT_FLAG_PLAYER_TARGET)
-    e2:SetCode(EVENT_BATTLE_DESTROYING)
-    e2:SetCondition(aux.bdcon)
-    e2:SetTarget(s.e2tg1)
+    e2:SetDescription(aux.Stringid(id, 1))
+    e2:SetCategory(CATEGORY_DESTROY)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1)
+    e2:SetTarget(s.e2tg)
     e2:SetOperation(s.e2op)
     c:RegisterEffect(e2)
-    local e2b = e2:Clone()
-    e2b:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
-    e2b:SetRange(LOCATION_MZONE)
-    e2b:SetCode(EVENT_DESTROYED)
-    e2b:SetCondition(s.e2con)
-    e2b:SetTarget(s.e2tg2)
-    c:RegisterEffect(e2b)
 
-    -- destroy
+    -- inflict damage
     local e3 = Effect.CreateEffect(c)
-    e3:SetDescription(aux.Stringid(id, 1))
-    e3:SetCategory(CATEGORY_DESTROY)
-    e3:SetType(EFFECT_TYPE_IGNITION)
-    e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e3:SetRange(LOCATION_MZONE)
-    e3:SetCountLimit(1)
-    e3:SetTarget(s.e3tg)
+    e3:SetDescription(aux.Stringid(id, 0))
+    e3:SetCategory(CATEGORY_DAMAGE)
+    e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e3:SetProperty(EFFECT_FLAG_DELAY + EFFECT_FLAG_PLAYER_TARGET)
+    e3:SetCode(EVENT_BATTLE_DESTROYING)
+    e3:SetCondition(aux.bdcon)
+    e3:SetTarget(s.e3tg1)
     e3:SetOperation(s.e3op)
     c:RegisterEffect(e3)
+    local e3b = e3:Clone()
+    e3b:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
+    e3b:SetRange(LOCATION_MZONE)
+    e3b:SetCode(EVENT_DESTROYED)
+    e3b:SetCondition(s.e3con)
+    e3b:SetTarget(s.e3tg2)
+    c:RegisterEffect(e3b)
 end
 
-function s.e2filter(c, tp)
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    local c = e:GetHandler()
+    local ct = c:GetLinkedGroupCount()
+
+    if chk == 0 then
+        return ct > 0 and Duel.IsExistingMatchingCard(aux.TRUE, tp, LOCATION_ONFIELD, LOCATION_ONFIELD, 1, c)
+    end
+
+    Duel.SetOperationInfo(0, CATEGORY_DESTROY, nil, ct, 0, LOCATION_ONFIELD)
+end
+
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    local ct = c:GetLinkedGroupCount()
+    if ct <= 0 then return end
+
+    local g = Utility.SelectMatchingCard(HINTMSG_DESTROY, tp, aux.TRUE, tp, LOCATION_ONFIELD, LOCATION_ONFIELD, 1, ct, c)
+    Duel.Destroy(g, REASON_EFFECT)
+end
+
+function s.e3filter(c, tp)
     return c:IsType(TYPE_MONSTER) and c:IsControler(tp)
 end
 
-function s.e2con(e, tp, eg, ep, ev, re, r, rp)
+function s.e3con(e, tp, eg, ep, ev, re, r, rp)
     return (r & REASON_EFFECT) ~= 0 and re and re:GetOwner() == e:GetHandler()
 end
 
-function s.e2tg1(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e3tg1(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
     local bc = c:GetBattleTarget()
     local dmg = bc:GetBaseAttack()
@@ -68,8 +88,8 @@ function s.e2tg1(e, tp, eg, ep, ev, re, r, rp, chk)
     Duel.SetOperationInfo(0, CATEGORY_DAMAGE, nil, 0, 1 - tp, dmg)
 end
 
-function s.e2tg2(e, tp, eg, ep, ev, re, r, rp, chk)
-    local _, dmg = eg:Filter(s.e2filter, nil, 1 - tp):GetMaxGroup(Card.GetBaseAttack)
+function s.e3tg2(e, tp, eg, ep, ev, re, r, rp, chk)
+    local _, dmg = eg:Filter(s.e3filter, nil, 1 - tp):GetMaxGroup(Card.GetBaseAttack)
     if chk == 0 then return dmg and dmg > 0 end
 
     Duel.SetTargetPlayer(1 - tp)
@@ -77,37 +97,11 @@ function s.e2tg2(e, tp, eg, ep, ev, re, r, rp, chk)
     Duel.SetOperationInfo(0, CATEGORY_DAMAGE, nil, 0, 1 - tp, dmg)
 end
 
-function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     if not c:IsRelateToEffect(e) or c:IsFacedown() then return end
 
     local p, d = Duel.GetChainInfo(0, CHAININFO_TARGET_PLAYER,
         CHAININFO_TARGET_PARAM)
     Duel.Damage(p, d, REASON_EFFECT)
-end
-
-function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
-    local c = e:GetHandler()
-    local ct = c:GetLinkedGroupCount()
-
-    if chk == 0 then
-        return Duel.IsExistingTarget(aux.TRUE, tp, LOCATION_ONFIELD, LOCATION_ONFIELD, 1, nil) and
-            ct > 0
-    end
-
-    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_DESTROY)
-    local g = Duel.SelectTarget(tp, aux.TRUE, tp, LOCATION_ONFIELD, LOCATION_ONFIELD, 1, ct,
-        nil)
-
-    for tc in aux.Next(g) do Duel.SetChainLimit(s.e3limit(tc)) end
-    Duel.SetOperationInfo(0, CATEGORY_DESTROY, g, #g, 0, 0)
-end
-
-function s.e3limit(tc)
-    return function(e) return e:GetHandler() ~= tc end
-end
-
-function s.e3op(e, tp, eg, ep, ev, re, r, rp)
-    local g = Duel.GetTargetCards(e):Filter(Card.IsRelateToEffect, nil, e)
-    Duel.Destroy(g, REASON_EFFECT)
 end
