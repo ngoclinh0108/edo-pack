f066ba3c8e649a6528595f1c3275ca84f37e5538
Luc THQ <luc.ta@cptech.vn>
2022-08-14 12:51:30 +0700

    up

--- script/c900000300.lua
+++ script/c900000300.lua
@@ -19,4 +19,16 @@ function s.initial_effect(c)
         return c:IsType(TYPE_SYNCHRO) and c:IsControler(e:GetHandlerPlayer())
     end)
     c:RegisterEffect(e1)
+
+    -- cannot to extra
+    local e2 = Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetCode(EFFECT_CANNOT_TO_DECK)
+    e2:SetRange(LOCATION_FZONE)
+    e2:SetTargetRange(LOCATION_ONFIELD, LOCATION_ONFIELD)
+    e2:SetTarget(function(e, c)
+        return c:IsFaceup() and c:IsRace(RACE_DRAGON) and c:IsType(TYPE_SYNCHRO)
+    end)
+    e2:SetValue(1)
+    c:RegisterEffect(e2)
 end
