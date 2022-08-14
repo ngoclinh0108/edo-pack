6ca0a5d6362511e4dede1ebcf09f735c261bcd46
Luc THQ <luc.ta@cptech.vn>
2022-08-14 16:43:09 +0700

    up

--- pack/signer-dragons/script/c900000313.lua
+++ pack/signer-dragons/script/c900000313.lua
@@ -8,4 +8,12 @@ function s.initial_effect(c)
 
     -- synchro summon
     Synchro.AddProcedure(c, nil, 1, 1, Synchro.NonTuner(nil), 1, 1)
+
+    -- non-tuner for a synchro summon
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE + EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_CANNOT_NEGATE)
+    e1:SetCode(EFFECT_NONTUNER)
+    e1:SetRange(LOCATION_MZONE)
+    c:RegisterEffect(e1)
 end
