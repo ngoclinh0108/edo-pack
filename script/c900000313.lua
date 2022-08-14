ee4aa49d2d77155fafc30be84bfd7ec9d0ca4fa2
Luc THQ <luc.ta@cptech.vn>
2022-08-14 16:37:17 +0700

    up

---
+++ script/c900000313.lua
@@ -0,0 +1,11 @@
+-- Shooting Star Synchron
+Duel.LoadScript("util.lua")
+Duel.LoadScript("util_signer_dragon.lua")
+local s, id = GetID()
+
+function s.initial_effect(c)
+    c:EnableReviveLimit()
+
+    -- synchro summon
+    Synchro.AddProcedure(c, nil, 1, 1, Synchro.NonTuner(nil), 1, 1)
+end
