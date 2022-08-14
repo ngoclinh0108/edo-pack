54a62a475d5471225743f19884698bbeeab20640
Luc THQ <luc.ta@cptech.vn>
2022-08-14 11:24:50 +0700

    up

--- script/c900000302.lua
+++ script/c900000302.lua
@@ -3,30 +3,10 @@ Duel.LoadScript("util.lua")
 Duel.LoadScript("util_signer_dragon.lua")
 
 local s, id = GetID()
-s.material = {SignerDragon.CARD_MAJESTIC_DRAGON, CARD_BLACK_WINGED_DRAGON}
-s.listed_names = {SignerDragon.CARD_MAJESTIC_DRAGON, CARD_BLACK_WINGED_DRAGON}
-s.synchro_nt_required = 1
 
 function s.initial_effect(c)
     c:EnableReviveLimit()
 
     -- synchro summon
-    Synchro.AddMajesticProcedure(c, aux.FilterBoolFunction(Card.IsCode, SignerDragon.CARD_MAJESTIC_DRAGON), true,
-        aux.FilterBoolFunction(Card.IsCode, CARD_BLACK_WINGED_DRAGON), true, Synchro.NonTuner(nil), false)
-end
-FFECT_TYPE_FIELD+EFFECT_TYPE_TRIGGER_F)
-	e4:SetCategory(CATEGORY_TODECK+CATEGORY_SPECIAL_SUMMON)
-	e4:SetRange(LOCATION_MZONE)
-	e4:SetProperty(EFFECT_FLAG_CARD_TARGET)
-	e4:SetCountLimit(1)
-	e4:SetCode(EVENT_PHASE+PHASE_END)
-	e4:SetTarget(s.sptg)
-	e4:SetOperation(s.spop)
-	c:RegisterEffect(e4)
-	--double tuner check
-	local e5=Effect.CreateEffect(c)
-	e5:SetType(EFFECT_TYPE_SINGLE)
-	e5:SetCode(EFFECT_MATERIAL_CHECK)
-	e5:SetValue(s.valcheck)
-	c:RegisterEffect(e5)
+    Synchro.AddMajesticSynchro(c, s, CARD_BLACK_WINGED_DRAGON)
 end
--- utility/util_signer_dragon.lua
+++ utility/util_signer_dragon.lua
@@ -8,4 +8,11 @@ if not SignerDragon then SignerDragon = aux.SignerDragonProcedure end
 SignerDragon.CARD_MAJESTIC_DRAGON = 21159309
 
 -- function
-    
+function SignerDragon.AddMajesticSynchro(c, s, card_code)
+    Synchro.AddMajesticProcedure(c, aux.FilterBoolFunction(Card.IsCode, SignerDragon.CARD_MAJESTIC_DRAGON), true,
+        aux.FilterBoolFunction(Card.IsCode, card_code), true, Synchro.NonTuner(nil), false)
+
+    s.material = {SignerDragon.CARD_MAJESTIC_DRAGON, card_code}
+    s.listed_names = {SignerDragon.CARD_MAJESTIC_DRAGON, card_code}
+    s.synchro_nt_required = 1
+end
