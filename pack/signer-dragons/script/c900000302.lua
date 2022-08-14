a6364932948c31aeb5177440ad1cb9807369e47f
Luc THQ <luc.ta@cptech.vn>
2022-08-14 11:15:34 +0700

    up

---
+++ pack/signer-dragons/script/c900000302.lua
@@ -0,0 +1,16 @@
+﻿-- Majestic Black Dragon
+Duel.LoadScript("util.lua")
+Duel.LoadScript("util_signer_dragon.lua")
+
+local s, id = GetID()
+s.material = {SignerDragon.CARD_MAJESTIC_DRAGON, CARD_BLACK_WINGED_DRAGON}
+s.listed_names = {SignerDragon.CARD_MAJESTIC_DRAGON, CARD_BLACK_WINGED_DRAGON}
+s.synchro_nt_required = 1
+
+function s.initial_effect(c)
+    c:EnableReviveLimit()
+
+    -- synchro summon
+    Synchro.AddMajesticProcedure(c, aux.FilterBoolFunction(Card.IsCode, SignerDragon.CARD_MAJESTIC_DRAGON), true,
+        aux.FilterBoolFunction(Card.IsCode, CARD_BLACK_WINGED_DRAGON), true, Synchro.NonTuner(nil), false)
+end
---
+++ utility/util_signer_dragon.lua
@@ -0,0 +1,11 @@
+-- init
+if not aux.SignerDragonProcedure then
+    aux.SignerDragonProcedure = {}
+end
+if not SignerDragon then SignerDragon = aux.SignerDragonProcedure end
+
+-- constant
+SignerDragon.CARD_MAJESTIC_DRAGON = 21159309
+
+-- function
+    
