12baec2e30573fe05f524704382dd7bd1fac3bfb
Luc THQ <luc.ta@cptech.vn>
2022-08-15 15:31:40 +0700

    up

--- pack/signer-dragons/script/c900000302.lua
+++ pack/signer-dragons/script/c900000302.lua
@@ -56,15 +56,16 @@ function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     end
 
     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_NEGATE)
-    local g = Duel.SelectTarget(tp, s.e2filter, tp, 0, LOCATION_MZONE, 1, 1, nil)
+    local tc = Duel.SelectTarget(tp, s.e2filter, tp, 0, LOCATION_MZONE, 1, 1, nil):GetFirst()
 
-    Duel.SetOperationInfo(0, CATEGORY_DISABLE, g, 1, 0, 0)
+    Duel.SetOperationInfo(0, CATEGORY_DISABLE, tc, 1, 0, 0)
+    Duel.SetOperationInfo(0, CATEGORY_DAMAGE, nil, 0, 1 - tp, tc:GetAttack())
 end
 
 function s.e2op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     local tc = Duel.GetFirstTarget()
-    if not tc or not tc:IsRelateToEffect(e) or tc:IsFacedown() then
+    if not tc or not tc:IsRelateToEffect(e) or tc:IsFacedown() or tc:IsDisabled() then
         return
     end
 
@@ -76,4 +77,10 @@ function s.e2op(e, tp, eg, ep, ev, re, r, rp)
     local ec1b = ec1:Clone()
     ec1b:SetCode(EFFECT_DISABLE_EFFECT)
     tc:RegisterEffect(ec1b)
+
+    if tc:IsImmuneToEffect(ec1) or tc:IsImmuneToEffect(ec1b) then
+        return
+    end
+    Duel.AdjustInstantly(tc)
+    Duel.Damage(1 - tp, tc:GetAttack(), REASON_EFFECT)
 end
