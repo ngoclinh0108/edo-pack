c39724e74e46af691505e618558253b7580d2cd5
Luc THQ <luc.ta@cptech.vn>
2022-08-15 13:53:58 +0700

    up

--- script/c900000306.lua
+++ script/c900000306.lua
@@ -37,10 +37,10 @@ function s.initial_effect(c)
     end)
     c:RegisterEffect(e2)
 
-    -- negate
+    -- negate activation
     local e3 = Effect.CreateEffect(c)
     e3:SetDescription(aux.Stringid(id, 0))
-    e3:SetCategory(CATEGORY_NEGATE)
+    e3:SetCategory(CATEGORY_NEGATE + CATEGORY_DESTROY)
     e3:SetType(EFFECT_TYPE_QUICK_O)
     e3:SetProperty(EFFECT_FLAG_DAMAGE_STEP + EFFECT_FLAG_DAMAGE_CAL)
     e3:SetCode(EVENT_CHAINING)
@@ -76,17 +76,24 @@ end
 
 function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
+    local rc = re:GetHandler()
     if chk == 0 then
         return true
     end
 
-    Duel.SetOperationInfo(0, CATEGORY_NEGATE, eg, 1, 0, 0)
+    Duel.SetOperationInfo(0, CATEGORY_NEGATE, eg, #eg, 0, 0)
+    if rc:IsDestructable() and rc:IsRelateToEffect(re) then
+        Duel.SetOperationInfo(0, CATEGORY_DESTROY, eg, #eg, 0, 0)
+    end
 end
 
 function s.e3op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     c:RegisterFlagEffect(id, RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END, 0, 0)
-    Duel.NegateActivation(ev)
+
+    if Duel.NegateActivation(ev) and re:GetHandler():IsRelateToEffect(re) then
+        Duel.Destroy(eg, REASON_EFFECT)
+    end
 end
 
 function s.e3retop(e, tp, eg, ep, ev, re, r, rp)
