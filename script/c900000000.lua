2590a524faf589b6b6a776204e8b9ac1b896290f
Luc THQ <luc.ta@cptech.vn>
2022-08-15 19:12:40 +0700

    up

--- script/c900000000.lua
+++ script/c900000000.lua
@@ -17,12 +17,11 @@ end
 
 function s.startup(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
-    
+
     -- remove from duel
     Duel.DisableShuffleCheck(true)
     Duel.SendtoDeck(c, tp, -2, REASON_RULE)
-    if c:IsPreviousLocation(LOCATION_HAND) and
-        Duel.GetFieldGroupCount(tp, LOCATION_DECK, 0) > 0 then
+    if c:IsPreviousLocation(LOCATION_HAND) and Duel.GetFieldGroupCount(tp, LOCATION_DECK, 0) > 0 then
         Duel.Draw(p, 1, REASON_RULE)
     end
     e:Reset()
@@ -57,15 +56,14 @@ function s.startup(e, tp, eg, ep, ev, re, r, rp)
     mulligan:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
     mulligan:SetCode(EVENT_ADJUST)
     mulligan:SetCountLimit(1, id, EFFECT_COUNT_CODE_DUEL)
-    mulligan:SetCondition(function() return Duel.GetTurnCount() == 1 end)
+    mulligan:SetCondition(function()
+        return Duel.GetTurnCount() == 1
+    end)
     mulligan:SetOperation(function(e, tp)
-        if Duel.GetFieldGroupCount(tp, LOCATION_HAND, 0) > 0 and
-            Duel.GetFieldGroupCount(tp, LOCATION_DECK, 0) > 0 and
+        if Duel.GetFieldGroupCount(tp, LOCATION_HAND, 0) > 0 and Duel.GetFieldGroupCount(tp, LOCATION_DECK, 0) > 0 and
             Duel.SelectYesNo(tp, aux.Stringid(id, 0)) then
             local max = Duel.GetFieldGroupCount(tp, LOCATION_HAND, 0)
-            local g = Utility.SelectMatchingCard(HINTMSG_TODECK, tp, aux.TRUE,
-                                                 tp, LOCATION_HAND, 0, 1, max,
-                                                 nil)
+            local g = Utility.SelectMatchingCard(HINTMSG_TODECK, tp, aux.TRUE, tp, LOCATION_HAND, 0, 1, max, nil)
             local ct = Duel.SendtoDeck(g, nil, SEQ_DECKBOTTOM, REASON_RULE)
             Duel.Draw(tp, ct, REASON_RULE)
             Duel.ShuffleDeck(tp)
@@ -85,14 +83,12 @@ function s.startup(e, tp, eg, ep, ev, re, r, rp)
     end)
     field:SetOperation(function(e, tp, eg, ep, ev, re, r, rp)
         local g = Duel.GetMatchingGroup(function(c)
-            return c:IsType(TYPE_FIELD) and
-                       Utility.CheckActivateEffectCanApply(c, e, tp, false,
-                                                           true, false)
-        end, tp, LOCATION_HAND + LOCATION_DECK + LOCATION_GRAVE +
-                                            LOCATION_REMOVED, 0, nil)
-        if Duel.IsExistingMatchingCard(Card.IsFaceup, tp, LOCATION_FZONE, 0, 1,
-                                       nil) or #g == 0 or
-            not Duel.SelectYesNo(tp, aux.Stringid(id, 1)) then return end
+            return c:IsType(TYPE_FIELD) and Utility.CheckActivateEffectCanApply(c, e, tp, false, true, false)
+        end, tp, LOCATION_HAND + LOCATION_DECK + LOCATION_GRAVE + LOCATION_REMOVED, 0, nil)
+        if Duel.IsExistingMatchingCard(Card.IsFaceup, tp, LOCATION_FZONE, 0, 1, nil) or #g == 0 or
+            not Duel.SelectYesNo(tp, aux.Stringid(id, 1)) then
+            return
+        end
 
         local sc = Utility.GroupSelect(HINTMSG_TOFIELD, g, tp):GetFirst()
         aux.PlayFieldSpell(sc, e, tp, eg, ep, ev, re, r, rp)
@@ -111,8 +107,7 @@ function s.startup(e, tp, eg, ep, ev, re, r, rp)
     ddraw:SetCode(EVENT_PREDRAW)
     ddraw:SetCountLimit(1)
     ddraw:SetCondition(function(e, tp)
-        return s.destiny_draw == 1 and Duel.IsTurnPlayer(tp) and
-                   Duel.GetFieldGroupCount(tp, LOCATION_DECK, 0) > 1 and
+        return s.destiny_draw == 1 and Duel.IsTurnPlayer(tp) and Duel.GetFieldGroupCount(tp, LOCATION_DECK, 0) > 1 and
                    Duel.GetTurnCount() > 1
     end)
     ddraw:SetOperation(function(e, tp, eg, ep, ev, re, r, rp)
@@ -128,7 +123,9 @@ Duel.Draw = function(...)
     local tp = tb[1]
     local count = tb[2]
 
-    if s.destiny_draw == 1 then s.destinydraw(tp, count) end
+    if s.destiny_draw == 1 then
+        s.destinydraw(tp, count)
+    end
     return ddr(...)
 end
 
@@ -137,8 +134,7 @@ function s.destinydraw(tp, count)
         return
     end
 
-    local g = Utility.SelectMatchingCard(HINTMSG_SELECT, tp, aux.TRUE, tp,
-                                         LOCATION_DECK, 0, count, count, nil)
+    local g = Utility.SelectMatchingCard(HINTMSG_SELECT, tp, aux.TRUE, tp, LOCATION_DECK, 0, count, count, nil)
     for tc in aux.Next(g) do
         Duel.MoveSequence(tc, 0)
     end
--- script/c900000310.lua
+++ script/c900000310.lua
@@ -16,4 +16,78 @@ function s.initial_effect(c)
     code:SetCode(EFFECT_ADD_CODE)
     code:SetValue(SignerDragon.CARD_ANCIENT_FAIRY_DRAGON)
     c:RegisterEffect(code)
+
+    -- recover
+    local e1reg = Effect.CreateEffect(c)
+    e1reg:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
+    e1reg:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    e1reg:SetCode(EVENT_CHAINING)
+    e1reg:SetRange(LOCATION_MZONE)
+    e1reg:SetOperation(aux.chainreg)
+    c:RegisterEffect(e1reg)
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
+    e1:SetCode(EVENT_CHAIN_SOLVED)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetOperation(s.e1op)
+    c:RegisterEffect(e1)
+
+    -- destroy field
+    local e2 = Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id, 1))
+    e2:SetCategory(CATEGORY_DESTROY + CATEGORY_ATKCHANGE + CATEGORY_SEARCH)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1)
+    e2:SetTarget(s.e2tg)
+    e2:SetOperation(s.e2op)
+    c:RegisterEffect(e2)
+end
+
+function s.e1op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    if re:IsHasType(EFFECT_TYPE_ACTIVATE) and re:IsActiveType(TYPE_FIELD) and c:GetFlagEffect(1) > 0 and
+        Duel.SelectYesNo(tp, aux.Stringid(id, 0)) then
+        Duel.Recover(tp, 1000, REASON_EFFECT)
+    end
+end
+
+function s.e2filter(c)
+    return c:IsType(TYPE_FIELD) and c:IsAbleToHand()
+end
+
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    local g = Duel.GetMatchingGroup(aux.TRUE, 0, LOCATION_FZONE, LOCATION_FZONE, nil)
+    if chk == 0 then
+        return #g > 0
+    end
+
+    Duel.SetOperationInfo(0, CATEGORY_DESTROY, g, #g, 0, 0)
+    Duel.SetPossibleOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp, LOCATION_DECK + LOCATION_GRAVE)
+end
+
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    local dg = Duel.GetMatchingGroup(aux.TRUE, 0, LOCATION_FZONE, LOCATION_FZONE, nil)
+    if #dg == 0 or Duel.Destroy(dg, REASON_EFFECT) == 0 then
+        return
+    end
+
+    local ec1 = Effect.CreateEffect(c)
+    ec1:SetType(EFFECT_TYPE_SINGLE)
+    ec1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    ec1:SetCode(EFFECT_UPDATE_ATTACK)
+    ec1:SetValue(1000)
+    ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END + RESET_OPPO_TURN)
+    c:RegisterEffect(ec1)
+
+    local sg = Duel.GetMatchingGroup(s.e2filter, tp, LOCATION_DECK + LOCATION_GRAVE, 0, nil)
+    if #sg > 0 and Duel.SelectYesNo(tp, aux.Stringid(id, 2)) then
+        Duel.BreakEffect()
+
+        local sc = Utility.GroupSelect(HINTMSG_ATOHAND, sg, tp):GetFirst()
+        Duel.SendtoHand(sc, nil, REASON_EFFECT)
+        Duel.ConfirmCards(1 - tp, sc)
+    end
+
 end
