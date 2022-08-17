c84a232dbf7c4f3f081cc7f108a7e26309c4b34e
Luc THQ <luc.ta@cptech.vn>
2022-08-17 20:52:19 +0700

    up

---
+++ pack/signer-dragons/script/c900000312.lua
@@ -0,0 +1,8 @@
+-- Shooting Quasar Dragon
+Duel.LoadScript("util.lua")
+Duel.LoadScript("util_signer_dragon.lua")
+local s, id = GetID()
+
+function s.initial_effect(c)
+    
+end
---
+++ script/c900000312.lua
@@ -0,0 +1,7 @@
+-- Shooting Quasar Dragon
+Duel.LoadScript("util.lua")
+local s, id = GetID()
+
+function s.initial_effect(c)
+    
+end
