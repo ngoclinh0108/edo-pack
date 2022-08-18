797f9f1204a0a4bf0e8e00b827791066175e36d9
Luc THQ <luc.ta@cptech.vn>
2022-08-18 21:46:30 +0700

    up

--- script/c900000322.lua
+++ script/c900000322.lua
@@ -1,7 +1,8 @@
--- Junk
+-- Junk Accelerator
 Duel.LoadScript("util.lua")
+Duel.LoadScript("util_signer_dragon.lua")
 local s, id = GetID()
 
 function s.initial_effect(c)
-    
+    c:EnableReviveLimit()
 end
