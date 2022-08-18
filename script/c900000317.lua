cea74fd45929608e6ef2c364c06afb5742c81f20
Luc THQ <luc.ta@cptech.vn>
2022-08-18 21:46:27 +0700

    up

--- script/c900000317.lua
+++ script/c900000317.lua
@@ -1,7 +1,8 @@
 -- Assault Stardust Warrior
 Duel.LoadScript("util.lua")
+Duel.LoadScript("util_signer_dragon.lua")
 local s, id = GetID()
 
 function s.initial_effect(c)
-    
+    c:EnableReviveLimit()
 end
--- script/c900000320.lua
+++ script/c900000320.lua
@@ -1,7 +1,8 @@
 -- Quick-Span Warrior
 Duel.LoadScript("util.lua")
+Duel.LoadScript("util_signer_dragon.lua")
 local s, id = GetID()
 
 function s.initial_effect(c)
-    
+    c:EnableReviveLimit()
 end
