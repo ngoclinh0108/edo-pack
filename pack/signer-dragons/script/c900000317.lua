56113e5106cff194b1a0f7020c1820595d794850
Luc THQ <luc.ta@cptech.vn>
2022-08-19 20:39:22 +0700

    up

--- pack/signer-dragons/script/c900000317.lua
+++ pack/signer-dragons/script/c900000317.lua
@@ -3,6 +3,16 @@ Duel.LoadScript("util.lua")
 Duel.LoadScript("util_signer_dragon.lua")
 local s, id = GetID()
 
+s.material = {20932152}
+s.material_setcode = {0x1017}
+s.listed_names = {20932152}
+
 function s.initial_effect(c)
     c:EnableReviveLimit()
+
+    -- synchro summon
+    Synchro.AddProcedure(c, function(c, sc, stype, tp)
+        return c:IsSummonCode(sc, stype, tp, 20932152) or c:IsHasEffect(20932152) or c:IsSetCard(0x1017)
+    end, 1, 1, Synchro.NonTuner(nil), 1, 99)
 end
+
