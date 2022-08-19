8604d60f996abc1e1fb1479083bd5e40448d5ebe
Luc THQ <luc.ta@cptech.vn>
2022-08-19 11:50:01 +0700

    up

--- pack/signer-dragons/script/c900000315.lua
+++ pack/signer-dragons/script/c900000315.lua
@@ -11,28 +11,33 @@ function s.initial_effect(c)
     -- synhcro summon
     Synchro.AddProcedure(c, nil, 1, 1, Synchro.NonTuner(nil), 1, 99)
 
-    -- add card
-    local e1 = Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id, 0))
-    e1:SetCategory(CATEGORY_TOHAND + CATEGORY_SEARCH)
-    e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
-    e1:SetProperty(EFFECT_FLAG_DELAY + EFFECT_FLAG_DAMAGE_STEP + EFFECT_FLAG_DAMAGE_CAL)
-    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e1:SetCountLimit(1, {id, 1})
-    e1:SetTarget(s.e1tg)
-    e1:SetOperation(s.e1op)
-    c:RegisterEffect(e1)
-
     -- special summon dragon
     local e2 = Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id, 1))
+    e2:SetDescription(aux.Stringid(id, 0))
     e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
     e2:SetType(EFFECT_TYPE_IGNITION)
     e2:SetRange(LOCATION_MZONE)
-    e2:SetCountLimit(1, {id, 2})
+    e2:SetCountLimit(1, {id, 1})
     e2:SetTarget(s.e2tg)
     e2:SetOperation(s.e2op)
     c:RegisterEffect(e2)
+
+    -- special summon tuner, then synchro summon
+    local e3 = Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id, 3))
+    e3:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e3:SetType(EFFECT_TYPE_QUICK_O)
+    e3:SetCode(EVENT_FREE_CHAIN)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetHintTiming(0, TIMING_MAIN_END)
+    e3:SetCountLimit(1, {id, 2})
+    e3:SetCondition(s.e3con)
+    e3:SetCost(aux.CostWithReplace(s.e3cost, 84012625, function()
+        return e3:GetLabel() == 1
+    end))
+    e3:SetTarget(s.e3tg)
+    e3:SetOperation(s.e3op)
+    c:RegisterEffect(e3)
 end
 
 function s.e1filter(c)
@@ -47,8 +52,8 @@ function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
 end
 
 function s.e1op(e, tp, eg, ep, ev, re, r, rp)
-    local g = Utility.SelectMatchingCard(HINTMSG_ATOHAND, tp, aux.NecroValleyFilter(s.e1filter), tp, LOCATION_DECK + LOCATION_GRAVE, 0, 1, 1,
-        nil)
+    local g = Utility.SelectMatchingCard(HINTMSG_ATOHAND, tp, aux.NecroValleyFilter(s.e1filter), tp,
+        LOCATION_DECK + LOCATION_GRAVE, 0, 1, 1, nil)
     if #g > 0 then
         Duel.SendtoHand(g, nil, REASON_EFFECT)
         Duel.ConfirmCards(1 - tp, g)
@@ -72,7 +77,7 @@ function s.e2op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
 
     local ec1 = Effect.CreateEffect(c)
-    ec1:SetDescription(aux.Stringid(id, 2))
+    ec1:SetDescription(aux.Stringid(id, 1))
     ec1:SetType(EFFECT_TYPE_FIELD)
     ec1:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_CLIENT_HINT + EFFECT_FLAG_OATH)
     ec1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
@@ -90,7 +95,7 @@ function s.e2op(e, tp, eg, ep, ev, re, r, rp)
     local tc = Utility.SelectMatchingCard(HINTMSG_SPSUMMON, tp, aux.NecroValleyFilter(s.e2filter), tp,
         LOCATION_GRAVE + LOCATION_HAND, 0, 1, 1, nil, e, tp):GetFirst()
     if tc and Duel.SpecialSummon(tc, 0, tp, tp, false, false, POS_FACEUP_DEFENSE) ~= 0 and
-        Duel.SelectYesNo(tp, aux.Stringid(id, 3)) then
+        Duel.SelectYesNo(tp, aux.Stringid(id, 2)) then
         Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_LVRANK)
         local lv = Duel.AnnounceLevel(tp, 1, 12, tc:GetLevel())
 
@@ -101,5 +106,66 @@ function s.e2op(e, tp, eg, ep, ev, re, r, rp)
         ec2:SetValue(lv)
         ec2:SetReset(RESET_EVENT + RESETS_STANDARD)
         tc:RegisterEffect(ec2)
+    end
+end
+
+function s.e3filter1(c, e, tp, rc)
+    local mg = Duel.GetMatchingGroup(Card.IsCanBeSynchroMaterial, tp, LOCATION_MZONE, 0, rc)
+    local pg = aux.GetMustBeMaterialGroup(tp, Group.CreateGroup(), tp, c, nil, REASON_SYNCHRO)
+    return #pg <= 0 and c:IsType(TYPE_TUNER) and Duel.GetLocationCountFromEx(tp, tp, rc, c) > 0 and
+               c:IsCanBeSpecialSummoned(e, SUMMON_TYPE_SYNCHRO, tp, false, false) and aux.CheckSummonGate(tp, 2) and
+               Duel.IsExistingMatchingCard(s.e3filter2, tp, LOCATION_EXTRA, 0, 1, nil, mg + c)
+end
+
+function s.e3filter2(c, mg)
+    return c:IsSynchroSummonable(nil, mg)
+end
+
+function s.e3con(e, tp, eg, ep, ev, re, r, rp)
+    return Duel.GetTurnPlayer() ~= tp and
+               (Duel.GetCurrentPhase() == PHASE_MAIN1 or Duel.GetCurrentPhase() == PHASE_MAIN2)
+end
+
+function s.e3cost(e, tp, eg, ep, ev, re, r, rp, chk)
+    local c = e:GetHandler()
+    if chk == 0 then
+        return c:IsReleasable() and Duel.IsPlayerCanSpecialSummonCount(tp, 2) and
+                   Duel.IsExistingMatchingCard(s.e3filter1, tp, LOCATION_EXTRA, 0, 1, nil, e, tp, c)
+    end
+    Duel.Release(c, REASON_COST)
+end
+
+function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    local c = e:GetHandler()
+    if chk == 0 then
+        if c:IsReleasable() and Duel.IsPlayerCanSpecialSummonCount(tp, 2) and
+            Duel.IsExistingMatchingCard(s.e3filter1, tp, LOCATION_EXTRA, 0, 1, nil, e, tp) then
+            e:SetLabel(1)
+        else
+            e:SetLabel(0)
+        end
+        return true
+    end
+
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp, LOCATION_EXTRA)
+end
+
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
+    local tc = Duel.SelectMatchingCard(tp, s.e3filter1, tp, LOCATION_EXTRA, 0, 1, 1, nil, e, tp):GetFirst()
+    if not tc or Duel.SpecialSummon(tc, SUMMON_TYPE_SYNCHRO, tp, tp, false, false, POS_FACEUP_DEFENSE) == 0 then
+        return
+    end
+
+    tc:CompleteProcedure()
+    local mg = Duel.GetMatchingGroup(aux.FilterFaceupFunction(Card.IsCanBeSynchroMaterial), tp, LOCATION_MZONE, 0, nil)
+    local eg = Duel.GetMatchingGroup(s.e3filter2, tp, LOCATION_EXTRA, 0, nil, mg)
+    if #eg > 0 then
+        Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
+        local sc = eg:Select(tp, 1, 1, nil):GetFirst()
+        if not sc then
+            return
+        end
+        Duel.SynchroSummon(tp, sc, nil, mg)
     end
 end
