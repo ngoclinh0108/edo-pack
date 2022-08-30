6cff5aca5ab1291e15ad6df2806fafbb8c08d0e3
Luc THQ <luc.ta@cptech.vn>
2022-08-31 02:53:50 +0700

    up

--- script/c900000322.lua
+++ script/c900000322.lua
@@ -14,4 +14,72 @@ function s.initial_effect(c)
     Synchro.AddProcedure(c, function(c, sc, stype, tp)
         return c:IsSummonCode(sc, stype, tp, 83295594) or c:IsHasEffect(83295594)
     end, 1, 1, Synchro.NonTuner(nil), 1, 99)
+
+    -- shuffle
+    local e1 = Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id, 0))
+    e1:SetCategory(CATEGORY_TODECK + CATEGORY_DRAW)
+    e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET + EFFECT_FLAG_DELAY)
+    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetCountLimit(1, id)
+    e1:SetCondition(s.e1con)
+    e1:SetTarget(s.e1tg)
+    e1:SetOperation(s.e1op)
+    c:RegisterEffect(e1)
+
+    -- cannot be target
+    local e2 = Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetValue(aux.tgoval)
+    c:RegisterEffect(e2)
+
+    -- battle indes
+    local e3 = Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e3:SetCode(EFFECT_INDESTRUCTABLE_COUNT)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCountLimit(1)
+    e3:SetValue(function(e, re, r, rp)
+        return (r & REASON_BATTLE) ~= 0
+    end)
+    c:RegisterEffect(e3)
+end
+
+function s.e1con(e, tp, eg, ep, ev, re, r, rp)
+    return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
+end
+
+function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    if chk == 0 then
+        return Duel.IsExistingTarget(Card.IsAbleToDeck, tp, LOCATION_GRAVE, 0, 1, nil) and Duel.IsPlayerCanDraw(tp, 1)
+    end
+
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_TODECK)
+    local g = Duel.SelectTarget(tp, Card.IsAbleToDeck, tp, LOCATION_GRAVE, 0, 1, 5, nil)
+
+    Duel.SetOperationInfo(0, CATEGORY_TODECK, g, #g, 0, 0)
+    Duel.SetOperationInfo(0, CATEGORY_DRAW, nil, 0, tp, 1)
+end
+
+function s.e1op(e, tp, eg, ep, ev, re, r, rp)
+    local tg = Duel.GetChainInfo(0, CHAININFO_TARGET_CARDS)
+    if not tg or tg:FilterCount(Card.IsRelateToEffect, nil, e) == 0 then
+        return
+    end
+
+    Duel.SendtoDeck(tg, nil, 0, REASON_EFFECT)
+    local g = Duel.GetOperatedGroup()
+    if g:IsExists(Card.IsLocation, 1, nil, LOCATION_DECK) then
+        Duel.ShuffleDeck(tp)
+    end
+
+    if g:FilterCount(Card.IsLocation, nil, LOCATION_DECK + LOCATION_EXTRA) > 0 then
+        Duel.BreakEffect()
+        Duel.Draw(tp, 1, REASON_EFFECT)
+    end
 end
