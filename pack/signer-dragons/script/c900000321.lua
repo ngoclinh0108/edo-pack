7c5ec42ec36fdd8e3c4c82cc280605140b8ac6ef
Luc THQ <luc.ta@cptech.vn>
2022-08-31 02:39:35 +0700

    up

---
+++ pack/signer-dragons/script/c900000321.lua
@@ -0,0 +1,17 @@
+-- Quick-Span Warrior
+Duel.LoadScript("util.lua")
+Duel.LoadScript("util_signer_dragon.lua")
+local s, id = GetID()
+
+s.material = {11287364}
+s.material_setcode = {0x1017}
+s.listed_names = {11287364}
+
+function s.initial_effect(c)
+    c:EnableReviveLimit()
+
+    -- synchro summon
+    Synchro.AddProcedure(c, function(c, sc, stype, tp)
+        return c:IsSummonCode(sc, stype, tp, 11287364) or c:IsHasEffect(20932152) or c:IsSetCard(0x1017)
+    end, 1, 1, Synchro.NonTuner(nil), 1, 99)
+end
