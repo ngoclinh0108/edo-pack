71ffa27237363a2691b6accb6c55cf0568b5f0cf
Luc THQ <luc.ta@cptech.vn>
2022-08-30 21:22:37 +0700

    up

--- pack/signer-dragons/script/c900000319.lua
+++ pack/signer-dragons/script/c900000319.lua
@@ -1,4 +1,4 @@
--- Junk Dragon
+-- Junk Assault Dragon
 Duel.LoadScript("util.lua")
 local s, id = GetID()
 
@@ -18,12 +18,34 @@ function s.initial_effect(c)
     e1:SetDescription(aux.Stringid(id, 0))
     e1:SetCategory(CATEGORY_DESTROY)
     e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
-    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET + EFFECT_FLAG_DELAY + EFFECT_FLAG_DAMAGE_STEP + EFFECT_FLAG_DAMAGE_CAL)
     e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetCountLimit(1, id)
     e1:SetCondition(s.e1con)
     e1:SetTarget(s.e1tg)
     e1:SetOperation(s.e1op)
     c:RegisterEffect(e1)
+
+    -- cannot be destroyed by effects
+    local e2 = Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e2:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetValue(1)
+    c:RegisterEffect(e2)
+
+    -- inflict damage
+    local e3 = Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id, 1))
+    e3:SetCategory(CATEGORY_DAMAGE)
+    e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_F)
+    e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e3:SetCode(EVENT_BATTLE_DESTROYING)
+    e3:SetCondition(aux.bdcon)
+    e3:SetTarget(s.e3tg)
+    e3:SetOperation(s.e3op)
+    c:RegisterEffect(e3)
 end
 
 function s.e1con(e, tp, eg, ep, ev, re, r, rp)
@@ -32,26 +54,51 @@ end
 
 function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     local c = e:GetHandler()
-    local mc = c:GetMaterialCount()
+    local ct = c:GetMaterialCount() - 1
     if chk == 0 then
-        return mc > 1 and Duel.IsExistingMatchingCard(aux.TRUE, tp, LOCATION_ONFIELD, LOCATION_ONFIELD, 1, c)
+        return ct > 0 and Duel.IsExistingTarget(aux.TRUE, tp, LOCATION_ONFIELD, LOCATION_ONFIELD, 1, c)
     end
 
-    Duel.SetOperationInfo(0, CATEGORY_DESTROY, nil, mc - 1, 0, LOCATION_ONFIELD)
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_DESTROY)
+    local g = Duel.SelectTarget(tp, aux.TRUE, tp, LOCATION_ONFIELD, LOCATION_ONFIELD, 1, ct, c)
+    Duel.SetOperationInfo(0, CATEGORY_DESTROY, g, #g, 0, 0)
 end
 
 function s.e1op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
+    local tg = Duel.GetChainInfo(0, CHAININFO_TARGET_CARDS)
+    local g = tg:Filter(Card.IsRelateToEffect, nil, e)
+    if #g == 0 then
+        return
+    end
 
-    local g = Utility.SelectMatchingCard(HINTMSG_DESTROY, tp, aux.TRUE, tp, LOCATION_ONFIELD, LOCATION_ONFIELD, 1,
-        c:GetMaterialCount() - 1, c)
     local ct = Duel.Destroy(g, REASON_EFFECT)
     if ct > 0 and c:IsFaceup() and c:IsRelateToEffect(e) then
         local ec1 = Effect.CreateEffect(c)
         ec1:SetType(EFFECT_TYPE_SINGLE)
         ec1:SetCode(EFFECT_UPDATE_ATTACK)
-        ec1:SetValue(ct * 200)
+        ec1:SetValue(ct * 500)
         ec1:SetReset(RESET_EVENT + RESETS_STANDARD_DISABLE)
         c:RegisterEffect(ec1)
     end
+end
+
+function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then
+        return true
+    end
+
+    local dmg = e:GetHandler():GetBattleTarget():GetBaseAttack()
+    if dmg < 0 then
+        dmg = 0
+    end
+
+    Duel.SetTargetPlayer(1 - tp)
+    Duel.SetTargetParam(dmg)
+    Duel.SetOperationInfo(0, CATEGORY_DAMAGE, nil, 0, 1 - tp, dmg)
+end
+
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+    local p, d = Duel.GetChainInfo(0, CHAININFO_TARGET_PLAYER, CHAININFO_TARGET_PARAM)
+    Duel.Damage(p, d, REASON_EFFECT)
 end
