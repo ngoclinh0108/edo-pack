2e4571133c09057dd36447dbbaa1441fe3dabe59
Luc THQ <luc.ta@cptech.vn>
2022-09-02 01:34:11 +0700

    up

--- script/c900000322.lua
+++ script/c900000322.lua
@@ -49,17 +49,22 @@ function s.initial_effect(c)
     c:RegisterEffect(e3)
 end
 
+function s.e1filter(c)
+    return c:IsFaceup() and c:IsAbleToDeck()
+end
+
 function s.e1con(e, tp, eg, ep, ev, re, r, rp)
     return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
 end
 
 function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
     if chk == 0 then
-        return Duel.IsExistingTarget(Card.IsAbleToDeck, tp, LOCATION_GRAVE, 0, 1, nil) and Duel.IsPlayerCanDraw(tp, 1)
+        return Duel.IsExistingTarget(s.e1filter, tp, LOCATION_GRAVE + LOCATION_REMOVED, 0, 1, nil) and
+                   Duel.IsPlayerCanDraw(tp, 1)
     end
 
     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_TODECK)
-    local g = Duel.SelectTarget(tp, Card.IsAbleToDeck, tp, LOCATION_GRAVE, 0, 1, 5, nil)
+    local g = Duel.SelectTarget(tp, s.e1filter, tp, LOCATION_GRAVE + LOCATION_REMOVED, 0, 1, 5, nil)
 
     Duel.SetOperationInfo(0, CATEGORY_TODECK, g, #g, 0, 0)
     Duel.SetOperationInfo(0, CATEGORY_DRAW, nil, 0, tp, 1)
