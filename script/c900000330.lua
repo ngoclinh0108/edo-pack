a74e8a7348bd66e5039eaa6f679144611420f1d2
Luc THQ <luc.ta@cptech.vn>
2022-09-05 19:55:32 +0700

    up

--- script/c900000330.lua
+++ script/c900000330.lua
@@ -1,13 +1,13 @@
--- Core Warrior
+-- Synchron Warrior
 Duel.LoadScript("util.lua")
 local s, id = GetID()
 
 s.listed_series = {0x1017}
 
 function s.initial_effect(c)
-    -- search
+    -- search & summon
     local e1 = Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_TOHAND + CATEGORY_SEARCH)
+    e1:SetCategory(CATEGORY_TOHAND + CATEGORY_SEARCH + CATEGORY_SUMMON)
     e1:SetType(EFFECT_TYPE_IGNITION)
     e1:SetRange(LOCATION_HAND)
     e1:SetCountLimit(1, id)
@@ -17,8 +17,12 @@ function s.initial_effect(c)
     c:RegisterEffect(e1)
 end
 
-function s.e1filter(c)
-    return c:IsSetCard(0x1017) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+function s.e1filter1(c)
+    return not c:IsCode(id) and c:IsSetCard(0x1017) and c:IsType(TYPE_MONSTER) and c:IsAbleToHand()
+end
+
+function s.e1filter2(c)
+    return c:IsSummonable(true, nil) and c:IsType(TYPE_TUNER)
 end
 
 function s.e1cost(e, tp, eg, ep, ev, re, r, rp, chk)
@@ -32,29 +36,31 @@ end
 
 function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then
-        return Duel.IsExistingMatchingCard(s.e1filter, tp, LOCATION_DECK, 0, 1, nil)
+        return Duel.IsExistingMatchingCard(s.e1filter1, tp, LOCATION_DECK, 0, 1, nil)
     end
 
     Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp, LOCATION_DECK)
+    Duel.SetPossibleOperationInfo(0, CATEGORY_SUMMON, nil, 1, 0, 0)
 end
 
 function s.e1op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
-    local g = Utility.SelectMatchingCard(HINTMSG_ATOHAND, tp, s.e1filter, tp, LOCATION_DECK + LOCATION_GRAVE, 0, 1, 1,
-        nil)
+    local g = Utility.SelectMatchingCard(HINTMSG_ATOHAND, tp, s.e1filter1, tp, LOCATION_DECK, 0, 1, 1, nil)
     if #g > 0 then
         Duel.SendtoHand(g, nil, REASON_EFFECT)
         Duel.ConfirmCards(1 - tp, g)
     end
 
-    if Duel.GetFlagEffect(tp, id) == 0 then
-        local ec1 = Effect.CreateEffect(c)
-        ec1:SetDescription(aux.Stringid(id, 0))
-        ec1:SetType(EFFECT_TYPE_FIELD)
-        ec1:SetCode(EFFECT_EXTRA_SUMMON_COUNT)
-        ec1:SetTargetRange(LOCATION_HAND, 0)
-        e1:SetTarget(aux.TargetBoolFunction(Card.IsSetCard, 0x1017))
-        ec1:SetReset(RESET_PHASE + PHASE_END)
-        Duel.RegisterEffect(ec1, tp)
+    if Duel.IsExistingMatchingCard(s.e1filter2, tp, LOCATION_HAND, 0, 1, nil) and
+        Duel.SelectYesNo(tp, aux.Stringid(id, 0)) then
+        Duel.BreakEffect()
+        Duel.ShuffleHand(tp)
+
+        Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SUMMON)
+        local tc = Utility.SelectMatchingCard(HINTMSG_SUMMON, tp, s.e1filter2, tp, LOCATION_HAND + LOCATION_MZONE, 0, 1,
+            1, nil):GetFirst()
+        if tc then
+            Duel.Summon(tp, tc, true, nil)
+        end
     end
 end
