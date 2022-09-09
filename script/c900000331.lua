8d110ea999f96146d4b1776adaed03ad63c73679
Luc THQ <luc.ta@cptech.vn>
2022-09-10 00:10:35 +0700

    up

--- script/c900000331.lua
+++ script/c900000331.lua
@@ -11,6 +11,13 @@ function s.initial_effect(c)
     e1:SetLabel(id)
     e1:SetValue(s.e1val)
     c:RegisterEffect(e1)
+
+    -- recycle
+    local e2 = Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_CONTINUOUS)
+    e2:SetCode(EVENT_BE_MATERIAL)
+    e2:SetOperation(s.e2regop)
+    c:RegisterEffect(e2)
 end
 
 function s.e1val(e, tc, sc)
@@ -75,4 +82,55 @@ function s.e1synctg(e, c, sg, tg, ntg, tsg, ntsg)
     else
         return true
     end
+end
+
+function s.e2regop(e, tp, eg, ep, ev, re, r, rp)
+    if r ~= REASON_SYNCHRO then
+        return
+    end
+
+    local c = e:GetHandler()
+    local ec1 = Effect.CreateEffect(c)
+    ec1:SetCategory(CATEGORY_TOHAND + CATEGORY_SPECIAL_SUMMON)
+    ec1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_TRIGGER_O)
+    ec1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    ec1:SetCode(EVENT_PHASE + PHASE_END)
+    ec1:SetRange(LOCATION_GRAVE)
+    ec1:SetCountLimit(1, id)
+    ec1:SetTarget(s.e2tg)
+    ec1:SetOperation(s.e2op)
+    ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
+    c:RegisterEffect(ec1)
+end
+
+function s.e2filter(c, e, tp, ft)
+    return (c:GetReason() & REASON_SYNCHRO) == REASON_SYNCHRO and not c:IsCode(id) and
+               (c:IsAbleToHand() or (c:IsCanBeSpecialSummoned(e, 0, tp, false, false) and ft > 0))
+end
+
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    local ft = Duel.GetLocationCount(tp, LOCATION_MZONE)
+    if chk == 0 then
+        return Duel.IsExistingMatchingCard(s.e2filter, tp, LOCATION_GRAVE, 0, 1, nil, e, tp, ft)
+    end
+
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_ATOHAND)
+    local g = Duel.SelectTarget(tp, s.e2filter, tp, LOCATION_GRAVE, 0, 1, 1, nil, e, tp, ft)
+
+    Duel.SetOperationInfo(0, CATEGORY_TOHAND, g, #g, 0, 0)
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, g, #g, 0, 0)
+end
+
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+    local tc = Duel.GetFirstTarget()
+    if not tc or not tc:IsRelateToEffect(e) then
+        return
+    end
+
+    local ft = Duel.GetLocationCount(tp, LOCATION_MZONE)
+    aux.ToHandOrElse(tc, tp, function(c)
+        return tc:IsCanBeSpecialSummoned(e, 0, tp, false, false) and ft > 0
+    end, function(c)
+        Duel.SpecialSummon(tc, 0, tp, tp, false, false, POS_FACEUP)
+    end, 2)
 end
