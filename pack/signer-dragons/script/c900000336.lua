4810dddde6de3c501175982f5b090bc0a2705109
Luc THQ <luc.ta@cptech.vn>
2022-09-11 18:59:11 +0700

    up

--- pack/signer-dragons/script/c900000336.lua
+++ pack/signer-dragons/script/c900000336.lua
@@ -1,7 +1,9 @@
 -- Nova Rising Dragon
 Duel.LoadScript("util.lua")
+Duel.LoadScript("util_signer_dragon.lua")
 local s, id = GetID()
 
+s.listed_names = {SignerDragon.CARD_RED_DRAGON_ARCHFIEND}
 s.listed_series = {0x1045}
 
 function s.initial_effect(c)
@@ -24,6 +26,16 @@ function s.initial_effect(c)
     e1:SetTarget(s.e1tg)
     e1:SetOperation(s.e1op)
     c:RegisterEffect(e1)
+
+    -- set
+    local e2 = Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_GRAVE)
+    e2:SetCountLimit(1, {id, 2})
+    e2:SetCost(aux.bfgcost)
+    e2:SetTarget(s.e2tg)
+    e2:SetOperation(s.e2op)
+    c:RegisterEffect(e2)
 end
 
 function s.e1filter(c, e, tp)
@@ -69,5 +81,34 @@ function s.e1op(e, tp, eg, ep, ev, re, r, rp)
         ec1:SetValue(aux.indoval)
         ec1:SetReset(RESET_EVENT + RESETS_STANDARD)
         c:RegisterEffect(ec1)
+    end
+end
+
+function s.e1filter(c)
+    return c:IsType(TYPE_SPELL + TYPE_TRAP) and c:IsSSetable() and
+               aux.IsCodeListed(c, SignerDragon.CARD_RED_DRAGON_ARCHFIEND)
+end
+
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then
+        return Duel.IsExistingMatchingCard(s.e1filter, tp, LOCATION_DECK + LOCATION_GRAVE, 0, 1, nil)
+    end
+end
+
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    local tc = Utility.SelectMatchingCard(HINTMSG_SET, tp, aux.NecroValleyFilter(s.e1filter), tp,
+        LOCATION_DECK + LOCATION_GRAVE, 0, 1, 1, nil):GetFirst()
+    if tc and Duel.SSet(tp, tc) > 0 and tc:IsType(TYPE_QUICKPLAY + TYPE_TRAP) then
+        local ec1 = Effect.CreateEffect(c)
+        ec1:SetType(EFFECT_TYPE_SINGLE)
+        ec1:SetProperty(EFFECT_FLAG_SET_AVAILABLE)
+        if tc:IsType(TYPE_QUICKPLAY) then
+            ec1:SetCode(EFFECT_QP_ACT_IN_SET_TURN)
+        elseif tc:IsType(TYPE_TRAP) then
+            ec1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
+        end
+        ec1:SetReset(RESET_EVENT + RESETS_STANDARD)
+        tc:RegisterEffect(ec1)
     end
 end
