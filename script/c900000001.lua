531003314a1013739e2b69611d077b09f2f3ada6
Luc THQ <luc.ta@cptech.vn>
2022-09-13 14:08:29 +0700

    up

--- script/c900000001.lua
+++ script/c900000001.lua
@@ -1,7 +1,9 @@
 -- Giant Divine Soldier of Obelisk
 Duel.LoadScript("util.lua")
+Duel.LoadScript("util_egyptian.lua")
 local s, id = GetID()
 
 function s.initial_effect(c)
-    
+    Utility.AvatarInfinity(s, c)
+    Divine.DivineHierarchy(s, c, 1, true, true)
 end
