4f583dcc9b94c1cc54062a46ae57afdddae9e32d
Luc THQ <luc.ta@cptech.vn>
2022-09-25 05:40:48 +0700

    up

--- pack/egyptian/script/c900000004.lua
+++ pack/egyptian/script/c900000004.lua
@@ -41,113 +41,75 @@ function s.initial_effect(c)
         end
     })
 
-    -- summon ra to opponent's field
-    local e1 = Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id, 1))
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE + EFFECT_FLAG_SPSUM_PARAM)
-    e1:SetCode(EFFECT_LIMIT_SUMMON_PROC)
-    e1:SetTargetRange(POS_FACEUP_ATTACK, 1)
-    e1:SetCondition(s.e1con)
-    e1:SetTarget(s.e1tg)
-    e1:SetOperation(s.e1op)
-    e1:SetValue(SUMMON_TYPE_TRIBUTE)
-    local e1grant = Effect.CreateEffect(c)
-    e1grant:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
-    e1grant:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
-    e1grant:SetCode(EVENT_STARTUP)
-    e1grant:SetRange(LOCATION_ALL)
-    e1grant:SetCountLimit(1, id)
-    e1grant:SetOperation(function(e, tp, eg, ep, ev, re, r, rp)
-        local c = e:GetHandler()
-        local g = Duel.GetMatchingGroup(Card.IsCode, tp, LOCATION_ALL, 0, nil, CARD_RA)
-        for tc in aux.Next(g) do
-            local eff = tc:GetCardEffect(EFFECT_LIMIT_SUMMON_PROC)
-            eff:SetDescription(aux.Stringid(id, 0))
-        end
-
-        local grant = Effect.CreateEffect(c)
-        grant:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_GRANT)
-        grant:SetTargetRange(LOCATION_HAND, 0)
-        grant:SetLabelObject(e1)
-        grant:SetCondition(function(e)
-            return Dimension.IsInDimensionZone(e:GetHandler())
-        end)
-        grant:SetTarget(aux.TargetBoolFunction(Card.IsCode, CARD_RA))
-        Duel.RegisterEffect(grant, tp)
-        c:IsHasEffect(id)
-    end)
-    c:RegisterEffect(e1grant)
-
     -- cannot switch control
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetCode(EFFECT_CANNOT_CHANGE_CONTROL)
+    e1:SetRange(LOCATION_MZONE)
+    c:RegisterEffect(e1)
+
+    -- cannot be Tributed, or be used as a material
     local e2 = Effect.CreateEffect(c)
     e2:SetType(EFFECT_TYPE_SINGLE)
     e2:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e2:SetCode(EFFECT_CANNOT_CHANGE_CONTROL)
+    e2:SetCode(EFFECT_UNRELEASABLE_SUM)
     e2:SetRange(LOCATION_MZONE)
+    e2:SetValue(1)
     c:RegisterEffect(e2)
-
-    -- cannot be Tributed, or be used as a material
-    local e3 = Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_SINGLE)
-    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e3:SetCode(EFFECT_UNRELEASABLE_SUM)
-    e3:SetRange(LOCATION_MZONE)
-    e3:SetValue(1)
-    c:RegisterEffect(e3)
-    local e3b = e3:Clone()
-    e3b:SetCode(EFFECT_UNRELEASABLE_NONSUM)
-    c:RegisterEffect(e3b)
-    local e3b = e3:Clone()
-    e3b:SetCode(EFFECT_CANNOT_BE_MATERIAL)
-    c:RegisterEffect(e3b)
+    local e2b = e2:Clone()
+    e2b:SetCode(EFFECT_UNRELEASABLE_NONSUM)
+    c:RegisterEffect(e2b)
+    local e2b = e2:Clone()
+    e2b:SetCode(EFFECT_CANNOT_BE_MATERIAL)
+    c:RegisterEffect(e2b)
 
     -- immune
-    local e4 = Effect.CreateEffect(c)
-    e4:SetType(EFFECT_TYPE_SINGLE)
-    e4:SetProperty(EFFECT_FLAG_SINGLE_RANGE + EFFECT_FLAG_CANNOT_DISABLE)
-    e4:SetCode(EFFECT_IMMUNE_EFFECT)
-    e4:SetRange(LOCATION_MZONE)
-    e4:SetValue(function(e, te)
+    local e3 = Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE + EFFECT_FLAG_CANNOT_DISABLE)
+    e3:SetCode(EFFECT_IMMUNE_EFFECT)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetValue(function(e, te)
         local c = e:GetHandler()
         local tc = te:GetHandler()
         return c ~= tc and Divine.GetDivineHierarchy(tc) <= Divine.GetDivineHierarchy(c)
     end)
-    c:RegisterEffect(e4)
+    c:RegisterEffect(e3)
 
     -- cannot attack
-    local e5 = Effect.CreateEffect(c)
-    e5:SetType(EFFECT_TYPE_SINGLE)
-    e5:SetCode(EFFECT_CANNOT_ATTACK)
-    c:RegisterEffect(e5)
+    local e4 = Effect.CreateEffect(c)
+    e4:SetType(EFFECT_TYPE_SINGLE)
+    e4:SetCode(EFFECT_CANNOT_ATTACK)
+    c:RegisterEffect(e4)
 
     -- untargetable
-    local e6 = Effect.CreateEffect(c)
-    e6:SetType(EFFECT_TYPE_SINGLE)
-    e6:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e6:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
-    e6:SetRange(LOCATION_MZONE)
-    e6:SetValue(1)
-    c:RegisterEffect(e6)
-    local e6b = e6:Clone()
-    e6b:SetCode(EFFECT_IGNORE_BATTLE_TARGET)
-    c:RegisterEffect(e6b)
+    local e5 = Effect.CreateEffect(c)
+    e5:SetType(EFFECT_TYPE_SINGLE)
+    e5:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e5:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+    e5:SetRange(LOCATION_MZONE)
+    e5:SetValue(1)
+    c:RegisterEffect(e5)
+    local e5b = e5:Clone()
+    e5b:SetCode(EFFECT_IGNORE_BATTLE_TARGET)
+    c:RegisterEffect(e5b)
 
     -- battle mode
-    local e7 = Effect.CreateEffect(c)
-    e7:SetDescription(aux.Stringid(id, 2))
-    e7:SetType(EFFECT_TYPE_IGNITION)
-    e7:SetProperty(EFFECT_FLAG_BOTH_SIDE + EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_CANNOT_NEGATE +
+    local e6 = Effect.CreateEffect(c)
+    e6:SetDescription(aux.Stringid(id, 0))
+    e6:SetType(EFFECT_TYPE_IGNITION)
+    e6:SetProperty(EFFECT_FLAG_BOTH_SIDE + EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_CANNOT_NEGATE +
                        EFFECT_FLAG_CANNOT_INACTIVATE)
-    e7:SetRange(LOCATION_MZONE)
-    e7:SetCountLimit(1)
-    e7:SetCondition(function(e)
+    e6:SetRange(LOCATION_MZONE)
+    e6:SetCountLimit(1)
+    e6:SetCondition(function(e)
         return Duel.GetTurnCount() ~= e:GetHandler():GetTurnID()
     end)
-    e7:SetCost(s.e7cost)
-    e7:SetTarget(s.e7tg)
-    e7:SetOperation(s.e7op)
-    c:RegisterEffect(e7)
+    e6:SetCost(s.e6cost)
+    e6:SetTarget(s.e6tg)
+    e6:SetOperation(s.e6op)
+    c:RegisterEffect(e6)
 end
 
 function s.e1con(e, c, minc, zone, relzone, exeff)
@@ -210,21 +172,21 @@ function s.e1op(e, tp, eg, ep, ev, re, r, rp, c, minc, zone, relzone, exeff)
     g:DeleteGroup()
 end
 
-function s.e7cost(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e6cost(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
     local g = Duel.GetMatchingGroup(Card.IsCode, tp, LOCATION_ALL, 0, nil, 78665705)
     if chk == 0 then
         return tp == c:GetOwner() or #g > 0
     end
 
-    if tp == c:GetOwner() and (#g == 0 or not Duel.SelectYesNo(tp, aux.Stringid(id, 3))) then
+    if tp == c:GetOwner() and (#g == 0 or not Duel.SelectYesNo(tp, aux.Stringid(id, 2))) then
         return
     end
 
     Duel.ConfirmCards(1 - tp, g:GetFirst())
 end
 
-function s.e7tg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e6tg(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
     local mc = c:GetMaterial():GetFirst()
     if chk == 0 then
@@ -233,7 +195,7 @@ function s.e7tg(e, tp, eg, ep, ev, re, r, rp, chk)
     end
 end
 
-function s.e7op(e, tp, eg, ep, ev, re, r, rp)
+function s.e6op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     local tc = c:GetMaterial():GetFirst()
 
--- pack/egyptian/script/c900000008.lua
+++ pack/egyptian/script/c900000008.lua
@@ -5,5 +5,88 @@ local s, id = GetID()
 s.listed_names = {CARD_RA}
 
 function s.initial_effect(c)
+    local EFFECT_FLAG_CANNOT_NEGATE_ACTIV_EFF = EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_CANNOT_NEGATE +
+                                                    EFFECT_FLAG_CANNOT_INACTIVATE
 
+    -- special summon ra
+    local e1 = Effect.CreateEffect(c)
+    e1:SetDescription(aux.Stringid(id, 0))
+    e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+    e1:SetType(EFFECT_TYPE_ACTIVATE)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_NEGATE_ACTIV_EFF)
+    e1:SetCode(EVENT_FREE_CHAIN)
+    e1:SetCountLimit(1, {id, 0})
+    e1:SetTarget(s.e1tg)
+    e1:SetOperation(s.e1op)
+    c:RegisterEffect(e1)
+end
+
+function s.e1filter1(c, ft, tp)
+    return ft > 0 or (c:IsControler(tp) and c:GetSequence() < 5)
+end
+
+function s.e1filter2(c, e, tp)
+    return c:IsCode(CARD_RA) and c:IsCanBeSpecialSummoned(e, 0, tp, true, false)
+end
+
+function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    local ft = Duel.GetLocationCount(tp, LOCATION_MZONE)
+    local check1 = Duel.CheckReleaseGroup(tp, s.e1filter1, 3, nil, ft, tp)
+    local check2 = ft > 0 and Duel.CheckReleaseGroup(1 - tp, nil, 3, nil)
+
+    if chk == 0 then
+        return (check1 or check2) and Duel.IsExistingMatchingCard(s.e1filter2, tp, LOCATION_HAND, 0, 1, nil, e, tp)
+    end
+
+    Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp, LOCATION_HAND)
+end
+
+function s.e1op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    local ft = Duel.GetLocationCount(tp, LOCATION_MZONE)
+    local check1 = Duel.CheckReleaseGroup(tp, s.e1filter1, 3, nil, ft, tp)
+    local check2 = ft > 0 and Duel.CheckReleaseGroup(1 - tp, nil, 3, nil)
+
+    local opt = {}
+    local sel = {}
+    if check1 then
+        table.insert(sel, 1)
+        table.insert(opt, aux.Stringid(id, 1))
+    end
+    if check2 then
+        table.insert(sel, 2)
+        table.insert(opt, aux.Stringid(id, 2))
+    end
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_EFFECT)
+    local op = sel[Duel.SelectOption(tp, table.unpack(opt)) + 1]
+
+    local g = Group.CreateGroup()
+    if op == 1 then
+        g = Duel.SelectReleaseGroup(tp, s.e1filter1, 3, 3, nil, ft, tp)
+    else
+        g = Duel.SelectReleaseGroup(1 - tp, nil, 3, 3, nil)
+    end
+
+    local tc =
+        Utility.SelectMatchingCard(HINTMSG_SPSUMMON, tp, s.e1filter2, tp, LOCATION_HAND, 0, 1, 1, nil, e, tp):GetFirst()
+    if tc and Duel.Release(g, REASON_EFFECT) == 3 and Duel.SpecialSummonStep(tc, 0, tp, tp, true, false, POS_FACEUP) then
+        local atk = 0
+        local def = 0
+        for mc in aux.Next(g) do
+            atk = atk + mc:GetAttack()
+            def = def + mc:GetDefense()
+        end
+
+        local ec1 = Effect.CreateEffect(c)
+        ec1:SetType(EFFECT_TYPE_SINGLE)
+        ec1:SetCode(EFFECT_SET_BASE_ATTACK)
+        ec1:SetValue(atk)
+        ec1:SetReset(RESET_EVENT + RESETS_STANDARD)
+        tc:RegisterEffect(ec1)
+        local ec1b = ec1:Clone()
+        ec1b:SetCode(EFFECT_SET_BASE_DEFENSE)
+        ec1b:SetValue(def)
+        tc:RegisterEffect(ec1b)
+    end
+    Duel.SpecialSummonComplete()
 end
