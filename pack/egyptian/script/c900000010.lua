73f410ddfb09e9f8a36563eacf2b38a07d4561f4
Luc THQ <luc.ta@cptech.vn>
2022-10-15 16:00:56 +0700

    up

---
+++ pack/egyptian/script/c900000010.lua
@@ -0,0 +1,7 @@
+-- In the Name of the Pharaoh
+Duel.LoadScript("util.lua")
+local s, id = GetID()
+
+function s.initial_effect(c)
+    
+end
---
+++ pack/egyptian/script/c900000012.lua
@@ -0,0 +1,7 @@
+-- Egyptian God Slime 3
+Duel.LoadScript("util.lua")
+local s, id = GetID()
+
+function s.initial_effect(c)
+    
+end
