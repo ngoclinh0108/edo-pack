6d816039e8cbfda71fb2101ef8040c9cc2a47b16
Luc THQ <luc.ta@cptech.vn>
2022-11-18 19:22:51 +0700

    up

--- pack/egyptian/script/c900000013.lua
+++ pack/egyptian/script/c900000013.lua
@@ -2,6 +2,19 @@
 Duel.LoadScript("util.lua")
 local s, id = GetID()
 
+s.listed_names = {39913299}
+
 function s.initial_effect(c)
-    
+    local EFFECT_FLAG_CANNOT_NEGATE_ACTIV_EFF = EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_CANNOT_NEGATE +
+                                                    EFFECT_FLAG_CANNOT_INACTIVATE
+
+    -- act in set turn
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SET_AVAILABLE + EFFECT_FLAG_CANNOT_NEGATE_ACTIV_EFF)
+    e1:SetCode(EFFECT_TRAP_ACT_IN_SET_TURN)
+    e1:SetCondition(function(e)
+        return not Duel.IsExistingMatchingCard(Card.IsCode, e:GetHandlerPlayer(), LOCATION_GRAVE, 0, 1, 39913299)
+    end)
+    c:RegisterEffect(e1)
 end
