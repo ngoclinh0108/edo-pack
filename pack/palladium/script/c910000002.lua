4a7ee8731f89dac0400140412ba6e3dd9ebb6489
Luc THQ <luc.ta@cptech.vn>
2022-11-26 16:18:00 +0700

    up

---
+++ pack/palladium/script/c910000002.lua
@@ -0,0 +1,123 @@
+-- Chaos Palladium Oracle Aknamkanon
+Duel.LoadScript("util.lua")
+local s, id = GetID()
+
+s.listed_names = {71703785}
+s.listed_series = {0x13a}
+
+function s.initial_effect(c)
+    c:EnableReviveLimit()
+
+    -- summon success
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_CONTINUOUS)
+    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetOperation(s.e1op)
+    c:RegisterEffect(e1)
+
+    -- search
+    local e2 = Effect.CreateEffect(c)
+    e2:SetCategory(CATEGORY_TOHAND + CATEGORY_SEARCH + CATEGORY_TODECK)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_HAND)
+    e2:SetCountLimit(1, {id, 1})
+    e2:SetCost(s.e2cost)
+    e2:SetTarget(s.e2tg)
+    e2:SetOperation(s.e2op)
+    c:RegisterEffect(e2)
+
+    -- to hand
+    local e3 = Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id, 1))
+    e3:SetCategory(CATEGORY_TOHAND)
+    e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e3:SetCode(EVENT_BATTLE_DESTROYING)
+    e3:SetCountLimit(1, {id, 2})
+    e3:SetCondition(aux.bdocon)
+    e3:SetTarget(s.e3tg)
+    e3:SetOperation(s.e3op)
+    c:RegisterEffect(e3)
+end
+
+function s.ritual_custom_check(e, tp, g, c)
+    return g:IsExists(Card.IsRace, 1, nil, RACE_SPELLCASTER)
+end
+
+function s.e1op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    Utility.HintCard(c)
+
+    local ec1 = Effect.CreateEffect(c)
+    ec1:SetDescription(aux.Stringid(id, 0))
+    ec1:SetType(EFFECT_TYPE_FIELD)
+    ec1:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_CLIENT_HINT)
+    ec1:SetCode(EFFECT_CANNOT_ACTIVATE)
+    ec1:SetTargetRange(0, 1)
+    ec1:SetValue(function(e, re)
+        return re:IsActiveType(TYPE_MONSTER)
+    end)
+    ec1:SetReset(RESET_PHASE + PHASE_END)
+    Duel.RegisterEffect(ec1, tp)
+end
+
+function s.e2filter(c)
+    if not c:IsAbleToHand() then
+        return false
+    end
+
+    return c:IsCode(71703785) or (c:ListsCode(71703785) and c:IsMonster() and not c:IsType(TYPE_RITUAL))
+end
+
+function s.e2cost(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then
+        return not e:GetHandler():IsPublic()
+    end
+
+    Duel.ConfirmCards(1 - tp, e:GetHandler())
+end
+
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then
+        return Duel.IsExistingMatchingCard(s.e2filter, tp, LOCATION_DECK + LOCATION_GRAVE, 0, 1, nil)
+    end
+
+    Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp, LOCATION_DECK + LOCATION_GRAVE)
+    Duel.SetOperationInfo(0, CATEGORY_TODECK, nil, 1, tp, LOCATION_HAND)
+end
+
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+    local tc = Utility.SelectMatchingCard(HINTMSG_ATOHAND, tp, aux.NecroValleyFilter(s.e2filter), tp,
+        LOCATION_DECK + LOCATION_GRAVE, 0, 1, 1, nil):GetFirst()
+    if tc and Duel.SendtoHand(tc, nil, REASON_EFFECT) > 0 and tc:IsLocation(LOCATION_HAND) then
+        Duel.ConfirmCards(1 - tp, tc)
+        Duel.ShuffleHand(tp)
+        Duel.ShuffleDeck(tp)
+        Duel.BreakEffect()
+
+        local g = Utility.SelectMatchingCard(HINTMSG_TODECK, tp, Card.IsAbleToDeck, tp, LOCATION_HAND, 0, 1, 1, nil)
+        Duel.SendtoDeck(g, nil, SEQ_DECKTOP, REASON_EFFECT)
+    end
+end
+
+function s.e3filter(c)
+    return c:IsType(TYPE_SPELL + TYPE_TRAP) and c:IsAbleToHand()
+end
+
+function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then
+        return Duel.IsExistingTarget(s.e3filter, tp, LOCATION_GRAVE, 0, 1, nil)
+    end
+
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_ATOHAND)
+    local g = Duel.SelectTarget(tp, s.e3filter, tp, LOCATION_GRAVE, 0, 1, 1, nil)
+    Duel.SetOperationInfo(0, CATEGORY_TOHAND, g, #g, 0, 0)
+end
+
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+    local tc = Duel.GetFirstTarget()
+    if not tc:IsRelateToEffect(e) then
+        return
+    end
+    Duel.SendtoHand(tc, nil, REASON_EFFECT)
+end
