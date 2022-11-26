d44f532815ea27e37356d4ac3205dfbeae7d0b86
Luc THQ <luc.ta@cptech.vn>
2022-11-26 19:06:00 +0700

    up

--- pack/palladium/script/c910000002.lua
+++ pack/palladium/script/c910000002.lua
@@ -51,8 +51,7 @@ end
 function s.e1cost(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
     if chk == 0 then
-        return
-            not c:IsPublic() and Duel.GetActivityCount(tp, ACTIVITY_SUMMON) == 0
+        return not c:IsPublic() and Duel.GetActivityCount(tp, ACTIVITY_SUMMON) == 0
     end
 
     Duel.ConfirmCards(1 - tp, c)
@@ -60,8 +59,7 @@ function s.e1cost(e, tp, eg, ep, ev, re, r, rp, chk)
     local ec1 = Effect.CreateEffect(c)
     ec1:SetDescription(aux.Stringid(id, 2))
     ec1:SetType(EFFECT_TYPE_FIELD)
-    ec1:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_OATH +
-                        EFFECT_FLAG_CLIENT_HINT)
+    ec1:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_OATH + EFFECT_FLAG_CLIENT_HINT)
     ec1:SetCode(EFFECT_CANNOT_SUMMON)
     ec1:SetTargetRange(1, 0)
     ec1:SetReset(RESET_PHASE + PHASE_END)
@@ -70,20 +68,15 @@ end
 
 function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then
-        return Duel.IsExistingMatchingCard(s.e1filter, tp,
-                                           LOCATION_DECK + LOCATION_GRAVE, 0, 1,
-                                           nil)
+        return Duel.IsExistingMatchingCard(s.e1filter, tp, LOCATION_DECK + LOCATION_GRAVE, 0, 1, nil)
     end
 
-    Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp,
-                          LOCATION_DECK + LOCATION_GRAVE)
+    Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp, LOCATION_DECK + LOCATION_GRAVE)
 end
 
 function s.e1op(e, tp, eg, ep, ev, re, r, rp)
-    local g = Utility.SelectMatchingCard(HINTMSG_ATOHAND, tp,
-                                         aux.NecroValleyFilter(s.e1filter), tp,
-                                         LOCATION_DECK + LOCATION_GRAVE, 0, 1,
-                                         1, nil)
+    local g = Utility.SelectMatchingCard(HINTMSG_ATOHAND, tp, aux.NecroValleyFilter(s.e1filter), tp,
+        LOCATION_DECK + LOCATION_GRAVE, 0, 1, 1, nil)
     if #g > 0 then
         Duel.SendtoHand(g, nil, REASON_EFFECT)
         Duel.ConfirmCards(1 - tp, g)
@@ -91,14 +84,14 @@ function s.e1op(e, tp, eg, ep, ev, re, r, rp)
 end
 
 function s.e2filter1(c, e, tp)
-    return c:IsFaceup() and c:IsCode(CARD_BLUEEYES_W_DRAGON) and
-               c:IsCanBeFusionMaterial() and
-               Duel.IsExistingMatchingCard(s.e2filter2, tp, LOCATION_EXTRA, 0,
-                                           1, nil, e, tp, c)
+    return c:IsFaceup() and c:IsSetCard(0xdd) and c:IsCanBeFusionMaterial() and
+               Duel.IsExistingMatchingCard(s.e2filter2, tp, LOCATION_EXTRA, 0, 1, nil, e, tp, c)
 end
 
 function s.e2filter2(c, e, tp, mc)
-    if Duel.GetLocationCountFromEx(tp, tp, mc, c) <= 0 then return false end
+    if Duel.GetLocationCountFromEx(tp, tp, mc, c) <= 0 then
+        return false
+    end
     local mustg = aux.GetMustBeMaterialGroup(tp, nil, tp, c, nil, REASON_FUSION)
     return c:IsType(TYPE_FUSION) and c:ListsCodeAsMaterial(mc:GetCode()) and
                c:IsCanBeSpecialSummoned(e, SUMMON_TYPE_FUSION, tp, false, false) and
@@ -107,14 +100,15 @@ end
 
 function s.e2cost(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
-    if chk == 0 then return c:IsDiscardable() end
+    if chk == 0 then
+        return c:IsDiscardable()
+    end
     Duel.SendtoGrave(c, REASON_COST + REASON_DISCARD)
 end
 
 function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then
-        return Duel.IsExistingTarget(s.e2filter1, tp, LOCATION_MZONE, 0, 1, nil,
-                                     e, tp)
+        return Duel.IsExistingTarget(s.e2filter1, tp, LOCATION_MZONE, 0, 1, nil, e, tp)
     end
 
     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_TARGET)
@@ -125,33 +119,30 @@ end
 
 function s.e2op(e, tp, eg, ep, ev, re, r, rp)
     local tc = Duel.GetFirstTarget()
-    if tc:IsFacedown() or not tc:IsRelateToEffect(e) or
-        not tc:IsCanBeFusionMaterial() or tc:IsImmuneToEffect(e) then return end
+    if tc:IsFacedown() or not tc:IsRelateToEffect(e) or not tc:IsCanBeFusionMaterial() or tc:IsImmuneToEffect(e) then
+        return
+    end
 
-    local sc = Utility.SelectMatchingCard(HINTMSG_FMATERIAL, tp, s.e2filter2,
-                                          tp, LOCATION_EXTRA, 0, 1, 1, nil, e,
-                                          tp, tc):GetFirst()
+    local sc = Utility.SelectMatchingCard(HINTMSG_FMATERIAL, tp, s.e2filter2, tp, LOCATION_EXTRA, 0, 1, 1, nil, e, tp,
+        tc):GetFirst()
     if sc then
         sc:SetMaterial(Group.FromCards(tc))
         Duel.SendtoGrave(tc, REASON_EFFECT + REASON_MATERIAL + REASON_FUSION)
         Duel.BreakEffect()
 
-        Duel.SpecialSummon(sc, SUMMON_TYPE_FUSION, tp, tp, false, false,
-                           POS_FACEUP)
+        Duel.SpecialSummon(sc, SUMMON_TYPE_FUSION, tp, tp, false, false, POS_FACEUP)
         sc:CompleteProcedure()
     end
 end
 
 function s.e3filter(c, e, tp)
-    return c:IsCode(CARD_BLUEEYES_W_DRAGON) and
-               c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
+    return c:IsCode(CARD_BLUEEYES_W_DRAGON) and c:IsCanBeSpecialSummoned(e, 0, tp, false, false)
 end
 
 function s.e3cost(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
     if chk == 0 then
-        return Duel.GetActivityCount(tp, ACTIVITY_SPSUMMON) == 0 and
-                   c:IsReleasable()
+        return Duel.GetActivityCount(tp, ACTIVITY_SPSUMMON) == 0 and c:IsReleasable()
     end
 
     Duel.Release(c, REASON_COST)
@@ -159,8 +150,7 @@ function s.e3cost(e, tp, eg, ep, ev, re, r, rp, chk)
     local ec1 = Effect.CreateEffect(c)
     ec1:SetDescription(aux.Stringid(id, 3))
     ec1:SetType(EFFECT_TYPE_FIELD)
-    ec1:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_OATH +
-                        EFFECT_FLAG_CLIENT_HINT)
+    ec1:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_OATH + EFFECT_FLAG_CLIENT_HINT)
     ec1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
     ec1:SetTargetRange(1, 0)
     ec1:SetLabelObject(e)
@@ -174,9 +164,8 @@ end
 function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
     local loc = LOCATION_HAND + LOCATION_DECK + LOCATION_GRAVE
     if chk == 0 then
-        return
-            Duel.IsExistingMatchingCard(s.e3filter, tp, loc, 0, 1, nil, e, tp) and
-                Duel.GetLocationCount(tp, LOCATION_MZONE) > -1
+        return Duel.IsExistingMatchingCard(s.e3filter, tp, loc, 0, 1, nil, e, tp) and
+                   Duel.GetLocationCount(tp, LOCATION_MZONE) > -1
     end
 
     Duel.SetOperationInfo(0, CATEGORY_SPECIAL_SUMMON, nil, 1, tp, loc)
@@ -184,14 +173,19 @@ end
 
 function s.e3op(e, tp, eg, ep, ev, re, r, rp)
     local ft = Duel.GetLocationCount(tp, LOCATION_MZONE)
-    if ft > 3 then ft = 3 end
-    if Duel.IsPlayerAffectedByEffect(tp, CARD_BLUEEYES_SPIRIT) then ft = 1 end
-    if ft <= 0 then return end
+    if ft > 3 then
+        ft = 3
+    end
+    if Duel.IsPlayerAffectedByEffect(tp, CARD_BLUEEYES_SPIRIT) then
+        ft = 1
+    end
+    if ft <= 0 then
+        return
+    end
 
-    local g = Utility.SelectMatchingCard(HINTMSG_SPSUMMON, tp,
-                                         aux.NecroValleyFilter(s.e3filter), tp,
-                                         LOCATION_HAND + LOCATION_DECK +
-                                             LOCATION_GRAVE, 0, 1, ft, nil, e,
-                                         tp)
-    if #g > 0 then Duel.SpecialSummon(g, 0, tp, tp, false, false, POS_FACEUP) end
+    local g = Utility.SelectMatchingCard(HINTMSG_SPSUMMON, tp, aux.NecroValleyFilter(s.e3filter), tp,
+        LOCATION_HAND + LOCATION_DECK + LOCATION_GRAVE, 0, 1, ft, nil, e, tp)
+    if #g > 0 then
+        Duel.SpecialSummon(g, 0, tp, tp, false, false, POS_FACEUP)
+    end
 end
--- pack/palladium/script/c910000011.lua
+++ pack/palladium/script/c910000011.lua
@@ -3,5 +3,5 @@ Duel.LoadScript("util.lua")
 local s, id = GetID()
 
 function s.initial_effect(c)
-    
+    c:AddSetcodesRule(id, true, 0xdd, 0x3b)
 end
