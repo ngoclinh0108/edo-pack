5b96ff059ba41586f428ea5c2f6a7d67b5493449
Luc THQ <luc.ta@cptech.vn>
2022-11-27 03:57:03 +0700

    up

--- pack/palladium/script/c910000017.lua
+++ pack/palladium/script/c910000017.lua
@@ -1,60 +1,65 @@
--- Palladium Beast Berfomet
+-- Palladium Beast Gazelle
 Duel.LoadScript("util.lua")
 local s, id = GetID()
 
 s.listed_series = {0x13a, 0x46}
 
 function s.initial_effect(c)
-    -- to hand
+    -- search
     local e1 = Effect.CreateEffect(c)
-    e1:SetCategory(CATEGORY_TOHAND + CATEGORY_SEARCH)
-    e1:SetType(EFFECT_TYPE_IGNITION)
-    e1:SetRange(LOCATION_HAND)
+    e1:SetCategory(CATEGORY_TOGRAVE + CATEGORY_TOHAND + CATEGORY_SEARCH)
+    e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e1:SetProperty(EFFECT_FLAG_DELAY)
+    e1:SetCode(EVENT_SUMMON_SUCCESS)
     e1:SetCountLimit(1, id)
     e1:SetCost(s.e1cost)
     e1:SetTarget(s.e1tg)
     e1:SetOperation(s.e1op)
     c:RegisterEffect(e1)
+    local e1b = e1:Clone()
+    e1b:SetCode(EVENT_SPSUMMON_SUCCESS)
+    c:RegisterEffect(e1b)
 
-    -- damage
+    -- piercing damage
     local e2 = Effect.CreateEffect(c)
-    e2:SetCategory(CATEGORY_DAMAGE)
-    e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_F)
-    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
-    e2:SetCode(EVENT_BATTLE_DESTROYING)
-    e2:SetCondition(aux.bdocon)
-    e2:SetTarget(s.e2tg)
-    e2:SetOperation(s.e2op)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_PIERCE)
     c:RegisterEffect(e2)
 
-    -- extra material
+    -- draw
     local e3 = Effect.CreateEffect(c)
-    e3:SetType(EFFECT_TYPE_SINGLE)
-    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e3:SetRange(LOCATION_GRAVE)
-    e3:SetCode(EFFECT_EXTRA_FUSION_MATERIAL)
-    e3:SetCondition(function(e)
-        return not Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(), 69832741)
-    end)
-    e3:SetOperation(Fusion.BanishMaterial)
-    e3:SetValue(function(e, c) return c:IsSetCard(0x13a) end)
+    e3:SetCategory(CATEGORY_DRAW)
+    e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_DELAY)
+    e3:SetCode(EVENT_BE_MATERIAL)
+    e3:SetCondition(s.e3con)
+    e3:SetTarget(s.e3tg)
+    e3:SetOperation(s.e3op)
     c:RegisterEffect(e3)
 end
 
-function s.e1filter(c)
-    if not c:IsAbleToHand() then return false end
-    return (c:IsSetCard(0x46) and c:IsType(TYPE_SPELL)) or
-               (c:IsLevelBelow(4) and c:IsSetCard(0x13a))
+function s.e1filter1(c)
+    return c:IsSetCard(0x13a) and c:IsLevelAbove(5) and c:IsAbleToGraveAsCost()
+end
+
+function s.e1filter2(c)
+    return c:IsSetCard(0x46) and c:IsType(TYPE_SPELL) and c:IsAbleToHand()
 end
 
 function s.e1cost(e, tp, eg, ep, ev, re, r, rp, chk)
-    if chk == 0 then return e:GetHandler():IsDiscardable() end
-    Duel.SendtoGrave(e:GetHandler(), REASON_COST + REASON_DISCARD)
+    if chk == 0 then
+        return Duel.IsExistingMatchingCard(s.e1filter1, tp, LOCATION_DECK, 0, 1,
+                                           nil)
+    end
+
+    local g = Utility.SelectMatchingCard(HINTMSG_TOGRAVE, tp, s.e1filter1, tp,
+                                         LOCATION_DECK, 0, 1, 1, nil)
+    Duel.SendtoGrave(g, REASON_COST)
 end
 
 function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then
-        return Duel.IsExistingMatchingCard(s.e1filter, tp,
+        return Duel.IsExistingMatchingCard(s.e1filter2, tp,
                                            LOCATION_DECK + LOCATION_GRAVE, 0, 1,
                                            nil)
     end
@@ -65,7 +70,7 @@ end
 
 function s.e1op(e, tp, eg, ep, ev, re, r, rp)
     local g = Utility.SelectMatchingCard(HINTMSG_ATOHAND, tp,
-                                         aux.NecroValleyFilter(s.e1filter), tp,
+                                         aux.NecroValleyFilter(s.e1filter2), tp,
                                          LOCATION_DECK + LOCATION_GRAVE, 0, 1,
                                          1, nil)
     if #g > 0 then
@@ -74,21 +79,18 @@ function s.e1op(e, tp, eg, ep, ev, re, r, rp)
     end
 end
 
-function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e3con(e, tp, eg, ep, ev, re, r, rp) return (r & REASON_FUSION) ~= 0 end
+
+function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then return true end
 
-    local c = e:GetHandler()
-    local bc = c:GetBattleTarget()
-    local dmg = bc:GetAttack()
-    if dmg < 0 then dmg = 0 end
-
-    Duel.SetTargetPlayer(1 - tp)
-    Duel.SetTargetParam(dmg)
-    Duel.SetOperationInfo(0, CATEGORY_DAMAGE, nil, 0, 1 - tp, dmg)
+    Duel.SetTargetPlayer(tp)
+    Duel.SetTargetParam(1)
+    Duel.SetOperationInfo(0, CATEGORY_DRAW, nil, 0, tp, 1)
 end
 
-function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
     local p, d = Duel.GetChainInfo(0, CHAININFO_TARGET_PLAYER,
                                    CHAININFO_TARGET_PARAM)
-    Duel.Damage(p, d, REASON_EFFECT)
+    Duel.Draw(p, d, REASON_EFFECT)
 end
---
+++ script/c910000018.lua
@@ -0,0 +1,94 @@
+-- Palladium Beast Berfomet
+Duel.LoadScript("util.lua")
+local s, id = GetID()
+
+s.listed_series = {0x13a, 0x46}
+
+function s.initial_effect(c)
+    -- to hand
+    local e1 = Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_TOHAND + CATEGORY_SEARCH)
+    e1:SetType(EFFECT_TYPE_IGNITION)
+    e1:SetRange(LOCATION_HAND)
+    e1:SetCountLimit(1, id)
+    e1:SetCost(s.e1cost)
+    e1:SetTarget(s.e1tg)
+    e1:SetOperation(s.e1op)
+    c:RegisterEffect(e1)
+
+    -- damage
+    local e2 = Effect.CreateEffect(c)
+    e2:SetCategory(CATEGORY_DAMAGE)
+    e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_F)
+    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e2:SetCode(EVENT_BATTLE_DESTROYING)
+    e2:SetCondition(aux.bdocon)
+    e2:SetTarget(s.e2tg)
+    e2:SetOperation(s.e2op)
+    c:RegisterEffect(e2)
+
+    -- extra material
+    local e3 = Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE)
+    e3:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e3:SetRange(LOCATION_GRAVE)
+    e3:SetCode(EFFECT_EXTRA_FUSION_MATERIAL)
+    e3:SetCondition(function(e)
+        return not Duel.IsPlayerAffectedByEffect(e:GetHandlerPlayer(), 69832741)
+    end)
+    e3:SetOperation(Fusion.BanishMaterial)
+    e3:SetValue(function(e, c) return c:IsSetCard(0x13a) end)
+    c:RegisterEffect(e3)
+end
+
+function s.e1filter(c)
+    if not c:IsAbleToHand() then return false end
+    return (c:IsSetCard(0x46) and c:IsType(TYPE_SPELL)) or
+               (c:IsLevelBelow(4) and c:IsSetCard(0x13a))
+end
+
+function s.e1cost(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return e:GetHandler():IsDiscardable() end
+    Duel.SendtoGrave(e:GetHandler(), REASON_COST + REASON_DISCARD)
+end
+
+function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then
+        return Duel.IsExistingMatchingCard(s.e1filter, tp,
+                                           LOCATION_DECK + LOCATION_GRAVE, 0, 1,
+                                           nil)
+    end
+
+    Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp,
+                          LOCATION_DECK + LOCATION_GRAVE)
+end
+
+function s.e1op(e, tp, eg, ep, ev, re, r, rp)
+    local g = Utility.SelectMatchingCard(HINTMSG_ATOHAND, tp,
+                                         aux.NecroValleyFilter(s.e1filter), tp,
+                                         LOCATION_DECK + LOCATION_GRAVE, 0, 1,
+                                         1, nil)
+    if #g > 0 then
+        Duel.SendtoHand(g, nil, REASON_EFFECT)
+        Duel.ConfirmCards(1 - tp, g)
+    end
+end
+
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return true end
+
+    local c = e:GetHandler()
+    local bc = c:GetBattleTarget()
+    local dmg = bc:GetAttack()
+    if dmg < 0 then dmg = 0 end
+
+    Duel.SetTargetPlayer(1 - tp)
+    Duel.SetTargetParam(dmg)
+    Duel.SetOperationInfo(0, CATEGORY_DAMAGE, nil, 0, 1 - tp, dmg)
+end
+
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+    local p, d = Duel.GetChainInfo(0, CHAININFO_TARGET_PLAYER,
+                                   CHAININFO_TARGET_PARAM)
+    Duel.Damage(p, d, REASON_EFFECT)
+end
