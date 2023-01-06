f0e61c71716e79dc5047c8d3c5fa1a48afe49fbd
Luc THQ <luc.ta@cptech.vn>
2023-01-06 11:29:21 +0700

    up

--- script/c900000002.lua
+++ script/c900000002.lua
@@ -10,14 +10,14 @@ function s.initial_effect(c)
     local e1 = Effect.CreateEffect(c)
     e1:SetType(EFFECT_TYPE_SINGLE)
     e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE + EFFECT_FLAG_CANNOT_DISABLE)
-    e1:SetCode(EFFECT_SET_BASE_ATTACK)
+    e1:SetCode(EFFECT_UPDATE_ATTACK)
     e1:SetRange(LOCATION_MZONE)
     e1:SetValue(function(e, c)
         return Duel.GetFieldGroupCount(c:GetControler(), LOCATION_HAND, 0) * 1000
     end)
     c:RegisterEffect(e1)
     local e1b = e1:Clone()
-    e1b:SetCode(EFFECT_SET_BASE_DEFENSE)
+    e1b:SetCode(EFFECT_UPDATE_ATTACK)
     c:RegisterEffect(e1b)
 
     -- atk/def down
