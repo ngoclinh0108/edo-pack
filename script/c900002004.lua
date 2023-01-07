6f56239927b5c6d2fb71ab63a942c17c97057a76
Luc THQ <luc.ta@cptech.vn>
2023-01-07 08:12:10 +0700

    up

--- script/c900002004.lua
+++ script/c900002004.lua
@@ -2,6 +2,189 @@
 Duel.LoadScript("util.lua")
 local s, id = GetID()
 
+s.listed_names = {6007213, 32491822, 69890967, 43378048}
+
 function s.initial_effect(c)
     c:EnableReviveLimit()
+
+    -- fusion summon
+    Fusion.AddProcMix(c, true, true, 6007213, 32491822, 69890967)
+    Fusion.AddContactProc(c, s.fusfilter, s.fusop, s.splimit)
+
+    -- special summon cannot be negated
+    local spsafe = Effect.CreateEffect(c)
+    spsafe:SetType(EFFECT_TYPE_SINGLE)
+    spsafe:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+    spsafe:SetCode(EFFECT_CANNOT_DISABLE_SPSUMMON)
+    c:RegisterEffect(spsafe)
+
+    -- no change battle position
+    local nopos = Effect.CreateEffect(c)
+    nopos:SetType(EFFECT_TYPE_SINGLE)
+    nopos:SetProperty(EFFECT_FLAG_SINGLE_RANGE + EFFECT_FLAG_CANNOT_DISABLE)
+    nopos:SetCode(EFFECT_CANNOT_CHANGE_POS_E)
+    nopos:SetRange(LOCATION_MZONE)
+    c:RegisterEffect(nopos)
+
+    -- cannot be tributed, or be used as a material
+    local norelease = Effect.CreateEffect(c)
+    norelease:SetType(EFFECT_TYPE_FIELD)
+    norelease:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_CANNOT_DISABLE)
+    norelease:SetCode(EFFECT_CANNOT_RELEASE)
+    norelease:SetRange(LOCATION_MZONE)
+    norelease:SetTargetRange(0, 1)
+    norelease:SetTarget(function(e, tc)
+        return tc == e:GetHandler()
+    end)
+    c:RegisterEffect(norelease)
+    local nomaterial = Effect.CreateEffect(c)
+    nomaterial:SetType(EFFECT_TYPE_SINGLE)
+    nomaterial:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    nomaterial:SetCode(EFFECT_CANNOT_BE_MATERIAL)
+    nomaterial:SetValue(function(e, tc)
+        return tc and tc:GetControler() ~= e:GetHandlerPlayer()
+    end)
+    c:RegisterEffect(nomaterial)
+
+    -- control switched
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_CONTINUOUS)
+    e1:SetProperty(EFFECT_CANNOT_DISABLE + EFFECT_FLAG_SET_AVAILABLE)
+    e1:SetCode(EVENT_CONTROL_CHANGED)
+    e1:SetCountLimit(1)
+    e1:SetCondition(s.e1con)
+    e1:SetOperation(s.e1op)
+    c:RegisterEffect(e1)
+
+    -- cannot be destroyed by battle
+    local e2 = Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_SINGLE)
+    e2:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+    e2:SetValue(1)
+    c:RegisterEffect(e2)
+
+    -- attack
+    local e3 = Effect.CreateEffect(c)
+    e3:SetDescription(aux.Stringid(id, 0))
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetProperty(EFFECT_FLAG_CARD_TARGET)
+    e3:SetRange(LOCATION_MZONE)
+    e3:SetCountLimit(1)
+    e3:SetTarget(s.e3tg)
+    e3:SetOperation(s.e3op)
+    c:RegisterEffect(e3)
+
+    -- give control
+    local e4 = Effect.CreateEffect(c)
+    e4:SetDescription(aux.Stringid(id, 1))
+    e4:SetCategory(CATEGORY_CONTROL)
+    e4:SetType(EFFECT_TYPE_IGNITION)
+    e4:SetRange(LOCATION_MZONE)
+    e4:SetCountLimit(1)
+    e4:SetTarget(s.e4tg)
+    e4:SetOperation(s.e4op)
+    c:RegisterEffect(e4)
+end
+
+function s.splimit(e, se, sp, st)
+    return not e:GetHandler():IsLocation(LOCATION_EXTRA)
+end
+
+function s.fusfilter(tp)
+    return Duel.GetMatchingGroup(Card.IsAbleToDeckOrExtraAsCost, tp, LOCATION_ONFIELD, 0, nil)
+end
+
+function s.fusop(g, tp)
+    Duel.ConfirmCards(1 - tp, g)
+    Duel.SendtoDeck(g, nil, SEQ_DECKSHUFFLE, REASON_COST + REASON_MATERIAL)
+end
+
+function s.e1con(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    return c:GetControler() ~= c:GetOwner()
+end
+
+function s.e1op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+
+    if c:IsFaceup() then
+        Duel.NegateRelatedChain(c, RESET_TURN_SET)
+        local ec1 = Effect.CreateEffect(c)
+        ec1:SetType(EFFECT_TYPE_SINGLE)
+        ec1:SetCode(EFFECT_DISABLE)
+        ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
+        c:RegisterEffect(ec1)
+        local ec1b = ec1:Clone()
+        ec1b:SetCode(EFFECT_DISABLE_EFFECT)
+        ec1b:SetValue(RESET_TURN_SET)
+        c:RegisterEffect(ec1)
+    end
+
+    local ec1 = Effect.CreateEffect(c)
+    ec1:SetCategory(CATEGORY_REMOVE)
+    ec1:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
+    ec1:SetCode(EVENT_PHASE + PHASE_END)
+    ec1:SetRange(LOCATION_MZONE)
+    ec1:SetCountLimit(1)
+    ec1:SetOperation(s.e1endop)
+    ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
+    c:RegisterEffect(ec1)
+end
+
+function s.e1endop(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    Utility.HintCard(c)
+
+    local g = Duel.GetMatchingGroup(Card.IsAbleToRemove, tp, LOCATION_ONFIELD, 0, c)
+    Duel.Remove(g, POS_FACEUP, REASON_EFFECT)
+
+    local ec1 = Effect.CreateEffect(c)
+    ec1:SetType(EFFECT_TYPE_SINGLE)
+    ec1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+    ec1:SetCode(EFFECT_SET_CONTROL)
+    ec1:SetValue(c:GetOwner())
+    ec1:SetReset(RESET_EVENT + RESETS_STANDARD - (RESET_TOFIELD + RESET_TEMP_REMOVE + RESET_TURN_SET))
+    c:RegisterEffect(ec1)
+end
+
+function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    local c = e:GetHandler()
+    if chk == 0 then
+        return c:CanAttack() and Duel.IsExistingTarget(nil, tp, 0, LOCATION_MZONE, 1, nil)
+    end
+
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_ATTACK)
+    local tc = Duel.SelectMatchingCard(tp, nil, tp, 0, LOCATION_MZONE, 1, 1, nil):GetFirst()
+    Duel.SetTargetCard(tc)
+end
+
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    local tc = Duel.GetFirstTarget()
+    if c:IsRelateToEffect(e) and c:IsFaceup() and tc and tc:IsRelateToEffect(e) and c:CanAttack() and
+        not c:IsImmuneToEffect(e) and not tc:IsImmuneToEffect(e) then
+        local ec1 = Effect.CreateEffect(c)
+        ec1:SetType(EFFECT_TYPE_SINGLE)
+        ec1:SetCode(EFFECT_SET_ATTACK_FINAL)
+        ec1:SetValue(10000)
+        ec1:SetReset(RESET_CHAIN)
+        c:RegisterEffect(ec1)
+        Duel.CalculateDamage(c, tc)
+    end
+end
+
+function s.e4tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    local c = e:GetHandler()
+    if chk == 0 then
+        return c:IsAbleToChangeControler()
+    end
+
+    Duel.SetOperationInfo(0, CATEGORY_CONTROL, c, 1, 0, 0)
+end
+
+function s.e4op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    if c:IsRelateToEffect(e) and c:IsFaceup() then
+        Duel.GetControl(c, 1 - tp)
+    end
 end
