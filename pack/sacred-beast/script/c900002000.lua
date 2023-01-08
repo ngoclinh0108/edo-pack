f37d7b4be33c4a81717d3c5eb20c64158aacf4e4
Luc THQ <luc.ta@cptech.vn>
2023-01-08 18:56:13 +0700

    up

--- pack/sacred-beast/script/c900002000.lua
+++ pack/sacred-beast/script/c900002000.lua
@@ -1,6 +1,7 @@
 -- The Eye of Phantasms
 Duel.LoadScript("util.lua")
 local s, id = GetID()
+s.listed_names = {6007213, 32491822, 69890967, 43378048}
 
 function s.initial_effect(c)
     -- activate
@@ -29,4 +30,71 @@ function s.initial_effect(c)
     local e1c = e1b:Clone()
     e1c:SetCode(EFFECT_CANNOT_DISEFFECT)
     c:RegisterEffect(e1c)
+
+    -- protect
+    local e2 = Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE)
+    e2:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
+    e2:SetRange(LOCATION_FZONE)
+    e2:SetTargetRange(LOCATION_MZONE, 0)
+    e2:SetTarget(s.e2tg)
+    e2:SetValue(aux.tgoval)
+    c:RegisterEffect(e2)
+    local e2b = e2:Clone()
+    e2b:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+    e2b:SetValue(s.e2val)
+    c:RegisterEffect(e2b)
+
+    -- draw
+    local e3 = Effect.CreateEffect(c)
+    e3:SetCategory(CATEGORY_DRAW)
+    e3:SetType(EFFECT_TYPE_IGNITION)
+    e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e3:SetRange(LOCATION_FZONE)
+    e3:SetCountLimit(1, id)
+    e3:SetCondition(s.e3con)
+    e3:SetTarget(s.e3tg)
+    e3:SetOperation(s.e3op)
+    c:RegisterEffect(e3)
+end
+
+function s.e2tg(e, c)
+    return c:IsCode(6007213, 32491822, 69890967, 43378048)
+end
+
+function s.e2val(e, re, rp)
+    return rp == 1 - e:GetHandlerPlayer()
+end
+
+function s.sbfilter(c)
+    return c:IsFaceup() and c:IsCode(6007213, 32491822, 69890967, 43378048)
+end
+
+function s.sbcount(tp)
+    return Duel.GetMatchingGroup(s.sbfilter, tp, LOCATION_ONFIELD, 0, nil):GetClassCount(Card.GetCode)
+end
+
+function s.e3con(e, tp, eg, ep, ev, re, r, rp)
+    return s.sbcount >= 1
+end
+
+function s.e3tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then
+        return Duel.IsPlayerCanDraw(tp, 2)
+    end
+
+    Duel.SetTargetPlayer(tp)
+    Duel.SetTargetParam(2)
+    Duel.SetOperationInfo(0, CATEGORY_DRAW, nil, 0, tp, 2)
+end
+
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    if not c:IsRelateToEffect(e) then
+        return
+    end
+
+    local p, d = Duel.GetChainInfo(0, CHAININFO_TARGET_PLAYER, CHAININFO_TARGET_PARAM)
+    Duel.Draw(p, d, REASON_EFFECT)
 end
