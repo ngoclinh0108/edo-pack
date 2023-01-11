91f66819eb351bf8c38886c6366099861487758b
Luc THQ <luc.ta@cptech.vn>
2023-01-12 06:11:04 +0700

    up

--- script/c900002103.lua
+++ script/c900002103.lua
@@ -84,7 +84,7 @@ function s.e2op(e, tp, eg, ep, ev, re, r, rp, chk)
         ec1:SetType(EFFECT_TYPE_SINGLE)
         ec1:SetCode(EFFECT_UPDATE_ATTACK)
         ec1:SetValue(1000)
-        ec1:SetReset(RESET_EVENT + RESETS_STANDARD)
+        ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END, 2)
         tc:RegisterEffect(ec1)
     end
 end
