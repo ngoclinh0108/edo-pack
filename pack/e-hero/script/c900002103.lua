2521c1a14cf09a1c06693b88a968f3f7a58c915f
Luc THQ <luc.ta@cptech.vn>
2023-01-12 06:07:41 +0700

    up

--- pack/e-hero/script/c900002103.lua
+++ pack/e-hero/script/c900002103.lua
@@ -26,6 +26,17 @@ function s.initial_effect(c)
     local e1b = e1:Clone()
     e1b:SetCode(EVENT_SPSUMMON_SUCCESS)
     c:RegisterEffect(e1b)
+
+    -- atk up
+    local e2 = Effect.CreateEffect(c)
+    e2:SetCategory(CATEGORY_ATKCHANGE)
+    e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
+    e2:SetCode(EVENT_BE_MATERIAL)
+    e2:SetCondition(s.e2con)
+    e2:SetTarget(s.e2tg)
+    e2:SetOperation(s.e2op)
+    c:RegisterEffect(e2)
 end
 
 function s.e1filter(c)
@@ -45,5 +56,35 @@ function s.e1op(e, tp, eg, ep, ev, re, r, rp)
         nil)
     if #g > 0 then
         Duel.SendtoGrave(g, REASON_EFFECT)
+    end
+end
+
+function s.e2filter(c)
+    return c:IsRace(RACE_FIEND) and c:IsFaceup()
+end
+
+function s.e2con(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    return (r & REASON_FUSION) == REASON_FUSION and c:IsLocation(LOCATION_GRAVE + LOCATION_REMOVED) and c:IsFaceup()
+end
+
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
+    if chk == 0 then
+        return Duel.IsExistingMatchingCard(s.e2filter, tp, LOCATION_MZONE, 0, 1, nil)
+    end
+
+    Duel.SetOperationInfo(0, CATEGORY_ATKCHANGE, nil, 1, 0, 0)
+end
+
+function s.e2op(e, tp, eg, ep, ev, re, r, rp, chk)
+    local c = e:GetHandler()
+    local tc = Utility.SelectMatchingCard(HINTMSG_FACEUP, tp, s.e2filter, tp, LOCATION_MZONE, 0, 1, 1, nil):GetFirst()
+    if tc then
+        local ec1 = Effect.CreateEffect(c)
+        ec1:SetType(EFFECT_TYPE_SINGLE)
+        ec1:SetCode(EFFECT_UPDATE_ATTACK)
+        ec1:SetValue(1000)
+        ec1:SetReset(RESET_EVENT + RESETS_STANDARD)
+        tc:RegisterEffect(ec1)
     end
 end
