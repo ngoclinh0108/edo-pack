34a64d9b66f8943a0fa774efedd54b63db945315
Luc THQ <luc.ta@cptech.vn>
2023-01-12 03:32:48 +0700

    up

--- script/c900002107.lua
+++ script/c900002107.lua
@@ -14,5 +14,16 @@ function s.initial_effect(c)
     fusname:SetOperation(function(sc, sumtype, tp)
         return (sumtype & MATERIAL_FUSION) ~= 0 or (sumtype & SUMMON_TYPE_FUSION) ~= 0
     end)
-    c:RegisterEffect(fusname)    
+    c:RegisterEffect(fusname)
+
+    -- unaffected by opponent's traps
+    local e1 = Effect.CreateEffect(c)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetCode(EFFECT_IMMUNE_EFFECT)
+    e1:SetRange(LOCATION_MZONE)
+    e1:SetValue(function(e, te)
+        return te:IsActiveType(TYPE_TRAP) and te:GetOwnerPlayer() ~= e:GetHandlerPlayer()
+    end)
+    c:RegisterEffect(e1)
 end
