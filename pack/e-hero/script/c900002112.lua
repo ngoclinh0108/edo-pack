736c308dcc692727f616374c9af7e936bc795069
Luc THQ <luc.ta@cptech.vn>
2023-01-13 01:49:29 +0700

    up

--- pack/e-hero/script/c900002112.lua
+++ pack/e-hero/script/c900002112.lua
@@ -2,8 +2,40 @@
 Duel.LoadScript("util.lua")
 local s, id = GetID()
 
+s.listed_names = {CARD_DARK_FUSION}
 s.listed_series = {0x6008}
 
 function s.initial_effect(c)
-    
+    -- return
+    local e2 = Effect.CreateEffect(c)
+    e2:SetCategory(CATEGORY_TOHAND)
+    e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e2:SetProperty(EFFECT_FLAG_DELAY + EFFECT_FLAG_DAMAGE_STEP)
+    e2:SetCode(EVENT_TO_GRAVE)
+    e2:SetCountLimit(1, id)
+    e2:SetTarget(s.e2tg)
+    e2:SetOperation(s.e2op)
+    c:RegisterEffect(e2)
+    local e2b = e2:Clone()
+    e2b:SetCode(EVENT_REMOVE)
+    c:RegisterEffect(e2b)
+end
+
+function s.e2filter(c) return (c:IsCode(CARD_DARK_FUSION) or c:ListsCode(CARD_DARK_FUSION)) and c:IsAbleToHand() end
+
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return Duel.IsExistingTarget(s.e2filter, tp, LOCATION_GRAVE, 0, 1, nil) end
+
+    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_ATOHAND)
+    local g = Duel.SelectTarget(tp, s.e2filter, tp, LOCATION_GRAVE, 0, 1, 1, nil)
+
+    Duel.SetOperationInfo(0, CATEGORY_TOHAND, g, 1, 0, 0)
+end
+
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+    local tc = Duel.GetFirstTarget()
+    if tc and tc:IsRelateToEffect(e) then
+        Duel.SendtoHand(tc, nil, REASON_EFFECT)
+        Duel.ConfirmCards(1 - tp, tc)
+    end
 end
