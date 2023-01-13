b71067cf77c04e28fcae75a2019af2a2c4045b9d
Luc THQ <luc.ta@cptech.vn>
2023-01-14 06:47:02 +0700

    up

--- pack/evil-hero/script/c900002116.lua
+++ pack/evil-hero/script/c900002116.lua
@@ -42,6 +42,18 @@ function s.initial_effect(c)
     e1:SetTarget(s.e1tg)
     e1:SetOperation(s.e1op)
     c:RegisterEffect(e1)
+
+    -- draw
+    local e2 = Effect.CreateEffect(c)
+    e2:SetDescription(aux.Stringid(id, 0))
+    e2:SetCategory(CATEGORY_DRAW)
+    e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e2:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+    e2:SetCode(EVENT_BATTLE_DESTROYING)
+    e2:SetCondition(aux.bdocon)
+    e2:SetTarget(s.e2tg)
+    e2:SetOperation(s.e2op)
+    c:RegisterEffect(e2)
 end
 
 function s.e1filter(c, ec) return c:IsCode(75524092) and c:CheckEquipTarget(ec) end
@@ -65,4 +77,17 @@ function s.e1op(e, tp, eg, ep, ev, re, r, rp)
     local tc = Utility.SelectMatchingCard(HINTMSG_EQUIP, tp, aux.NecroValleyFilter(s.e1filter), tp,
         LOCATION_HAND + LOCATION_DECK + LOCATION_GRAVE, 0, 1, 1, nil, c):GetFirst()
     if tc then Duel.Equip(tp, tc, c) end
+end
+
+function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+    if chk == 0 then return true end
+    
+    Duel.SetTargetPlayer(tp)
+    Duel.SetTargetParam(1)
+    Duel.SetOperationInfo(0, CATEGORY_DRAW, nil, 0, tp, 1)
+end
+
+function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+    local p, d = Duel.GetChainInfo(0, CHAININFO_TARGET_PLAYER, CHAININFO_TARGET_PARAM)
+    Duel.Draw(p, d, REASON_EFFECT)
 end
