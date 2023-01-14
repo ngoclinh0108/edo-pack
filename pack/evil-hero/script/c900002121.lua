b46863d5a62c5dd8906df46e359a3eeb64a5d297
Luc THQ <luc.ta@cptech.vn>
2023-01-14 13:52:03 +0700

    up

--- pack/evil-hero/script/c900002121.lua
+++ pack/evil-hero/script/c900002121.lua
@@ -15,20 +15,24 @@ function s.initial_effect(c)
             local check = function(tp, sg, fc) return sg:FilterCount(Card.IsLocation, nil, LOCATION_DECK) <= 1 end
             return g, check
         end,
-        stage2 = s.stage2
+        stage2 = s.e1op
     }))
 end
 
-function s.stage2(e, tc, tp, sg, chk)
+function s.e1op(e, tc, tp, sg, chk)
     local c = e:GetHandler()
     if chk == 1 then
         local ec1 = Effect.CreateEffect(c)
         ec1:SetDescription(3061)
         ec1:SetType(EFFECT_TYPE_SINGLE)
-        ec1:SetProperty(EFFECT_FLAG_CLIENT_HINT)
+        ec1:SetProperty(EFFECT_FLAG_IGNORE_IMMUNE + EFFECT_FLAG_CLIENT_HINT)
         ec1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
         ec1:SetValue(aux.tgoval)
-        ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END, 2)
+        if Duel.GetTurnPlayer() == tp then
+            ec1:SetReset(RESET_PHASE + PHASE_END + RESET_SELF_TURN, 2)
+        else
+            ec1:SetReset(RESET_PHASE + PHASE_END + RESET_SELF_TURN)
+        end
         tc:RegisterEffect(ec1)
     end
 end
