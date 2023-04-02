530a2c29e4b2b4c74e0aa89f6e7c527d3ffa16fe
Luc THQ <luc.ta@cptech.vn>
2023-04-03 02:34:18 +0700

    up

--- script/c900003100.lua
+++ script/c900003100.lua
@@ -3,5 +3,9 @@ Duel.LoadScript("util.lua")
 local s, id = GetID()
 
 function s.initial_effect(c)
-    
+    c:EnableReviveLimit()
+
+    -- synchro summon
+    Synchro.AddProcedure(c, aux.FilterBoolFunctionEx(Card.IsAttribute, ATTRIBUTE_DARK), 1, 1,
+        Synchro.NonTunerEx(Card.IsRace, RACE_FIEND), 1, 99)
 end
