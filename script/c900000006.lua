b67d58a829a33a07e19b6d3f06211eeebd65d6c1
Luc THQ <luc.ta@cptech.vn>
2023-04-10 05:44:38 +0700

    up

--- script/c900000006.lua
+++ script/c900000006.lua
@@ -5,40 +5,31 @@ local s, id = GetID()
 
 function s.initial_effect(c)
     Divine.WickedGod(s, c, 1)
-    
-    -- half atk/def
+
+    -- untargetabl
     local e1 = Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_FIELD)
-    e1:SetProperty(EFFECT_FLAG_DELAY)
-    e1:SetCode(EFFECT_SET_ATTACK_FINAL)
+    e1:SetType(EFFECT_TYPE_SINGLE)
+    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
+    e1:SetCode(EFFECT_CANNOT_BE_EFFECT_TARGET)
     e1:SetRange(LOCATION_MZONE)
-    e1:SetTargetRange(LOCATION_MZONE, LOCATION_MZONE)
-    e1:SetTarget(function(e, tc)
+    e1:SetValue(1)
+    c:RegisterEffect(e1)
+
+    -- half atk/def
+    local e2 = Effect.CreateEffect(c)
+    e2:SetType(EFFECT_TYPE_FIELD)
+    e2:SetProperty(EFFECT_FLAG_DELAY)
+    e2:SetCode(EFFECT_SET_ATTACK_FINAL)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetTargetRange(LOCATION_MZONE, LOCATION_MZONE)
+    e2:SetTarget(function(e, tc)
         local c = e:GetHandler()
         return tc ~= e:GetHandler() and Divine.GetDivineHierarchy(tc) <= Divine.GetDivineHierarchy(c)
     end)
-    e1:SetValue(function(e, c) return math.ceil(c:GetAttack() / 2) end)
-    c:RegisterEffect(e1)
-    local e1b = e1:Clone()
-    e1b:SetCode(EFFECT_SET_DEFENSE_FINAL)
-    e1b:SetValue(function(e, c) return math.ceil(c:GetDefense() / 2) end)
-    c:RegisterEffect(e1b)
-
-    -- activate limit
-    local e2 = Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetCode(EFFECT_CANNOT_TRIGGER)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetTargetRange(0, LOCATION_MZONE)
-    e2:SetCondition(s.e2con)
-    e2:SetTarget(s.e2tg)
+    e2:SetValue(function(e, c) return math.ceil(c:GetAttack() / 2) end)
     c:RegisterEffect(e2)
+    local e2b = e2:Clone()
+    e2b:SetCode(EFFECT_SET_DEFENSE_FINAL)
+    e2b:SetValue(function(e, c) return math.ceil(c:GetDefense() / 2) end)
+    c:RegisterEffect(e2b)
 end
-
-function s.e2con(e)
-    local c = e:GetHandler()
-    return Duel.GetAttacker() == c and c:GetBattleTarget() and
-               (Duel.GetCurrentPhase() == PHASE_DAMAGE or Duel.GetCurrentPhase() == PHASE_DAMAGE_CAL)
-end
-
-function s.e2tg(e, c) return c == e:GetHandler():GetBattleTarget() end
