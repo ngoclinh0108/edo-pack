5facbd9208df3aea7b4d0d830c74a7354c931b16
Luc THQ <luc.ta@cptech.vn>
2023-04-10 00:06:03 +0700

    up

--- script/util_egyptian.lua
+++ script/util_egyptian.lua
@@ -36,4 +36,91 @@ function Divine.DivineHierarchy(s, c, divine_hierarchy)
     sumsafe:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
     sumsafe:SetCode(EFFECT_CANNOT_DISABLE_SUMMON)
     c:RegisterEffect(sumsafe)
+
+    -- no change control and battle position
+    local noswitch = Effect.CreateEffect(c)
+    noswitch:SetType(EFFECT_TYPE_SINGLE)
+    noswitch:SetProperty(EFFECT_FLAG_SINGLE_RANGE + EFFECT_FLAG_CANNOT_DISABLE)
+    noswitch:SetCode(EFFECT_CANNOT_CHANGE_CONTROL)
+    noswitch:SetRange(LOCATION_MZONE)
+    c:RegisterEffect(noswitch)
+    local nopos = noswitch:Clone()
+    nopos:SetCode(EFFECT_CANNOT_CHANGE_POS_E)
+    c:RegisterEffect(nopos)
+
+    -- cannot be tributed, or be used as a material
+    local norelease = Effect.CreateEffect(c)
+    norelease:SetType(EFFECT_TYPE_FIELD)
+    norelease:SetProperty(EFFECT_FLAG_PLAYER_TARGET + EFFECT_FLAG_CANNOT_DISABLE)
+    norelease:SetCode(EFFECT_CANNOT_RELEASE)
+    norelease:SetRange(LOCATION_MZONE)
+    norelease:SetTargetRange(0, 1)
+    norelease:SetTarget(function(e, tc) return tc == e:GetHandler() end)
+    c:RegisterEffect(norelease)
+    local nomaterial = Effect.CreateEffect(c)
+    nomaterial:SetType(EFFECT_TYPE_SINGLE)
+    nomaterial:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    nomaterial:SetCode(EFFECT_CANNOT_BE_MATERIAL)
+    nomaterial:SetValue(function(e, tc) return tc and tc:GetControler() ~= e:GetHandlerPlayer() end)
+    c:RegisterEffect(nomaterial)
+
+    -- battle indes & avoid damage
+    local indes = Effect.CreateEffect(c)
+    indes:SetType(EFFECT_TYPE_SINGLE)
+    indes:SetProperty(EFFECT_FLAG_CANNOT_DISABLE)
+    indes:SetCode(EFFECT_INDESTRUCTABLE_BATTLE)
+    indes:SetValue(function(e, tc)
+        return tc and Divine.GetDivineHierarchy(tc) > 0 and Divine.GetDivineHierarchy(tc) <
+                   Divine.GetDivineHierarchy(e:GetHandler())
+    end)
+    c:RegisterEffect(indes)
+    local nodmg = indes:Clone()
+    nodmg:SetCode(EFFECT_AVOID_BATTLE_DAMAGE)
+    c:RegisterEffect(nodmg)
+
+    -- no leave
+    local noleave = Effect.CreateEffect(c)
+    noleave:SetType(EFFECT_TYPE_SINGLE)
+    noleave:SetProperty(EFFECT_FLAG_SINGLE_RANGE + EFFECT_FLAG_CANNOT_DISABLE)
+    noleave:SetCode(EFFECT_IMMUNE_EFFECT)
+    noleave:SetRange(LOCATION_MZONE)
+    noleave:SetValue(function(e, re)
+        if not re then return false end
+
+        local rc = re:GetHandler()
+        return
+            rc ~= e:GetHandler() and re:IsHasCategory(CATEGORY_TOHAND + CATEGORY_TODECK + CATEGORY_TOGRAVE + CATEGORY_REMOVE) and
+                (not rc:IsMonster() or Divine.GetDivineHierarchy(rc) <= Divine.GetDivineHierarchy(c))
+    end)
+    c:RegisterEffect(noleave)
+    local noleave_destroy = noleave:Clone()
+    noleave_destroy:SetCode(EFFECT_INDESTRUCTABLE_EFFECT)
+    noleave_destroy:SetValue(function(e, re)
+        local rc = re:GetHandler()
+        return rc ~= e:GetHandler() and (not rc:IsMonster() or Divine.GetDivineHierarchy(rc) <= Divine.GetDivineHierarchy(c))
+    end)
+    c:RegisterEffect(noleave_destroy)
+    local noleave_release = noleave_destroy:Clone()
+    noleave_release:SetCode(EFFECT_UNRELEASABLE_EFFECT)
+    c:RegisterEffect(noleave_release)
+
+    -- reset effect
+    local reset = Effect.CreateEffect(c)
+    reset:SetType(EFFECT_TYPE_FIELD + EFFECT_TYPE_CONTINUOUS)
+    reset:SetProperty(EFFECT_FLAG_SINGLE_RANGE + EFFECT_FLAG_CANNOT_DISABLE)
+    reset:SetRange(LOCATION_MZONE)
+    reset:SetCode(EVENT_ADJUST)
+    reset:SetCondition(function(e, tp, eg, ep, ev, re, r, rp)
+        return Duel.GetCurrentPhase() == PHASE_END and Utility.GetListEffect(e:GetHandler(), ResetEffectFilter)
+    end)
+    reset:SetOperation(function(e, tp, eg, ep, ev, re, r, rp) Utility.ResetListEffect(e:GetHandler(), ResetEffectFilter) end)
+    c:RegisterEffect(reset)
+end
+
+function ResetEffectFilter(te, c)
+    local tc = te:GetOwner()
+    if tc == c or tc:ListsCode(c:GetCode()) then return false end
+    return not te:IsHasProperty(EFFECT_FLAG_IGNORE_IMMUNE) and te:GetCode() ~= EFFECT_SPSUMMON_PROC and
+               (te:GetTarget() == aux.PersistentTargetFilter or not te:IsHasType(EFFECT_TYPE_GRANT)) and
+               not te:IsHasProperty(EFFECT_FLAG_FIELD_ONLY)
 end
