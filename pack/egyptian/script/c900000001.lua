3294ee8d06e1545322a553880f4ee181ab16e858
Luc THQ <luc.ta@cptech.vn>
2023-04-09 23:37:45 +0700

    up

--- pack/egyptian/script/c900000001.lua
+++ pack/egyptian/script/c900000001.lua
@@ -5,112 +5,13 @@ local s, id = GetID()
 
 function s.initial_effect(c)
     Utility.AvatarInfinity(s, c)
-    Divine.DivineHierarchy(s, c, 1, false)
+    Divine.DivineHierarchy(s, c, 1)
 
-    -- damage & destroy
-    local e1 = Effect.CreateEffect(c)
-    e1:SetDescription(aux.Stringid(id, 1))
-    e1:SetCategory(CATEGORY_DESTROY + CATEGORY_DAMAGE)
-    e1:SetType(EFFECT_TYPE_QUICK_O)
-    e1:SetCode(EVENT_FREE_CHAIN)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetHintTiming(0, TIMINGS_CHECK_MONSTER)
-    e1:SetCountLimit(1)
-    e1:SetCondition(s.e1con)
-    e1:SetCost(s.e1cost)
-    e1:SetTarget(s.e1tg)
-    e1:SetOperation(s.e1op)
-    c:RegisterEffect(e1)
-
-    -- soul energy MAX
-    local e2 = Effect.CreateEffect(c)
-    e2:SetDescription(aux.Stringid(id, 2))
-    e2:SetType(EFFECT_TYPE_QUICK_O)
-    e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-    e2:SetCode(EVENT_FREE_CHAIN)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetHintTiming(0, TIMING_DAMAGE_STEP)
-    e2:SetCountLimit(1)
-    e2:SetCondition(s.e2con)
-    e2:SetCost(s.e2cost)
-    e2:SetOperation(s.e2op)
-    c:RegisterEffect(e2)
-end
-
-function s.e1con(e, tp, eg, ep, ev, re, r, rp) return e:GetHandler():CanAttack() and Duel.GetCurrentChain(true) == 0 end
-
-function s.e1cost(e, tp, eg, ep, ev, re, r, rp, chk)
-    local c = e:GetHandler()
-    if chk == 0 then return c:GetAttackAnnouncedCount() == 0 and Duel.CheckReleaseGroupCost(tp, nil, 2, false, nil, c) end
-
-    local ec1 = Effect.CreateEffect(c)
-    ec1:SetDescription(3206)
-    ec1:SetType(EFFECT_TYPE_SINGLE)
-    ec1:SetProperty(EFFECT_FLAG_OATH + EFFECT_FLAG_CLIENT_HINT)
-    ec1:SetCode(EFFECT_CANNOT_ATTACK_ANNOUNCE)
-    ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
-    c:RegisterEffect(ec1)
-
-    local g = Duel.SelectReleaseGroupCost(tp, nil, 2, 2, false, nil, c)
-    Duel.Release(g, REASON_COST)
-end
-
-function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
-    local c = e:GetHandler()
-    if chk == 0 then return true end
-
-    local dmg = c:GetAttack()
-    local g = Duel.GetFieldGroup(tp, 0, LOCATION_MZONE)
-
-    Duel.SetTargetPlayer(1 - tp)
-    Duel.SetTargetParam(dmg)
-    Duel.SetOperationInfo(0, CATEGORY_DESTROY, g, #g, 0, 0)
-    Duel.SetOperationInfo(0, CATEGORY_DAMAGE, nil, 0, 1 - tp, dmg)
-end
-
-function s.e1op(e, tp, eg, ep, ev, re, r, rp)
-    local c = e:GetHandler()
-    if not c:IsRelateToEffect(e) then return end
-
-    local g = Duel.GetFieldGroup(tp, 0, LOCATION_MZONE)
-    Duel.Destroy(g, REASON_EFFECT)
-
-    local p = Duel.GetChainInfo(0, CHAININFO_TARGET_PLAYER)
-    Duel.Damage(p, c:GetAttack(), REASON_EFFECT)
-end
-
-function s.e2filter(c) return c:IsFaceup() and Divine.GetDivineHierarchy(c) >= 2 end
-
-function s.e2con(e, tp, eg, ep, ev, re, r, rp)
-    return Duel.IsExistingMatchingCard(function(tc) return tc:IsFaceup() and Divine.GetDivineHierarchy(tc) >= 2 end, tp,
-        LOCATION_MZONE, LOCATION_MZONE, 1, nil)
-end
-
-function s.e2cost(e, tp, eg, ep, ev, re, r, rp, chk)
-    local c = e:GetHandler()
-    if chk == 0 then return Duel.CheckReleaseGroupCost(tp, nil, 2, false, nil, c) end
-
-    local g = Duel.SelectReleaseGroupCost(tp, nil, 2, 2, false, nil, c)
-    Duel.Release(g, REASON_COST)
-end
-
-function s.e2op(e, tp, eg, ep, ev, re, r, rp)
-    local c = e:GetHandler()
-    c:RegisterFlagEffect(id, RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END, EFFECT_FLAG_CLIENT_HINT, 1, 0,
-        aux.Stringid(id, 0))
-
-    local ec1 = Effect.CreateEffect(c)
-    ec1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_CONTINUOUS)
-    ec1:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
-    ec1:SetCountLimit(1)
-    ec1:SetCondition(function(e, tp, eg, ep, ev, re, r, rp)
-        local c = e:GetHandler()
-        local bc = c:GetBattleTarget()
-        return bc and bc:IsControler(1 - tp) and c:GetFlagEffect(id) ~= 0
-    end)
-    ec1:SetOperation(function(e, tp, eg, ep, ev, re, r, rp)
-        Utility.GainInfinityAtk(e:GetHandler(), RESET_PHASE + PHASE_DAMAGE_CAL)
-    end)
-    ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
-    c:RegisterEffect(ec1)
+    -- cannot special summon, except owner 
+    local splimit = Effect.CreateEffect(c)
+    splimit:SetType(EFFECT_TYPE_SINGLE)
+    splimit:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+    splimit:SetCode(EFFECT_SPSUMMON_CONDITION)
+    splimit:SetValue(function(e, se, sp, st) return sp == e:GetOwnerPlayer() end)
+    c:RegisterEffect(splimit)
 end
--- script/c900000006.lua
+++ script/c900000006.lua
@@ -4,41 +4,13 @@ Duel.LoadScript("util_egyptian.lua")
 local s, id = GetID()
 
 function s.initial_effect(c)
-    Divine.DivineHierarchy(s, c, 1, false)
+    Utility.AvatarInfinity(s, c)
+    Divine.DivineHierarchy(s, c, 1)
 
-    -- half atk/def
-    local e1 = Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_FIELD)
-    e1:SetProperty(EFFECT_FLAG_DELAY)
-    e1:SetCode(EFFECT_SET_ATTACK_FINAL)
-    e1:SetRange(LOCATION_MZONE)
-    e1:SetTargetRange(LOCATION_MZONE, LOCATION_MZONE)
-    e1:SetTarget(function(e, tc)
-        local c = e:GetHandler()
-        return tc ~= e:GetHandler() and Divine.GetDivineHierarchy(tc) <= Divine.GetDivineHierarchy(c)
-    end)
-    e1:SetValue(function(e, c) return math.ceil(c:GetAttack() / 2) end)
-    c:RegisterEffect(e1)
-    local e1b = e1:Clone()
-    e1b:SetCode(EFFECT_SET_DEFENSE_FINAL)
-    e1b:SetValue(function(e, c) return math.ceil(c:GetDefense() / 2) end)
-    c:RegisterEffect(e1b)
-
-    -- activate limit
-    local e2 = Effect.CreateEffect(c)
-    e2:SetType(EFFECT_TYPE_FIELD)
-    e2:SetCode(EFFECT_CANNOT_TRIGGER)
-    e2:SetRange(LOCATION_MZONE)
-    e2:SetTargetRange(0, LOCATION_MZONE)
-    e2:SetCondition(s.e2con)
-    e2:SetTarget(s.e2tg)
-    c:RegisterEffect(e2)
+    -- cannot special summon
+    local splimit = Effect.CreateEffect(c)
+    splimit:SetType(EFFECT_TYPE_SINGLE)
+    splimit:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_UNCOPYABLE)
+    splimit:SetCode(EFFECT_SPSUMMON_CONDITION)
+    c:RegisterEffect(splimit)
 end
-
-function s.e2con(e)
-    local c = e:GetHandler()
-    return Duel.GetAttacker() == c and c:GetBattleTarget() and
-               (Duel.GetCurrentPhase() == PHASE_DAMAGE or Duel.GetCurrentPhase() == PHASE_DAMAGE_CAL)
-end
-
-function s.e2tg(e, c) return c == e:GetHandler():GetBattleTarget() end
