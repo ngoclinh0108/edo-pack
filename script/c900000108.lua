a7d6c7cb8592bc04072a4f0d7591fef25634fb99
Luc THQ <luc.ta@cptech.vn>
2023-04-11 23:28:50 +0700

    up

--- script/c900000108.lua
+++ script/c900000108.lua
@@ -5,55 +5,42 @@ local s, id = GetID()
 function s.initial_effect(c)
     c:AddSetcodesRule(id, true, 0x13a)
 
-    -- normal monster
-    local e1 = Effect.CreateEffect(c)
-    e1:SetType(EFFECT_TYPE_SINGLE)
-    e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-    e1:SetCode(EFFECT_ADD_TYPE)
-    e1:SetRange(LOCATION_HAND + LOCATION_GRAVE)
-    e1:SetValue(TYPE_NORMAL)
-    c:RegisterEffect(e1)
-    local e1b = e1:Clone()
-    e1b:SetCode(EFFECT_REMOVE_TYPE)
-    e1b:SetValue(TYPE_EFFECT)
-    c:RegisterEffect(e1b)
-    
     -- destroy
-    local e2 = Effect.CreateEffect(c)
-    e2:SetCategory(CATEGORY_DESTROY)
-    e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
-    e2:SetProperty(EFFECT_FLAG_CARD_TARGET + EFFECT_FLAG_DELAY)
-    e2:SetCode(EVENT_SPSUMMON_SUCCESS)
-    e2:SetCountLimit(1, {id, 1})
-    e2:SetCost(s.e2cost)
-    e2:SetTarget(s.e2tg)
-    e2:SetOperation(s.e2op)
-    c:RegisterEffect(e2)
+    local e1 = Effect.CreateEffect(c)
+    e1:SetCategory(CATEGORY_DESTROY)
+    e1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e1:SetProperty(EFFECT_FLAG_CARD_TARGET + EFFECT_FLAG_DELAY)
+    e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+    e1:SetCountLimit(1, {id, 1})
+    e1:SetCost(s.e1cost)
+    e1:SetTarget(s.e1tg)
+    e1:SetOperation(s.e1op)
+    c:RegisterEffect(e1)
 
     -- fusion summon
     local params = {nil, Fusion.OnFieldMat, nil, nil, Fusion.ForcedHandler}
-    local e3 = Effect.CreateEffect(c)
-    e3:SetDescription(1170)
-    e3:SetCategory(CATEGORY_SPECIAL_SUMMON + CATEGORY_FUSION_SUMMON)
-    e3:SetType(EFFECT_TYPE_IGNITION)
-    e3:SetRange(LOCATION_MZONE)
-    e3:SetCountLimit(1, {id, 2})
-    e3:SetTarget(Fusion.SummonEffTG(table.unpack(params)))
-    e3:SetOperation(Fusion.SummonEffOP(table.unpack(params)))
-    c:RegisterEffect(e3)
+    local e2 = Effect.CreateEffect(c)
+    e2:SetDescription(1170)
+    e2:SetCategory(CATEGORY_SPECIAL_SUMMON + CATEGORY_FUSION_SUMMON)
+    e2:SetType(EFFECT_TYPE_IGNITION)
+    e2:SetRange(LOCATION_MZONE)
+    e2:SetCountLimit(1, {id, 2})
+    e2:SetTarget(Fusion.SummonEffTG(table.unpack(params)))
+    e2:SetOperation(Fusion.SummonEffOP(table.unpack(params)))
+    c:RegisterEffect(e2)
 
     -- gain effect
-    local e4 = Effect.CreateEffect(c)
-    e4:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_CONTINUOUS)
-    e4:SetCode(EVENT_BE_MATERIAL)
-    e4:SetCondition(s.e4con)
-    e4:SetOperation(s.e4op)
-    c:RegisterEffect(e4)
+    local e3 = Effect.CreateEffect(c)
+    e3:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_CONTINUOUS)
+    e3:SetCode(EVENT_BE_MATERIAL)
+    e3:SetCondition(s.e3con)
+    e3:SetOperation(s.e3op)
+    c:RegisterEffect(e3)
 end
 
-function s.e2filter(c) return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_WARRIOR) end
+function s.e1filter(c) return c:IsFaceup() and c:IsAttribute(ATTRIBUTE_LIGHT) and c:IsRace(RACE_WARRIOR) end
 
-function s.e2cost(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e1cost(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
     if chk == 0 then return c:GetAttackAnnouncedCount() == 0 end
 
@@ -66,30 +53,30 @@ function s.e2cost(e, tp, eg, ep, ev, re, r, rp, chk)
     c:RegisterEffect(ec1)
 end
 
-function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk)
+function s.e1tg(e, tp, eg, ep, ev, re, r, rp, chk)
     if chk == 0 then
-        return Duel.IsExistingMatchingCard(s.e2filter, tp, LOCATION_MZONE, 0, 1, nil) and
+        return Duel.IsExistingMatchingCard(s.e1filter, tp, LOCATION_MZONE, 0, 1, nil) and
                    Duel.IsExistingTarget(Card.IsFaceup, tp, 0, LOCATION_ONFIELD, 1, nil)
     end
 
-    local ct = Duel.GetMatchingGroupCount(s.e2filter, tp, LOCATION_MZONE, 0, nil)
+    local ct = Duel.GetMatchingGroupCount(s.e1filter, tp, LOCATION_MZONE, 0, nil)
     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_DESTROY)
     local g = Duel.SelectTarget(tp, Card.IsFaceup, tp, 0, LOCATION_ONFIELD, 1, ct, nil)
     Duel.SetOperationInfo(0, HINTMSG_DESTROY, g, #g, 0, 0)
 end
 
-function s.e2op(e, tp, eg, ep, ev, re, r, rp)
+function s.e1op(e, tp, eg, ep, ev, re, r, rp)
     local g = Duel.GetTargetCards(e)
     if #g > 0 then Duel.Destroy(g, REASON_EFFECT) end
 end
 
-function s.e4con(e, tp, eg, ep, ev, re, r, rp)
+function s.e3con(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     local rc = c:GetReasonCard()
     return (r & REASON_FUSION + REASON_LINK) ~= 0 and rc:IsAttribute(ATTRIBUTE_LIGHT) and rc:IsRace(RACE_WARRIOR)
 end
 
-function s.e4op(e, tp, eg, ep, ev, re, r, rp)
+function s.e3op(e, tp, eg, ep, ev, re, r, rp)
     local c = e:GetHandler()
     local rc = c:GetReasonCard()
 
