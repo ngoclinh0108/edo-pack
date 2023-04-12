56ddc2e52ff2dc4914413533613697934bb3f685
Luc THQ <luc.ta@cptech.vn>
2023-04-13 03:55:42 +0700

    up

--- script/c900000001.lua
+++ script/c900000001.lua
@@ -11,9 +11,10 @@ function s.initial_effect(c)
     e1:SetDescription(aux.Stringid(id, 0))
     e1:SetCategory(CATEGORY_DESTROY + CATEGORY_DAMAGE)
     e1:SetType(EFFECT_TYPE_QUICK_O)
+    e1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_CANNOT_NEGATE + EFFECT_FLAG_CANNOT_INACTIVATE)
     e1:SetCode(EVENT_FREE_CHAIN)
     e1:SetRange(LOCATION_MZONE)
-    e1:SetHintTiming(0, TIMINGS_CHECK_MONSTER)
+    e1:SetHintTiming(0, TIMINGS_CHECK_MONSTER + TIMING_MAIN_END)
     e1:SetCountLimit(1, 0, EFFECT_COUNT_CODE_SINGLE)
     e1:SetCondition(s.e1con)
     e1:SetCost(s.e1cost)
@@ -24,11 +25,11 @@ function s.initial_effect(c)
     -- soul energy MAX
     local e2 = Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id, 1))
+    e2:SetCategory(CATEGORY_ATKCHANGE)
     e2:SetType(EFFECT_TYPE_QUICK_O)
-    e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP)
-    e2:SetCode(EVENT_FREE_CHAIN)
+    e2:SetProperty(EFFECT_FLAG_DAMAGE_STEP + EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_CANNOT_NEGATE + EFFECT_FLAG_CANNOT_INACTIVATE)
+    e2:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
     e2:SetRange(LOCATION_MZONE)
-    e2:SetHintTiming(0, TIMING_DAMAGE_STEP)
     e2:SetCountLimit(1, 0, EFFECT_COUNT_CODE_SINGLE)
     e2:SetCondition(s.e2con)
     e2:SetCost(s.e2cost)
@@ -37,11 +38,14 @@ function s.initial_effect(c)
     Utility.AvatarInfinity(s, c)
 end
 
-function s.e1con(e, tp, eg, ep, ev, re, r, rp) return Duel.GetCurrentChain(true) == 0 and e:GetHandler():CanAttack() end
+function s.e1con(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    return Duel.GetCurrentChain(true) == 0 and c:CanAttack()
+end
 
 function s.e1cost(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
-    if chk == 0 then return c:GetAttackAnnouncedCount() == 0 and Duel.CheckReleaseGroupCost(tp, nil, 2, false, nil, c) end
+    if chk == 0 then return Duel.CheckReleaseGroupCost(tp, nil, 2, false, nil, c) and c:GetAttackAnnouncedCount() == 0 end
 
     local g = Duel.SelectReleaseGroupCost(tp, nil, 2, 2, false, nil, c)
     Duel.Release(g, REASON_COST)
@@ -82,8 +86,9 @@ end
 function s.e2filter(c) return c:IsFaceup() and Divine.GetDivineHierarchy(c) >= 2 end
 
 function s.e2con(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
     return Duel.IsExistingMatchingCard(function(tc) return tc:IsFaceup() and Divine.GetDivineHierarchy(tc) >= 2 end, tp, LOCATION_MZONE,
-        LOCATION_MZONE, 1, nil)
+        LOCATION_MZONE, 1, nil) and c:CanAttack() and c:GetBattleTarget() ~= nil
 end
 
 function s.e2cost(e, tp, eg, ep, ev, re, r, rp, chk)
@@ -94,21 +99,5 @@ function s.e2cost(e, tp, eg, ep, ev, re, r, rp, chk)
     Duel.Release(g, REASON_COST)
 end
 
-function s.e2op(e, tp, eg, ep, ev, re, r, rp)
-    local c = e:GetHandler()
-    c:RegisterFlagEffect(id, RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END, EFFECT_FLAG_CLIENT_HINT, 1, 0, aux.Stringid(id, 2))
-
-    local ec1 = Effect.CreateEffect(c)
-    ec1:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_CONTINUOUS)
-    ec1:SetCode(EVENT_PRE_DAMAGE_CALCULATE)
-    ec1:SetCountLimit(1)
-    ec1:SetCondition(function(e, tp, eg, ep, ev, re, r, rp)
-        local c = e:GetHandler()
-        local bc = c:GetBattleTarget()
-        return bc and bc:IsControler(1 - tp) and c:GetFlagEffect(id) ~= 0
-    end)
-    ec1:SetOperation(function(e, tp, eg, ep, ev, re, r, rp) Utility.GainInfinityAtk(e:GetHandler(), RESET_PHASE + PHASE_DAMAGE_CAL) end)
-    ec1:SetReset(RESET_EVENT + RESETS_STANDARD + RESET_PHASE + PHASE_END)
-    c:RegisterEffect(ec1)
-end
+function s.e2op(e, tp, eg, ep, ev, re, r, rp) Utility.GainInfinityAtk(e:GetHandler(), RESET_PHASE + PHASE_DAMAGE_CAL) end
 
