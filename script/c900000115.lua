627022636b7276565ca29f926bd2b1381db4e17c
Luc THQ <luc.ta@cptech.vn>
2023-04-14 03:33:21 +0700

    up

--- script/c900000115.lua
+++ script/c900000115.lua
@@ -26,9 +26,8 @@ function s.initial_effect(c)
     local e2 = Effect.CreateEffect(c)
     e2:SetDescription(aux.Stringid(id, 0))
     e2:SetCategory(CATEGORY_TOHAND)
-    e2:SetType(EFFECT_TYPE_IGNITION)
-    e2:SetProperty(EFFECT_FLAG_CARD_TARGET)
-    e2:SetRange(LOCATION_MZONE)
+    e2:SetType(EFFECT_TYPE_SINGLE + EFFECT_TYPE_TRIGGER_O)
+    e2:SetCode(EVENT_BATTLE_CONFIRM)
     e2:SetCondition(s.e2con)
     e2:SetTarget(s.e2tg)
     e2:SetOperation(s.e2op)
@@ -69,24 +68,25 @@ function s.e1op(e, tp, eg, ep, ev, re, r, rp)
     end
 end
 
-function s.e2con(e, tp, eg, ep, ev, re, r, rp) return e:GetHandler():IsPosition(POS_FACEUP_ATTACK) end
+function s.e2con(e, tp, eg, ep, ev, re, r, rp)
+    local c = e:GetHandler()
+    local bc = c:GetBattleTarget()
+    return c:IsRelateToBattle() and bc and bc:IsRelateToBattle()
+end
 
 function s.e2tg(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
-    if chk == 0 then return Duel.IsExistingTarget(Card.IsFaceup, tp, 0, LOCATION_MZONE, 1, nil) end
-
-    Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_RTOHAND)
-    local g = Duel.SelectTarget(tp, Card.IsFaceup, tp, 0, LOCATION_MZONE, 1, 1, nil)
-
-    Duel.SetOperationInfo(0, CATEGORY_TOHAND, g, 1, 0, 0)
+    local c = e:GetHandler()
+    local bc = c:GetBattleTarget()
+    if chk == 0 then return c:IsPosition(POS_FACEUP_ATTACK) and c:IsCanChangePosition() and bc:IsAbleToHand() end
+    Duel.SetOperationInfo(0, CATEGORY_TOHAND, bc, 1, 0, 0)
 end
 
 function s.e2op(e, tp, eg, ep, ev, re, r, rp, chk)
     local c = e:GetHandler()
-    local tc = Duel.GetFirstTarget()
-    if c:IsRelateToEffect(e) and c:IsPosition(POS_FACEUP_ATTACK) then
-        Duel.ChangePosition(c, POS_FACEUP_DEFENSE)
-        if tc:IsRelateToEffect(e) then Duel.SendtoHand(tc, nil, REASON_EFFECT) end
-
+    local bc = c:GetBattleTarget()
+    if c:IsFaceup() and c:IsRelateToBattle() and bc:IsFaceup() and bc:IsRelateToBattle() and c:IsPosition(POS_FACEUP_ATTACK) and
+        Duel.ChangePosition(c, POS_FACEUP_DEFENSE) > 0 then
+        Duel.SendtoHand(bc, nil, REASON_EFFECT)
         local ec1 = Effect.CreateEffect(c)
         ec1:SetType(EFFECT_TYPE_SINGLE)
         ec1:SetProperty(EFFECT_FLAG_CANNOT_DISABLE + EFFECT_FLAG_COPY_INHERIT)
